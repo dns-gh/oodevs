041ed0051c8323a5235e67fcabc8c84b89de42bb
Henri Morlaye <henri.morlaye@masagroup.net>
2005-10-04 16:52:00 +0000

    MOS_Light2: ajout en retard de population_part

---
+++ src/applications/MOS_Light2/MOS_PopulationPart.cpp
@@ -0,0 +1,19 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2005 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifdef __GNUG__
+#   pragma implementation
+#endif
+
+#include "MOS_Light2_pch.h"
+#include "MOS_PopulationPart.h"
+
+
+using namespace MOS;
+
