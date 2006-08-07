28a2c9e7405db93faaa493de918afd54edb7b387
Adrien Gervaise <adrien.gervaise@masagroup.net>
2006-08-07 08:16:17 +0000

    Création des libs astec

---
+++ src/libraries/astec_gui/astec_gui_pch.h
@@ -0,0 +1,14 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2006 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef __astec_gui_pch_h_
+#define __astec_gui_pch_h_
+
+
+#endif // __astec_gui_pch_h_
