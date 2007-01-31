d30201938b509594fd1a565d5024a5c2406cd1a1
Nicolas Léonard <nicolas.leonard@masagroup.net>
2007-01-31 15:34:50 +0000

    network - network tools moved from network lib to tools lib           asn split into 2 libraries (game_asn / master_asn)

---
+++ src/libraries/game_asn/Asn.h
@@ -0,0 +1,35 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2006 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef __Game_Asn_h__
+#define __Game_Asn_h__
+
+#pragma warning( disable : 4800 ) // $$$$ SBO 2007-01-09: forcing value to true or false (performance warning)
+
+#pragma warning( push, 3 )
+
+#include "generated/ScipioBase.h"
+#include "generated/ScipioArmy.h"
+#include "generated/ScipioKnowledgeGroup.h"
+#include "generated/ScipioFormation.h"
+#include "generated/ScipioControl.h"
+#include "generated/ScipioLimaLimite.h"
+#include "generated/ScipioFire.h"
+#include "generated/ScipioMessages.h"
+#include "generated/ScipioUnitKnowledge.h"
+#include "generated/ScipioUnit.h"
+#include "generated/ScipioAutomate.h"
+#include "generated/ScipioOrder.h"
+#include "generated/ScipioObject.h"
+#include "generated/ScipioPopulation.h"
+#include "generated/ScipioPopulationKnowledge.h"
+
+#pragma warning( pop ) 
+
+#endif // __Game_Asn_h__
