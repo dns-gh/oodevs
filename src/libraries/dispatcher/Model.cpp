4caf911d301f3850a3db2d0e469ef2cd874fb526
Adrien Gervaise <adrien.gervaise@masagroup.net>
2007-06-20 15:34:34 +0000

    decisional_state dans le dispatcher

--- src/libraries/dispatcher/Model.cpp
+++ src/libraries/dispatcher/Model.cpp
@@ -164,8 +164,8 @@ void Model::Update( const ASN1T_MsgsSimToClient& asnMsg )
         case T_MsgsSimToClient_msg_msg_explosion:
         case T_MsgsSimToClient_msg_msg_cr:
         case T_MsgsSimToClient_msg_msg_trace:
-        case T_MsgsSimToClient_msg_msg_decisional_state:
             break;  // $$$$ AGE 2007-04-18: Evenements, modèle client => rien, ou remanier
+        case T_MsgsSimToClient_msg_msg_decisional_state:                     agents_     .Get( asnMsg.msg.u.msg_decisional_state->unit_id ).Update( *asnMsg.msg.u.msg_decisional_state ); break;
         case T_MsgsSimToClient_msg_msg_start_fire_effect:                    CreateUpdate( fireEffects_, asnMsg.msg.u.msg_start_fire_effect->oid_effet, *asnMsg.msg.u.msg_start_fire_effect ); break;
         case T_MsgsSimToClient_msg_msg_stop_fire_effect:                     fireEffects_.Destroy( asnMsg.msg.u.msg_stop_fire_effect ); break;
 
