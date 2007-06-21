b51def98fb23d28f414d92f1540027b9273919d5
Sébastien Brossolasco <sebastien.brossolasco@masagroup.net>
2007-06-21 14:28:30 +0000

    crossbow: an ArcMap extension for CSword

---
+++ src/applications/crossbow_app/FilterViewTool.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.SystemUI;
+using System.Windows.Forms;
+
+namespace crossbow
+{
+    /// <summary>
+    /// Summary description for FilterViewTool.
+    /// </summary>
+    [Guid("f44a3523-8fae-4fd8-9895-fa0bde4efff5")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("csword.FilterViewTool")]
+    public sealed class FilterViewTool : BaseCommand, IToolControl
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+
+        }
+
+        #endregion
+        #endregion
+
+        private IApplication m_application;
+        private System.Windows.Forms.ComboBox m_teamCombo;
+
+        public FilterViewTool()
+        {
+            base.m_category = "CSword"; //localizable text 
+            base.m_caption = "Filter view";  //localizable text 
+            base.m_message = "Filter view to show situation from one team's point of view";  //localizable text
+            base.m_toolTip = "Filter view";  //localizable text
+            base.m_name = "CSword_FilterView";   //unique id, non-localizable (e.g. "MyCategory_ArcMapTool")
+            try
+            {
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overriden Class Methods
+
+        /// <summary>
+        /// Occurs when this tool is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            m_application = hook as IApplication;
+
+            //Disable if it is not ArcMap
+            if (hook is IMxApplication)
+                base.m_enabled = true;
+            else
+                base.m_enabled = false;
+            InitControl();
+        }
+
+        public override bool Enabled
+        {
+            get
+            {
+                if (m_teamCombo != null)
+                    m_teamCombo.Refresh();
+                return base.Enabled;
+            }
+        }
+
+        /// <summary>
+        /// Occurs when this tool is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            // NOTHING
+        }
+        #endregion
+
+        #region IToolControl Members
+
+        public bool OnDrop(esriCmdBarType barType)
+        {
+            return true;
+        }
+
+        public void OnFocus(ICompletionNotify complete)
+        {
+            // NOTHING
+        }
+
+        public int hWnd
+        {
+            get
+            {
+                if (m_teamCombo != null)
+                    return m_teamCombo.Handle.ToInt32();
+                else
+                    return 0;
+            }
+        }
+
+        #endregion
+
+        public void InitControl()
+        {
+            if (!base.m_enabled)
+                return;
+            m_teamCombo = new System.Windows.Forms.ComboBox();
+            m_teamCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+            m_teamCombo.Width = 150;
+            m_teamCombo.SelectedIndexChanged += new System.EventHandler(OnSelectionChanged);
+            int index = m_teamCombo.Items.Add("All");
+            m_teamCombo.Items.Add( "Friends" );
+            m_teamCombo.Items.Add( "Hostiles" );
+            m_teamCombo.SelectedIndex = index;
+        }
+
+        public void OnSelectionChanged(object sender, System.EventArgs e)
+        {
+            System.String filter = m_teamCombo.SelectedItem.ToString();
+            IMxDocument mxDocument = Tools.GetMxDocument(m_application);
+            ESRI.ArcGIS.Carto.IFeatureLayer layer = Tools.GetIFeatureLayerFromLayerName(mxDocument.ActiveView, "UnitForces");
+            if (layer != null)
+            {
+                FilterLayer(layer, filter);
+                mxDocument.ActiveView.Refresh();
+            }
+        }
+
+        public void FilterLayer(ESRI.ArcGIS.Carto.IFeatureLayer layer, System.String filter)
+        {
+            ESRI.ArcGIS.Carto.IFeatureLayerDefinition definition = (ESRI.ArcGIS.Carto.IFeatureLayerDefinition)(layer);
+            definition.DefinitionExpression = "";
+            if( filter != "All" )
+                definition.DefinitionExpression = "Symbol_ID like '?" + filter[0] + "*'";
+        }
+    }
+}
---
+++ src/applications/crossbow_app/Tools.cs
@@ -0,0 +1,132 @@
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+
+namespace crossbow
+{
+    static class Tools
+    {
+
+        #region "Get MxDocument from ArcMap"
+        // ArcGIS Snippet Title: 
+        // Get MxDocument from ArcMap
+        //
+        // Add the following references to the project:
+        // ESRI.ArcGIS.ArcMapUI
+        // ESRI.ArcGIS.Framework
+        // ESRI.ArcGIS.System
+        // 
+        // Intended ArcGIS Products for this snippet:
+        // ArcGIS Desktop
+        //
+        // Required ArcGIS Extensions:
+        // (NONE)
+        //
+        // Notes:
+        // This snippet is intended to be inserted at the base level of a Class.
+        // It is not intended to be nested within an existing Method.
+        //
+        // Use the following XML documentation comments to use this snippet:
+        /// <summary>Get MxDocument from ArcMap.</summary>
+        ///
+        /// <param name="application">An IApplication interface that is the ArcMap application.</param>
+        /// 
+        /// <returns>An IMxDocument interface.</returns>
+        /// 
+        /// <remarks></remarks>
+        public static IMxDocument GetMxDocument(ESRI.ArcGIS.Framework.IApplication application)
+        {
+
+            if (application == null)
+            {
+                return null;
+            }
+            IMxDocument mxDocument = ((IMxDocument)(application.Document)); // Explicit Cast
+
+            return mxDocument;
+
+        }
+        #endregion
+        
+        #region "Get Index Number from Layer Name"
+        // ArcGIS Snippet Title: 
+        // Get Index Number from Layer Name
+        //
+        // Add the following references to the project:
+        // ESRI.ArcGIS.Carto
+        // 
+        // Intended ArcGIS Products for this snippet:
+        // ArcGIS Desktop
+        // ArcGIS Engine
+        // ArcGIS Server
+        //
+        // Required ArcGIS Extensions:
+        // (NONE)
+        //
+        // Notes:
+        // This snippet is intended to be inserted at the base level of a Class.
+        // It is not intended to be nested within an existing Method.
+        //
+        // Use the following XML documentation comments to use this snippet:
+        /// <summary>Get the index number for the specified layer name.</summary>
+        ///
+        /// <param name="activeView">An IActiveView interface</param>
+        /// <param name="layerName">A System.String that is the layer name in the active view. Example: "states"</param>
+        /// 
+        /// <returns>An System.Int32 representing a layer number</returns>
+        /// 
+        /// <remarks>Returns a System.Int32 where values of 0 and greater are valid layers. A return value of -1 means the layer name was not found.</remarks>
+        public static System.Int32 GetIndexNumberOfLayerName(ESRI.ArcGIS.Carto.IActiveView activeView, System.String layerName)
+        {
+            if (activeView == null || layerName == null)
+            {
+                return -1;
+            }
+            ESRI.ArcGIS.Carto.IMap map = activeView.FocusMap;
+
+            // Get the number of layers
+            int numberOfLayers = map.LayerCount;
+
+            // Loop through the layers and get the correct layer index
+            for (System.Int32 i = 0; i < numberOfLayers; i++)
+            {
+                if (layerName == map.get_Layer(i).Name)
+                {
+
+                    // Layer was found
+                    return i;
+                }
+            }
+
+            // No layer was found
+            return -1;
+        }
+        #endregion
+                
+        #region "Get FeatureLayer from Layer Name"
+        public static IFeatureLayer GetIFeatureLayerFromLayerName(ESRI.ArcGIS.Carto.IActiveView activeView, System.String name)
+        {
+            if (activeView == null || name == null)
+                return null;
+            int index = GetIndexNumberOfLayerName(activeView, name);
+            if( index < 0 )
+                return null;
+            IMap map = activeView.FocusMap;
+            ILayer layer = map.get_Layer( index );
+            if (layer is IFeatureLayer)
+                return (IFeatureLayer)(layer);
+            else if (layer is ICompositeLayer)
+            {
+                ICompositeLayer composite = (ICompositeLayer)(layer);
+                for (int i = 0; i < composite.Count - 1; ++i)
+                {
+                    layer = composite.get_Layer(i);
+                    if (layer is IFeatureLayer)
+                        return (IFeatureLayer)(layer);
+                }
+            }
+            return null;
+        }
+    }
+        #endregion
+
+}
