536ab975bdfd3ddd416424019c2ede5ad2092ea4
Adrien Gervaise <adrien.gervaise@masagroup.net>
2007-12-13 14:45:38 +0000

    traductions

--- src/applications/frontend_app/frontend_app_fr.ts
+++ src/applications/frontend_app/frontend_app_fr.ts
@@ -3,304 +3,306 @@
     <name>Actions</name>
     <message>
         <source>Create terrain</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer un terrain</translation>
     </message>
     <message>
         <source>Import new terrain from VMap, DTED and USRP bases</source>
-        <translation type="unfinished"></translation>
+        <translation>Importe un nouveau terrain de bases VMap, DTED et USRP</translation>
     </message>
     <message>
         <source>Create exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer un exercice</translation>
     </message>
     <message>
         <source>Create new exercise using an existing terrain and an existing physical/decisional data set</source>
-        <translation type="unfinished"></translation>
+        <translation>Crée un nouvel exercice à partir d&apos;un terrain et d&apos;un jeu de données physiques/décisionnelles existants</translation>
     </message>
     <message>
         <source>Edit exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Editer un exercice</translation>
     </message>
     <message>
         <source>Change exercise Order of Battle, User Profiles and Weather settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifie l&apos;ODB, les profiles utilisateurs et les paramètres météorologiques d&apos;un exercice</translation>
     </message>
     <message>
         <source>Start exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarrer un exercice</translation>
     </message>
     <message>
         <source>Start a new simulation session using an existing exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarre une simulation à partir d&apos;un exercice existant</translation>
     </message>
     <message>
         <source>Restart exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Redémarrer un exercice</translation>
     </message>
     <message>
         <source>Restart a simulation session using an existing checkpoint</source>
-        <translation type="unfinished"></translation>
+        <translation>Redémarre un exercice en utilisant une sauvegarde existante</translation>
     </message>
     <message>
         <source>Join exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoindre un exercice</translation>
     </message>
     <message>
         <source>Join a simulation session</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoint une session de simulation</translation>
     </message>
     <message>
         <source>Start analysis</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarre une analyse</translation>
     </message>
     <message>
         <source>Start the analysis of a previous simulation session</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarre l&apos;analyse d&apos;une session de simulation passée</translation>
     </message>
     <message>
         <source>Join analysis</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoindre une analyse</translation>
     </message>
     <message>
         <source>Join an analysis session</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoint une session d&apos;analyse</translation>
     </message>
 </context>
 <context>
     <name>EditExercisePanel</name>
     <message>
         <source>Choose the exercise to edit:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez l&apos;exercice à éditer:</translation>
     </message>
     <message>
         <source>Edit exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Editer l&apos;exercice</translation>
     </message>
 </context>
 <context>
     <name>ExerciseCreationPanel</name>
     <message>
         <source>New exercise name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom du nouvel exercice:</translation>
     </message>
     <message>
         <source>Enter exercise name</source>
-        <translation type="unfinished"></translation>
+        <translation>Saisissez le nom de l&apos;exercice</translation>
     </message>
     <message>
         <source>Choose the model data set:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez le jeu de données décisionnelles:</translation>
     </message>
     <message>
         <source>Choose the physical data set:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez le jeu de données physiques:</translation>
     </message>
     <message>
         <source>Choose the terrain:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez le terrain:</translation>
     </message>
     <message>
         <source>Create</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer</translation>
     </message>
     <message>
         <source>An exercise with this name already exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>Un exercice de ce nom existe déjà.</translation>
     </message>
     <message>
         <source>The new exercise will be created in:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Le nouvel exercice sera crée dans:
+%1</translation>
     </message>
 </context>
 <context>
     <name>GameConfigPanel</name>
     <message>
         <source>Simulation</source>
-        <translation type="unfinished"></translation>
+        <translation>Simulation</translation>
     </message>
     <message>
         <source>Debug</source>
-        <translation type="unfinished"></translation>
+        <translation>Déboggage</translation>
     </message>
     <message>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Système</translation>
     </message>
     <message>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestion du temps</translation>
     </message>
     <message>
         <source>Time step:</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de temps:</translation>
     </message>
     <message>
         <source>Time factor:</source>
-        <translation type="unfinished"></translation>
+        <translation>Facteur d&apos;accélération:</translation>
     </message>
     <message>
         <source>Checkpoints</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauvegardes automatiques</translation>
     </message>
     <message>
         <source>Frequency:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fréquence:</translation>
     </message>
     <message>
         <source>Rotations:</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotations:</translation>
     </message>
     <message>
         <source>Logs</source>
-        <translation type="unfinished"></translation>
+        <translation>Journalisation</translation>
     </message>
     <message>
         <source>Decisional</source>
-        <translation type="unfinished"></translation>
+        <translation>Décisionnel</translation>
     </message>
     <message>
         <source>Pathfind</source>
-        <translation type="unfinished"></translation>
+        <translation>Cherche-chemin</translation>
     </message>
     <message>
         <source>DIA Debugger</source>
-        <translation type="unfinished"></translation>
+        <translation>Déboggueur DirectIA</translation>
     </message>
     <message>
         <source>Network Console</source>
-        <translation type="unfinished"></translation>
+        <translation>Console réseau</translation>
     </message>
     <message>
         <source>Pathfind threads:</source>
-        <translation type="unfinished"></translation>
+        <translation>Threads de cherche-chemin:</translation>
     </message>
     <message>
         <source>Profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Profile</translation>
     </message>
     <message>
         <source>Check ODB Composition</source>
-        <translation type="unfinished"></translation>
+        <translation>Vérifier la composition de l&apos;ODB</translation>
     </message>
 </context>
 <context>
     <name>JoinExercisePanel</name>
     <message>
         <source>Choose the exercise to join:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choissisez l&apos;exercice à rejoindre:</translation>
     </message>
     <message>
         <source>Exercise number:</source>
-        <translation type="unfinished"></translation>
+        <translation>Numéro de l&apos;exercice:</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>Prepare</source>
-        <translation type="unfinished"></translation>
+        <translation>Préparer</translation>
     </message>
     <message>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Jouer</translation>
     </message>
     <message>
         <source>Analyse</source>
-        <translation type="unfinished"></translation>
+        <translation>Analyser</translation>
     </message>
 </context>
 <context>
     <name>RestartExercisePanel</name>
     <message>
         <source>Choose the exercise to start:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez l&apos;exercice à démarrer:</translation>
     </message>
     <message>
         <source>Choose the checkpoint to load:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez la sauvegarde à reprendre:</translation>
     </message>
     <message>
         <source>Start exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarrer l&apos;exercice</translation>
     </message>
     <message>
         <source>Restart game: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Redémarre l&apos;exercice: %1</translation>
     </message>
     <message>
         <source>The selected exercise as no checkpoint to restart.</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;exercice séléctionné n&apos;a pas de sauvegarde.</translation>
     </message>
 </context>
 <context>
     <name>StartAnalysePanel</name>
     <message>
         <source>Choose the exercise to analyse:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez l&apos;exercice à analyser:</translation>
     </message>
     <message>
         <source>Choose the replay to analyse:</source>
-        <translation type="unfinished"></translation>
+        <translation>Choissisez l&apos;enregistrement à analyser:</translation>
     </message>
     <message>
         <source>Start replay</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarrer l&apos;analyse</translation>
     </message>
     <message>
         <source>Exercise number:</source>
-        <translation type="unfinished"></translation>
+        <translation>Numéro de l&apos;exercice:</translation>
     </message>
     <message>
         <source>Replay game: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Analyse l&apos;exercice: %1</translation>
     </message>
     <message>
         <source>The selected exercise has no replay.</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;exercice sélectionné n&apos;a pas d&apos;enregistrement.</translation>
     </message>
 </context>
 <context>
     <name>StartExercisePanel</name>
     <message>
         <source>Exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Exercice</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Options</translation>
     </message>
     <message>
         <source>Start exercise</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarrer l&apos;exercice</translation>
     </message>
     <message>
         <source>Exercise number:</source>
-        <translation type="unfinished"></translation>
+        <translation>Numéro de l&apos;exercice:</translation>
     </message>
 </context>
 <context>
     <name>TerrainCreationPanel</name>
     <message>
         <source>New terrain name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom du nouveau terrain:</translation>
     </message>
     <message>
         <source>Enter terrain name</source>
-        <translation type="unfinished"></translation>
+        <translation>Saisissez le nom du terrain</translation>
     </message>
     <message>
         <source>Create</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer</translation>
     </message>
     <message>
         <source>A terrain with this name already exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>Un terrain de ce nom existe déjà.</translation>
     </message>
     <message>
         <source>The new terrain will be created in:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Le nouveau terrain sera crée dans: 
+%1</translation>
     </message>
 </context>
 </TS>
