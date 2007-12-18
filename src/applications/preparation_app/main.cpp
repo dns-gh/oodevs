3058670e08f6acc3f6679821f498088d1133157c
Adrien Gervaise <adrien.gervaise@masagroup.net>
2007-12-18 14:11:37 +0000

    traductions

--- src/applications/preparation_app/main.cpp
+++ src/applications/preparation_app/main.cpp
@@ -14,8 +14,8 @@
 namespace
 {
     ////
-//    static const QString locale = "_en";
-    static const QString locale = "_fr";
+    static const QString locale = "_en";
+//    static const QString locale = "_fr";
     ////
 
     void PureHandler()
