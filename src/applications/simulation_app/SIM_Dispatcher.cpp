314555d1d1ba7bdaf5bda5306ce91110c5943df8
Adrien Gervaise <adrien.gervaise@masagroup.net>
2008-04-15 11:05:05 +0000

    fix

--- src/applications/simulation_app/SIM_Dispatcher.cpp
+++ src/applications/simulation_app/SIM_Dispatcher.cpp
@@ -14,7 +14,6 @@
 #include "hla_plugin/HlaPluginFactory.h"
 #include "dis_plugin/DisPluginFactory.h"
 #include "tic_plugin/TicPluginFactory.h"
-#include "messenger_plugin/MessengerpluginFactory.h"
 #ifdef CROSSBOW_PLUGIN
 #   include "gearth_plugin/GearthPluginFactory.h"
 #   include "crossbow_plugin/CrossbowPluginFactory.h"
@@ -32,7 +31,6 @@ SIM_Dispatcher::SIM_Dispatcher( SIM_Config& config )
     dispatcher_.RegisterPluginFactory( *new hla::HlaPluginFactory() );
     dispatcher_.RegisterPluginFactory( *new dis::DisPluginFactory() );
     dispatcher_.RegisterPluginFactory( *new tic::TicPluginFactory() );
-    dispatcher_.RegisterPluginFactory( *new messenger::MessengerPluginFactory() );
 #ifdef CROSSBOW_PLUGIN
     dispatcher_.RegisterPluginFactory( *new crossbow::CrossbowPluginFactory() );
     dispatcher_.RegisterPluginFactory( *new gearth::GearthPluginFactory() );
