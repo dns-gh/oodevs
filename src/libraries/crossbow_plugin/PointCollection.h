2f06f86de74f8d8e9527bf2723a9f9b09078f4ed
Jérome Comptdaer <jerome.comptdaer@masagroup.net>
2008-05-19 12:15:33 +0000

    crossbow_plugin: build fix

---
+++ src/libraries/crossbow_plugin/PointCollection.h
@@ -0,0 +1,76 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2008 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef __PointCollection_h_
+#define __PointCollection_h_
+
+
+#include "game_asn/Simulation.h"
+#include "Shape_ABC.h"
+#include "ESRI.h"
+
+namespace crossbow
+{
+    class Point;
+
+// =============================================================================
+/** @class  PointCollection
+*/
+// Created: JCR 2008-04-25
+// =============================================================================
+class PointCollection : public Shape_ABC
+{
+public:
+    //! @name Constructors/Destructor
+    //@{
+    virtual ~PointCollection();
+    //@}
+    
+    //! @name Operations
+    //@{
+    virtual void Accept( ShapeVisitor_ABC& visitor ) const;
+    virtual void Serialize( ASN1T_Location& asn ) const;
+    //@}
+
+protected:
+    //! @name Constructors/Destructor
+    //@{
+             PointCollection();
+    explicit PointCollection( const ASN1T_CoordLatLongList& asn );
+    explicit PointCollection( IGeometryPtr geometry );    
+    //@}
+
+    //! @name 
+    //@{
+    void UpdateGeometry( IGeometryPtr geometry, ISpatialReferencePtr spatialReference ) const;    
+    //@}
+
+private:
+    //! @name Copy/Assignement
+    //@{
+    PointCollection( const PointCollection& );            //!< Copy constructor
+    PointCollection& operator=( const PointCollection& ); //!< Assignement operator
+    //@}
+
+    //! @name Helpers
+    //@{
+    typedef std::vector< Point >       T_Points;
+    typedef T_Points::const_iterator CIT_Points;
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    T_Points points_;
+    //@}
+};
+
+}
+
+#endif // __PointCollection_h_
