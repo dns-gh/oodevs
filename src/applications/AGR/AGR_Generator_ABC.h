dc3d40e22301c32f771450cf0afabb3b0b64d702
Mathieu Champlon <mathieu.champlon@masagroup.net>
2008-09-24 11:21:36 +0000

    Nettoyage

--- src/applications/AGR/AGR_Generator_ABC.h
+++ src/applications/AGR/AGR_Generator_ABC.h
@@ -6,22 +6,9 @@
 // Copyright (c) 2004 Mathématiques Appliquées SA (MASA)
 //
 // *****************************************************************************
-//
-// $Created: AGE 2004-09-21 $
-// $Archive: /MVW_v10/Build/SDK/AGR/src/AGR_Generator_ABC.h $
-// $Author: Age $
-// $Modtime: 21/09/04 11:09 $
-// $Revision: 1 $
-// $Workfile: AGR_Generator_ABC.h $
-//
-// *****************************************************************************
 
 #ifndef __AGR_Generator_ABC_h_
 #define __AGR_Generator_ABC_h_
-
-#ifdef __GNUG__
-#   pragma interface
-#endif
 
 class AGR_Workspace;
 
@@ -37,8 +24,8 @@ class AGR_Generator_ABC
 public:
     //! @name Constructors/Destructor
     //@{
-             AGR_Generator_ABC() {};
-    virtual ~AGR_Generator_ABC() {};
+             AGR_Generator_ABC() {}
+    virtual ~AGR_Generator_ABC() {}
     //@}
 
     //! @name Operations
