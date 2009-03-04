243da9f6f611cc9e961daa306f45dec69a17e412
Sébastien Brossolasco <sebastien.brossolasco@masagroup.net>
2009-03-04 15:38:34 +0000

    scripts: added missing file

---
+++ src/applications/gaming_app/FilterViewCommand.cpp
@@ -0,0 +1,45 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2009 MASA Group
+//
+// *****************************************************************************
+
+#include "gaming_app_pch.h"
+#include "FilterViewCommand.h"
+#include "gaming/Command.h"
+#include "gaming/CommandHandler.h"
+#include "gaming/ProfileFilter.h"
+#include <string>
+
+// -----------------------------------------------------------------------------
+// Name: FilterViewCommand constructor
+// Created: SBO 2009-03-04
+// -----------------------------------------------------------------------------
+FilterViewCommand::FilterViewCommand( CommandHandler& handler, ProfileFilter& filter )
+    : handler_( handler )
+    , filter_( filter )
+{
+    handler_.Register( "clearfilter", *this );
+}
+
+// -----------------------------------------------------------------------------
+// Name: FilterViewCommand destructor
+// Created: SBO 2009-03-04
+// -----------------------------------------------------------------------------
+FilterViewCommand::~FilterViewCommand()
+{
+    handler_.Unregister( "clearfilter", *this );
+}
+
+// -----------------------------------------------------------------------------
+// Name: FilterViewCommand::Receive
+// Created: SBO 2009-03-04
+// -----------------------------------------------------------------------------
+void FilterViewCommand::Receive( const Command& command )
+{
+    if( command.Name() == "clearfilter" )
+        filter_.RemoveFilter();
+}
---
+++ src/applications/gaming_app/FilterViewCommand.h
@@ -0,0 +1,54 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2009 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __FilterViewCommand_h_
+#define __FilterViewCommand_h_
+
+#include "gaming/CommandHandler_ABC.h"
+
+class CommandHandler;
+class ProfileFilter;
+
+// =============================================================================
+/** @class  FilterViewCommand
+    @brief  FilterViewCommand
+*/
+// Created: SBO 2009-03-04
+// =============================================================================
+class FilterViewCommand : public CommandHandler_ABC
+{
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             FilterViewCommand( CommandHandler& handler, ProfileFilter& filter );
+    virtual ~FilterViewCommand();
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual void Receive( const Command& command );
+    //@}
+
+private:
+    //! @name Copy/Assignment
+    //@{
+    FilterViewCommand( const FilterViewCommand& );            //!< Copy constructor
+    FilterViewCommand& operator=( const FilterViewCommand& ); //!< Assignment operator
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    CommandHandler& handler_;
+    ProfileFilter& filter_;
+    //@}
+};
+
+#endif // __FilterViewCommand_h_
