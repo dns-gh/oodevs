f658d0197f6aa8d07631150afad47a85f51c1c64
Sébastien Brossolasco <sebastien.brossolasco@masagroup.net>
2009-03-12 16:27:49 +0000

    3a: added Threshold reduction function to be used for normalization computing

---
+++ src/libraries/3a/Threshold.h
@@ -0,0 +1,117 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2009 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __Threshold_h_
+#define __Threshold_h_
+
+#include "Reductor_ABC.h"
+#include "TypeChecks.h"
+#include <set>
+#include <map>
+#include <xeumeuleu/xml.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+
+#pragma warning( push )
+#pragma warning( disable : 4800 4804 )
+
+namespace xml { class xistream; }
+
+// =============================================================================
+/** @class  Threshold
+    @brief  Threshold
+*/
+// Created: SBO 2009-03-12
+// =============================================================================
+template< typename K, typename T >
+class Threshold : public Reductor_ABC< K, T >, private types::Arithmetic< T >
+{
+public:
+    //! @name Types
+    //@{
+    typedef T Result_Type;
+    //@}
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             Threshold( xml::xistream& xis, Function1_ABC< K, T >& handler )
+                : handler_( handler )
+                , thresholds_( ReadThresholds( xis ) ) {};
+    virtual ~Threshold() {};
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual std::string GetName() const { return "Threshold"; }
+    virtual void OnBeginTick()
+    {
+        handler_.BeginTick();
+    };
+    virtual void SetKey( const K& /*key*/ )
+    {
+    }
+    virtual void Apply( const T& value )
+    {
+        handler_.Apply( ApplyThreshold( value ) );
+    };
+    virtual void OnEndTick()
+    {
+        handler_.EndTick();
+    }
+    //@}
+
+private:
+    //! @name Copy/Assignment
+    //@{
+    Threshold( const Threshold& );            //!< Copy constructor
+    Threshold& operator=( const Threshold& ); //!< Assignment operator
+    //@}
+
+    //! @name Helpers
+    //@{
+    std::map< T, T > ReadThresholds( xml::xistream& xis )
+    {
+        std::vector< std::string > split;
+        boost::algorithm::split( split, xml::attribute< std::string >( xis, "thresholds" ), boost::algorithm::is_any_of( "," ) );
+        std::set< T > ranges;
+        for( std::vector< std::string >::const_iterator it = split.begin(); it != split.end(); ++it )
+            ranges.insert( boost::lexical_cast< T, std::string >( *it ) );
+        ranges.insert( std::numeric_limits< T >::max() );
+        
+        boost::algorithm::split( split, xml::attribute< std::string >( xis, "values" ), boost::algorithm::is_any_of( "," ) );
+        if( split.size() < ranges.size() )
+        {
+            const std::string lastValue = split.empty() ? "0" : split.back();
+            std::fill_n( std::back_inserter( split ), ranges.size() - split.size(), lastValue );
+        }
+       
+        std::map< T, T > thresholds;
+        std::vector< std::string >::iterator valuesIt = split.begin();
+        for( std::set< T >::const_iterator it = ranges.begin(); it != ranges.end(); ++it )
+            thresholds.insert( std::make_pair( *it, boost::lexical_cast< T, std::string >( *valuesIt++ ) ) );
+        return thresholds;
+    }
+
+    T ApplyThreshold( const T& value )
+    {
+        std::map< T, T >::const_iterator it = thresholds_.lower_bound( value );
+        return it != thresholds_.end() ? it->second : T();
+    }
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    Function1_ABC< K, T >& handler_;
+    const std::map< T, T > thresholds_;
+    //@}
+};
+
+#endif // __Threshold_h_
--- src/tests/3a_test/ReductionTest.cpp
+++ src/tests/3a_test/ReductionTest.cpp
@@ -16,6 +16,7 @@
 #include "3a/Minimum.h"
 #include "3a/Maximum.h"
 #include "3a/Functions.h"
+#include "3a/Threshold.h"
 #include <xeumeuleu/xml.h>
 
 using namespace mockpp;
@@ -176,5 +177,84 @@ BOOST_AUTO_TEST_CASE( Reduction_TestMaximum )
         handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 18.f ) );
         handler.EndTick_mocker.expects( exactly( 2 ) );
         ReduceTwoTicks( *selector );
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: ReductionTest_TestThresholdSingleValue
+// Created: SBO 2009-03-12
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE( Reduction_TestThresholdSingleValue )
+{
+    const std::string input = "<reduce type='float' thresholds='20' values='0.1,0.2' input='in' id='out'/>";
+    xml::xistringstream xis( input );
+    xis >> xml::start( "reduce" );
+    MockFunction1< unsigned long, float > handler;
+    std::auto_ptr< Function1_ABC< unsigned long, float > > stepper( new Threshold< unsigned long, float >( xis, handler ) );
+    {
+        handler.BeginTick_mocker.expects( once() );
+        handler.Apply_mocker.expects( once() ).with( eq( 0.1f ) );
+        handler.Apply_mocker.expects( once() ).with( eq( 0.2f ) );
+        handler.EndTick_mocker.expects( once() );
+        ReduceSimple( *stepper );
+    }
+    {
+        handler.BeginTick_mocker.expects( exactly( 2 ) );
+        handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 0.1f ) );
+        handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 0.2f ) );
+        handler.EndTick_mocker.expects( exactly( 2 ) );
+        ReduceTwoTicks( *stepper );
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: ReductionTest_TestThresholdMultipleValues
+// Created: SBO 2009-03-12
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE( Reduction_TestThresholdMultipleValues )
+{
+    const std::string input = "<reduce type='float' thresholds='0,10,20' values='0,0.25,0.5,1' input='in' id='out'/>";
+    xml::xistringstream xis( input );
+    xis >> xml::start( "reduce" );
+    MockFunction1< unsigned long, float > handler;
+    std::auto_ptr< Function1_ABC< unsigned long, float > > stepper( new Threshold< unsigned long, float >( xis, handler ) );
+    {
+        handler.BeginTick_mocker.expects( once() );
+        handler.Apply_mocker.expects( once() ).with( eq( 0.5f ) );
+        handler.Apply_mocker.expects( once() ).with( eq( 1.0f ) );
+        handler.EndTick_mocker.expects( once() );
+        ReduceSimple( *stepper );
+    }
+    {
+        handler.BeginTick_mocker.expects( exactly( 2 ) );
+        handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 0.5f ) );
+        handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 1.0f ) );
+        handler.EndTick_mocker.expects( exactly( 2 ) );
+        ReduceTwoTicks( *stepper );
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: Reduction_TestThresholdMoreValuesThanRanges
+// Created: SBO 2009-03-12
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE( Reduction_TestThresholdMoreValuesThanRanges )
+{
+    const std::string input = "<reduce type='float' thresholds='0,10,20' values='0,0.25' input='in' id='out'/>";
+    xml::xistringstream xis( input );
+    xis >> xml::start( "reduce" );
+    MockFunction1< unsigned long, float > handler;
+    std::auto_ptr< Function1_ABC< unsigned long, float > > stepper( new Threshold< unsigned long, float >( xis, handler ) );
+    {
+        handler.BeginTick_mocker.expects( once() );
+        handler.Apply_mocker.expects( exactly( 2 ) ).with( eq( 0.25f ) );
+        handler.EndTick_mocker.expects( once() );
+        ReduceSimple( *stepper );
+    }
+    {
+        handler.BeginTick_mocker.expects( exactly( 2 ) );
+        handler.Apply_mocker.expects( exactly( 4 ) ).with( eq( 0.25f ) );
+        handler.EndTick_mocker.expects( exactly( 2 ) );
+        ReduceTwoTicks( *stepper );
     }
 }
