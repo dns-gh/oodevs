53b5764c350f45e2b044c25a9b0b689f66acf56b
Sébastien Brossolasco <sebastien.brossolasco@masagroup.net>
2009-05-26 17:36:10 +0000

    package_app: issue #1809 : fixed otpak with name containing spaces

--- src/applications/package_app/package_app.cpp
+++ src/applications/package_app/package_app.cpp
@@ -9,7 +9,13 @@
 
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/convenience.hpp>
+#pragma warning( push )
+#pragma warning( disable: 4127 4512 )
+#include <boost/program_options.hpp>
+#pragma warning( pop )
 #include <windows.h>
+
+namespace po = boost::program_options;
 
 namespace
 {
@@ -20,21 +26,11 @@ namespace
             SetCurrentDirectory( boost::filesystem::path( buffer, boost::filesystem::native ).branch_path().native_file_string().c_str() );
     }
 
-    std::string CommandLine( int argc, char* argv[] )
+    void Execute( const std::string& commandLine )
     {
-        std::string result( "frontend_app.exe" );
-        const std::string separator( " " );
-        for( int i = 1; i < argc; ++i )
-            result += separator + argv[i];
-        return result;
-    }
-
-    void Execute( int argc, char* argv[] )
-    {
-        const std::string commandLine( CommandLine( argc, argv ) );
         char* command = const_cast< char* >( commandLine.c_str() );
         STARTUPINFO startupInfo = { sizeof( STARTUPINFOA ), 0, 0, 0,
-            (unsigned long)CW_USEDEFAULT, (unsigned long)CW_USEDEFAULT, (unsigned long)CW_USEDEFAULT, (unsigned long)CW_USEDEFAULT,
+            unsigned long( CW_USEDEFAULT ), unsigned long( CW_USEDEFAULT ), unsigned long( CW_USEDEFAULT ), unsigned long( CW_USEDEFAULT ),
             0, 0, 0,
             0,
             0, 0, 0,
@@ -52,7 +48,17 @@ int main( int argc, char* argv[] )
     try
     {
         SetDirectory();
-        Execute( argc, argv );
+        std::string package;
+        po::options_description options;
+        po::variables_map       values;
+        options.add_options()( "install", po::value< std::string >( &package ), "package to install" );
+        po::store( po::command_line_parser( argc, argv ).options( options ).run(), values );
+        po::notify( values );
+        if( values.count( "install" ) )
+        {
+            const std::string command = "frontend_app.exe --install=\"" + package + "\"";
+            Execute( command );
+        }
     }
     catch( std::exception& e )
     {
