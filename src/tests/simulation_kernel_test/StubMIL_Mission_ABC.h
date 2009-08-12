e86d561f8c0721d754751ad2f9a546f680e5b65e
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2009-08-12 14:01:15 +0000

    Fix build

--- src/tests/simulation_kernel_test/StubMIL_Mission_ABC.h
+++ src/tests/simulation_kernel_test/StubMIL_Mission_ABC.h
@@ -3,8 +3,8 @@
 
 #include "Entities/Orders/MIL_Mission_ABC.h"
 #include "Entities/Orders/MIL_ParameterType_ABC.h"
+#include "Entities/Orders/MIL_MissionParameterVisitor_ABC.h"
 #include "StubMIL_MissionParameter_ABC.h"
-#include "MIL_MissionParameterVisitor_ABC.h"
 
 class StubMIL_Mission_ABC 
     : public MIL_Mission_ABC
