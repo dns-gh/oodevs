dce5aa74c973a0e2454a73767697093951751ad3
Sébastien Le Gall <sebastien.legall@masagroup.net>
2009-11-19 14:28:05 +0000

    MGD : Add missing file for urbanblock knowledge

---
+++ data/data/models/baseTestBM/decisional/models/sword/net/masagroup/sword/military/world/UrbanBlock.lua
@@ -0,0 +1,28 @@
+defaultMethods
+{
+   
+}
+return
+{
+    moveToIt = function( urbanBlock )
+        return integration.moveToIt( urbanBlock )
+    end,
+    
+    reachEfficiency = function( urbanBlock, keypoint )
+        return integration.reachEfficiency( urbanBlock, keypoint )
+    end,
+	
+	reachPriority = function( point )
+        return integration.reachPriority( point )
+    end,
+    
+    observeIt = function( urbanBlock )
+        --TODO 
+    end,
+    safeApproachIt = function( urbanBlock )
+        --TODO
+    end,
+    recce = function( urbanBlock )
+        --TODO
+    end
+}
