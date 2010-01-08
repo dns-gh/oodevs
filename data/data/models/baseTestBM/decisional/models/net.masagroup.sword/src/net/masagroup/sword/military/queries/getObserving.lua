d131597048f3e6b770c1ba65ab2135aaea8159ab
Morgan Grignard <morgan.grignard@masagroup.net>
2010-01-08 09:54:56 +0000

    commit forgotten query

---
+++ data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getObserving.lua
@@ -0,0 +1,12 @@
+queryImplementation "getObserving" { ["execute"] = function ( params )
+    local allRes = {}
+    for _, objective in pairs( params.objectives ) do
+        local res = {}
+        DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 10000, res )
+        if objective:isOfType( net.masagroup.military.generic.knowledge.Observing ) then
+          res[ #res + 1 ] = objective -- add objectif itself
+        end
+        allRes[ objective ] = res
+    end
+    return allRes
+end}
