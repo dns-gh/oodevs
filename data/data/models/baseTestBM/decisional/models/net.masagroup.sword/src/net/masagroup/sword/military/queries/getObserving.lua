4aade3ca6a661f82530fea1dfbbc1b460222738d
Robert Piechaud <robert.piechaud@masagroup.net>
2010-01-15 14:37:43 +0000

    fixing build by test cards folder to data/

--- data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getObserving.lua
+++ data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getObserving.lua
@@ -2,7 +2,7 @@ queryImplementation "getObserving" { ["execute"] = function ( params )
     local allRes = {}
     for _, objective in pairs( params.objectives ) do
         local res = {}
-        DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 10000, res )
+        DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 499, res )
         if objective:isOfType( net.masagroup.military.generic.knowledge.Observing ) then
           res[ #res + 1 ] = objective -- add objectif itself
         end
--- data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getReconnoiteringCrossroads.lua
+++ data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getReconnoiteringCrossroads.lua
@@ -2,7 +2,7 @@ queryImplementation "getReconnoiteringCrossroads" { ["execute"] = function ( par
     local allRes = {}
     for _, objective in pairs( params.objectives ) do
         local res = {}
-        DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 10000, res )
+        DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 500, res )
         if objective:isOfType( net.masagroup.military.generic.knowledge.Reconnoitring ) then
           res[ #res + 1 ] = objective -- add objectif itself
         end
