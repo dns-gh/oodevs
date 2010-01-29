98cb697186bc3a92713f26589e6b931bef4e4dfb
Morgan Grignard <morgan.grignard@masagroup.net>
2010-01-29 12:48:01 +0000

    Remove old unused skill. Redefine sword MoveToward to avoid query and test generic MoveAndTakePosition

--- data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getCrossroads.lua
+++ data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getCrossroads.lua
@@ -2,7 +2,6 @@ queryImplementation "getCrossroads" { ["execute"] = function ( params )
     local allRes = {}
     for _, objective in pairs( params.objectives ) do
         local res = {}
-        --BreakForDebug( objective.sim_pos.x )
         DEC_Crossroads( objective.sim_pos.x, objective.sim_pos.y, objective.sim_pos.z, 5000, res )
         if objective:isOfType( net.masagroup.military.generic.knowledge.Reaching ) then
           res[ #res + 1 ] = objective -- add objectif itself
