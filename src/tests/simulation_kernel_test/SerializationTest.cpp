2442ac7c8d3abbb4e4113505df60ef24f3fb1ae0
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2010-03-01 18:00:51 +0000

    simulation: Checkpoints: RoleContainers must NOT serialize their roles because it leads to order dependencies -> commented test before removal.

--- src/tests/simulation_kernel_test/SerializationTest.cpp
+++ src/tests/simulation_kernel_test/SerializationTest.cpp
@@ -106,146 +106,146 @@ BOOST_CLASS_EXPORT( MyRole2 )
 // Name: SerializationTest_SimpleDerivedRole
 // Created: SBO 2009-08-14
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( SerializationTest_SimpleDerivedRole )
-{
-    const std::string filename = BOOST_RESOLVE( "output-simple.bin" );
-    {
-        std::ofstream os( filename.c_str(), std::ios::out );
-        BOOST_CHECK( os && os.is_open() );
-        OutputArchive out( os );
-        MyEntity* entity( new MyEntity() );
-        entity->Attach( *new MyRole( *entity ) );
-        BOOST_CHECK_NO_THROW( out << entity );
-        delete entity;
-    }
-    {
-        std::ifstream is( filename.c_str(), std::ios::in );
-        BOOST_CHECK( is && is.is_open() );
-        InputArchive in( is );
-        MyEntity* entity;
-        BOOST_CHECK_NO_THROW( in >> entity );
-        BOOST_REQUIRE( entity );
-        BOOST_CHECK_NO_THROW( entity->Get< MyRole >() );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole >().GetId(), 4242u );
-        delete entity;
-    }
-}
+//BOOST_AUTO_TEST_CASE( SerializationTest_SimpleDerivedRole )
+//{
+//    const std::string filename = BOOST_RESOLVE( "output-simple.bin" );
+//    {
+//        std::ofstream os( filename.c_str(), std::ios::out );
+//        BOOST_CHECK( os && os.is_open() );
+//        OutputArchive out( os );
+//        MyEntity* entity( new MyEntity() );
+//        entity->Attach( *new MyRole( *entity ) );
+//        BOOST_CHECK_NO_THROW( out << entity );
+//        delete entity;
+//    }
+//    {
+//        std::ifstream is( filename.c_str(), std::ios::in );
+//        BOOST_CHECK( is && is.is_open() );
+//        InputArchive in( is );
+//        MyEntity* entity;
+//        BOOST_CHECK_NO_THROW( in >> entity );
+//        BOOST_REQUIRE( entity );
+//        BOOST_CHECK_NO_THROW( entity->Get< MyRole >() );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole >().GetId(), 4242u );
+//        delete entity;
+//    }
+//}
 
 // -----------------------------------------------------------------------------
 // Name: SerializationTest_MultipleDerivedRoles
 // Created: SBO 2009-08-14
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( SerializationTest_MultipleDerivedRoles )
-{
-    const std::string filename = BOOST_RESOLVE( "output-multiple.bin" );
-    {
-        std::ofstream os( filename.c_str(), std::ios::out );
-        BOOST_CHECK( os && os.is_open() );
-        OutputArchive out( os );
-        MyEntity* entity = new MyEntity();
-        entity->Attach( *new MyRole ( *entity ) );
-        entity->Attach( *new MyRole2( *entity ) );
-        BOOST_CHECK_NO_THROW( out << entity );
-        delete entity;
-    }
-    {
-        std::ifstream is( filename.c_str(), std::ios::in );
-        BOOST_CHECK( is && is.is_open() );
-        InputArchive in( is );
-        MyEntity* entity;
-        BOOST_CHECK_NO_THROW( in >> entity );
-        BOOST_REQUIRE( entity );
-        BOOST_CHECK_NO_THROW( entity->Get< MyRole >() );
-        BOOST_CHECK_NO_THROW( entity->Get< MyRole2 >() );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole >().GetId(), 4242u );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole2 >().GetValue(), 4.2f );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole2 >().GetName(), "my role 2" );
-        delete entity;
-    }
-}
+//BOOST_AUTO_TEST_CASE( SerializationTest_MultipleDerivedRoles )
+//{
+//    const std::string filename = BOOST_RESOLVE( "output-multiple.bin" );
+//    {
+//        std::ofstream os( filename.c_str(), std::ios::out );
+//        BOOST_CHECK( os && os.is_open() );
+//        OutputArchive out( os );
+//        MyEntity* entity = new MyEntity();
+//        entity->Attach( *new MyRole ( *entity ) );
+//        entity->Attach( *new MyRole2( *entity ) );
+//        BOOST_CHECK_NO_THROW( out << entity );
+//        delete entity;
+//    }
+//    {
+//        std::ifstream is( filename.c_str(), std::ios::in );
+//        BOOST_CHECK( is && is.is_open() );
+//        InputArchive in( is );
+//        MyEntity* entity;
+//        BOOST_CHECK_NO_THROW( in >> entity );
+//        BOOST_REQUIRE( entity );
+//        BOOST_CHECK_NO_THROW( entity->Get< MyRole >() );
+//        BOOST_CHECK_NO_THROW( entity->Get< MyRole2 >() );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole >().GetId(), 4242u );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole2 >().GetValue(), 4.2f );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole2 >().GetName(), "my role 2" );
+//        delete entity;
+//    }
+//}
+//
+//class MyRole3 : public tools::Role_ABC
+//{
+//public:
+//             MyRole3( MyEntity& entity, unsigned int id ) : entity_( entity ), id_( id ), value_( 4.2f ), name_( "My role 3" ) {}
+//    virtual ~MyRole3() {}
+//
+//    float GetValue() const { return value_; }
+//    std::string GetName() const { return name_; }
+//
+//private:
+//    friend class boost::serialization::access;
+//    BOOST_SERIALIZATION_SPLIT_MEMBER()
+//    void load( InputArchive& archive, const unsigned int )
+//    {
+//        archive >> boost::serialization::base_object< tools::Role_ABC >( *this )
+//                >> value_
+//                >> name_;
+//        entity_.Attach( *this );
+//    }
+//    void save( OutputArchive& archive, const unsigned int ) const
+//    {
+//        archive << boost::serialization::base_object< tools::Role_ABC >( *this )
+//                << value_
+//                << name_;
+//    }
+//
+//private:
+//    template< typename Archive > friend void save_construct_data( Archive& archive, const MyRole3* element, const unsigned int version );
+//    template< typename Archive > friend void load_construct_data( Archive& archive, MyRole3* element, const unsigned int version );
+//
+//private:
+//    MyEntity& entity_;
+//    const unsigned int id_;
+//    float value_;
+//    std::string name_;
+//};
+//
+//template< typename Archive >
+//void save_construct_data( Archive& archive, const MyRole3* element, const unsigned int /*version*/ )
+//{
+//    MyEntity* const ref = &element->entity_;
+//    archive << ref << element->id_;
+//}
+//
+//template< typename Archive >
+//void load_construct_data( Archive& archive, MyRole3* element, const unsigned int /*version*/ )
+//{
+//    MyEntity* entity;
+//    unsigned int id;
+//    archive >> entity >> id;
+//    BOOST_CHECK( entity );
+//    ::new( element )MyRole3( *entity, id );
+//}
 
-class MyRole3 : public tools::Role_ABC
-{
-public:
-             MyRole3( MyEntity& entity, unsigned int id ) : entity_( entity ), id_( id ), value_( 4.2f ), name_( "My role 3" ) {}
-    virtual ~MyRole3() {}
-
-    float GetValue() const { return value_; }
-    std::string GetName() const { return name_; }
-
-private:
-    friend class boost::serialization::access;
-    BOOST_SERIALIZATION_SPLIT_MEMBER()
-    void load( InputArchive& archive, const unsigned int )
-    {
-        archive >> boost::serialization::base_object< tools::Role_ABC >( *this )
-                >> value_
-                >> name_;
-        entity_.Attach( *this );
-    }
-    void save( OutputArchive& archive, const unsigned int ) const
-    {
-        archive << boost::serialization::base_object< tools::Role_ABC >( *this )
-                << value_
-                << name_;
-    }
-
-private:
-    template< typename Archive > friend void save_construct_data( Archive& archive, const MyRole3* element, const unsigned int version );
-    template< typename Archive > friend void load_construct_data( Archive& archive, MyRole3* element, const unsigned int version );
-
-private:
-    MyEntity& entity_;
-    const unsigned int id_;
-    float value_;
-    std::string name_;
-};
-
-template< typename Archive >
-void save_construct_data( Archive& archive, const MyRole3* element, const unsigned int /*version*/ )
-{
-    MyEntity* const ref = &element->entity_;
-    archive << ref << element->id_;
-}
-
-template< typename Archive >
-void load_construct_data( Archive& archive, MyRole3* element, const unsigned int /*version*/ )
-{
-    MyEntity* entity;
-    unsigned int id;
-    archive >> entity >> id;
-    BOOST_CHECK( entity );
-    ::new( element )MyRole3( *entity, id );
-}
-
-BOOST_CLASS_EXPORT( MyRole3 )
+//BOOST_CLASS_EXPORT( MyRole3 )
 
 // -----------------------------------------------------------------------------
 // Name: SerializationTest_NoDefaultConstructor
 // Created: SBO 2009-08-14
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( SerializationTest_NoDefaultConstructor )
-{
-    const std::string filename = BOOST_RESOLVE( "output-no-default-constructor.bin" );
-    {
-        std::ofstream os( filename.c_str(), std::ios::out );
-        BOOST_CHECK( os && os.is_open() );
-        OutputArchive out( os );
-        MyEntity* entity = new MyEntity();
-        entity->Attach( *new MyRole3( *entity, 4242 ) );
-        BOOST_CHECK_NO_THROW( out << entity );
-        delete entity;
-    }
-    {
-        std::ifstream is( filename.c_str(), std::ios::in );
-        BOOST_CHECK( is && is.is_open() );
-        InputArchive in( is );
-        MyEntity* entity;
-        BOOST_CHECK_NO_THROW( in >> entity );
-        BOOST_CHECK( entity );
-        BOOST_CHECK_NO_THROW( entity->Get< MyRole3 >() );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole3 >().GetValue(), 4.2f );
-        BOOST_CHECK_EQUAL( entity->Get< MyRole3 >().GetName(), "My role 3" );
-        delete entity;
-    }
-}
+//BOOST_AUTO_TEST_CASE( SerializationTest_NoDefaultConstructor )
+//{
+//    const std::string filename = BOOST_RESOLVE( "output-no-default-constructor.bin" );
+//    {
+//        std::ofstream os( filename.c_str(), std::ios::out );
+//        BOOST_CHECK( os && os.is_open() );
+//        OutputArchive out( os );
+//        MyEntity* entity = new MyEntity();
+//        entity->Attach( *new MyRole3( *entity, 4242 ) );
+//        BOOST_CHECK_NO_THROW( out << entity );
+//        delete entity;
+//    }
+//    {
+//        std::ifstream is( filename.c_str(), std::ios::in );
+//        BOOST_CHECK( is && is.is_open() );
+//        InputArchive in( is );
+//        MyEntity* entity;
+//        BOOST_CHECK_NO_THROW( in >> entity );
+//        BOOST_CHECK( entity );
+//        BOOST_CHECK_NO_THROW( entity->Get< MyRole3 >() );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole3 >().GetValue(), 4.2f );
+//        BOOST_CHECK_EQUAL( entity->Get< MyRole3 >().GetName(), "My role 3" );
+//        delete entity;
+//    }
+//}
