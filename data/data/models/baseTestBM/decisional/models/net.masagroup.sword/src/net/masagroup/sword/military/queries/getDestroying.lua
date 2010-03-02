760d334089329bfba7d9a7e2b58b648d0a7c8a72
Morgan Grignard <morgan.grignard@masagroup.net>
2010-03-02 08:25:00 +0000

    Fix query corresponding to Destroy skill changes

--- data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getDestroying.lua
+++ data/data/models/baseTestBM/decisional/models/net.masagroup.sword/src/net/masagroup/sword/military/queries/getDestroying.lua
@@ -3,11 +3,9 @@ queryImplementation "getDestroying" { ["execute"] = function ( params )
       allRes = {}
       -- Add position of myself, add position of all keypoints nearby
       for _, objective in pairs( params.entities ) do
-        local res = {}
         if objective:isOfType( net.masagroup.military.generic.knowledge.Destroying ) then
-          res[ #res + 1 ] = objective -- add objectif itself
+          allRes[ #allRes + 1 ] = objective -- add objectif itself
         end
-        allRes[ objective ] = res
       end
     return allRes
 end}
