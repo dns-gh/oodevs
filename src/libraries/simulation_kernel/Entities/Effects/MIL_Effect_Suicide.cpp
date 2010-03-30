c35ecf36743d164be573e03ea530edd14178d55a
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2010-03-30 14:31:45 +0000

    simulation: Attentats terroristes avec paramètre suicide optionnel et consommation de dotation obligatoire.

---
+++ src/libraries/simulation_kernel/Entities/Effects/MIL_Effect_Suicide.cpp
@@ -0,0 +1,52 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#include "simulation_kernel_pch.h"
+#include "MIL_Effect_Suicide.h"
+#include "Entities/Agents/MIL_Agent_ABC.h"
+#include "Entities/Agents/Roles/Composantes/PHY_RolePion_Composantes.h"
+#include "Entities/Agents/Roles/Humans/PHY_RoleInterface_Humans.h"
+#include "Entities/Agents/Units/Humans/PHY_HumanRank.h"
+
+// -----------------------------------------------------------------------------
+// Name: MIL_Effect_Suicide constructor
+// Created: LDC 2010-03-29
+// -----------------------------------------------------------------------------
+MIL_Effect_Suicide::MIL_Effect_Suicide( MIL_Agent_ABC& target )
+: target_( target )
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: MIL_Effect_Suicide destructor
+// Created: LDC 2010-03-29
+// -----------------------------------------------------------------------------
+MIL_Effect_Suicide::~MIL_Effect_Suicide()
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: MIL_Effect_Suicide::Execute
+// Created: LDC 2010-03-29
+// -----------------------------------------------------------------------------
+bool MIL_Effect_Suicide::Execute()
+{
+    human::PHY_RoleInterface_Humans& humanRole = target_.GetRole< human::PHY_RoleInterface_Humans >();
+    const PHY_HumanRank::T_HumanRankMap& ranks = PHY_HumanRank::GetHumanRanks();
+    for( PHY_HumanRank::CIT_HumanRankMap it = ranks.begin(); it != ranks.end(); ++it )
+        humanRole.ChangeHumansAvailability( *it->second, 0 );
+
+    PHY_RolePion_Composantes& roleComposantes = target_.GetRole< PHY_RolePion_Composantes >();
+    roleComposantes.DestroyAllComposantes();
+
+    delete this;
+    return false;
+}
---
+++ src/libraries/simulation_kernel/Entities/Effects/MIL_Effect_Suicide.h
@@ -0,0 +1,52 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __MIL_Effect_Suicide_h_
+#define __MIL_Effect_Suicide_h_
+
+#include "MIL_Effect_ABC.h"
+
+class MIL_Agent_ABC;
+
+// =============================================================================
+/** @class  MIL_Effect_Suicide
+    @brief  MIL_Effect_Suicide
+*/
+// Created: LDC 2010-03-29
+// =============================================================================
+class MIL_Effect_Suicide : public MIL_Effect_ABC
+{
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             MIL_Effect_Suicide( MIL_Agent_ABC& target );
+    virtual ~MIL_Effect_Suicide();
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual bool Execute();
+    //@}
+
+private:
+    //! @name Copy/Assignment
+    //@{
+    MIL_Effect_Suicide( const MIL_Effect_Suicide& );            //!< Copy constructor
+    MIL_Effect_Suicide& operator=( const MIL_Effect_Suicide& ); //!< Assignment operator
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    MIL_Agent_ABC& target_;
+    //@}
+};
+
+#endif // __MIL_Effect_Suicide_h_
