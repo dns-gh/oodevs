e94a1a0b5854401b86f2fd09a12f99e00a17f3bd
Morgan Grignard <morgan.grignard@masagroup.net>
2010-04-22 14:07:30 +0000

    Forgotten file

---
+++ src/libraries/simulation_kernel/MovementHandler_ABC.h
@@ -0,0 +1,31 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __MovementHandler_ABC_H__
+#define __MovementHandler_ABC_H__
+
+namespace location {
+
+// =============================================================================
+/** @class  MovementHandler_ABC
+    @brief  MovementHandler_ABC
+*/
+// Created: MGD 2010-04-21
+// =============================================================================
+class MovementHandler_ABC {
+public:
+             MovementHandler_ABC() {}
+    virtual ~MovementHandler_ABC() {}
+
+    virtual void NotifyHasMove( const MT_Vector2D& newPos ) = 0;
+};
+
+} // namespace component
+
+#endif /* __MovementHandler_ABC_H__ */
