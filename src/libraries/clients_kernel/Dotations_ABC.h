7316f4fcb2684f6fd870b6810d5a1a6358cb85c6
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2010-06-08 06:44:56 +0000

    clients_kernel: Correction missing file

---
+++ src/libraries/clients_kernel/Dotations_ABC.h
@@ -0,0 +1,43 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __Dotations_ABC_h_
+#define __Dotations_ABC_h_
+
+#include "Extension_ABC.h"
+
+namespace kernel {
+
+class DotationType;
+
+// =============================================================================
+/** @class  Dotations_ABC
+    @brief  Dotations_ABC
+*/
+// Created: LDC 2010-06-07
+// =============================================================================
+class Dotations_ABC : public kernel::Extension_ABC
+{
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             Dotations_ABC() {}
+    virtual ~Dotations_ABC() {}
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual bool Accept( const kernel::DotationType& type ) const = 0;
+    //@}
+};
+
+} // end namespace
+
+#endif // __Dotations_ABC_h_
