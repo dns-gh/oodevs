a10478bac174bcde98c9c429e7fc7d9628043f7d
Helene Barraud <helene.barraud@masagroup.net>
2010-06-25 07:14:02 +0000

    Fixing build : missing file

---
+++ src/libraries/gaming/KnowledgeGroupFactory.cpp
@@ -0,0 +1,67 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#include "gaming_pch.h"
+#include "KnowledgeGroupFactory.h"
+#include "AgentKnowledges.h"
+#include "AgentKnowledgeFactory.h"
+#include "KnowledgeGroup.h"
+#include "KnowledgeGroupHierarchies.h"
+#include "KnowledgeGroupsModel.h"
+#include "Model.h"
+#include "ObjectKnowledges.h"
+#include "PopulationKnowledges.h"
+#include "StaticModel.h"
+#include "TeamsModel.h"
+#include "clients_kernel/AgentTypes.h"
+#include "protocol/SimulationSenders.h"
+
+using namespace kernel;
+
+// -----------------------------------------------------------------------------
+// Name: KnowledgeGroupFactory constructor
+// Created: HBD 2010-06-23
+// -----------------------------------------------------------------------------
+KnowledgeGroupFactory::KnowledgeGroupFactory( kernel::Controllers& controllers, Model& model )
+    : controllers_( controllers )
+    , model_( model )
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: KnowledgeGroupFactory destructor
+// Created: HBD 2010-06-23
+// -----------------------------------------------------------------------------
+KnowledgeGroupFactory::~KnowledgeGroupFactory()
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: KnowledgeGroupFactory::CreateKnowledgeGroup
+// Created: HBD 2010-06-23
+// -----------------------------------------------------------------------------
+kernel::KnowledgeGroup_ABC* KnowledgeGroupFactory::CreateKnowledgeGroup( const MsgsSimToClient::MsgKnowledgeGroupCreation& message )
+{
+    // LTO begin
+    Entity_ABC* superior = message.has_oid_parent() ? 
+        (Entity_ABC*) &model_.knowledgeGroups_.Resolver< KnowledgeGroup_ABC >::Get( message.oid_parent() ) :
+    (Entity_ABC*) &model_.teams_.Resolver< Team_ABC >::Get( message.oid_camp() );
+    // LTO end
+
+    KnowledgeGroup* result = new KnowledgeGroup( message.oid(), controllers_.controller_, message.type(), model_.static_.types_ );
+    result->Attach( *new AgentKnowledges( controllers_.controller_, *result, model_.agentsKnowledgeFactory_ ) );
+    if( message.has_jam() && message.jam() )
+        result->Attach( *new ObjectKnowledges( *result, controllers_.controller_, model_.objectKnowledgeFactory_ ) );
+    result->Attach( *new PopulationKnowledges( controllers_.controller_, *result, model_.agentsKnowledgeFactory_ ) );
+    result->Attach< CommunicationHierarchies >( *new KnowledgeGroupHierarchies( controllers_.controller_, superior, *result, model_.knowledgeGroups_ ) ); // LTO
+    result->Polish();
+    return result;
+}
---
+++ src/libraries/gaming/KnowledgeGroupFactory.h
@@ -0,0 +1,62 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __KnowledgeGroupFactory_h_
+#define __KnowledgeGroupFactory_h_
+
+#include "KnowledgeGroupFactory_ABC.h"
+
+namespace kernel
+{
+    class Controllers;
+}
+
+class Model;
+
+// =============================================================================
+/** @class  KnowledgeGroupFactory
+    @brief  KnowledgeGroupFactory
+*/
+// Created: HBD 2010-06-23
+// =============================================================================
+class KnowledgeGroupFactory : public KnowledgeGroupFactory_ABC
+{
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             KnowledgeGroupFactory( kernel::Controllers& controllers, Model& model );
+    virtual ~KnowledgeGroupFactory();
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual kernel::KnowledgeGroup_ABC* CreateKnowledgeGroup( const MsgsSimToClient::MsgKnowledgeGroupCreation& message ); // LTO
+    //@}
+
+private:
+    //! @name Copy/Assignment
+    //@{
+    KnowledgeGroupFactory( const KnowledgeGroupFactory& );            //!< Copy constructor
+    KnowledgeGroupFactory& operator=( const KnowledgeGroupFactory& ); //!< Assignment operator
+    //@}
+
+    //! @name Helpers
+    //@{
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    kernel::Controllers& controllers_;
+    Model& model_;
+    //@}
+};
+
+#endif // __KnowledgeGroupFactory_h_
