1544791cfcab8a6f1ba7859150c4ba7f041e88b8
Silvin Lubecki <silvin.lubecki@masagroup.net>
2010-07-07 14:44:49 +0000

    Nettoyage et warning fix

--- src/tests/simulation_kernel_test/PkComputerTest.cpp
+++ src/tests/simulation_kernel_test/PkComputerTest.cpp
@@ -21,7 +21,7 @@
 #include <Urban/CoordinateConverter_ABC.h>
 #include <Urban/CoordinateConverter.h>
 #include <Urban/Drawer_ABC.h>
-#include <xeumeuleu/xml.h>
+#include <xeumeuleu/xml.hpp>
 
 // -----------------------------------------------------------------------------
 // Name: PkComputerTest
@@ -35,24 +35,30 @@ BOOST_AUTO_TEST_CASE( PkComputerUrbanProtectionTest )
 
     const PHY_DotationCategory* pCategory = PHY_DotationType::FindDotationCategory( "ammo" );
 
-    xml::xistringstream xisModel( "<urbanObject id='3' name='School'>"
-        "<footprint><point location='31TCM1058982147'/><point location='31TCM1055881170'/><point location='31TCM1157881118'/><point location='31TCM1158882115'/></footprint>"
-        "<physical><architecture floor-number='30' height='40' material='Brique' roof-shape='flat' occupation ='0.8' trafficability='60'/>"
-        "</physical>"
-        "</urbanObject>");
+    xml::xistringstream xisModel(
+        "<urbanObject id='3' name='School'>"
+        "   <footprint>"
+        "       <point location='31TCM1058982147'/>"
+        "       <point location='31TCM1055881170'/>"
+        "       <point location='31TCM1157881118'/>"
+        "       <point location='31TCM1158882115'/>"
+        "   </footprint>"
+        "   <physical>"
+        "       <architecture floor-number='30' height='40' material='Brique' roof-shape='flat' occupation ='0.8' trafficability='60'/>"
+        "   </physical>"
+        "</urbanObject>" );
 
     MockAgent firer;
-    urban::Drawer_ABC* drawer = 0;
     urban::CoordinateConverter_ABC* coord = new urban::CoordinateConverter();
     xisModel >> xml::start( "urbanObject" );
     std::auto_ptr< urban::UrbanObject > urbanBlock;
     urbanBlock.reset( new urban::UrbanObject ( xisModel, 0, *coord ) );
     MIL_ObjectLoader loader;
     {
-        xml::xistringstream xis( "<objects>"
-            "<object type='urban block'/>"
-            "</objects>"
-            );
+        xml::xistringstream xis( 
+            "<objects>"
+            "   <object type='urban block'/>"
+            "</objects>" );
         BOOST_CHECK_NO_THROW( loader.Initialize( xis ) );
     }
     MIL_Object_ABC* pObject = loader.CreateUrbanObject( *urbanBlock );
