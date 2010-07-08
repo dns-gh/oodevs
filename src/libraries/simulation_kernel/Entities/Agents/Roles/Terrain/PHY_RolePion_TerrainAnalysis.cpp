6b73c90ba304896c51e3f7a6ac0af3261faa6085
Silvin Lubecki <silvin.lubecki@masagroup.net>
2010-07-08 13:30:38 +0000

    Nettoyage

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Terrain/PHY_RolePion_TerrainAnalysis.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Terrain/PHY_RolePion_TerrainAnalysis.cpp
@@ -14,9 +14,7 @@
 #include "Entities/Agents/Roles/Location/PHY_RoleInterface_Location.h"
 #include "Entities/Orders/MIL_Fuseau.h"
 #include "Entities/Orders/MIL_PionOrderManager.h"
-
 #include "simulation_terrain/TER_PathFindManager.h"
-
 
 // -----------------------------------------------------------------------------
 // Name: PHY_RolePion_TerrainAnalysis constructor
@@ -24,9 +22,8 @@
 // -----------------------------------------------------------------------------
 PHY_RolePion_TerrainAnalysis::PHY_RolePion_TerrainAnalysis( MIL_Agent_ABC& pion )
     : pion_( pion )
-    , lastPos_()
 {
-
+    // NOTHING
 }
 
 // -----------------------------------------------------------------------------
@@ -45,9 +42,7 @@ PHY_RolePion_TerrainAnalysis::~PHY_RolePion_TerrainAnalysis()
 void PHY_RolePion_TerrainAnalysis::GetCrossroads( std::vector< boost::shared_ptr< MT_Vector2D > >& points ) const
 {
     for( std::map< MT_Vector2D, boost::shared_ptr< MT_Vector2D > >::const_iterator it = crossroadsBuffer_.begin(); it != crossroadsBuffer_.end(); it++ )
-    {
         points.push_back( it->second );
-    }
 }
 
 // -----------------------------------------------------------------------------
@@ -56,8 +51,8 @@ void PHY_RolePion_TerrainAnalysis::GetCrossroads( std::vector< boost::shared_ptr
 // -----------------------------------------------------------------------------
 void PHY_RolePion_TerrainAnalysis::NotifyHasMove( const MT_Vector2D& newPos )
 {
-    double range = std::max( pion_.GetRole< PHY_RoleInterface_Perceiver >().GetMaxAgentPerceptionDistance(), 3000. );
-    if( lastPos_.SquareDistance( newPos ) > range*0.5 * range*0.5 )
+    const double range = std::max( pion_.GetRole< PHY_RoleInterface_Perceiver >().GetMaxAgentPerceptionDistance(), 3000. );
+    if( lastPos_.SquareDistance( newPos ) > range * 0.5 * range * 0.5 )
     {
         //Remove old points out of range
         for( std::map< MT_Vector2D, boost::shared_ptr< MT_Vector2D > >::iterator it = crossroadsBuffer_.begin(); it != crossroadsBuffer_.end(); )
@@ -65,31 +60,24 @@ void PHY_RolePion_TerrainAnalysis::NotifyHasMove( const MT_Vector2D& newPos )
             if( it->second->SquareDistance( newPos ) < range * range )
                 it = crossroadsBuffer_.erase( it );
             else
-                it++;
+                ++it;
         }
-
         //Add new points
-        std::vector< boost::shared_ptr< MT_Vector2D > > temp = TER_PathFindManager::GetPathFindManager().FindCrossroadsWithinCircle( newPos, range );
+        std::vector< boost::shared_ptr< MT_Vector2D > > temp = TER_PathFindManager::GetPathFindManager().FindCrossroadsWithinCircle( newPos, static_cast< float >( range ) );
         for( std::vector< boost::shared_ptr< MT_Vector2D > >::const_iterator it = temp.begin(); it != temp.end(); it++ )
-        {
             crossroadsBuffer_.insert( std::pair< MT_Vector2D, boost::shared_ptr< MT_Vector2D > >( **it, *it ) );
-        }
-
-
         //Remove outside fuseau
         const MIL_Fuseau& fuseau = pion_.GetOrderManager().GetFuseau();
         if( !fuseau.IsNull() )
         {
             for( std::map< MT_Vector2D, boost::shared_ptr< MT_Vector2D > >::iterator it = crossroadsBuffer_.begin(); it != crossroadsBuffer_.end(); )
             {
-
                 if( fuseau.IsInside( it->first ) )
                     it = crossroadsBuffer_.erase( it );
                 else
-                    it++;
+                    ++it;
             }
         }
-
         lastPos_ = newPos;
     }
 }
