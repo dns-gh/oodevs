897dc046f2ea57a6cf435ac4efba2b75660da7d1
Joël Soicher <joel.soicher@masagroup.net>
2010-07-12 13:54:58 +0000

    frontend: Lancement de GEN par le frontend (3775)

--- src/libraries/frontend/CreateTerrain.cpp
+++ src/libraries/frontend/CreateTerrain.cpp
@@ -48,7 +48,7 @@ CreateTerrain::CreateTerrain( const tools::GeneralConfig& config, const QString&
     const std::string directory = config.GetTerrainDir( name.ascii() );
     bfs::create_directories( directory );
 
-    addArgument( QString( "\"%1\"" ).arg( directory.c_str() ) );
+    addArgument( QString( "--out=\"%1\"" ).arg( directory.c_str() ) );
     SetWorkingDirectory( GetDirectory() );
 }
 
