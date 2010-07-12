9ea84fdb806b7dc166a386824c647aa7f85bbe6b
Loïc Geffroy <loic.geffroy@masagroup.net>
2010-07-12 09:49:50 +0000

    Nettoyage

--- src/libraries/tools/WinArguments.cpp
+++ src/libraries/tools/WinArguments.cpp
@@ -9,10 +9,7 @@
 
 #include "tools_pch.h"
 #include "WinArguments.h"
-//#pragma warning( push )
-//#pragma warning( disable: 4512 )
 #include <boost/program_options.hpp>
-//#pragma warning( pop )
 
 // -----------------------------------------------------------------------------
 // Name: WinArguments constructor
