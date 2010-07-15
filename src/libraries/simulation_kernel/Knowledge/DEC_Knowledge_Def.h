6c3e79b3cff3bd49913cda0e01b6f3b6709a2d00
Loïc Geffroy <loic.geffroy@masagroup.net>
2010-07-15 16:42:04 +0000

    simulation : cleaning

--- src/libraries/simulation_kernel/Knowledge/DEC_Knowledge_Def.h
+++ src/libraries/simulation_kernel/Knowledge/DEC_Knowledge_Def.h
@@ -24,10 +24,6 @@ class PHY_Volume;
 
 //! @name Types
 //@{
-typedef std::vector< DEC_Knowledge_AgentPerception* >      T_KnowledgeAgentPerceptionVector;
-typedef T_KnowledgeAgentPerceptionVector::iterator        IT_KnowledgeAgentPerceptionVector;
-typedef T_KnowledgeAgentPerceptionVector::const_iterator CIT_KnowledgeAgentPerceptionVector;
-
 typedef std::vector< boost::shared_ptr< DEC_Knowledge_Agent > > T_KnowledgeAgentVector;
 typedef T_KnowledgeAgentVector::iterator                       IT_KnowledgeAgentVector;
 typedef T_KnowledgeAgentVector::const_iterator                CIT_KnowledgeAgentVector;
@@ -67,17 +63,9 @@ typedef std::vector< DEC_Knowledge_AgentComposante >  T_KnowledgeComposanteVecto
 typedef T_KnowledgeComposanteVector::iterator        IT_KnowledgeComposanteVector;
 typedef T_KnowledgeComposanteVector::const_iterator CIT_KnowledgeComposanteVector;
 
-typedef std::vector< void* >                                    T_PopulationKnowledgeAgentDiaIDVector;
-typedef T_PopulationKnowledgeAgentDiaIDVector::iterator        IT_PopulationKnowledgeAgentDiaIDVector;
-typedef T_PopulationKnowledgeAgentDiaIDVector::const_iterator CIT_PopulationKnowledgeAgentDiaIDVector;
-
 typedef std::vector< DEC_Knowledge_Population* >      T_KnowledgePopulationVector;
 typedef T_KnowledgePopulationVector::iterator        IT_KnowledgePopulationVector;
 typedef T_KnowledgePopulationVector::const_iterator CIT_KnowledgePopulationVector;
-
-typedef std::vector< void* >                                     T_PopulationKnowledgeObjectDiaIDVector;
-typedef T_PopulationKnowledgeObjectDiaIDVector::iterator        IT_PopulationKnowledgeObjectDiaIDVector;
-typedef T_PopulationKnowledgeObjectDiaIDVector::const_iterator CIT_PopulationKnowledgeObjectDiaIDVector;
 //@}
 
 //! @name DIA Queries
