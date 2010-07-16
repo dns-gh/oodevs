1ed3e1461bc94e4f9b25ad108aa3208a7dc7a952
Sébastien Brossolasco <sebastien.brossolasco@masagroup.net>
2010-07-16 19:18:51 +0000

    adaptation: removed Q_OBJECT macro causing errors

--- src/applications/adaptation_app/ADN_Composantes_ConsumptionsTable.h
+++ src/applications/adaptation_app/ADN_Composantes_ConsumptionsTable.h
@@ -39,8 +39,6 @@ class ADN_ListView_Composantes;
 // =============================================================================
 class ADN_Composantes_ConsumptionsTable : public ADN_Table2
 {
-    Q_OBJECT;
-
     MT_COPYNOTALLOWED( ADN_Composantes_ConsumptionsTable )
 
 public:
