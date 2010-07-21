6cd9ba895259449d4a0ef3c2c4516400f59db208
Loïc Geffroy <loic.geffroy@masagroup.net>
2010-07-21 13:16:16 +0000

    simulation : cleaning

--- src/libraries/simulation_kernel/Hla/HLA_InteractionManager_ABC.h
+++ src/libraries/simulation_kernel/Hla/HLA_InteractionManager_ABC.h
@@ -6,15 +6,6 @@
 // Copyright (c) 2004 Mathématiques Appliquées SA (MASA)
 //
 // *****************************************************************************
-//
-// $Created: AGE 2004-11-24 $
-// $Archive: /MVW_v10/Build/SDK/MIL/src/Hla/HLA_InteractionManager_ABC.h $
-// $Author: Age $
-// $Modtime: 6/12/04 16:49 $
-// $Revision: 4 $
-// $Workfile: HLA_InteractionManager_ABC.h $
-//
-// *****************************************************************************
 
 #ifndef __HLA_InteractionManager_ABC_h_
 #define __HLA_InteractionManager_ABC_h_
@@ -38,8 +29,8 @@ class HLA_InteractionManager_ABC
 public:
     //! @name Constructors/Destructor
     //@{
-             HLA_InteractionManager_ABC();
-    virtual ~HLA_InteractionManager_ABC();
+             HLA_InteractionManager_ABC() {}
+    virtual ~HLA_InteractionManager_ABC() {}
     //@}
 
     //! @name Operations
--- src/libraries/simulation_kernel/Hla/HLA_Interaction_ABC.h
+++ src/libraries/simulation_kernel/Hla/HLA_Interaction_ABC.h
@@ -6,15 +6,6 @@
 // Copyright (c) 2004 Mathématiques Appliquées SA (MASA)
 //
 // *****************************************************************************
-//
-// $Created: AGE 2004-11-29 $
-// $Archive: /MVW_v10/Build/SDK/MIL/src/Hla/HLA_Interaction_ABC.h $
-// $Author: Age $
-// $Modtime: 29/11/04 16:42 $
-// $Revision: 1 $
-// $Workfile: HLA_Interaction_ABC.h $
-//
-// *****************************************************************************
 
 #ifndef __HLA_Interaction_ABC_h_
 #define __HLA_Interaction_ABC_h_
@@ -30,8 +21,8 @@ class HLA_Interaction_ABC
 public:
     //! @name Constructors/Destructor
     //@{
-             HLA_Interaction_ABC();
-    virtual ~HLA_Interaction_ABC();
+             HLA_Interaction_ABC() {}
+    virtual ~HLA_Interaction_ABC() {}
     //@}
 
     //! @name Operations
