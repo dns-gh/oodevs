b66d8981ad5d7329d5853a6a6cedf74ff792d2d5
Loïc Geffroy <loic.geffroy@masagroup.net>
2010-07-22 07:44:47 +0000

    simulation : warning fix

--- src/libraries/tools/WinArguments.cpp
+++ src/libraries/tools/WinArguments.cpp
@@ -9,7 +9,10 @@
 
 #include "tools_pch.h"
 #include "WinArguments.h"
+#pragma warning( push )
+#pragma warning( disable: 4512 )
 #include <boost/program_options.hpp>
+#pragma warning( pop )
 
 // -----------------------------------------------------------------------------
 // Name: WinArguments constructor
@@ -47,4 +50,3 @@ int WinArguments::Argc() const
 {
     return cArgv_.size();
 }
-
