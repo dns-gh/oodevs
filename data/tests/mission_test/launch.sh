75128a688725d3a3d08df4569ff1561fe28bfbc7
Paul-Henri Calas <paul-henri.calas@masagroup.net>
2010-08-25 09:18:53 +0000

    tests automatiques

--- data/tests/mission_test/launch.sh
+++ data/tests/mission_test/launch.sh
@@ -1,9 +1,18 @@
-nbfile=`find ../../exercises/testings/* -prune -print | wc -l`
-cd "../../../run/vc80"
-for i in `seq 1 $nbfile`
-do
-    echo "Running testings/testings$i ..."
-    echo "Log file : ${1}_$i.log"
-    ../../out/applications/simulation_app/vc80/Release/simulation_app.exe --root-dir=../../data --exercise=testings/testings$i --session=default > ../../data/tests/mission_test/${1}_$i.log
-done
+if (( $2 ))
+then 
+    cd "../../../run/vc80"
+    echo "Running testings/testings$2 ..."
+    echo "Log file : ${1}_$2.log"
+    ../../out/applications/simulation_app/vc80/Release/simulation_app.exe --root-dir=../../data --exercise=testings/testings$2 --session=default > ../../data/tests/mission_test/${1}_$2.log
+else
+    echo "all tests !"
+    nbfile=`find ../../exercises/testings/* -prune -print | wc -l`
+    cd "../../../run/vc80"
+    for i in `seq 1 $nbfile`
+    do
+        echo "Running testings/testings$i ..."
+        echo "Log file : ${1}_$i.log"
+        ../../out/applications/simulation_app/vc80/Release/simulation_app.exe --root-dir=../../data --exercise=testings/testings$i --session=default > ../../data/tests/mission_test/${1}_$i.log
+    done
+fi
 
