57a37c21056d56fd076e0e3d82df9c1c48dbff9a
Helene Barraud <helene.barraud@masagroup.net>
2010-09-22 14:33:38 +0000

    Fixing build

---
+++ src/applications/preparation_app/StringQVButtonGroup.cpp
@@ -0,0 +1,50 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#include "preparation_app_pch.h"
+#include "StringQVButtonGroup.h"
+#include "moc_StringQVButtonGroup.cpp"
+
+// -----------------------------------------------------------------------------
+// Name: StringQVButtonGroup constructor
+// Created: HBD 2010-09-16
+// -----------------------------------------------------------------------------
+StringQVButtonGroup::StringQVButtonGroup( const QString& title, QWidget* parent )
+: QVButtonGroup( title, parent )
+{
+    connect( this, SIGNAL( clicked( int ) ), this, SLOT ( OnClicked( int )) );
+}
+
+// -----------------------------------------------------------------------------
+// Name: StringQVButtonGroup destructor
+// Created: HBD 2010-09-16
+// -----------------------------------------------------------------------------
+StringQVButtonGroup::~StringQVButtonGroup()
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: StringQVButtonGroup::insert
+// Created: HBD 2010-09-16
+// -----------------------------------------------------------------------------
+void StringQVButtonGroup::InsertCheckbox(const std::string& name  )
+{
+    new QCheckBox( name.c_str(), this );
+    values_.push_back( name );
+}
+
+// -----------------------------------------------------------------------------
+// Name: StringQVButtonGroup::OnClicked
+// Created: HBD 2010-09-16
+// -----------------------------------------------------------------------------
+void StringQVButtonGroup::OnClicked( int id )
+{
+    emit clicked( values_[ id ] );
+}
---
+++ src/applications/preparation_app/StringQVButtonGroup.h
@@ -0,0 +1,64 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __StringQVButtonGroup_h_
+#define __StringQVButtonGroup_h_
+
+#include <qvbuttongroup.h>
+
+#include <vector>
+
+// =============================================================================
+/** @class  StringQVButtonGroup
+    @brief  StringQVButtonGroup
+*/
+// Created: HBD 2010-09-16
+// =============================================================================
+class StringQVButtonGroup : public QVButtonGroup
+{
+    Q_OBJECT;
+public:
+
+
+    //! @name Constructors/Destructor
+    //@{
+             StringQVButtonGroup( const QString& title, QWidget* parent );
+    virtual ~StringQVButtonGroup();
+    //@}
+
+    //! @name Operations
+    //@{
+    void InsertCheckbox( const std::string& name );
+   //@}
+
+signals:
+    void clicked( const std::string& );
+
+public slots:
+    void OnClicked( int id );    
+
+private:
+    //! @name Copy/Assignment
+    //@{
+    StringQVButtonGroup( const StringQVButtonGroup& );            //!< Copy constructor
+    StringQVButtonGroup& operator=( const StringQVButtonGroup& ); //!< Assignment operator
+    //@}
+
+    //! @name Helpers
+    //@{
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    std::vector< std::string > values_; 
+    //@}
+};
+
+#endif // __StringQVButtonGroup_h_
