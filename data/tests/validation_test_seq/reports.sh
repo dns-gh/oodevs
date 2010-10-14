3973e58d80e5e6f9c5233e08ec0bec353c9e08a5
Paul-Henri Calas <paul-henri.calas@masagroup.net>
2010-10-14 15:26:02 +0000

    validation_test_seq

--- data/tests/validation_test_seq/reports.sh
+++ data/tests/validation_test_seq/reports.sh
@@ -1,16 +1,22 @@
 target_exercise=$1
 reports_dir=$2
 
-nbfile=`find $target_exercise/* -prune | wc -l`
+mkdir -p $reports_dir
 
-for i in `seq 1 $nbfile`
+for line in `find $target_exercise -name '*Dispatcher.log'`
 do 
-    exercisename=`find $target_exercise/* -prune | sed $i{D} | grep -o "[^/]*$"`
-    dispatcher=`find $target_exercise/$exercisename -name 'Dispatcher.log'`
-    messages=`find $target_exercise/$exercisename -name 'Messages.log'`
-    sim=`find $target_exercise/$exercisename -name 'Sim.log'`
-    mkdir -p $reports_dir
-    cp $dispatcher $reports_dir/${exercisename}_Dispatcher.log
-    cp $messages $reports_dir/${exercisename}_Messages.log
-    cp $sim $reports_dir/${exercisename}_Sim.log
+    cp $line $reports_dir/`echo $line | cut -d "/" -f 5`_dispatcher.log
 done
+
+for line in `find $target_exercise -name '*Sim.log'`
+do 
+    cp $line $reports_dir/`echo $line | cut -d "/" -f 5`_sim.log
+done
+
+for line in `find $target_exercise -name '*Messages.log'`
+do 
+    cp $line $reports_dir/`echo $line | cut -d "/" -f 5`_messages.log
+done
+
+echo "Errors:" `grep -r "Error" $reports_dir | wc -l`
+echo "mission impossible:" `grep -r "mission impossible" $reports_dir | wc -l`
