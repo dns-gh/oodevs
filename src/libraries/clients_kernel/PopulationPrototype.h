9aa7b07e655a13179fe82cda9ea3427f32b36167
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2010-10-26 06:43:38 +0000

    [gaming]Possibilité de créer une population avec un nombre donné. Added missing file.

---
+++ src/libraries/clients_kernel/PopulationPrototype.h
@@ -0,0 +1,33 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2010 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __PopulationPrototype_h_
+#define __PopulationPrototype_h_
+
+namespace kernel
+{
+    class PopulationType;
+
+// =============================================================================
+/** @class  PopulationPrototype
+    @brief  PopulationPrototype
+*/
+// Created: LDC 2010-10-25
+// =============================================================================
+struct PopulationPrototype
+{
+public:
+    PopulationPrototype() {}
+    ~PopulationPrototype() {}
+    const PopulationType* type_;
+    int number_;
+};
+}
+
+#endif // __PopulationPrototype_h_
