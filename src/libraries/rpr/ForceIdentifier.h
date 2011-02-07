3fb0611c34dbd127fd8733fed8248417af9b069a
Silvin Lubecki <silvin.lubecki@masagroup.net>
2011-02-07 11:59:42 +0000

    build-fix

---
+++ src/libraries/rpr/ForceIdentifier.h
@@ -0,0 +1,31 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 MASA Group
+//
+// *****************************************************************************
+
+#ifndef rpr_ForceIdentifier_h
+#define rpr_ForceIdentifier_h
+
+namespace rpr
+{
+// =============================================================================
+/** @class  ForceIdentifier
+    @brief  Force identifier
+*/
+// Created: SLI 2011-02-07
+// =============================================================================
+enum ForceIdentifier
+{
+    Other = 0,
+    Friendly = 1,
+    Opposing = 2,
+    Neutral = 3
+};
+
+}
+
+#endif // rpr_ForceIdentifier_h
