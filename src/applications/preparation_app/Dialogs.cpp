e3390bc4abb6eefd2f8eadef4f25d84110e008f9
Antoine Blossier <antoine.blossier@masagroup.net>
2011-03-04 17:41:33 +0000

    [preparation] revert

--- src/applications/preparation_app/Dialogs.cpp
+++ src/applications/preparation_app/Dialogs.cpp
@@ -10,7 +10,6 @@
 #include "preparation_app_pch.h"
 #include "Dialogs.h"
 #include "ChangeDiplomacyDialog.h"
-#include "InitialStateDialog.h"
 #include "clients_kernel/ObjectTypes.h"
 #include "preparation/StaticModel.h"
 
@@ -20,11 +19,10 @@ using namespace kernel;
 // Name: Dialogs constructor
 // Created: AGE 2006-04-20
 // -----------------------------------------------------------------------------
-Dialogs::Dialogs( QWidget* parent, Controllers& controllers, const StaticModel& model, const kernel::Profile_ABC& profile )
+Dialogs::Dialogs( QWidget* parent, Controllers& controllers, const StaticModel& /*model*/, const kernel::Profile_ABC& profile )
     : QObject( parent )
 {
     new ChangeDiplomacyDialog( parent, controllers, profile );
-    new InitialStateDialog( parent, controllers, model );
 }
 
 // -----------------------------------------------------------------------------
