90bc894d7afb1974fc66e7c924acf80a8bf3754a
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2011-04-07 11:24:19 +0000

    [adaptation]Fix build time per ton must be > 0 when defined. Plus fixed worldwide aircraft carrier.

--- src/applications/adaptation_app/ADN_Composantes_GUI.cpp
+++ src/applications/adaptation_app/ADN_Composantes_GUI.cpp
@@ -139,8 +139,10 @@ void ADN_Composantes_GUI::Build()
     vInfosConnectors[eCanCarryCargo] = &pCargoGroupBox->GetConnector();
 
     builder.AddField<ADN_EditLine_Double>( pCargoGroupBox, tr( "Weight capacity" ), vInfosConnectors[eWeightCapacity], tr( "T" ), eGreaterZero );
-    builder.AddField<ADN_TimeField>( pCargoGroupBox, tr( "Loading time per ton" ), vInfosConnectors[eEmbarkingTimePerTon] );
-    builder.AddField<ADN_TimeField>( pCargoGroupBox, tr( "Unloading time per ton" ), vInfosConnectors[eDisembarkingTimePerTon] );
+    ADN_TimeField* loadTime = builder.AddField<ADN_TimeField>( pCargoGroupBox, tr( "Loading time per ton" ), vInfosConnectors[eEmbarkingTimePerTon] );
+    loadTime->SetMinimumValueInSecond( 1 );
+    ADN_TimeField* unloadTime = builder.AddField<ADN_TimeField>( pCargoGroupBox, tr( "Unloading time per ton" ), vInfosConnectors[eDisembarkingTimePerTon] );
+    unloadTime->SetMinimumValueInSecond( 1 );
 
     // Breakdowns
     pBreakdownsGroup_ = new QGroupBox( 1, Qt::Horizontal, tr( "Breakdowns" ), pDataPage );
