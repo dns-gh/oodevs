112027ddb9a4702954f6b1c70cc2313bdd7d552d
Antony Hubervic <antony.hubervic@masagroup.net>
2011-05-24 09:53:17 +0000

    build fix

--- src/libraries/protocol/AuthenticationSenders.h
+++ src/libraries/protocol/AuthenticationSenders.h
@@ -21,6 +21,8 @@ namespace authentication
     GENERATE_MESSAGE( sword::ClientToAuthentication, sword, ProfileCreationRequest   , profile_creation_request    )
     GENERATE_MESSAGE( sword::ClientToAuthentication, sword, ProfileUpdateRequest     , profile_update_request      )
     GENERATE_MESSAGE( sword::ClientToAuthentication, sword, ProfileDestructionRequest, profile_destruction_request )
+    GENERATE_MESSAGE( sword::ClientToAuthentication, sword, ConnectedProfilesRequest , connected_profiles_request )
+
 
     GENERATE_MESSAGE( sword::AuthenticationToClient, sword, AuthenticationResponse       , authentication_response         )
     GENERATE_MESSAGE( sword::AuthenticationToClient, sword, ProfileCreation              , profile_creation                )
@@ -29,6 +31,7 @@ namespace authentication
     GENERATE_MESSAGE( sword::AuthenticationToClient, sword, ProfileCreationRequestAck    , profile_creation_request_ack    )
     GENERATE_MESSAGE( sword::AuthenticationToClient, sword, ProfileUpdateRequestAck      , profile_update_request_ack      )
     GENERATE_MESSAGE( sword::AuthenticationToClient, sword, ProfileDestructionRequestAck , profile_destruction_request_ack )
+    GENERATE_MESSAGE( sword::AuthenticationToClient, sword, ConnectedProfileList         , connected_profile_list )
 }
 
 #endif // __protocol_AuthenticationSenders_h_
