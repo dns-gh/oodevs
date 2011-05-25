f681963965cd66f9b97f99a5e3c01a70d39f226c
Romain Bosa <romain.bosa@masagroup.net>
2011-05-25 08:11:45 +0000

    [client_gui] fix mantis 0005578: Il faut modifier la taille par défaut de la fenêtre de préférences d'affichage

--- src/libraries/clients_gui/PreferencesDialog.cpp
+++ src/libraries/clients_gui/PreferencesDialog.cpp
@@ -44,7 +44,7 @@ PreferencesDialog::PreferencesDialog( QWidget* parent, Controllers& controllers,
     grid->setColStretch( 0, 0 );
     grid->setColStretch( 1, 3 );
     grid->setRowStretch( 0, 1 );
-    grid->setRowStretch( 1, 5 );
+    grid->setRowStretch( 1, 6 );
     grid->setRowStretch( 2, 1 );
 
     QHBox* box = new QHBox( this );
@@ -119,7 +119,7 @@ void PreferencesDialog::AddPage( const QString& name, PreferencePanel_ABC& page 
 // -----------------------------------------------------------------------------
 QSize PreferencesDialog::sizeHint() const
 {
-    return QSize( 500, 400 );
+    return QSize( 500, 540 );
 }
 
 // -----------------------------------------------------------------------------
