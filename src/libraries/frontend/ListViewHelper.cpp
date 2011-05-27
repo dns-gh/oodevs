a5d8cf27d70f05b9e951a1a0944f703a5d96bb16
Romain Bosa <romain.bosa@masagroup.net>
2011-05-27 14:06:58 +0000

    [selftraining] Simplification de l'export d'otpacks

--- src/libraries/frontend/ListViewHelper.cpp
+++ src/libraries/frontend/ListViewHelper.cpp
@@ -20,27 +20,13 @@ namespace bfs = boost::filesystem;
 
 namespace
 {
-    QListViewItem* InsertValidatedEntry( QListViewItem* parent, const std::string& entry, const std::string& root )
+    void InsertValidatedEntry( QListViewItem* parent, const std::string& entry, const std::string& root )
     {
-        frontend::CheckListItem* item = new frontend::CheckListItem( parent, entry.c_str(), false );
+        frontend::CheckListItem* item = new frontend::CheckListItem( parent, entry.c_str(), true );
         if( ! bfs::exists( bfs::path( root + "/" + entry ) ) )
         {
             item->setEnabled( false );
             item->setText( 0, std::string( entry + " -- Not present." ).c_str() );
-        }
-        parent->insertItem( item );
-        return item;
-    }
-
-    void BuildCategory( QListViewItem* parent, const QStringList& list, const QString& base, const std::string& category )
-    {
-        if( !list.isEmpty() )
-        {
-            QListViewItem* item = new QListViewItem( parent, category.c_str() );
-            item->setOpen( true );
-            for ( QStringList::const_iterator it = list.constBegin(); it != list.constEnd(); ++it )
-                item->insertItem( new frontend::CheckListItem( item,  base + std::string( "/" + category + "/" ).c_str() + *it, false ) );
-            parent->insertItem( item );
         }
     }
 }
@@ -64,19 +50,10 @@ namespace frontend
         QListViewItem* dataItem = new QListViewItem( content, "data" );
         dataItem->setOpen( true );
 
-        QListViewItem* terrainItem = InsertValidatedEntry( dataItem, "data/terrains/" + terrain, config.GetRootDir() );
-        if( terrainItem )
-        {
-            terrainItem->insertItem( new QListViewItem( terrainItem, std::string( "data/terrains/" + terrain + "/Detection" ).c_str() ) );
-            terrainItem->insertItem( new QListViewItem( terrainItem, std::string( "data/terrains/" + terrain + "/Graphics" ).c_str() ) );
-            terrainItem->insertItem( new QListViewItem( terrainItem, std::string( "data/terrains/" + terrain + "/Pathfind" ).c_str() ) );
-        }
-
+        InsertValidatedEntry( dataItem, "data/terrains/" + terrain, config.GetRootDir() );
         if( !population.empty() )
         {
-            QListViewItem* populationItem = InsertValidatedEntry( dataItem, "data/population/" + population, config.GetRootDir() );
-            populationItem->insertItem( new QListViewItem( populationItem, std::string( "data/population/" + population + "/model" ).c_str() ) );
-            populationItem->insertItem( new QListViewItem( populationItem, std::string( "data/population/" + population + "/navteq" ).c_str() ) );
+            InsertValidatedEntry( dataItem, "data/population/" + population, config.GetRootDir() );
         }
         InsertValidatedEntry( dataItem, "data/models/" + dataset + "/physical/" + physical, config.GetRootDir() );
         return dataItem;
@@ -96,27 +73,19 @@ namespace frontend
         exerciseItem->setOpen( true );
         exerciseItem->setOn( true );
         exerciseItem->setEnabled( false );
-        QStringList sessions( frontend::commands::ListSessions( config, exercise ) );
+        
+        const QStringList sessions( frontend::commands::ListSessions( config, exercise ) );
         for ( QStringList::const_iterator it = sessions.constBegin(); it != sessions.constEnd(); ++it )
         {
-            CheckListItem* sessionItem = new CheckListItem( exerciseItem, base + "/sessions/" + *it, false );
-            sessionItem->setOpen( true );
-
-            CheckListItem* recordItem = new CheckListItem( sessionItem, base + "/sessions/" + *it + "/record", true );
-
-            std::string category( QString( "sessions/" + *it + "/checkpoints" ).ascii() );
-            const QStringList checkpoints = frontend::commands::ListCheckpoints( config, exercise, ( *it ).ascii() );
-            BuildCategory( sessionItem, checkpoints, base, category );
+            (void) new CheckListItem( exerciseItem, base + "/sessions/" + *it, true );
         }
+
+        const QStringList others = frontend::commands::ListOtherDirectories( config, exercise );
+        for ( QStringList::const_iterator it = others.constBegin(); it != others.constEnd(); ++it )
         {
-            const QStringList others = frontend::commands::ListOtherDirectories( config, exercise );
-            for ( QStringList::const_iterator it = others.constBegin(); it != others.constEnd(); ++it )
-            {
-                CheckListItem* dirItem = new CheckListItem( rootItem, base + "/" + *it, true );
-                dirItem->setOpen( true );
-                dirItem->setOn( false );
-            }
+            (void) new CheckListItem( rootItem, base + "/" + *it, true );
         }
+
         return rootItem;
     }
 
