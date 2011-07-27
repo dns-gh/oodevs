829bb8f442bf861d9f69e37641cec08ac3dc6814
Loïc Geffroy <loic.geffroy@masagroup.net>
2011-07-27 16:08:50 +0000

    [preparation] histoire "En prépa, on peut modifier manuellement le symbole d'un automate et d'une formation" part II affichage du menu contextuel build fix

---
+++ src/applications/preparation_app/SymbolEditor.cpp
@@ -0,0 +1,148 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 MASA Group
+//
+// *****************************************************************************
+
+#include "preparation_app_pch.h"
+#include "SymbolEditor.h"
+#include "moc_SymbolEditor.cpp"
+#include "clients_gui/EntitySymbols.h"
+#include "clients_kernel/Controllers.h"
+#include "clients_kernel/Controller.h"
+#include "clients_kernel/Automat_ABC.h"
+#include "clients_kernel/Formation_ABC.h"
+#include "clients_kernel/Agent_ABC.h"
+#include "clients_kernel/Diplomacies_ABC.h"
+#include "clients_kernel/Karma.h"
+#include "clients_kernel/SymbolFactory.h"
+#include "clients_kernel/TacticalHierarchies.h"
+#include "clients_kernel/CommunicationHierarchies.h"
+#include "clients_kernel/tools.h"
+#include "tools/Iterator.h"
+#include <boost/foreach.hpp>
+#include <set>
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor constructor
+// Created: LGY 2011-07-21
+// -----------------------------------------------------------------------------
+SymbolEditor::SymbolEditor( QWidget* parent, kernel::Controllers& controllers, const gui::EntitySymbols& symbols,
+                            const tools::ExerciseConfig& config )
+    : QObject( parent )
+    , controllers_( controllers )
+    , symbols_    ( symbols )
+    , pFactory_   ( new kernel::SymbolFactory() )
+    , selected_   ( controllers )
+    , menu_       ( 0 )
+{
+    pFactory_->Load( config );
+    controllers_.Register( *this );
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor destructor
+// Created: LGY 2011-07-21
+// -----------------------------------------------------------------------------
+SymbolEditor::~SymbolEditor()
+{
+    controllers_.Unregister( *this );
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::NotifyContextMenu
+// Created: LGY 2011-07-21
+// -----------------------------------------------------------------------------
+void SymbolEditor::NotifyContextMenu( const kernel::Formation_ABC& entity, kernel::ContextMenu& menu )
+{
+    Update( entity, menu );
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::NotifyContextMenu
+// Created: LGY 2011-07-21
+// -----------------------------------------------------------------------------
+void SymbolEditor::NotifyContextMenu( const kernel::Automat_ABC& entity, kernel::ContextMenu& menu )
+{
+    Update( entity, menu );
+}
+
+namespace
+{
+    std::string Convert( const kernel::Karma& karma )
+    {
+        switch( karma.GetUId() )
+        {
+        case 1:
+            return "f";
+        case 2:
+            return "h";
+        case 3:
+            return "n";
+        default:
+            return "u";
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::IsValid
+// Created: LGY 2011-07-26
+// -----------------------------------------------------------------------------
+bool SymbolEditor::IsValid( const T_Symbols& symbols ) const
+{
+    BOOST_FOREACH( const T_Symbols::value_type& symbol, symbols )
+        if( symbol.second.isNull() )
+            return false;
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::Update
+// Created: LGY 2011-07-26
+// -----------------------------------------------------------------------------
+void SymbolEditor::Update()
+{
+    if( menu_ )
+        Update( *selected_, menu_ );
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::Update
+// Created: LGY 2011-07-21
+// -----------------------------------------------------------------------------
+void SymbolEditor::Update( const kernel::Entity_ABC& entity, kernel::ContextMenu& menu )
+{
+    menu_ = menu.SubMenu( "Symbols", tr( "Symbol" ) );
+    selected_ = const_cast< kernel::Entity_ABC* >( &entity );
+    Update();
+}
+
+// -----------------------------------------------------------------------------
+// Name: SymbolEditor::Update
+// Created: LGY 2011-07-27
+// -----------------------------------------------------------------------------
+void SymbolEditor::Update( const kernel::Entity_ABC& entity, QPopupMenu* menu )
+{
+    const kernel::TacticalHierarchies& pHierarchy = entity.Get< kernel::TacticalHierarchies >();
+    const std::string karma = Convert( pHierarchy.GetTop().Get< kernel::Diplomacies_ABC >().GetKarma() );
+    std::set< std::string > symbols;
+    tools::Iterator< const kernel::Entity_ABC& > it = pHierarchy.CreateSubordinateIterator();
+    while( it.HasMoreElements() )
+        pFactory_->FillSymbols( it.NextElement().Get< kernel::TacticalHierarchies >().GetSymbol(), karma, symbols );
+    T_Symbols pixmaps;
+    BOOST_FOREACH( const std::string& symbol, symbols )
+        pixmaps[ symbol ] = symbols_.GetSymbol( entity, symbol, pHierarchy.GetLevel(), QSize( 32, 32 ) );
+    if( ! IsValid( pixmaps ) )
+        QTimer::singleShot( 100, this, SLOT( Update() ) );
+    else
+        BOOST_FOREACH( const T_Symbols::value_type& pixmap, pixmaps )
+        {
+            int id = menu->insertItem( pixmap.second );
+            if( pHierarchy.GetSymbol() == pixmap.first )
+                menu->setItemChecked( id, true );
+        }
+}
---
+++ src/applications/preparation_app/SymbolEditor.h
@@ -0,0 +1,96 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 MASA Group
+//
+// *****************************************************************************
+
+#ifndef __SymbolEditor_h_
+#define __SymbolEditor_h_
+
+#include "clients_kernel/ContextMenuObserver_ABC.h"
+#include "clients_kernel/SafePointer.h"
+#include "tools/SelectionObserver_ABC.h"
+
+namespace kernel
+{
+    class Entity_ABC;
+    class Formation_ABC;
+    class Agent_ABC;
+    class Automat_ABC;
+    class Controllers;
+    class SymbolFactory;
+}
+
+namespace gui
+{
+    class EntitySymbols;
+}
+
+namespace tools
+{
+    class ExerciseConfig;
+}
+
+// =============================================================================
+/** @class  SymbolEditor
+    @brief  Symbol editor
+*/
+// Created: LGY 2011-07-21
+// =============================================================================
+class SymbolEditor: public QObject
+                  , public tools::Observer_ABC
+                  , public kernel::ContextMenuObserver_ABC< kernel::Formation_ABC >
+                  , public kernel::ContextMenuObserver_ABC< kernel::Automat_ABC >
+                  , private boost::noncopyable
+{
+    Q_OBJECT;
+
+public:
+    //! @name Constructors/Destructor
+    //@{
+             SymbolEditor( QWidget* parent, kernel::Controllers& controllers, const gui::EntitySymbols& symbols,
+                           const tools::ExerciseConfig& config );
+    virtual ~SymbolEditor();
+    //@}
+
+    //! @name Operations
+    //@{
+    virtual void NotifyContextMenu( const kernel::Formation_ABC& formation, kernel::ContextMenu& menu );
+    virtual void NotifyContextMenu( const kernel::Automat_ABC& automat, kernel::ContextMenu& menu );
+    //@}
+
+private slots:
+    //! @name Slots
+    //@{
+    void Update();
+    //@}
+
+private:
+    //! @name Types
+    //@{
+    typedef std::map< std::string, QPixmap > T_Symbols;
+    //@}
+
+private:
+    //! @name Helpers
+    //@{
+    void Update( const kernel::Entity_ABC& entity, QPopupMenu* menu );
+    void Update( const kernel::Entity_ABC& entity, kernel::ContextMenu& menu );
+    bool IsValid( const T_Symbols& symbols ) const;
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    kernel::Controllers& controllers_;
+    const gui::EntitySymbols& symbols_;
+    std::auto_ptr< kernel::SymbolFactory > pFactory_;
+    kernel::SafePointer< kernel::Entity_ABC > selected_;
+    QPopupMenu* menu_;
+    //@}
+};
+
+#endif // __SymbolEditor_h_
