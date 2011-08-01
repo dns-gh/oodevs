4d89a47887dca04793f235a7e7b02ffdc72d199f
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2011-08-01 16:50:53 +0000

    [selftraining]Fix mantis 5970: En multijoueur, les paramètres de sauvegarde auto ne sont pas pris en comtpe

--- src/applications/selftraining_app/BattleCenterLauncherPage.cpp
+++ src/applications/selftraining_app/BattleCenterLauncherPage.cpp
@@ -19,6 +19,7 @@
 #include "frontend/Exercise_ABC.h"
 #include "frontend/JoinExercise.h"
 #include "frontend/ProcessWrapper.h"
+#include "frontend/PluginConfig_ABC.h"
 
 // -----------------------------------------------------------------------------
 // Name: BattleCenterLauncherPage constructor
@@ -57,11 +58,16 @@ void BattleCenterLauncherPage::OnStart()
     if( !CanBeStarted() || ! dialogs::KillRunningProcesses( this ) )
         return;
     exercise_->Start( MULTIPLAYER_SESSION );
+    const std::string exerciseName = exercise_->GetName();
     {
-        frontend::CreateSession action( config_, exercise_->GetName(), MULTIPLAYER_SESSION );
+        frontend::CreateSession action( config_, exerciseName, MULTIPLAYER_SESSION );
         action.SetDefaultValues();
         action.SetOption( "session/config/gaming/network/@server", QString( "%1:%2" ).arg( "127.0.0.1" ).arg( exercise_->GetPort() ) );
         action.Commit();
+    }
+    {
+        for( T_Plugins::const_iterator it = plugins_.begin(); it != plugins_.end(); ++it )
+            (*it)->Commit( exerciseName, MULTIPLAYER_SESSION );
     }
     boost::shared_ptr< frontend::SpawnCommand > command( new frontend::JoinExercise( config_, exercise_->GetName().c_str(), MULTIPLAYER_SESSION.c_str(), profile_.GetLogin(), true ) );
     boost::shared_ptr< frontend::ProcessWrapper > process( new frontend::ProcessWrapper( *progressPage_, command ) );
