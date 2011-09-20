aa6835edf88d2ec3aef6ebd081e15535162d096b
Jérome Martinet <jerome.martinet@masagroup.net>
2011-09-20 08:25:21 +0000

    [msdl] Library for preparation to import / export MSDL files

---
+++ src/libraries/msdl/msdl_pch.cpp
@@ -0,0 +1,10 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#include "msdl_pch.h"
---
+++ src/libraries/msdl/msdl_pch.h
@@ -0,0 +1,18 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef __msdl_pch_h_
+#define __msdl_pch_h_
+
+#include <string>
+#include <memory>
+#include <map>
+#include <vector>
+
+#endif // __msdl_pch_h_
