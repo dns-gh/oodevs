043afc21bca6ec10e795385455d4f3dda832444f
Mathieu Champlon <mathieu.champlon@masagroup.net>
2011-09-30 10:40:07 +0000

    launcher_test : ajout de SEH et d'un MiniDump pour déboguer le "vector< T > too long"

--- src/tests/launcher_test/ServiceTest.cpp
+++ src/tests/launcher_test/ServiceTest.cpp
@@ -22,14 +22,74 @@
 #pragma warning( push, 0 )
 #include <QtGui/qapplication.h>
 #pragma warning( pop )
+#include <windows.h>
+#include <dbghelp.h>
+#include <shellapi.h>
+#include <shlobj.h>
+#include <strsafe.h>
+
+#pragma comment( lib, "Dbghelp.lib" )
 
 using namespace launcher_test;
+
+int GenerateDump(EXCEPTION_POINTERS* pExceptionPointers)
+{
+    BOOL bMiniDumpSuccessful;
+    CHAR szFileName[MAX_PATH];
+    HANDLE hDumpFile;
+    SYSTEMTIME stLocalTime;
+    MINIDUMP_EXCEPTION_INFORMATION ExpParam;
+
+    GetLocalTime( &stLocalTime );
+
+    StringCchPrintf( szFileName, MAX_PATH, "%04d%02d%02d-%02d%02d%02d-%ld-%ld.dmp", 
+               stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay, 
+               stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond, 
+               GetCurrentProcessId(), GetCurrentThreadId());
+    hDumpFile = CreateFile(szFileName, GENERIC_READ|GENERIC_WRITE, 
+                FILE_SHARE_WRITE|FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+
+    ExpParam.ThreadId = GetCurrentThreadId();
+    ExpParam.ExceptionPointers = pExceptionPointers;
+    ExpParam.ClientPointers = TRUE;
+
+    bMiniDumpSuccessful = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), 
+                    hDumpFile, MiniDumpWithDataSegs, &ExpParam, NULL, NULL);
+
+    std::cout << "dumped " << szFileName << std::endl;
+
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+#define WATCHED_FIXTURE_TEST_CASE( test_name, F )                         \
+struct test_name : public F { void test_method(); \
+    void test_method_hook() { \
+        __try { test_method(); } \
+        __except( GenerateDump(GetExceptionInformation()) ) {} \
+    } \
+};                    \
+                                                                        \
+static void BOOST_AUTO_TC_INVOKER( test_name )()                        \
+{                                                                       \
+    test_name t;                                                        \
+    t.test_method_hook(); \
+}                                                                       \
+                                                                        \
+struct BOOST_AUTO_TC_UNIQUE_ID( test_name ) {};                         \
+                                                                        \
+BOOST_AUTO_TU_REGISTRAR( test_name )(                                   \
+    boost::unit_test::make_test_case(                                   \
+        &BOOST_AUTO_TC_INVOKER( test_name ), #test_name ),              \
+    boost::unit_test::ut_detail::auto_tc_exp_fail<                      \
+        BOOST_AUTO_TC_UNIQUE_ID( test_name )>::instance()->value() );   \
+                                                                        \
+void test_name::test_method()                                           \
 
 // -----------------------------------------------------------------------------
 // Name: ClientCanConnectToServer
 // Created: SBO 2010-09-29
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanConnectToServer, Fixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanConnectToServer, Fixture )
 {
     BOOST_CHECK( client.Connected() );
 }
@@ -38,7 +98,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanConnectToServer, Fixture )
 // Name: ClientCanListAvailableExercises
 // Created: SBO 2010-09-29
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanListAvailableExercises, Fixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanListAvailableExercises, Fixture )
 {
     BOOST_REQUIRE( client.Connected() );
     ExerciseListener listener( controllers ); // $$$$ MCO : use a mock
@@ -53,7 +113,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanListAvailableExercises, Fixture )
 // Name: ClientCanStartExercise
 // Created: SBO 2010-11-22
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanStartExercise, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanStartExercise, ExerciseFixture )
 {
     sword::ProfileListResponse launcherResponse;
     MOCK_EXPECT( handler, HandleProfileListResponse ).once().with( mock::retrieve( launcherResponse ) );
@@ -65,7 +125,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanStartExercise, ExerciseFixture )
 // Name: ClientCanPauseExercise
 // Created: AHC 2011-05-19
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanPauseExercise, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanPauseExercise, ExerciseFixture )
 {
     // send pause request
     exercise->Pause( session );
@@ -87,18 +147,18 @@ BOOST_FIXTURE_TEST_CASE( ClientCanPauseExercise, ExerciseFixture )
 // Name: ClientCanResumeExercise
 // Created: AHC 2011-05-20
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanResumeExercise, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanResumeExercise, ExerciseFixture )
 {
+    sword::SessionCommandExecutionResponse launcherResponse;
+    client::ControlResumeAck dispatcherResponse;
     // send resume request
     exercise->Resume( session );
     VerifySendRequest( "context: 2 message { control_resume { } }" );
 
     // retrieve resume response
-    client::ControlResumeAck dispatcherResponse;
     dispatcherResponse().set_error_code( sword::ControlAck::error_not_paused );
     dispatcherResponse.Send( dispatcher, 2 );
 
-    sword::SessionCommandExecutionResponse launcherResponse;
     MOCK_EXPECT( handler, HandleSessionCommandExecutionResponse ).once().with( mock::retrieve( launcherResponse ) );
 
     Wait( launcherResponse );
@@ -109,7 +169,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanResumeExercise, ExerciseFixture )
 // Name: ClientCanChangeDateTime
 // Created: LGY 2011-06-22
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanChangeDateTime, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanChangeDateTime, ExerciseFixture )
 {
     // send resume request
     exercise->ChangeDateTime( session, "dateISO860" );
@@ -131,7 +191,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanChangeDateTime, ExerciseFixture )
 // Name: ClientCanSaveCheckPoint
 // Created: AHC 2011-05-20
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanSaveCheckPoint, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanSaveCheckPoint, ExerciseFixture )
 {
     // send checkpoint request
     exercise->SaveCheckpoint( session, "checkpoint" );
@@ -153,7 +213,7 @@ BOOST_FIXTURE_TEST_CASE( ClientCanSaveCheckPoint, ExerciseFixture )
 // Name: NotifyUnitExtension
 // Created: LGY 2011-05-23
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( NotifyUnitExtension, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( NotifyUnitExtension, ExerciseFixture )
 {
     client::UnitAttributes attributes;
     attributes().mutable_unit()->set_id( 42 );
@@ -173,7 +233,7 @@ BOOST_FIXTURE_TEST_CASE( NotifyUnitExtension, ExerciseFixture )
 // Name: NotifyFormationExtension
 // Created: LGY 2011-05-23
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( NotifyFormationExtension, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( NotifyFormationExtension, ExerciseFixture )
 {
     client::FormationUpdate attributes;
     attributes().mutable_formation()->set_id( 42 );
@@ -193,7 +253,7 @@ BOOST_FIXTURE_TEST_CASE( NotifyFormationExtension, ExerciseFixture )
 // Name: NotifyProfileCreation
 // Created: LGY 2011-05-23
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( NotifyProfileCreation, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( NotifyProfileCreation, ExerciseFixture )
 {
     authentication::ProfileCreation creation;
     creation().mutable_profile()->set_login( "login" );
@@ -212,7 +272,7 @@ BOOST_FIXTURE_TEST_CASE( NotifyProfileCreation, ExerciseFixture )
 // Name: NotifyProfileUpdate
 // Created: LGY 2011-05-23
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( NotifyProfileUpdate, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( NotifyProfileUpdate, ExerciseFixture )
 {
     authentication::ProfileUpdate update;
     update().set_login( "login" );
@@ -232,7 +292,7 @@ BOOST_FIXTURE_TEST_CASE( NotifyProfileUpdate, ExerciseFixture )
 // Name: NotifyControlInformation
 // Created: LGY 2011-05-23
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( NotifyControlInformation, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( NotifyControlInformation, ExerciseFixture )
 {
     client::ControlInformation information;
     information().set_current_tick( 42 );
@@ -262,7 +322,7 @@ BOOST_FIXTURE_TEST_CASE( NotifyControlInformation, ExerciseFixture )
 // Name: ClientCanListConnectedProfiles
 // Created: AHC 2011-05-20
 // -----------------------------------------------------------------------------
-BOOST_FIXTURE_TEST_CASE( ClientCanListConnectedProfiles, ExerciseFixture )
+WATCHED_FIXTURE_TEST_CASE( ClientCanListConnectedProfiles, ExerciseFixture )
 {
     // send checkpoint request
     exercise->QueryConnectedProfileList( session );
