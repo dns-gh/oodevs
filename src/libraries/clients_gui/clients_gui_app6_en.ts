d734aa810423a8cdda2734369d3cee0be157ad15
Antoine Blossier <antoine.blossier@masagroup.net>
2011-10-03 11:54:11 +0000

    [preparation] fix mantis 5691: Nom des formations en anglais

---
+++ src/libraries/clients_gui/clients_gui_app6_en.ts
@@ -0,0 +1,54 @@
+<!DOCTYPE TS>
+<TS>
+    <context>
+    <name>models::app6</name>
+    <message>
+        <source>o</source>
+        <translation>o Squad</translation>
+    </message>
+    <message>
+        <source>b</source>
+        <translation>b Pair</translation>
+    </message>
+    <message>
+        <source>c</source>
+        <translation>c Detachment</translation>
+    </message>
+    <message>
+        <source>oo</source>
+        <translation>oo Section</translation>
+    </message>
+    <message>
+        <source>ooo</source>
+        <translation>ooo Platoon</translation>
+    </message>
+    <message>
+        <source>i</source>
+        <translation>i Company</translation>
+    </message>
+    <message>
+        <source>ii</source>
+        <translation>ii Battalion</translation>
+    </message>
+    <message>
+        <source>iii</source>
+        <translation>iii Regiment</translation>
+    </message>
+    <message>
+        <source>x</source>
+        <translation>x Brigade</translation>
+    </message>
+    <message>
+        <source>xx</source>
+        <translation>xx Division</translation>
+    </message>
+    <message>
+        <source>xxx</source>
+        <translation>xxx Corps</translation>
+    </message>
+    <message>
+        <source>xxxx</source>
+        <translation>xxxx Army</translation>
+    </message>
+</context>
+</TS>
