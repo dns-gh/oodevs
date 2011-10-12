f8eb5b21024c0d9fa18daf1986272678e5829b05
Silvin Lubecki <silvin.lubecki@masagroup.net>
2011-10-12 10:20:09 +0000

    [hla_plugin]: Ajout de la réception de l'interaction NetnOfferConvoy

---
+++ src/libraries/hla_plugin/NetnOfferConvoyReceiver.cpp
@@ -0,0 +1,57 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2011 MASA Group
+//
+// *****************************************************************************
+
+#include "hla_plugin_pch.h"
+#include "NetnOfferConvoyReceiver.h"
+#include "TransportationController_ABC.h"
+#include "Interactions.h"
+
+using namespace plugins::hla;
+
+// -----------------------------------------------------------------------------
+// Name: NetnOfferConvoyReceiver constructor
+// Created: SLI 2011-10-12
+// -----------------------------------------------------------------------------
+NetnOfferConvoyReceiver::NetnOfferConvoyReceiver( TransportationController_ABC& controller )
+    : controller_( controller )
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: NetnOfferConvoyReceiver destructor
+// Created: SLI 2011-10-12
+// -----------------------------------------------------------------------------
+NetnOfferConvoyReceiver::~NetnOfferConvoyReceiver()
+{
+    // NOTHING
+}
+
+namespace
+{
+    bool CheckOffer( const interactions::NetnOfferConvoy& offer )
+    {
+        return offer.isOffering && offer.offerType == 1;
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: NetnOfferConvoyReceiver::Receive
+// Created: SLI 2011-10-12
+// -----------------------------------------------------------------------------
+void NetnOfferConvoyReceiver::Receive( interactions::NetnOfferConvoy& interaction )
+{
+    if( interaction.serviceType != 4 )
+        return;
+    if( interaction.transportData.convoyType != 0 )
+        return;
+    if( interaction.serviceId.issuingObjectIdentifier.str() != "SWORD" )
+        return;
+    controller_.OfferReceived( interaction.serviceId.eventCount, CheckOffer( interaction ), interaction.provider.str() );
+}
