d3b478895f7bb95171039da757a468986e1b6a29
Robert Piechaud <robert.piechaud@masagroup.net>
2011-10-17 14:37:31 +0000

    [install] merged 4.3.2 svn rev 23102 (fixed mantis 6315)

--- build/nsis/Scipio-Sword_post_install.bat
+++ build/nsis/Scipio-Sword_post_install.bat
@@ -2,6 +2,8 @@
 
 regedit /S "%~dp0\Scipio-Sword_post_install.reg"
 
+if not exist "d:\MASA\SWORDROOT" mkdir "d:\MASA\SWORDROOT"
+
 if exist "%~dp0\vcredist_vc100.exe" goto x86
 if exist "%~dp0\vcredist_vc100_x64.exe" goto x64
 goto end
