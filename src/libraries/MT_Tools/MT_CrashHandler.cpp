cb70352c43423d100168af475a76334e284af834
Mathieu Champlon <mathieu.champlon@masagroup.net>
2011-10-22 15:00:28 +0000

    simulation_app : correction de #6448 : ajout d'un minidump lors d'un plantage

--- src/libraries/MT_Tools/MT_CrashHandler.cpp
+++ src/libraries/MT_Tools/MT_CrashHandler.cpp
@@ -14,12 +14,109 @@
 #include <dbghelp.h>
 #include <shellapi.h>
 #include <shlobj.h>
+#include <tchar.h>
 #include <strsafe.h>
+#include <psapi.h>
 
 #pragma comment( lib, "Dbghelp.lib" )
 
 namespace
 {
+    bool Log( HANDLE hFile, BOOL bMiniDumpSuccessful )
+    {
+        bool bSuccess = false;
+        TCHAR pszFilename[MAX_PATH+1];
+        HANDLE hFileMap;
+
+        // Get the file size.
+        DWORD dwFileSizeHi = 0;
+        DWORD dwFileSizeLo = GetFileSize(hFile, &dwFileSizeHi); 
+
+        if( dwFileSizeLo == 0 && dwFileSizeHi == 0 )
+            return false;
+
+        // Create a file mapping object.
+        hFileMap = CreateFileMapping(hFile, 
+            NULL, 
+            PAGE_READONLY,
+            0, 
+            1,
+            NULL);
+
+        if (hFileMap) 
+        {
+            // Create a file mapping to get the file name.
+            void* pMem = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 1);
+
+            if (pMem) 
+            {
+                if (GetMappedFileName (GetCurrentProcess(), 
+                    pMem, 
+                    pszFilename,
+                    MAX_PATH)) 
+                {
+
+                    // Translate path with device name to drive letters.
+                    TCHAR szTemp[MAX_PATH];
+                    szTemp[0] = '\0';
+
+                    if (GetLogicalDriveStrings(MAX_PATH-1, szTemp)) 
+                    {
+                        TCHAR szName[MAX_PATH];
+                        TCHAR szDrive[3] = TEXT(" :");
+                        BOOL bFound = FALSE;
+                        TCHAR* p = szTemp;
+
+                        do 
+                        {
+                            // Copy the drive letter to the template string
+                            *szDrive = *p;
+
+                            // Look up each device name
+                            if (QueryDosDevice(szDrive, szName, MAX_PATH))
+                            {
+                                size_t uNameLen = _tcslen(szName);
+
+                                if (uNameLen < MAX_PATH)
+                                {
+                                    bFound = _tcsnicmp(pszFilename, szName, uNameLen) == 0
+                                        && *(pszFilename + uNameLen) == _T('\\');
+
+                                    if (bFound)
+                                    {
+                                        // Reconstruct pszFilename using szTempFile
+                                        // Replace device path with DOS path
+                                        TCHAR szTempFile[MAX_PATH];
+                                        StringCchPrintf(szTempFile,
+                                            MAX_PATH,
+                                            TEXT("%s%s"),
+                                            szDrive,
+                                            pszFilename+uNameLen);
+                                        StringCchCopyN(pszFilename, MAX_PATH+1, szTempFile, _tcslen(szTempFile));
+                                    }
+                                }
+                            }
+
+                            // Go to the next NULL character.
+                            while (*p++);
+                        } while (!bFound && *p); // end of string
+                    }
+                }
+                bSuccess = true;
+                UnmapViewOfFile(pMem);
+            } 
+
+            CloseHandle(hFileMap);
+        }
+        if( ! bSuccess )
+            return bSuccess;
+        if( bMiniDumpSuccessful )
+            MT_LOG_ERROR_MSG( "Crash - dumped " << pszFilename );
+        if( ! bMiniDumpSuccessful )
+            MT_LOG_ERROR_MSG( "Crash - unable to create dump file " << pszFilename );
+        return(bSuccess);
+    }
+
     void Dump( EXCEPTION_POINTERS* pExceptionPointers )
     {
         BOOL bMiniDumpSuccessful;
@@ -43,18 +140,19 @@ namespace
             return;
         }
 
-        GetFinalPathNameByHandle( hDumpFile, szFileName, MAX_PATH, 0 );
-
         ExpParam.ThreadId = GetCurrentThreadId();
         ExpParam.ExceptionPointers = pExceptionPointers;
         ExpParam.ClientPointers = TRUE;
 
         bMiniDumpSuccessful = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), 
                         hDumpFile, MiniDumpWithDataSegs, &ExpParam, NULL, NULL);
-        if( bMiniDumpSuccessful )
-            MT_LOG_ERROR_MSG( "Crash - dumped " << szFileName );
-        if( ! bMiniDumpSuccessful )
-            MT_LOG_ERROR_MSG( "Crash - unable to create dump file " << szFileName );
+        if( ! Log( hDumpFile, bMiniDumpSuccessful ) )
+        {
+            if( bMiniDumpSuccessful )
+                MT_LOG_ERROR_MSG( "Crash - dumped " << szFileName );
+            if( ! bMiniDumpSuccessful )
+                MT_LOG_ERROR_MSG( "Crash - unable to create dump file " << szFileName );
+        }
     }
 }
 
