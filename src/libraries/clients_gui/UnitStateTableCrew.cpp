8c3ba9e05d5579dc9f14f7475999bdfd29f9e6c0
Antoine Blossier <antoine.blossier@masagroup.net>
2011-10-26 12:15:24 +0000

    [clients] Fix crash on CommonDelegate and bug on UnitStateTableCrew

--- src/libraries/clients_gui/UnitStateTableCrew.cpp
+++ src/libraries/clients_gui/UnitStateTableCrew.cpp
@@ -74,7 +74,7 @@ void UnitStateTableCrew::OnClearItems()
 }
 
 // -----------------------------------------------------------------------------
-// Name: UnitStateTableCrew::OnRemoveItem
+// Name: UnitStateTableCrew::OnRemoveCurrentItem
 // Created: ABR 2011-02-28
 // -----------------------------------------------------------------------------
 void UnitStateTableCrew::OnRemoveCurrentItem()
@@ -104,7 +104,7 @@ void UnitStateTableCrew::contextMenuEvent( QContextMenuEvent* e )
         if( index.isValid() && index.row() >= eNbrHumanRank && index.row() <= dataModel_.rowCount() )
         {
             setCurrentIndex( index );
-            popupMenu.insertItem( tr( "Delete" ), this, SLOT( OnRemoveItem() ) );
+            popupMenu.insertItem( tr( "Delete" ), this, SLOT( OnRemoveCurrentItem() ) );
         }
         popupMenu.insertItem( tr( "Clear" ), this, SLOT( OnClearItems() ) );
     }
