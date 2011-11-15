5072c212521fca73b8df8f307ed5e3c746824800
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2011-11-15 14:01:19 +0000

    [adaptation]Organisations can define stocks

--- src/applications/adaptation_app/ADN_Units_GUI.cpp
+++ src/applications/adaptation_app/ADN_Units_GUI.cpp
@@ -275,7 +275,8 @@ void ADN_Units_GUI::OnTypeChanged()
     if( strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionLOGBLDRavitaillement, ENT_Tr_ABC::eToTr ).c_str()
         || strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionLOGBLTRavitaillement, ENT_Tr_ABC::eToTr ).c_str()
         || strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionLOGConvoi, ENT_Tr_ABC::eToTr ).c_str()
-        || strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionLOGTC2, ENT_Tr_ABC::eToTr ).c_str() )
+        || strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionLOGTC2, ENT_Tr_ABC::eToTr ).c_str()
+        || strType == ADN_Tr::ConvertFromAgentTypePion( eAgentTypePionOrganization, ENT_Tr_ABC::eToTr ).c_str() )
     {
         // insert stock category list if not exist yet
         ADN_Units_Data::UnitInfos* pInfos = (ADN_Units_Data::UnitInfos*)pListUnits_->GetCurrentData();
