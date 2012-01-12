af169374cff9e918a6b5b974bf5f93a1006deb87
Robert Piechaud <robert.piechaud@masagroup.net>
2012-01-12 13:04:54 +0000

    [gaming] fixed missing translation.

--- src/applications/gaming_app/gaming_app_fr.ts
+++ src/applications/gaming_app/gaming_app_fr.ts
@@ -3199,7 +3199,7 @@ Etes-vous sûr de vouloir remplacer le modèle ?</translation>
     </message>
     <message>
         <source>Finish logistic handlings</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminer les consignes logistiques</translation>
     </message>
 </context>
 <context>
