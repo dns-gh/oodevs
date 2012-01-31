145b79e7b73d33a6bb2757a25f2ea615b2e3d9a0
Joël Soicher <joel.soicher@masagroup.net>
2012-01-31 15:09:17 +0000

    Merged revision(s) 26308 from branches/4.3.3/src: [traductions] fix SWBUG-6955 Scipio Textes mal traduits

--- src/applications/preparation_app/preparation_app_fr.ts
+++ src/applications/preparation_app/preparation_app_fr.ts
@@ -219,7 +219,7 @@
     </message>
     <message>
         <source>Communication</source>
-        <translation>Communication</translation>
+        <translation>Groupes de connaissance</translation>
     </message>
     <message>
         <source>Logistic</source>
@@ -456,7 +456,7 @@
     </message>
     <message>
         <source>Export orbat, resources, stocks, weather schedule, diplomacy matrix, profiles and logistic links as CSV files.</source>
-        <translation>Exporter l&apos;orbat, les dotations, les stocks, la météo, la diplomatie, les profils, la liste de diffusion ainsi que les liens logistiques en fichier CSV.</translation>
+        <translation>Exporter l&apos;ODB, les dotations, les stocks, la météo, la diplomatie, les profils, la liste de diffusion ainsi que les liens logistiques en fichier CSV.</translation>
     </message>
     <message>
         <source>Select output directory</source>
@@ -535,7 +535,7 @@
     <name>FilterOrbatReIndexer</name>
     <message>
         <source>Load orbat file</source>
-        <translation>Charger le fichier orbat</translation>
+        <translation>Charger le fichier ODB</translation>
     </message>
     <message>
         <source>Error loading file</source>
@@ -547,7 +547,7 @@
     </message>
     <message>
         <source>Enter path to the new orbat here</source>
-        <translation>Entrez le chemin du nouvel orbat ici</translation>
+        <translation>Entrez le chemin du nouvel ODB ici</translation>
     </message>
     <message>
         <source>Browse...</source>
