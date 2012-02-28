624e85f2736f1e59fdfb491c4b1204badbe207b1
Loïc Geffroy <loic.geffroy@masagroup.net>
2012-02-28 09:11:11 +0000

    [migrations] build fix => passage a la version 5.0.0

--- src/libraries/tools/Version.cpp
+++ src/libraries/tools/Version.cpp
@@ -12,7 +12,7 @@
 #include <boost/preprocessor/stringize.hpp>
 
 #ifndef APP_MAJOR_VERSION
-#   define APP_MAJOR_VERSION 4.3.4
+#   define APP_MAJOR_VERSION 5.0.0
 #endif
 
 #ifndef APP_PROJECT_VERSION
