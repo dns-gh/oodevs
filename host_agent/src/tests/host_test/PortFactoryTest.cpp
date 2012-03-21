b7877d645f196a7fbacedb65bdaa4cdb6cc52dae
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-03-21 09:31:25 +0000

    Simplify tests

--- host_agent/src/tests/host_test/PortFactoryTest.cpp
+++ host_agent/src/tests/host_test/PortFactoryTest.cpp
@@ -26,13 +26,21 @@ namespace
         PortFactory factory;
     };
 
-    template< typename T >
-    void RemoveAddBackAndCheck( PortFactory& factory, T& data, size_t idx )
+    typedef std::vector< std::auto_ptr< Port_ABC > > T_Ports;
+
+    T_Ports MakePorts( PortFactory& factory, size_t size )
     {
-        int unused = data[ idx ]->Get();
-        typename T::iterator it = data.begin();
-        std::advance( it, idx );
-        data.erase( it );
+        T_Ports reply;
+        for( size_t i = 0; i < size; ++i )
+            reply.push_back( factory.Create() );
+        return reply;
+    }
+
+    template< typename T >
+    void RemoveAddCheck( PortFactory& factory, T& data, size_t idx )
+    {
+        int unused = data[idx]->Get();
+        data.erase( data.begin() + idx );
         data.push_back( factory.Create() );
         BOOST_CHECK_EQUAL( unused, data.back()->Get() );
     }
@@ -40,33 +48,25 @@ namespace
 
 BOOST_FIXTURE_TEST_CASE( port_factory_throws_when_filled, Fixture )
 {
-    std::vector< std::auto_ptr< Port_ABC > > data;
-    for( int i = 0; i < 10; ++i )
-        data.push_back( factory.Create() );
+    T_Ports data = MakePorts( factory, 10 );
     BOOST_CHECK_THROW( factory.Create(), std::exception );
 }
 
-BOOST_FIXTURE_TEST_CASE( port_factory_returns_unused_port, Fixture )
+BOOST_FIXTURE_TEST_CASE( factory_can_return_first_unused_port, Fixture )
 {
-    std::vector< std::auto_ptr< Port_ABC > > data;
-    for( int i = 0; i < 10; ++i )
-        data.push_back( factory.Create() );
-    // edge-cases
-    RemoveAddBackAndCheck( factory, data, data.size() - 1 );
-    RemoveAddBackAndCheck( factory, data, 0 );
-    // full rotation
-    for( int i = 0; i < 10; ++i )
-        RemoveAddBackAndCheck( factory, data, i );
+    T_Ports data = MakePorts( factory, 10 );
+    RemoveAddCheck( factory, data, 0 );
 }
 
-BOOST_FIXTURE_TEST_CASE( port_factory_is_stable, Fixture )
+BOOST_FIXTURE_TEST_CASE( factory_can_return_last_unused_port, Fixture )
 {
-    std::vector< std::auto_ptr< Port_ABC > > data;
-    for( int i = 0; i < 10; ++i )
-        data.push_back( factory.Create() );
-    int unused = data[5]->Get();
-    data.erase( data.begin() + 5 );
-    for( int i = 0; i < 10; ++i )
-        BOOST_CHECK_EQUAL( unused, factory.Create()->Get() );
+    T_Ports data = MakePorts( factory, 10 );
+    RemoveAddCheck( factory, data, data.size() - 1 );
 }
 
+BOOST_FIXTURE_TEST_CASE( factory_can_return_any_unused_port, Fixture )
+{
+    T_Ports data = MakePorts( factory, 10 );
+    for( int i = 0; i < 10; ++i )
+        RemoveAddCheck( factory, data, i );
+}
