172032d7c11cefd1f06b101506068921b854e76b
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-03-26 09:47:45 +0000

    Replace Thymeleaf with FreeMarker which support inline macros

--- node/src/web/Handler.java
+++ node/src/web/Handler.java
@@ -2,6 +2,7 @@ package web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -12,18 +13,19 @@ import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import eu.medsea.mimeutil.MimeType;
 import eu.medsea.mimeutil.MimeUtil2;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class Handler extends AbstractHandler {
 
     @SuppressWarnings("unused")
     private static final Logger log_ = Logger.getLogger(Handler.class);
-    private final TemplateEngine engine_;
+    private final Configuration cfg_;
     private final File root_;
     private final MimeUtil2 mimes_;
 
@@ -31,13 +33,12 @@ public class Handler extends AbstractHandler {
         root_ = new File(root);
         if (!root_.isDirectory())
             throw new Exception(root_ + " is not a directory");
-        final FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setTemplateMode("HTML5");
-        resolver.setPrefix(root_.getAbsolutePath());
-        resolver.setSuffix(".html");
-        resolver.setCacheable(!isDebug);
-        engine_ = new TemplateEngine();
-        engine_.setTemplateResolver(resolver);
+        cfg_ = new Configuration();
+        cfg_.setDirectoryForTemplateLoading(root_);
+        cfg_.setObjectWrapper(new DefaultObjectWrapper());
+        if (isDebug)
+            cfg_.setTemplateUpdateDelay(0);
+        cfg_.setOutputEncoding("UTF-8");
         mimes_ = new MimeUtil2();
         mimes_.registerMimeDetector("eu.medsea.mimeutil.detector.ExtensionMimeDetector");
     }
@@ -57,25 +58,32 @@ public class Handler extends AbstractHandler {
     private void serveTemplate(final HttpServletResponse response, final Request request, final String target) throws IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        final Context ctx = new Context();
+        final Template ctx = cfg_.getTemplate(target);
+        final Map<String, String> root = new HashMap<String, String>();
         @SuppressWarnings("unchecked")
         final Map<String, String[]> map = request.getParameterMap();
         for (final Map.Entry<String, String[]> it : map.entrySet())
-            ctx.setVariable(it.getKey(), it.getValue()[0]);
-        engine_.process(target, ctx, response.getWriter());
+            root.put(it.getKey(), it.getValue()[0]);
+        try {
+            ctx.process(root, response.getWriter());
+        } catch (final TemplateException e) {
+            throw new IOException("Unable to process template " + target + ": " + e);
+        }
     }
 
     @Override
     public void handle(String uri, final Request base, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         if (uri.equals("/"))
             uri = "/index";
+        if (uri.startsWith("/"))
+            uri = uri.substring(1);
         File target;
         if (base.getMethod() != "GET")
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "invalid method " + base.getMethod());
         else if ((target = new File(root_, uri)).isFile())
             serveFile(response, target);
         else if ((target = new File(root_, uri + ".html")).isFile())
-            serveTemplate(response, base, uri);
+            serveTemplate(response, base, uri + ".html");
         else
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
         base.setHandled(true);
