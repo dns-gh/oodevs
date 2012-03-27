e03607c73fe1eac930142098d5625ecf1dbfcd03
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-03-27 16:28:57 +0000

    Cosmetics

--- node/www/js/masa.coffee
+++ node/www/js/masa.coffee
@@ -1,24 +1,30 @@
-$("#session_create .modal-footer .btn_click").click( (event) -> (
-    name = $("#session_name")
-    if !name.val().length
-        name.parent().parent().addClass("error")
-        box = $("#session_create .modal-footer .alert")
-        box.html("Unable to use empty name")
-        box.show()
-        return
-    data = { name: name.val(), exercise: $("#session_exercise").val() }
-    done = (data) -> (
-        $("#session_create").modal("hide")
-    )
-    error = (obj, textStatus, data) -> (
-        box = $("#session_create .modal-footer .alert")
-        box.html("Unexpected error " + textStatus + " " + data)
-        box.show()
-    )
-    $.ajax({url: "create_session", data: data, dataType: "json", success: done, error: error})
-))
+ajax = ( url, data, success, error ) ->
+    $.ajax( { url: url, data: data, success: success, error: error, dataType: "json" } )
 
-$("#session_create").on("hidden", () -> (
-    $("#session_name").parent().parent().removeClass("error")
-    $("#session_create .modal-footer .alert").hide()
-))
+invalidate_session = ( control, box, name ) ->
+    control.parent().parent().addClass( "error" )
+    box.html( "Missing " + name )
+    box.show()
+
+on_session_click = () ->
+    box = $( "#session_create .modal-footer .alert" )
+    name = $( "#session_name" )
+    exercise = $( "#session_exercise" )
+    if !name.val().length
+        return invalidate_session( name, box, "name" )
+    if !exercise.val().length
+        return invalidate_session( exercise, box, "exercise" )
+    data = { name: name.val(), exercise: exercise.val() }
+    done = () ->
+        $( "#session_create" ).modal( "hide" )
+    error = ( obj, text, data ) ->
+        box.html( "Unexpected error " + text + " " + data )
+        box.show()
+    ajax( "create_session", data, done, error )
+
+on_session_hide = () ->
+    $( "#session_name" ).parent().parent().removeClass( "error" )
+    $( "#session_create .modal-footer .alert" ).hide()
+
+$( "#session_create .modal-footer .btn_click" ).click( on_session_click )
+$( "#session_create" ).on( "hidden", on_session_hide )
--- node/www/js/masa.js
+++ node/www/js/masa.js
@@ -1,39 +1,52 @@
 (function() {
+  var ajax, invalidate_session, on_session_click, on_session_hide;
 
-  $("#session_create .modal-footer .btn_click").click(function(event) {
-    var box, data, done, error, name;
+  ajax = function(url, data, success, error) {
+    return $.ajax({
+      url: url,
+      data: data,
+      success: success,
+      error: error,
+      dataType: "json"
+    });
+  };
+
+  invalidate_session = function(control, box, name) {
+    control.parent().parent().addClass("error");
+    box.html("Missing " + name);
+    return box.show();
+  };
+
+  on_session_click = function() {
+    var box, data, done, error, exercise, name;
+    box = $("#session_create .modal-footer .alert");
     name = $("#session_name");
-    if (!name.val().length) {
-      name.parent().parent().addClass("error");
-      box = $("#session_create .modal-footer .alert");
-      box.html("Unable to use empty name");
-      box.show();
-      return;
+    exercise = $("#session_exercise");
+    if (!name.val().length) return invalidate_session(name, box, "name");
+    if (!exercise.val().length) {
+      return invalidate_session(exercise, box, "exercise");
     }
     data = {
       name: name.val(),
-      exercise: $("#session_exercise").val()
+      exercise: exercise.val()
     };
-    done = function(data) {
+    done = function() {
       return $("#session_create").modal("hide");
     };
-    error = function(obj, textStatus, data) {
-      box = $("#session_create .modal-footer .alert");
-      box.html("Unexpected error " + textStatus + " " + data);
+    error = function(obj, text, data) {
+      box.html("Unexpected error " + text + " " + data);
       return box.show();
     };
-    return $.ajax({
-      url: "create_session",
-      data: data,
-      dataType: "json",
-      success: done,
-      error: error
-    });
-  });
+    return ajax("create_session", data, done, error);
+  };
 
-  $("#session_create").on("hidden", function() {
+  on_session_hide = function() {
     $("#session_name").parent().parent().removeClass("error");
     return $("#session_create .modal-footer .alert").hide();
-  });
+  };
+
+  $("#session_create .modal-footer .btn_click").click(on_session_click);
+
+  $("#session_create").on("hidden", on_session_hide);
 
 }).call(this);
