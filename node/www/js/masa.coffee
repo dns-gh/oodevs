5979920866bfa84410ca4c3bd121c48539933ad5
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-03-29 08:59:32 +0000

    Add backbone-powered session management view

--- node/www/js/masa.coffee
+++ node/www/js/masa.coffee
@@ -1,10 +1,128 @@
 ajax = (url, data, success, error) ->
     $.ajax
-        url:      url,
+        cache:    false
         data:     data,
-        success:  success,
-        error:    error,
         dataType: "json"
+        error:    error,
+        success:  success,
+        url:      url,
+
+class SessionItem extends Backbone.Model
+    sync: (method, model, options) =>
+        if method == "create"
+            params =
+                name:     model.get "name"
+                exercise: model.get "exercise"
+            return ajax "/create_session", params, options.success, options.error
+        if method == "read"
+            return ajax "/get_session", {id: model.get "id"}, options.success, options.error
+        if method == "delete"
+            return ajax "/delete_session", {id: model.get "id"}, options.success, options.error
+        return Backbone.sync method, model, options
+
+class SessionList extends Backbone.Collection
+    model: SessionItem
+
+    sync: (method, model, options) =>
+        if method == "read"
+            return ajax "/list_sessions", null, options.success, options.error
+        return Backbone.sync method, model, options
+
+class SessionItemView extends Backbone.View
+    tagName:   "div"
+    className: "row"
+
+    initialize: ->
+        @model.bind 'change', @render
+
+    events:
+        "click .delete" : "delete"
+
+    render: =>
+        $(@el).html """
+<div id="id_#{@model.get "id"}" class="span9">
+    <div class="well run session_row">
+        <div class="span3">
+            <div class="session_name">#{@model.get "name"}</div>
+            <div class="session_exercise">#{@model.get "exercise"}</div>
+            <div class="session_model">Model: 2010-12-10</div>
+            <span class="label">#{@model.get "id"}</span>
+        </div>
+        <div class="span4">
+            <div>Started 2012-01-01 at 00:00:00</div>
+            <div>Time running 00:00:00</div>
+            <div>0 clients connected</div>
+        </div>
+        <div class="btn-group session_top_right">
+            <a href="#" class="btn"><i class="icon-play"></i></a>
+            <a href="#" class="btn"><i class="icon-pause"></i></a>
+            <a href="#" class="btn"><i class="icon-stop"></i></a>
+            <a href="#" class="btn"><i class="icon-plus-sign"></i></a>
+            <a href="#" class="btn delete"><i class="icon-trash"></i></a>
+        </div>
+        <div class="session_bottom_right">
+            <a href="#" class="btn"><i class="icon-th-list"></i> More <span class="caret"></span></a>
+        </div>
+    </div>
+</div>
+"""
+        return this
+
+    delete: =>
+        @model.destroy wait: true
+
+diff_models = (prev, next) ->
+    not_found = []
+    found = []
+    prev_ids = _(prev).map (item) -> item.id
+    for item in next
+        if prev_ids.indexOf(item.id) == -1
+            not_found.push item
+        else
+            found.push item
+    return [not_found, found]
+
+class SessionListView extends Backbone.View
+    el: $( "#sessions" )
+
+    initialize: ->
+        @model = new SessionList
+        @model.bind "add",    @add
+        @model.bind "remove", @remove
+        @model.bind "reset",  @reset
+        @model.fetch()
+        setInterval @delta, 5*1000
+
+    reset: (list, options) =>
+        for item in list.models
+            @add item
+        return this
+
+    add: (item) =>
+        view = new SessionItemView model: item
+        $(@el).append view.render().el
+
+    remove: (item, list, index) =>
+        $("#id_" + item.get "id").parent().remove()
+
+    create: (data) =>
+        item = new SessionItem
+        item.set data
+        @model.create item, wait: true
+
+    delta: =>
+        prev = @model
+        next = new SessionList
+        next.fetch success: () ->
+            rpy = diff_models next.models, prev.models
+            prev.remove rpy[0]
+            rpy = diff_models prev.models, next.models
+            prev.add rpy[0]
+            for item in rpy[1]
+                prev.get(item.id).set item.attributes
+            return
+
+session_view = new SessionListView
 
 invalidate_session = (control, box, name) ->
     control.parent().parent().addClass "error"
@@ -12,22 +130,14 @@ invalidate_session = (control, box, name) ->
     box.show()
 
 on_session_click = ->
-    box = $("#session_create .modal-footer .alert")
     name = $("#session_name")
     exercise = $("#session_exercise")
     if !name.val().length
         return invalidate_session name, box, "name"
     if !exercise.val()?
         return invalidate_session exercise, box, "exercise"
-    data =
-        name:     name.val()
-        exercise: exercise.val()
-    done = ->
-        $("#session_create").modal "hide"
-    error = (obj, text, data) ->
-        box.html "Unexpected error " + text + " " + data
-        box.show()
-    ajax "create_session", data, done, error
+    $("#session_create").modal "hide"
+    session_view.create name: name.val(), exercise: exercise.val()
 
 on_session_hide = ->
     $("#session_name").parent().parent().removeClass "error"
@@ -44,7 +154,7 @@ on_session_load = ->
         box = $("#session_create .modal-footer .alert")
         box.html data + " [" + text + "]"
         box.show()
-    ajax "list_exercises", limit:40, done, error
+    ajax "list_exercises", limit: 40, done, error
 
 $("#session_create .modal-footer .btn_click").click on_session_click
 $("#session_create").on "hidden", on_session_hide
--- node/www/js/masa.js
+++ node/www/js/masa.js
@@ -1,15 +1,212 @@
 (function() {
-  var ajax, invalidate_session, on_session_click, on_session_hide, on_session_load;
+  var SessionItem, SessionItemView, SessionList, SessionListView, ajax, diff_models, invalidate_session, on_session_click, on_session_hide, on_session_load, session_view,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   ajax = function(url, data, success, error) {
     return $.ajax({
-      url: url,
+      cache: false,
       data: data,
-      success: success,
+      dataType: "json",
       error: error,
-      dataType: "json"
+      success: success,
+      url: url
     });
   };
+
+  SessionItem = (function(_super) {
+
+    __extends(SessionItem, _super);
+
+    function SessionItem() {
+      this.sync = __bind(this.sync, this);
+      SessionItem.__super__.constructor.apply(this, arguments);
+    }
+
+    SessionItem.prototype.sync = function(method, model, options) {
+      var params;
+      if (method === "create") {
+        params = {
+          name: model.get("name"),
+          exercise: model.get("exercise")
+        };
+        return ajax("/create_session", params, options.success, options.error);
+      }
+      if (method === "read") {
+        return ajax("/get_session", {
+          id: model.get("id")
+        }, options.success, options.error);
+      }
+      if (method === "delete") {
+        return ajax("/delete_session", {
+          id: model.get("id")
+        }, options.success, options.error);
+      }
+      return Backbone.sync(method, model, options);
+    };
+
+    return SessionItem;
+
+  })(Backbone.Model);
+
+  SessionList = (function(_super) {
+
+    __extends(SessionList, _super);
+
+    function SessionList() {
+      this.sync = __bind(this.sync, this);
+      SessionList.__super__.constructor.apply(this, arguments);
+    }
+
+    SessionList.prototype.model = SessionItem;
+
+    SessionList.prototype.sync = function(method, model, options) {
+      if (method === "read") {
+        return ajax("/list_sessions", null, options.success, options.error);
+      }
+      return Backbone.sync(method, model, options);
+    };
+
+    return SessionList;
+
+  })(Backbone.Collection);
+
+  SessionItemView = (function(_super) {
+
+    __extends(SessionItemView, _super);
+
+    function SessionItemView() {
+      this["delete"] = __bind(this["delete"], this);
+      this.render = __bind(this.render, this);
+      SessionItemView.__super__.constructor.apply(this, arguments);
+    }
+
+    SessionItemView.prototype.tagName = "div";
+
+    SessionItemView.prototype.className = "row";
+
+    SessionItemView.prototype.initialize = function() {
+      return this.model.bind('change', this.render);
+    };
+
+    SessionItemView.prototype.events = {
+      "click .delete": "delete"
+    };
+
+    SessionItemView.prototype.render = function() {
+      $(this.el).html("<div id=\"id_" + (this.model.get("id")) + "\" class=\"span9\">\n    <div class=\"well run session_row\">\n        <div class=\"span3\">\n            <div class=\"session_name\">" + (this.model.get("name")) + "</div>\n            <div class=\"session_exercise\">" + (this.model.get("exercise")) + "</div>\n            <div class=\"session_model\">Model: 2010-12-10</div>\n            <span class=\"label\">" + (this.model.get("id")) + "</span>\n        </div>\n        <div class=\"span4\">\n            <div>Started 2012-01-01 at 00:00:00</div>\n            <div>Time running 00:00:00</div>\n            <div>0 clients connected</div>\n        </div>\n        <div class=\"btn-group session_top_right\">\n            <a href=\"#\" class=\"btn\"><i class=\"icon-play\"></i></a>\n            <a href=\"#\" class=\"btn\"><i class=\"icon-pause\"></i></a>\n            <a href=\"#\" class=\"btn\"><i class=\"icon-stop\"></i></a>\n            <a href=\"#\" class=\"btn\"><i class=\"icon-plus-sign\"></i></a>\n            <a href=\"#\" class=\"btn delete\"><i class=\"icon-trash\"></i></a>\n        </div>\n        <div class=\"session_bottom_right\">\n            <a href=\"#\" class=\"btn\"><i class=\"icon-th-list\"></i> More <span class=\"caret\"></span></a>\n        </div>\n    </div>\n</div>");
+      return this;
+    };
+
+    SessionItemView.prototype["delete"] = function() {
+      return this.model.destroy({
+        wait: true
+      });
+    };
+
+    return SessionItemView;
+
+  })(Backbone.View);
+
+  diff_models = function(prev, next) {
+    var found, item, not_found, prev_ids, _i, _len;
+    not_found = [];
+    found = [];
+    prev_ids = _(prev).map(function(item) {
+      return item.id;
+    });
+    for (_i = 0, _len = next.length; _i < _len; _i++) {
+      item = next[_i];
+      if (prev_ids.indexOf(item.id) === -1) {
+        not_found.push(item);
+      } else {
+        found.push(item);
+      }
+    }
+    return [not_found, found];
+  };
+
+  SessionListView = (function(_super) {
+
+    __extends(SessionListView, _super);
+
+    function SessionListView() {
+      this.delta = __bind(this.delta, this);
+      this.create = __bind(this.create, this);
+      this.remove = __bind(this.remove, this);
+      this.add = __bind(this.add, this);
+      this.reset = __bind(this.reset, this);
+      SessionListView.__super__.constructor.apply(this, arguments);
+    }
+
+    SessionListView.prototype.el = $("#sessions");
+
+    SessionListView.prototype.initialize = function() {
+      this.model = new SessionList;
+      this.model.bind("add", this.add);
+      this.model.bind("remove", this.remove);
+      this.model.bind("reset", this.reset);
+      this.model.fetch();
+      return setInterval(this.delta, 5 * 1000);
+    };
+
+    SessionListView.prototype.reset = function(list, options) {
+      var item, _i, _len, _ref;
+      _ref = list.models;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        item = _ref[_i];
+        this.add(item);
+      }
+      return this;
+    };
+
+    SessionListView.prototype.add = function(item) {
+      var view;
+      view = new SessionItemView({
+        model: item
+      });
+      return $(this.el).append(view.render().el);
+    };
+
+    SessionListView.prototype.remove = function(item, list, index) {
+      return $("#id_" + item.get("id")).parent().remove();
+    };
+
+    SessionListView.prototype.create = function(data) {
+      var item;
+      item = new SessionItem;
+      item.set(data);
+      return this.model.create(item, {
+        wait: true
+      });
+    };
+
+    SessionListView.prototype.delta = function() {
+      var next, prev;
+      prev = this.model;
+      next = new SessionList;
+      return next.fetch({
+        success: function() {
+          var item, rpy, _i, _len, _ref;
+          rpy = diff_models(next.models, prev.models);
+          prev.remove(rpy[0]);
+          rpy = diff_models(prev.models, next.models);
+          prev.add(rpy[0]);
+          _ref = rpy[1];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            item = _ref[_i];
+            prev.get(item.id).set(item.attributes);
+          }
+        }
+      });
+    };
+
+    return SessionListView;
+
+  })(Backbone.View);
+
+  session_view = new SessionListView;
 
   invalidate_session = function(control, box, name) {
     control.parent().parent().addClass("error");
@@ -18,26 +215,18 @@
   };
 
   on_session_click = function() {
-    var box, data, done, error, exercise, name;
-    box = $("#session_create .modal-footer .alert");
+    var exercise, name;
     name = $("#session_name");
     exercise = $("#session_exercise");
     if (!name.val().length) return invalidate_session(name, box, "name");
     if (!(exercise.val() != null)) {
       return invalidate_session(exercise, box, "exercise");
     }
-    data = {
+    $("#session_create").modal("hide");
+    return session_view.create({
       name: name.val(),
       exercise: exercise.val()
-    };
-    done = function() {
-      return $("#session_create").modal("hide");
-    };
-    error = function(obj, text, data) {
-      box.html("Unexpected error " + text + " " + data);
-      return box.show();
-    };
-    return ajax("create_session", data, done, error);
+    });
   };
 
   on_session_hide = function() {
