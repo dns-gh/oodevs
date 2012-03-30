653c37633311a7cc703aefd013ab1fffb3f39405
Grégory Gougé <gregory.gouge@masagroup.net>
2012-03-30 12:14:30 +0000

    [Decisional] Fix Jira #SWBUG-7565 Faire franchir / Exploiter site ne marchent pas

--- data/data/models/ada/decisional/dia5/integration/DefaultBuildableIntegration.lua
+++ data/data/models/ada/decisional/dia5/integration/DefaultBuildableIntegration.lua
@@ -38,9 +38,6 @@ end
 
 integration.startBuildIt = function( object )
     object[myself] = object[myself] or {}
-    local typeObject = DEC_GenObject_Type( object.source )
-    local externalIdentifier = DEC_GenObject_ExternalIdentifier( object.source )
-    local name = object.source:DEC_GenObject_Name()
     if DEC_GenObject_TypeObstacleManoeuvre( object.source ) then
         object[myself].actionBuild = DEC_StartPrepareObject( object.source )
     else
