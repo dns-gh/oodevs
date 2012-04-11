6c2505ce63318d4421751e532e2dc7e979bf4fb7
Loïc Geffroy <loic.geffroy@masagroup.net>
2012-04-11 11:16:31 +0000

    Merged revision(s) 28651 from branches/sword-scipio-stab/src/libraries/preparation: [preparation] fix jira 7672 : "Scipio - Ajout inutile de l'id dans le nom des automates" ........

--- src/libraries/preparation/Automat.cpp
+++ src/libraries/preparation/Automat.cpp
@@ -35,8 +35,7 @@ Automat::Automat( const AutomatType& type, Controller& controller, IdManager& id
     : EntityImplementation< Automat_ABC >( controller, idManager.GetNextId(), "" )
     , type_( type )
 {
-    name_ = name.isEmpty() ? type.GetName().c_str() + QString( " [%1]" ).arg( id_ )
-                           : name + " " + QString( "[%1]" ).arg( id_ );
+    name_ = name.isEmpty() ? type.GetName().c_str() : name;
     RegisterSelf( *this );
     CreateDictionary( controller );
 }
