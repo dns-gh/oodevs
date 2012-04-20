29604c630a1cd69871cd1313636bb44fd4c45d31
Grégory Gougé <gregory.gouge@masagroup.net>
2012-04-20 09:16:16 +0000

    [Decisional][Physical] SWBUG-7382 - Scipio 360009 - Impossible de dégager une destruction de pont et SWBUG-7017 - Scipio 250953 - Le renforcement d'une unité de génie ne réalise pas les travaux

--- data/data/models/ada/decisional/dia5/models/src/sword/military/queries/tactical/getPositionsToRemove.lua
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/queries/tactical/getPositionsToRemove.lua
@@ -5,11 +5,13 @@ queryImplementation "getPositionsToRemove"
         if not next( allRes ) then
             allRes = {}
             local point, objectPosition, scaledObject
-            for _, element in pairs ( params.elementsToRemove ) do     
-                scaledObject = DEC_Geometrie_AgrandirLocalisation(  element:getLocalisation() , 50 )
-                objectPosition = DEC_Geometrie_ComputeNearestBorder( meKnowledge:getPosition(), scaledObject )
-                point = CreateKnowledge( sword.military.world.Point, objectPosition )
-                allRes[ #allRes + 1 ] = point
+            for _, element in pairs ( params.elementsToRemove ) do
+                if element:isValid() then -- when the mission is given a second time by lead, the element could be already removed
+                    scaledObject = DEC_Geometrie_AgrandirLocalisation(  element:getLocalisation() , 50 )
+                    objectPosition = DEC_Geometrie_ComputeNearestBorder( meKnowledge:getPosition(), scaledObject )
+                    point = CreateKnowledge( sword.military.world.Point, objectPosition )
+                    allRes[ #allRes + 1 ] = point
+                end
             end
             knowledgeManager.setQueryResult( "getPositionsToRemove", allRes )
         end
