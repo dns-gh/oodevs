eeccd017c26f32385e9c4d793c55dc459d861ce1
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-04-20 09:44:33 +0000

    Add json stuff

---
+++ host_agent/src/libraries/host/Json.cpp
@@ -0,0 +1,35 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 MASA Group
+//
+// *****************************************************************************
+
+#include "Json.h"
+
+#include <boost/property_tree/json_parser.hpp>
+
+// -----------------------------------------------------------------------------
+// Name: ToJson
+// Created: BAX 2012-04-18
+// -----------------------------------------------------------------------------
+std::string ToJson( const boost::property_tree::ptree& tree )
+{
+    std::ostringstream out;
+    boost::property_tree::write_json( out, tree );
+    return out.str();
+}
+
+// -----------------------------------------------------------------------------
+// Name: FromJson
+// Created: BAX 2012-04-18
+// -----------------------------------------------------------------------------
+boost::property_tree::ptree FromJson( const std::string& data )
+{
+    std::istringstream input( data );
+    boost::property_tree::ptree tree;
+    boost::property_tree::read_json( input, tree );
+    return tree;
+}
---
+++ host_agent/src/libraries/host/Json.h
@@ -0,0 +1,21 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 MASA Group
+//
+// *****************************************************************************
+
+#ifndef JSON_H
+#define JSON_H
+
+#include <boost/property_tree/ptree_fwd.hpp>
+
+namespace host
+{
+    std::string ToJson( const boost::property_tree::ptree& tree );
+    boost::property_tree::ptree FromJson( const std::string& data );
+}
+
+#endif // JSON_H
