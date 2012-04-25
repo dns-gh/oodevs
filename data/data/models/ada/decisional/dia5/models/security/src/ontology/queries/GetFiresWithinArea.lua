e2a80ff3a7edd01e49a5eb91550b9f2414563895
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-04-25 15:26:52 +0000

    [ Security ] - cleaning skill description

--- data/data/models/ada/decisional/dia5/models/security/src/ontology/queries/GetFiresWithinArea.lua
+++ data/data/models/ada/decisional/dia5/models/security/src/ontology/queries/GetFiresWithinArea.lua
@@ -6,11 +6,11 @@ queryImplementation "GetFiresWithinArea"
 { 
     [ "execute" ] = function ( params )
         local allRes = {}
-        local simFires = DEC_ObjectKnowledgesInZone( params.area.source, { "fire", "Arson" } ) -- $$$ MIA TEMP
+        local simFires = integration.getObjectsKnowledgeInZoneWithCapacity( "burn", params.area )
         for i = 1, #simFires do
             allRes[ i ] = CreateKnowledge( ontology.classes.Object, simFires[ i ] )
         end
-        -- NO fires in the area
+        -- No fires in the area
         if not next( allRes ) then
             meKnowledge:sendReport( eRC_NoFiresWithinArea )
         end
