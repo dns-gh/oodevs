30368240aea272e28b0d2769067893868a6dc846
Lucie Mignot <lucie.mignot@masagroup.net>
2012-05-03 09:09:54 +0000

    [Decisionnel] Jira 7609 : SCIPIO Mission Automate Interroger inexistante : forgotten files

---
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/company/tasks/knowledges/InterrogateEffect.lua
@@ -0,0 +1,12 @@
+return
+{
+
+    getEntity = function( self, params, entity )
+        return myself.taskParams.entity
+    end,
+    
+    getNbrFront = function( self )
+      return 1
+    end
+
+}
