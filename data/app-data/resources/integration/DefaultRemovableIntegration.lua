8085dcd961b4162d5a1134515b579b8c3294d53d
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-05-10 16:31:27 +0000

    [ Security ] - update physical data and add reports for "deconstruction" skill integration

--- data/app-data/resources/integration/DefaultRemovableIntegration.lua
+++ data/app-data/resources/integration/DefaultRemovableIntegration.lua
@@ -130,13 +130,16 @@ end
 -- ============================================================================
 integration.updateRemoveItSecu = function( object )
     if object[myself].actionRemoveState == eActionObjetImpossible then
+        meKnowledge:sendReport( eRC_ConstructionObjetImpossible )
         DEC_Trace( "impossible works" )
         return true 
     elseif object[ myself ].actionRemoveState == eActionObjetManqueDotation then
         DEC_Trace( "not enough dotation" )
+         meKnowledge:sendReport( eRC_PasDotationConstructionObjet )
         return true 
     elseif object[ myself ].actionRemoveState == eActionObjetPasDeCapacite then
         DEC_Trace( "no capacity" )
+        meKnowledge:sendReport( eRC_PasDotationConstructionObjet )
         return true 
     elseif object[ myself ].actionRemoveState == eActionObjetTerminee then
         meKnowledge:RC( eRC_FinDegagement )
