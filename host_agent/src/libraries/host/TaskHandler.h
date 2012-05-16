7c46bfd872b5fc1452e6a9db98c8f4c1b3b35465
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-05-16 17:10:05 +0000

    Add Taskhandler helper

---
+++ host_agent/src/libraries/host/TaskHandler.h
@@ -0,0 +1,59 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 MASA Group
+//
+// *****************************************************************************
+
+#ifndef TASK_HANDLER_H
+#define TASK_HANDLER_H
+
+#include <boost/thread/future.hpp>
+
+namespace host
+{
+template< typename T >
+struct TaskHandler
+{
+    typedef boost::packaged_task< T > Package;
+    typedef boost::shared_future< T > Future;
+
+    template< typename U >
+    explicit TaskHandler( const U& task )
+        : package_( boost::make_shared< Package >( task ) )
+        , future_ ( package_->get_future() )
+    {
+    }
+
+    ~TaskHandler()
+    {
+        // NOTHING
+    }
+
+    void operator()()
+    {
+        (*package_)();
+    }
+
+    Future GetFuture()
+    {
+        return future_;
+    }
+
+    template< typename U >
+    static Future Go( const U& task )
+    {
+        TaskHandler< T > handler( task );
+        boost::thread detached( handler );
+        return handler.future_;
+    }
+
+private:
+    boost::shared_ptr< Package > package_;
+    Future future_;
+};
+}
+
+#endif // TASK_HANDLER_H
