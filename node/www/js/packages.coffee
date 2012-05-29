c97d4ace0b1a807d9742932b253d64fb4bb98cc6
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-05-29 15:33:41 +0000

    Add dynamic installed package view

--- node/www/js/packages.coffee
+++ node/www/js/packages.coffee
@@ -1,7 +1,64 @@
-for tr in $(".exercises tr")
-    continue unless tr.id?.length
-    uid = "#" + tr.id + "_briefing"
-    $(tr).popover
-        placement: "bottom",
-        title:     $(uid + " h1:nth-child(2)").contents()
-        content:   $(uid).contents()
+get_url = (url) ->
+    return window.location.protocol + "//" + window.location.hostname + ":" + proxy + url
+
+ajax = (url, data, success, error) ->
+    $.ajax
+        cache:    false
+        data:     data,
+        dataType: "json"
+        error:    error,
+        success:  success,
+        url:      get_url url
+
+Handlebars.registerHelper "forall", (items, type, options) ->
+    buffer = ''
+    for it in items
+        if it.type == type
+            buffer += options.fn it
+    return buffer
+
+package_template = Handlebars.compile $("#package_template").html()
+package_error_template = Handlebars.compile $("#package_error_template").html()
+
+class Package extends Backbone.Model
+    view: PackageView
+
+    sync: (method, model, options) =>
+        if method == "read"
+            return ajax "/api/get_install", id: uuid,
+                options.success, options.error
+        return Backbone.sync method, model, options
+
+class PackageView extends Backbone.View
+    el: $("#packages")
+
+    initialize: (obj) ->
+        @model = new Package
+        @model.bind 'change', @render
+        @model.fetch()
+        setTimeout @delta, 5000
+
+    render: =>
+        $(@el).empty()
+        return unless @model.attributes.items?
+
+        $(@el).html package_template @model.attributes
+
+        for it in $(@el).find ".action .more"
+            $(it).click ->
+                $("#" + $(@).parent().parent().attr "rel").toggle "fast"
+        return
+
+    delta: =>
+        list = new Package
+        list.fetch
+            success: =>
+                if list.attributes.items?
+                    @update list.attributes
+                else
+                    @model.clear()
+                setTimeout @delta, 5000
+            error: =>
+                setTimeout @delta, 5000
+
+package_view = new PackageView
--- node/www/js/packages.js
+++ node/www/js/packages.js
@@ -1,16 +1,118 @@
 (function() {
-  var tr, uid, _i, _len, _ref, _ref2;
+  var Package, PackageView, ajax, get_url, package_error_template, package_template, package_view,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
-  _ref = $(".exercises tr");
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-    tr = _ref[_i];
-    if (!((_ref2 = tr.id) != null ? _ref2.length : void 0)) continue;
-    uid = "#" + tr.id + "_briefing";
-    $(tr).popover({
-      placement: "bottom",
-      title: $(uid + " h1:nth-child(2)").contents(),
-      content: $(uid).contents()
+  get_url = function(url) {
+    return window.location.protocol + "//" + window.location.hostname + ":" + proxy + url;
+  };
+
+  ajax = function(url, data, success, error) {
+    return $.ajax({
+      cache: false,
+      data: data,
+      dataType: "json",
+      error: error,
+      success: success,
+      url: get_url(url)
     });
-  }
+  };
+
+  Handlebars.registerHelper("forall", function(items, type, options) {
+    var buffer, it, _i, _len;
+    buffer = '';
+    for (_i = 0, _len = items.length; _i < _len; _i++) {
+      it = items[_i];
+      if (it.type === type) buffer += options.fn(it);
+    }
+    return buffer;
+  });
+
+  package_template = Handlebars.compile($("#package_template").html());
+
+  package_error_template = Handlebars.compile($("#package_error_template").html());
+
+  Package = (function(_super) {
+
+    __extends(Package, _super);
+
+    function Package() {
+      this.sync = __bind(this.sync, this);
+      Package.__super__.constructor.apply(this, arguments);
+    }
+
+    Package.prototype.view = PackageView;
+
+    Package.prototype.sync = function(method, model, options) {
+      if (method === "read") {
+        return ajax("/api/get_install", {
+          id: uuid
+        }, options.success, options.error);
+      }
+      return Backbone.sync(method, model, options);
+    };
+
+    return Package;
+
+  })(Backbone.Model);
+
+  PackageView = (function(_super) {
+
+    __extends(PackageView, _super);
+
+    function PackageView() {
+      this.delta = __bind(this.delta, this);
+      this.render = __bind(this.render, this);
+      PackageView.__super__.constructor.apply(this, arguments);
+    }
+
+    PackageView.prototype.el = $("#packages");
+
+    PackageView.prototype.initialize = function(obj) {
+      this.model = new Package;
+      this.model.bind('change', this.render);
+      this.model.fetch();
+      return setTimeout(this.delta, 5000);
+    };
+
+    PackageView.prototype.render = function() {
+      var it, _i, _len, _ref;
+      $(this.el).empty();
+      if (this.model.attributes.items == null) return;
+      $(this.el).html(package_template(this.model.attributes));
+      _ref = $(this.el).find(".action .more");
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        it = _ref[_i];
+        $(it).click(function() {
+          return $("#" + $(this).parent().parent().attr("rel")).toggle("fast");
+        });
+      }
+    };
+
+    PackageView.prototype.delta = function() {
+      var list,
+        _this = this;
+      list = new Package;
+      return list.fetch({
+        success: function() {
+          if (list.attributes.items != null) {
+            _this.update(list.attributes);
+          } else {
+            _this.model.clear();
+          }
+          return setTimeout(_this.delta, 5000);
+        },
+        error: function() {
+          return setTimeout(_this.delta, 5000);
+        }
+      });
+    };
+
+    return PackageView;
+
+  })(Backbone.View);
+
+  package_view = new PackageView;
 
 }).call(this);
