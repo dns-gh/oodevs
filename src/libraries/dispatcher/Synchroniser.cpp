15fd379600bb6245ea89c56050913414d7bf1170
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2012-06-04 10:36:33 +0000

    [replayer]Fix JIRA #7944 Scipio 370028 - Crash REPLAYER pendant exercice QUASCI Correction crash si brouillage / groupes de connaissances detruits deux fois. Potentiellement la correction a un gros impact (knowledgeGroups_ etait visite deux fois dans Model::Visit sur le dispatcher). Restent des problemes de crash sur prêt de composante.

--- src/libraries/dispatcher/Synchroniser.cpp
+++ src/libraries/dispatcher/Synchroniser.cpp
@@ -96,15 +96,17 @@ void Synchroniser::Commit( ClientPublisher_ABC& publisher, Model& model )
         (*it)->Apply( &EntityPublisher_ABC::SendFullUpdate, publisher );
 
     Publisher p( publisher, model );
-    for( std::vector< kernel::Entity_ABC* >::reverse_iterator it = toDestroy_.rbegin(); it != toDestroy_.rend(); ++it )
+    int size = toDestroy_.size();
+    for( int i = size - 1; i >= 0; --i )
     {
         // $$$$ AGE 2008-06-20:
 //        (*it)->Apply( &EntityPublisher_ABC::SendDestruction, p );
         // $$$$ AGE 2008-06-20: causes reentrances in the destructor...
         // $$$$ AGE 2008-06-20: Change the whole thing (see SimulationDispatcher::EndSynchronisation)
 
-        const EntityPublisher_ABC* publisher = (*it)->Retrieve< EntityPublisher_ABC >();
+        const EntityPublisher_ABC* publisher = (toDestroy_[i])->Retrieve< EntityPublisher_ABC >();
         if( publisher )
             publisher->SendDestruction( p );
     }
+    toDestroy_.clear();
 }
