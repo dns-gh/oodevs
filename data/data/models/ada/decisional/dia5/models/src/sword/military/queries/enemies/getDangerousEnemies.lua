816b07f0c6ca48c44d23f308c84911e03715d21e
Grégory Gougé <gregory.gouge@masagroup.net>
2012-06-04 10:01:28 +0000

    [Decisional] Merge from scipio-stab rev 29905 Fix Jira SWBUG-7429 - Embarquement de la milice

---
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/queries/enemies/getDangerousEnemies.lua
@@ -0,0 +1,16 @@
+queryImplementation "getDangerousEnemies" 
+{ 
+    [ "execute" ] = function( params )
+
+        local dangerousEnemies = {}
+        local simEnemies = DEC_Connaissances_UnitesEnnemiesDangereuses( )
+        for _, simEny in pairs( simEnemies ) do
+            local eny = CreateKnowledge( sword.military.world.Platoon, simEny )
+            if eny:isNearby() and not eny:isTransported() then
+               dangerousEnemies[ #dangerousEnemies + 1 ] = eny
+            end
+        end
+        return dangerousEnemies
+
+    end
+}
