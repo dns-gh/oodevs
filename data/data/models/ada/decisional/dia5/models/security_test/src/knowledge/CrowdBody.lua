d1cb5d2d50f27c5029dd2759a6b8e3ade609920a
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-06-13 15:35:29 +0000

    [DATA PHY ] - [ Security ] - Fix bad role for crowd test

---
+++ data/data/models/ada/decisional/dia5/models/security_test/src/knowledge/CrowdBody.lua
@@ -0,0 +1,8 @@
+method "demonstrate"( masalife.brain.integration.startStopAction
+        {
+            start = integration.startDemonstrate,
+            started = integration.uptateDemonstrate,
+            stop = integration.stopDemonstrate 
+        } )
+
+return {}
