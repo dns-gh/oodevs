4ed8865c00920d1ab751b662df6bf6126ae7e259
Grégory Gougé <gregory.gouge@masagroup.net>
2012-06-15 14:39:22 +0000

    [Decisional] Fix Jira SWBUG-7985 - Scipio Quasci Mission impossible: PION MELEE Appuyer (sur un PC)

--- data/data/models/ada/decisional/dia5/models/src/sword/military/queries/enemies/getEnemiesEngagingFriends.lua
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/queries/enemies/getEnemiesEngagingFriends.lua
@@ -4,16 +4,27 @@ queryImplementation "getEnemiesEngagingFriends"
         local allRes = knowledgeManager.getQueryResult( "getEnemiesEngagingFriends" )
         local res = {}
         local tableKn = {}
+        local entities = {}
         if knowledgeManager.bCanCallStaticQuery or params.dynamic then
             allRes = {}
             for _, entity in pairs( params.friends ) do
                 res = {}
-                res = DEC_Connaissances_UnitesPrenantAPartieSurAmi( entity.source )
-                res[#res] = F_Pion_GetEnemyAttackedBy( entity.source )
-                for _, i in pairs( res ) do
-                    if not exists( tableKn, i ) then
-                        allRes[ #allRes + 1 ] = CreateKnowledge( sword.military.world.Platoon, i )
-                        tableKn[ #tableKn ] = i
+                entities = {}
+                if masalife.brain.core.class.isOfType( entity, sword.military.world.Company ) then -- on peut appuyer des automates alliés
+                    local entitiesFromAutomat = integration.getEntitiesFromAutomat( entity, "none", true)
+                    for j = 1, #entitiesFromAutomat do
+                        entities[#entities + 1] = entitiesFromAutomat[j]
+                    end
+                else -- on peut appuyer des unités alliés
+                    entities[#entities + 1] = entity
+                end
+                for k = 1, #entities do
+                    res = DEC_Connaissances_UnitesPrenantAPartieSurAmi( entities[k].source )
+                    for _, i in pairs( res ) do
+                        if not exists( tableKn, i ) then
+                            allRes[ #allRes + 1 ] = CreateKnowledge( sword.military.world.Platoon, i )
+                            tableKn[ #tableKn ] = i
+                        end
                     end
                 end
             end
