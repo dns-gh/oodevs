9d7c748082c5ca8c418e313ae9b63ab34118119e
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-06-18 16:05:27 +0000

    Add timer helper

---
+++ host_agent/src/libraries/runtime/Timer_ABC.h
@@ -0,0 +1,55 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 MASA Group
+//
+// *****************************************************************************
+
+#ifndef TIMER_ABC_H__
+#define TIMER_ABC_H__
+
+#include <boost/noncopyable.hpp>
+
+namespace boost
+{
+    template< typename T > class function;
+    template< typename T > class shared_ptr;
+namespace posix_time
+{
+    class time_duration;
+}
+}
+
+namespace runtime
+{
+    typedef boost::function< void( void ) > Task;
+    struct Pool_ABC;
+// =============================================================================
+/** @class  Timer_ABC
+    @brief  Timer_ABC class definition
+*/
+// Created: BAX 2012-06-18
+// =============================================================================
+struct Timer_ABC : public boost::noncopyable
+{
+    //! @name Constructors/Destructors
+    //@{
+              Timer_ABC() {}
+     virtual ~Timer_ABC() {}
+     //@}
+
+     //! @name Constructors/Destructors
+    //@{
+     virtual void Stop() = 0;
+     //@}
+};
+
+typedef boost::shared_ptr< Timer_ABC > Timer;
+
+Timer MakeTimer( Pool_ABC& pool, const boost::posix_time::time_duration& timeout, const Task& task );
+
+}
+
+#endif // TIMER_ABC_H__
