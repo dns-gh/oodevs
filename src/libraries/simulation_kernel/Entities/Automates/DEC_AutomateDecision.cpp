67fb988017b043b53e070ad4b241b8912d828244
Loïc Geffroy <loic.geffroy@masagroup.net>
2012-06-26 11:20:40 +0000

    [simulation] Ajout de fonctions DEC automate logistique => fix

--- src/libraries/simulation_kernel/Entities/Automates/DEC_AutomateDecision.cpp
+++ src/libraries/simulation_kernel/Entities/Automates/DEC_AutomateDecision.cpp
@@ -29,7 +29,6 @@
 #include "Decision/DEC_FireFunctions.h"
 #include "MT_Tools/MT_ScipioException.h"
 #include <boost/serialization/vector.hpp>
-#include <boost/lambda/lambda.hpp>
 #include <boost/bind.hpp>
 
 BOOST_CLASS_EXPORT_IMPLEMENT( DEC_AutomateDecision )
@@ -213,8 +212,7 @@ void DEC_AutomateDecision::RegisterUserFunctions( directia::brain::Brain& brain 
 
     // State
     brain[ "DEC_Automate_EstEmbraye" ] = boost::bind( &DEC_AutomateFunctions::IsEngaged, this );
-    brain[ "DEC_Automate_ROE" ] =
-        boost::function< int() >( boost::lambda::constant( nRulesOfEngagementState_ ) );
+    brain[ "DEC_Automate_ROE" ] = boost::bind( &DEC_AutomateFunctions::GetRoePopulation, boost::ref( GetAutomate() ) );
     brain[ "DEC_Automate_ChangeEtatROE" ] =
         boost::function< void( int ) >( boost::bind( &DEC_AutomateFunctions::NotifyRulesOfEngagementStateChanged, boost::ref( GetAutomate() ), _1 ) );
     brain[ "DEC_Automate_ChangeEtatROEPopulation" ] =
