4d064af65069a19acdf7b0a1bad495bc0017a069
Charles Madeira <charles.madeira@masagroup.net>
2012-06-27 13:00:53 +0000

    [simulation] fixing unit loading/unloading for movement in urban blocks

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Terrain/PHY_RolePion_TerrainAnalysis.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Terrain/PHY_RolePion_TerrainAnalysis.cpp
@@ -211,7 +211,7 @@ bool PHY_RolePion_TerrainAnalysis::CanMoveOnUrbanBlock( const std::vector< MT_Ve
     if( pion_.GetType().GetUnitType().CanFly() || points.empty() )
         return true;
 
-    double weight = pion_.GetRole< PHY_RoleInterface_Composantes >().GetMajorComponentWeight( true );
+    double weight = pion_.GetRole< PHY_RoleInterface_Composantes >().GetMajorComponentWeight();
     for( std::vector< MT_Vector2D >::const_iterator it = points.begin(); it != points.end(); ++it )
         if( DEC_GeometryFunctions::IsUrbanBlockTrafficable( *it, weight ) )
             return true;
