acd5e1e2f9183694d520bce05c0a0149739b36af
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-06-28 14:31:32 +0000

    Add missing file...

---
+++ host_agent/src/tests/host_test/MockUserController.h
@@ -0,0 +1,23 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef MOCK_USER_CONTROLLER_H
+#define MOCK_USER_CONTROLLER_H
+
+#include "host/UserController_ABC.h"
+
+namespace mocks
+{
+    MOCK_BASE_CLASS( MockUserController, host::UserController_ABC )
+    {
+        MOCK_METHOD( GetToken, 2 );
+    };
+};
+
+#endif // MOCK_USER_CONTROLLER_H
