b3edd2d8d62afad929bc0c1fa2c62da40dcd8f27
Lucie Mignot <lucie.mignot@masagroup.net>
2012-07-19 13:39:30 +0000

    [Decisionnel] Jira 8189 : Scipio VQ -MEL 23 Sur une zone boucler, les pions doivent être placer à l'exterieur de la zone et non à l'interieur; merge sword-scipio-stab-31467

--- data/data/models/ada/decisional/dia5/models/military/src/agent/tasks/knowledges/SealOff.lua
+++ data/data/models/ada/decisional/dia5/models/military/src/agent/tasks/knowledges/SealOff.lua
@@ -6,14 +6,15 @@ local result =
 
     fillParameters = function( self, companyTask, params )
         local objective = companyTask:getSealOffObjective( params )
+        local position = companyTask:getSealOffPosition( params )
         if objective then
-            return { objective = objective }
+            return { objective = objective, position = position }
         end
         return nil --use by keepBest in Lead skill
     end
 }
 
 local t = initTaskKnowledge( result )
-taskKnowledge["agent.tasks.knowledges.SealOff"] = t
+taskKnowledge["sword.military.platoon.stabilisation.tasks.knowledge.SealOff"] = t
 
 return result
--- data/data/models/ada/decisional/dia5/models/src/france/military/company/tasks/knowledges/Boucler.lua
+++ data/data/models/ada/decisional/dia5/models/src/france/military/company/tasks/knowledges/Boucler.lua
@@ -4,6 +4,15 @@ return
         return params.objective
     end,
 
+    getSealOffPosition = function( self, params )
+        myself.leadData.sealOffIndex = myself.leadData.sealOffIndex or 0 
+        myself.leadData.sealOffPositions = myself.leadData.sealOffPositions or DEC_Geometrie_ListePointsLocalisation(params.objective.source)
+        local number = math.floor( (#myself.leadData.sealOffPositions / meKnowledge.nbPionsMain ) +1 )
+
+        myself.leadData.sealOffIndex = ( myself.leadData.sealOffIndex + number ) % #myself.leadData.sealOffPositions
+        return CreateKnowledge( sword.military.world.Point, myself.leadData.sealOffPositions[myself.leadData.sealOffIndex] )
+    end,
+
     getMeetingPoint = function( self, params )
         return params.objective:getMyPosition()
     end,
