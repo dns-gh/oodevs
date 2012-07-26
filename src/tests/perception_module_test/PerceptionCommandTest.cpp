66d0a75ca529e15d9583cbeb44782c7c7bbca749
Silvin Lubecki <silvin.lubecki@masagroup.net>
2012-07-26 12:37:48 +0000

    [perception_module_test]: Refactoring

--- src/tests/perception_module_test/PerceptionCommandTest.cpp
+++ src/tests/perception_module_test/PerceptionCommandTest.cpp
@@ -8,12 +8,10 @@
 // *****************************************************************************
 
 #include "perception_module_test_pch.h"
-#include "MT_Tools/MT_Vector2D.h"
-#include <boost/bind/apply.hpp>
+#include "PerceptionFixture.h"
 
 using namespace sword::perception;
 
-class MIL_Agent_ABC;
 struct MIL_Report
 {
     enum E_EngineReport
@@ -24,139 +22,10 @@ struct MIL_Report
 
 namespace
 {
-    double ConvertSeconds( double seconds )
+    struct Fixture : public sword::perception::PerceptionFixture
     {
-        return seconds / 10.;
-    }
-    bool Assign( size_t* identifier )
-    {
-        *identifier = 0;
-        return true;
-    }
-    struct ConfigurationFixture : public ModuleFixture
-    {
-        ConfigurationFixture()
+        Fixture()
         {
-            const std::string xml( "<sensors>"
-                                   "  <alat-monitoring-times>"
-                                   "    <alat-monitoring-time terrain='Vide' time='1s'/>"
-                                   "    <alat-monitoring-time terrain='Foret' time='5s'/>"
-                                   "    <alat-monitoring-time terrain='Urbain' time='20s'/>"
-                                   "  </alat-monitoring-times>"
-                                   "  <radars>"
-                                   "    <radar action-range='60000' name='my-radar' type='radar'>"
-                                   "      <detectable-activities>"
-                                   "        <detectable-activity type='activity' value='true'/>"
-                                   "      </detectable-activities>"
-                                   "      <acquisition-times>"
-                                   "        <acquisition-time base-time='1m' level='detection'/>"
-                                   "      </acquisition-times>"
-                                   "    </radar>"
-                                   "  </radars>"
-                                   "  <sensors>"
-                                   "    <sensor detection-delay='0h' name='sensor-type'>"
-                                   "      <unit-detection angle='120' firer-detection-distance='0' scanning='true'>"
-                                   "        <base-distances close-range='100'>"
-                                   "          <base-distance distance='300' level='identification'/>"
-                                   "          <base-distance distance='1500' level='recognition'/>"
-                                   "          <base-distance distance='4000' level='detection'/>"
-                                   "        </base-distances>"
-                                   "        <distance-modifiers>"
-                                   "          <population-modifier density='0.5' modifier='0.1'/>"
-                                   "          <size-modifiers>"
-                                   "            <distance-modifier type='volume' value='1'/>"
-                                   "          </size-modifiers>"
-                                   "          <precipitation-modifiers>"
-                                   "            <distance-modifier type='precipitation' value='1'/>"
-                                   "          </precipitation-modifiers>"
-                                   "          <visibility-modifiers>"
-                                   "            <distance-modifier type='lighting' value='1'/>"
-                                   "          </visibility-modifiers>"
-                                   "          <source-posture-modifiers>"
-                                   "            <distance-modifier type='posture' value='1'/>"
-                                   "          </source-posture-modifiers>"
-                                   "          <target-posture-modifiers>"
-                                   "            <distance-modifier type='posture' value='1'/>"
-                                   "          </target-posture-modifiers>"
-                                   "          <terrain-modifiers/>"
-                                   "          <urbanBlock-material-modifiers>"
-                                   "            <distance-modifier type='urban' value='1'/>"
-                                   "          </urbanBlock-material-modifiers>"
-                                   "        </distance-modifiers>"
-                                   "      </unit-detection>"
-                                   "      <object-detection>"
-                                   "        <object detection-distance='300' type='object'>"
-                                   "          <population-modifier density='2' modifier='0.5'/>"
-                                   "          <source-posture-modifiers>"
-                                   "            <distance-modifier type='posture' value='1'/>"
-                                   "          </source-posture-modifiers>"
-                                   "        </object>"
-                                   "      </object-detection>"
-                                   "    </sensor>"
-                                   "  </sensors>"
-                                   "</sensors>" );
-            MOCK_EXPECT( GetConsumptionTypeSize ).returns( 1 );
-            MOCK_EXPECT( FindConsumptionType ).with( std::string( "activity" ), mock::any, mock::any ).calls( boost::bind( boost::apply< void >(), _2, 0, _3 ) );
-            MOCK_EXPECT( ConvertSecondsToSim ).calls( boost::bind( &ConvertSeconds, _1 ) );
-            MOCK_EXPECT( GetVolumeSize ).returns( 1 );
-            MOCK_EXPECT( GetVolumeIdentifier ).with( std::string( "volume" ), mock::any ).calls( boost::bind( &Assign, _2 ) );
-            MOCK_EXPECT( GetPrecipitationSize ).returns( 1 );
-            MOCK_EXPECT( GetPrecipitationIdentifier ).with( std::string( "precipitation" ), mock::any ).calls( boost::bind( &Assign, _2 ) );
-            MOCK_EXPECT( GetLightingSize ).returns( 1 );
-            MOCK_EXPECT( GetLightingIdentifier ).with( std::string( "lighting" ), mock::any ).calls( boost::bind( &Assign, _2 ) );
-            MOCK_EXPECT( GetPostureSize ).returns( 1 );
-            MOCK_EXPECT( GetPostureIdentifier ).with( std::string( "posture" ), mock::any ).calls( boost::bind( &Assign, _2 ) );
-            MOCK_EXPECT( PostureCanModifyDetection ).returns( true );
-            MOCK_EXPECT( GetMaterialTypeSize ).returns( 1 );
-            MOCK_EXPECT( IsMaterialType ).with( std::string( "urban" ) ).returns( true );
-            MOCK_EXPECT( FindObjectType ).with( std::string( "object" ) ).returns( 0 );
-            InitializePerceptionTypes( xml.c_str() );
-        }
-    };
-    struct PerceptionFixture : public ConfigurationFixture
-    {
-        PerceptionFixture()
-            : identifier           ( 42 )
-            , maxPerceptionDistance( 100 )
-            , theoreticalDistance  ( 120 )
-            , position             ( 0, 1 )
-            , direction            ( 1, 0 )
-            , agent                ( reinterpret_cast< MIL_Agent_ABC* >( 1337 ) )
-            , entity               ( model[ "entities" ][ identifier ] )
-        {
-            model[ "step" ] = 0;
-            entity[ "identifier" ] = identifier;
-            entity[ "is-dead" ] = false;
-            entity[ "is-underground" ] = false;
-            entity[ "is-transported" ] = false;
-            entity[ "is-prisoner" ] = false;
-            entity[ "pion" ].SetUserData( agent );
-            entity[ "perceptions/max-agent-perception-distance" ] = maxPerceptionDistance;
-            entity[ "perceptions/max-theoretical-agent-perception-distance" ] = theoreticalDistance;
-            entity[ "perceptions/peripherical-vision/next-step" ] = 1;
-            entity[ "perceptions/peripherical-vision/activated" ] = false;
-            entity[ "perceptions/vision/mode" ] = "normal";
-            entity[ "perceptions/notifications/agents" ];
-            entity[ "perceptions/notifications/objects" ];
-            entity[ "perceptions/notifications/population-flows" ];
-            entity[ "perceptions/notifications/population-concentrations" ];
-            entity[ "perceptions/notifications/urban-blocks" ];
-            entity[ "movement/position/x" ] = position.rX_;
-            entity[ "movement/position/y" ] = position.rY_;
-            entity[ "movement/direction/x" ] = direction.rX_;
-            entity[ "movement/direction/y" ] = direction.rY_;
-            entity[ "movement/height" ] = 0;
-            entity[ "perceptions/sensor/activated" ] = true;
-            entity[ "perceptions/scan/activated" ] = false;
-            entity[ "perceptions/alat/reco" ];
-            entity[ "perceptions/alat/monitoring" ];
-            entity[ "perceptions/record-mode/activated" ] = false;
-            core::Model& component = entity[ "components"].AddElement();
-            component[ "type/sensor-rotation-angle" ] = 3;
-            core::Model& sensor = component[ "sensors" ].AddElement();
-            sensor[ "height" ] = 0;
-            sensor[ "type" ] = "sensor-type";
-            component[ "radars" ].AddElement()[ "type" ] = "my-radar";
             MOCK_EXPECT( IsInCity ).returns( false );
             MOCK_EXPECT( GetUrbanObjectListWithinCircle );
             MOCK_EXPECT( AppendAddedKnowledge );
@@ -201,17 +70,10 @@ namespace
         {
             ExpectNotifications( T_Nofitications() );
         }
-        const size_t identifier;
-        const double maxPerceptionDistance;
-        const double theoreticalDistance;
-        const MT_Vector2D position;
-        const MT_Vector2D direction;
-        const MIL_Agent_ABC* agent;
-        core::Model& entity;
     };
 }
 
-BOOST_FIXTURE_TEST_CASE( perception_command_identifies_at_least_itself, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( perception_command_identifies_at_least_itself, Fixture )
 {
     ExpectEffect( entity[ "perceptions/notifications/objects" ], sword::test::MakeModel() );
     ExpectEffect( entity[ "perceptions/notifications/population-flows" ], sword::test::MakeModel() );
@@ -225,7 +87,7 @@ BOOST_FIXTURE_TEST_CASE( perception_command_identifies_at_least_itself, Percepti
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( perception_command_identifies_its_transporter, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( perception_command_identifies_its_transporter, Fixture )
 {
     const size_t transporterId = 1342;
     const SWORD_Model* transporter = core::Convert( &model[ "entities" ][ transporterId ] );
@@ -242,7 +104,7 @@ BOOST_FIXTURE_TEST_CASE( perception_command_identifies_its_transporter, Percepti
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( urban_objects_in_list_are_identified, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( urban_objects_in_list_are_identified, Fixture )
 {
     UrbanObjectWrapper* urbanObject = reinterpret_cast< UrbanObjectWrapper* >( 42 );
     MOCK_RESET( GetUrbanObjectListWithinCircle );
@@ -255,7 +117,7 @@ BOOST_FIXTURE_TEST_CASE( urban_objects_in_list_are_identified, PerceptionFixture
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( agents_in_list_are_identified_with_default_sensor, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( agents_in_list_are_identified_with_default_sensor, Fixture )
 {
     const SWORD_Model* other = core::Convert( &model[ "entities/other" ] );
     model[ "entities/other/pion" ].SetUserData< MIL_Agent_ABC* >( reinterpret_cast< MIL_Agent_ABC* >( 43 ) );
@@ -288,7 +150,7 @@ BOOST_FIXTURE_TEST_CASE( agents_in_list_are_identified_with_default_sensor, Perc
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( agents_in_list_are_recognized_with_scan_sensor, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( agents_in_list_are_recognized_with_scan_sensor, Fixture )
 {
     const SWORD_Model* other = core::Convert( &model[ "entities/other" ] );
     model[ "entities/other/pion" ].SetUserData< MIL_Agent_ABC* >( reinterpret_cast< MIL_Agent_ABC* >( 43 ) );
@@ -317,7 +179,7 @@ BOOST_FIXTURE_TEST_CASE( agents_in_list_are_recognized_with_scan_sensor, Percept
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( objects_in_list_are_identified_with_default_sensor, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( objects_in_list_are_identified_with_default_sensor, Fixture )
 {
     MIL_Object_ABC* object = reinterpret_cast< MIL_Object_ABC* >( 42 );
     const SWORD_Model* perceiver = core::Convert( &entity );
@@ -346,7 +208,7 @@ namespace
     }
 }
 
-BOOST_FIXTURE_TEST_CASE( population_flows_in_list_are_identified_with_default_sensor, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( population_flows_in_list_are_identified_with_default_sensor, Fixture )
 {
     const std::vector< MT_Vector2D > shape = boost::assign::list_of( MT_Vector2D( 0., 0. ) )
                                                                    ( MT_Vector2D( 1., 1. ) )
@@ -372,7 +234,7 @@ BOOST_FIXTURE_TEST_CASE( population_flows_in_list_are_identified_with_default_se
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( population_concentrations_in_list_are_identified_with_default_sensor, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( population_concentrations_in_list_are_identified_with_default_sensor, Fixture )
 {
     MIL_PopulationConcentration* concentration = reinterpret_cast< MIL_PopulationConcentration* >( 42 );
     const SWORD_Model* perceiver = core::Convert( &entity );
@@ -392,7 +254,7 @@ BOOST_FIXTURE_TEST_CASE( population_concentrations_in_list_are_identified_with_d
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( next_peripherical_vision_step_is_updated_with_effect, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( next_peripherical_vision_step_is_updated_with_effect, Fixture )
 {
     ExpectNotifications();
     commands.Post( "perception", core::MakeModel( "identifier", identifier ) );
@@ -409,7 +271,7 @@ BOOST_FIXTURE_TEST_CASE( next_peripherical_vision_step_is_updated_with_effect, P
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( is_point_visible_hook_computes_visibility_between_entity_and_point_depending_raytrace, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( is_point_visible_hook_computes_visibility_between_entity_and_point_depending_raytrace, Fixture )
 {
     MOCK_RESET( CreateEffect );
     MOCK_RESET( PostEffect );
@@ -426,7 +288,7 @@ BOOST_FIXTURE_TEST_CASE( is_point_visible_hook_computes_visibility_between_entit
     }
 }
 
-BOOST_FIXTURE_TEST_CASE( direction_vision_mode_uses_vision_direction_as_main_perception_direction, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( direction_vision_mode_uses_vision_direction_as_main_perception_direction, Fixture )
 {
     ExpectNotifications();
     commands.Post( "perception", core::MakeModel( "identifier", identifier ) );
@@ -446,7 +308,7 @@ BOOST_FIXTURE_TEST_CASE( direction_vision_mode_uses_vision_direction_as_main_per
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( location_vision_mode_computes_location_direction_as_main_perception_direction, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( location_vision_mode_computes_location_direction_as_main_perception_direction, Fixture )
 {
     ExpectNotifications();
     commands.Post( "perception", core::MakeModel( "identifier", identifier ) );
@@ -466,7 +328,7 @@ BOOST_FIXTURE_TEST_CASE( location_vision_mode_computes_location_direction_as_mai
     commands.Execute();
 }
 
-BOOST_FIXTURE_TEST_CASE( agent_has_radar_hook_checks_if_one_component_has_radar_type, ConfigurationFixture )
+BOOST_FIXTURE_TEST_CASE( agent_has_radar_hook_checks_if_one_component_has_radar_type, sword::perception::ConfigurationFixture )
 {
     {
         core::Model entity;
@@ -482,7 +344,7 @@ BOOST_FIXTURE_TEST_CASE( agent_has_radar_hook_checks_if_one_component_has_radar_
     }
 }
 
-BOOST_FIXTURE_TEST_CASE( get_perception_hook_between_two_points_returns_max_perception_energy, ConfigurationFixture )
+BOOST_FIXTURE_TEST_CASE( get_perception_hook_between_two_points_returns_max_perception_energy, sword::perception::ConfigurationFixture )
 {
     core::Model entity;
     {
@@ -504,7 +366,7 @@ BOOST_FIXTURE_TEST_CASE( get_perception_hook_between_two_points_returns_max_perc
     BOOST_CHECK_EQUAL( 43., GetPerception( core::Convert( &entity ), &point, &target ) );
 }
 
-BOOST_FIXTURE_TEST_CASE( compute_knowledge_object_perception_hook_returns_perception_level, PerceptionFixture )
+BOOST_FIXTURE_TEST_CASE( compute_knowledge_object_perception_hook_returns_perception_level, Fixture )
 {
     MOCK_RESET( CreateEffect );
     MOCK_RESET( PostEffect );
---
+++ src/tests/perception_module_test/PerceptionFixture.h
@@ -0,0 +1,78 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2012 MASA Group
+//
+// *****************************************************************************
+
+#ifndef PERCEPTION_FIXTURE_H
+#define PERCEPTION_FIXTURE_H
+
+#include "ConfigurationFixture.h"
+#include "MT_Tools/MT_Vector2D.h"
+
+class MIL_Agent_ABC;
+
+namespace sword
+{
+namespace perception
+{
+    struct PerceptionFixture : public ConfigurationFixture
+    {
+        PerceptionFixture()
+            : identifier           ( 42 )
+            , maxPerceptionDistance( 100 )
+            , theoreticalDistance  ( 120 )
+            , position             ( 0, 1 )
+            , direction            ( 1, 0 )
+            , agent                ( reinterpret_cast< MIL_Agent_ABC* >( 1337 ) )
+            , entity               ( model[ "entities" ][ identifier ] )
+        {
+            model[ "step" ] = 0;
+            entity[ "identifier" ] = identifier;
+            entity[ "is-dead" ] = false;
+            entity[ "is-underground" ] = false;
+            entity[ "is-transported" ] = false;
+            entity[ "is-prisoner" ] = false;
+            entity[ "pion" ].SetUserData( agent );
+            entity[ "perceptions/max-agent-perception-distance" ] = maxPerceptionDistance;
+            entity[ "perceptions/max-theoretical-agent-perception-distance" ] = theoreticalDistance;
+            entity[ "perceptions/peripherical-vision/next-step" ] = 1;
+            entity[ "perceptions/peripherical-vision/activated" ] = false;
+            entity[ "perceptions/vision/mode" ] = "normal";
+            entity[ "perceptions/notifications/agents" ];
+            entity[ "perceptions/notifications/objects" ];
+            entity[ "perceptions/notifications/population-flows" ];
+            entity[ "perceptions/notifications/population-concentrations" ];
+            entity[ "perceptions/notifications/urban-blocks" ];
+            entity[ "movement/position/x" ] = position.rX_;
+            entity[ "movement/position/y" ] = position.rY_;
+            entity[ "movement/direction/x" ] = direction.rX_;
+            entity[ "movement/direction/y" ] = direction.rY_;
+            entity[ "movement/height" ] = 0;
+            entity[ "perceptions/sensor/activated" ] = true;
+            entity[ "perceptions/scan/activated" ] = false;
+            entity[ "perceptions/alat/reco" ];
+            entity[ "perceptions/alat/monitoring" ];
+            entity[ "perceptions/record-mode/activated" ] = false;
+            core::Model& component = entity[ "components"].AddElement();
+            component[ "type/sensor-rotation-angle" ] = 3;
+            core::Model& sensor = component[ "sensors" ].AddElement();
+            sensor[ "height" ] = 0;
+            sensor[ "type" ] = "sensor-type";
+            component[ "radars" ].AddElement()[ "type" ] = "my-radar";
+        }
+        const size_t identifier;
+        const double maxPerceptionDistance;
+        const double theoreticalDistance;
+        const MT_Vector2D position;
+        const MT_Vector2D direction;
+        const MIL_Agent_ABC* agent;
+        core::Model& entity;
+    };
+}
+}
+
+#endif // PERCEPTION_FIXTURE_H
