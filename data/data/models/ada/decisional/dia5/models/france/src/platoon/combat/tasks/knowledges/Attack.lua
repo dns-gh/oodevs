4263da3ba97b85d5cc817be7bad5038d44856f59
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-07-27 15:13:52 +0000

    [ MILITARY ][ DEC ] - moving Attack mission  (for commander and agents) from Military to fr and ww

---
+++ data/data/models/ada/decisional/dia5/models/france/src/platoon/combat/tasks/knowledges/Attack.lua
@@ -0,0 +1,21 @@
+local result = 
+{
+    getPionEfficiency = function( self )
+        return ePionEfficiencyCombat
+    end,
+
+    fillParameters = function( self, companyTask, params, entity )
+        local fuseau = companyTask:getReachable( params )
+        if fuseau then
+          local point = CreateKnowledge( world.Point, DEC_Geometrie_CalculerPointArriveePourFuseau(fuseau.source))
+          DEC_Trace( "Hey!! "..tostringr( point ) )
+          return { objective = point, fuseau = fuseau }
+        end
+        return nil --use by keepBest in Lead skill
+    end,
+}
+
+local t = initTaskKnowledge( result )
+taskKnowledge["platoon.combat.tasks.knowledges.Attack"] = t
+
+return result
