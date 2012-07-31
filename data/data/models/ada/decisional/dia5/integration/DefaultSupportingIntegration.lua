a780fabbd60ba37a85a5dfea9f2477bd92b8f516
Grégory Gougé <gregory.gouge@masagroup.net>
2012-07-31 15:44:16 +0000

    [Decisional] cleaning, déplacement de fonction

--- data/data/models/ada/decisional/dia5/integration/DefaultSupportingIntegration.lua
+++ data/data/models/ada/decisional/dia5/integration/DefaultSupportingIntegration.lua
@@ -12,3 +12,19 @@
 integration.isInMiddleFiringRange = function( target )
     return integration.distance( meKnowledge, target ) < DEC_Tir_PorteeMaxPourTirer( 0.7 )/2
 end
+
+integration.getPositionToSupportFriend = function( friendToSupport )
+    local position
+    local rangeDistance = DEC_Tir_PorteeMaxTirIndirectSansChoisirMunition() / 2  -- indirect fire case
+    if rangeDistance <= 0 then -- direct fire case
+        rangeDistance = DEC_Tir_PorteeMaxPourTirer( 0.7 ) / 2
+    end
+    if DEC_HasMission( meKnowledge.source ) then
+        local mission = DEC_GetRawMission( meKnowledge.source )
+        local dir = DEC_GetDirectionDanger( mission )
+        local friendPos = friendToSupport.source:DEC_Agent_Position()
+        local positionToSupport = DEC_Geometrie_PositionTranslateDir( friendPos, dir, - rangeDistance )
+        return CreateKnowledge( sword.military.world.Point, positionToSupport )
+    end
+    return nil
+end
