6e96ba4aaf6caaac6f9b8490c3f5bf7d3c7d29cc
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-08-03 08:10:41 +0000

    Cosmetics

--- host_agent/src/libraries/web/SessionConfig.cpp
+++ host_agent/src/libraries/web/SessionConfig.cpp
@@ -71,10 +71,7 @@ std::string ConvertRngDistribution( RngDistribution src )
     if( src == RNG_DISTRIBUTION_GAUSSIAN ) return "gaussian";
     return "invalid";
 }
-}
 
-namespace
-{
 // -----------------------------------------------------------------------------
 // Name: TryRead
 // Created: BAX 2012-08-02
@@ -117,17 +114,17 @@ Config web::session::GetConfig( const Request_ABC& request )
     TryRead( cfg.checkpoints.enabled, request, "checkpoints_enabled" );
     TryRead( cfg.checkpoints.frequency, request, "checkpoints_frequency" );
     TryRead( cfg.checkpoints.keep, request, "checkpoints_keep" );
-    TryRead( cfg.pathfind.threads, request, "pathfind_threads" );
-    TryRead( cfg.recorder.frequency, request, "recorder_frequency" );
-    TryRead( cfg.rng.seed, request, "rng_seed" );
-    cfg.rng.breakdown = GetRngConfig( request, "rng_breakdown_" );
-    cfg.rng.fire = GetRngConfig( request, "rng_fire_" );
-    cfg.rng.perception = GetRngConfig( request, "rng_perception_" );
-    cfg.rng.wound = GetRngConfig( request, "rng_wound_" );
     TryRead( cfg.time.end_tick, request, "time_end_tick" );
     TryRead( cfg.time.factor, request, "time_factory" );
     TryRead( cfg.time.paused, request, "time_paused" );
     TryRead( cfg.time.step, request, "time_step" );
+    TryRead( cfg.pathfind.threads, request, "pathfind_threads" );
+    TryRead( cfg.recorder.frequency, request, "recorder_frequency" );
+    TryRead( cfg.rng.seed, request, "rng_seed" );
+    cfg.rng.breakdown  = GetRngConfig( request, "rng_breakdown_" );
+    cfg.rng.fire       = GetRngConfig( request, "rng_fire_" );
+    cfg.rng.perception = GetRngConfig( request, "rng_perception_" );
+    cfg.rng.wound      = GetRngConfig( request, "rng_wound_" );
     return cfg;
 }
 
@@ -172,13 +169,13 @@ Tree web::session::ConvertConfig( const Request_ABC& request )
     TryPut( dst, request, "time.factor", "time_factor" );
     TryPut( dst, request, "time.paused", "time_paused" );
     TryPut( dst, request, "time.step", "time_step" );
+    TryPut( dst, request, "pathfind.threads", "pathfind_threads" );
+    TryPut( dst, request, "recorder.frequency", "recorder_frequency" );
     TryPut( dst, request, "rng.seed", "rng_seed" );
     ConvertRngConfig( dst, request, "rng.breakdown.", "rng_breakdown_" );
     ConvertRngConfig( dst, request, "rng.fire.", "rng_fire_" );
     ConvertRngConfig( dst, request, "rng.perception.", "rng_perception_" );
     ConvertRngConfig( dst, request, "rng.wound.", "rng_wound_" );
-    TryPut( dst, request, "pathfind.threads", "pathfind_threads" );
-    TryPut( dst, request, "recorder.frequency", "recorder_frequency" );
     return dst;
 }
 
@@ -191,11 +188,11 @@ namespace
 bool ReadRngConfig( RngConfig& dst, const Tree& src, const std::string& prefix )
 {
     std::string dist;
-    bool modified = ::TryRead( dist, src, prefix + ".distribution" );
+    bool modified = ::TryRead( dist, src, prefix + "distribution" );
     if( modified )
         dst.distribution = ConvertRngDistribution( dist );
-    modified |= ::TryRead( dst.deviation, src, prefix + ".deviation" );
-    modified |= ::TryRead( dst.mean, src, prefix + ".mean" );
+    modified |= ::TryRead( dst.deviation, src, prefix + "deviation" );
+    modified |= ::TryRead( dst.mean, src, prefix + "mean" );
     return modified;
 }
 
@@ -205,9 +202,9 @@ bool ReadRngConfig( RngConfig& dst, const Tree& src, const std::string& prefix )
 // -----------------------------------------------------------------------------
 void WriteRngConfig( Tree& dst, const std::string& prefix, const RngConfig& cfg )
 {
-    dst.put( prefix + ".distribution", ConvertRngDistribution( cfg.distribution ) );
-    dst.put( prefix + ".deviation", cfg.deviation );
-    dst.put( prefix + ".mean", cfg.mean );
+    dst.put( prefix + "distribution", ConvertRngDistribution( cfg.distribution ) );
+    dst.put( prefix + "deviation", cfg.deviation );
+    dst.put( prefix + "mean", cfg.mean );
 }
 }
 
@@ -226,13 +223,13 @@ bool web::session::ReadConfig( Config& dst, const Tree& src )
     modified |= TryRead( dst.time.factor, src, "time.factor" );
     modified |= TryRead( dst.time.paused, src, "time.paused" );
     modified |= TryRead( dst.time.step, src, "time.step" );
-    modified |= TryRead( dst.rng.seed, src, "rng.seed" );
-    modified |= ReadRngConfig( dst.rng.breakdown, src, "rng.breakdown" );
-    modified |= ReadRngConfig( dst.rng.fire, src, "rng.fire" );
-    modified |= ReadRngConfig( dst.rng.perception, src, "rng.perception" );
-    modified |= ReadRngConfig( dst.rng.wound, src, "rng.wound" );
     modified |= TryRead( dst.pathfind.threads, src, "pathfind.threads" );
     modified |= TryRead( dst.recorder.frequency, src, "recorder.frequency" );
+    modified |= TryRead( dst.rng.seed, src, "rng.seed" );
+    modified |= ReadRngConfig( dst.rng.breakdown, src, "rng.breakdown." );
+    modified |= ReadRngConfig( dst.rng.fire, src, "rng.fire." );
+    modified |= ReadRngConfig( dst.rng.perception, src, "rng.perception." );
+    modified |= ReadRngConfig( dst.rng.wound, src, "rng.wound." );
     return modified;
 }
 
@@ -250,11 +247,11 @@ void web::session::WriteConfig( Tree& dst, const Config& cfg )
     dst.put( "time.factor", cfg.time.factor );
     dst.put( "time.paused", cfg.time.paused );
     dst.put( "time.step", cfg.time.step );
-    dst.put( "rng.seed", cfg.rng.seed );
-    WriteRngConfig( dst, "rng.breakdown", cfg.rng.breakdown );
-    WriteRngConfig( dst, "rng.fire", cfg.rng.fire );
-    WriteRngConfig( dst, "rng.perception", cfg.rng.perception );
-    WriteRngConfig( dst, "rng.wound", cfg.rng.wound );
     dst.put( "pathfind.threads", cfg.pathfind.threads );
     dst.put( "recorder.frequency", cfg.recorder.frequency );
+    dst.put( "rng.seed", cfg.rng.seed );
+    WriteRngConfig( dst, "rng.breakdown.", cfg.rng.breakdown );
+    WriteRngConfig( dst, "rng.fire.", cfg.rng.fire );
+    WriteRngConfig( dst, "rng.perception.", cfg.rng.perception );
+    WriteRngConfig( dst, "rng.wound.", cfg.rng.wound );
 }
