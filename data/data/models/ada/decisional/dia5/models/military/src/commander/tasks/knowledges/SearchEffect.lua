20301970b9ddb5943370e6cd643ff4c46e63bee2
Lucie Mignot <lucie.mignot@masagroup.net>
2012-08-06 16:16:28 +0000

    [Decisionnel] Jira 8671 : Scipio 345153 - La mission Ratisser ne fonctionne pas correctement; merge sword-scipio-stab-32038

--- data/data/models/ada/decisional/dia5/models/military/src/commander/tasks/knowledges/SearchEffect.lua
+++ data/data/models/ada/decisional/dia5/models/military/src/commander/tasks/knowledges/SearchEffect.lua
@@ -34,6 +34,19 @@ return
         return params.objective
     end,
     
+    getSealOffPosition = function( self, params )
+        myself.leadData.sealOffIndex = myself.leadData.sealOffIndex or 0 
+        if masalife.brain.core.class.isOfType( params.objective, world.UrbanBlock) then
+            myself.leadData.sealOffPositions = myself.leadData.sealOffPositions or DEC_Geometrie_ListePointsLocalisation(DEC_PolygoneBlocUrbain(params.objective.source))
+        else
+            myself.leadData.sealOffPositions = myself.leadData.sealOffPositions or DEC_Geometrie_ListePointsLocalisation(params.objective.source)
+        end
+        local number = math.floor( (#myself.leadData.sealOffPositions / meKnowledge.nbPionsMain ) +1 )
+
+        myself.leadData.sealOffIndex = ( myself.leadData.sealOffIndex + number ) % #myself.leadData.sealOffPositions + 1
+        return CreateKnowledge( world.Point, myself.leadData.sealOffPositions[myself.leadData.sealOffIndex] )
+    end,
+    
     getTotalRecce = function( self )
         return true
     end,
