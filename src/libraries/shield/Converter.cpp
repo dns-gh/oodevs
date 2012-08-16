1645907730e8c6790e84a71fa2c9e2e07c06ab3b
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2012-08-16 08:19:32 +0000

    [shield]Fix JIRA #8705 Scipio - 345183 - La mission Boucler ne créer pas de zone bouclée

--- src/libraries/shield/Converter.cpp
+++ src/libraries/shield/Converter.cpp
@@ -222,7 +222,7 @@ void Converter::ReceiveSimToClient( const sword::SimToClient& msg )
         client_.Send( out );
         return;
     }
-	if ( msg.message().has_object_creation() && msg.message().object_creation().type().id() !=  "seal off area" && msg.message().object_creation().type().id() !=  "controle de zone" )
+	if ( msg.message().has_object_creation() )
 	{
 		FORWARD( client_, SimulationToClient, object_creation )
 	}
