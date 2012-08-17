e1ee7edff932918a8067477667779e6ab8d4fa53
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2012-08-17 16:28:47 +0000

    [adaptation]Fix JIRA #8733 SCIPIO - Crash de l'outil adaptation lorsqu'on sélectionne un item alors qu'on éditait une valeur

--- src/applications/adaptation_app/ADN_Schedule_Table.cpp
+++ src/applications/adaptation_app/ADN_Schedule_Table.cpp
@@ -168,7 +168,6 @@ namespace
 // -----------------------------------------------------------------------------
 void ADN_Schedule_Table::OnPeopleChanged( void* pData )
 {
-    QWidget* focused = qApp->focusWidget();
     for( int i = numRows() - 1; i >= 0; --i )
         removeRow( i );
     pCurData_ = pData;
@@ -182,8 +181,6 @@ void ADN_Schedule_Table::OnPeopleChanged( void* pData )
             AddRow( it->first, ConvertDay( it->second->day_.GetData() ), it->second->from_.GetData(), it->second->to_.GetData(),
                     it->second->motivation_.GetData() );
     }
-    if( focused )
-        focused->setFocus();
 }
 
 // -----------------------------------------------------------------------------
