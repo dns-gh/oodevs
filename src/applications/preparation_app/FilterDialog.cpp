58b807aaf46738bd0f3e602da0ed6d74d9b3f47c
Martin Milenkovic <martin.milenkovic@masagroup.net>
2012-08-20 15:16:58 +0000

    [prepartion] fix jira #8587 "Scipio 370414 - VQ-201207051503 Fenetre import reste bloqué sur import rouge PoneyExpress"

--- src/applications/preparation_app/FilterDialog.cpp
+++ src/applications/preparation_app/FilterDialog.cpp
@@ -128,7 +128,15 @@ void FilterDialog::OnAccept()
     }
     catch( std::exception& e )
     {
+        setEnabled( true );
+        reject();
         QMessageBox::critical( this, tools::translate( "FilterDialog", "Error on filter execution" ), e.what() );
+    }
+    catch( ... )
+    {
+        setEnabled( true );
+        reject();
+        QMessageBox::critical( this, tools::translate( "FilterDialog", "Error on filter execution" ), "Unexpected exception" );
     }
 }
 
