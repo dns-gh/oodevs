34b99629783416288ffb6dea52cca711b3a145f7
Grégory Gougé <gregory.gouge@masagroup.net>
2012-08-30 10:16:11 +0000

    [Decisional] Fix Jira SWBUG-8795 - Scipio 345253 - Mission Impossible - Appuyer franchissement sur zone

--- data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/support/circu/tasks/knowledges/AppuyerFranchissementDansZoneEffect.lua
+++ data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/support/circu/tasks/knowledges/AppuyerFranchissementDansZoneEffect.lua
@@ -1,8 +1,15 @@
 return
 {
+    init = function( self, params )
+       self.obstacles = integration.query.getSiteFranchissementDansZone( myself.taskParams.zone )
+       if #self.obstacles < 1 then
+          meKnowledge:RC( eRC_MissionImpossibleReason, "Mission impossible : pas de site de franchissement dans la zone" )
+          myself.leadData.taskError = true
+       end
+    end,
 
     getPosition = function( self, params )
-       self.obstacles = self.obstacles or integration.query.getSiteFranchissementDansZone( params.zone )
+       self.obstacles = integration.query.getSiteFranchissementDansZone( params.zone )
        if not myself.leadData.objectiveIndex then
             myself.leadData.objectiveIndex = 0 
        end
