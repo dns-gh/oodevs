4909d539e4d0fa67619b7462e607bc2d0ae8183a
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-08-30 15:35:08 +0000

    [ MILITARY ][ DEC ][ PHY ] - Add FRAGO for WW bases (dec and phy).

--- data/data/models/ada/decisional/dia5/models/worldwide/src/agent/tasks/knowledges/Disengage.lua
+++ data/data/models/ada/decisional/dia5/models/worldwide/src/agent/tasks/knowledges/Disengage.lua
@@ -10,6 +10,6 @@ local result =
 }
 
 local t = initTaskKnowledge( result )
-taskKnowledge["agent.tasks.knowledges.Disengage"] = t
+taskKnowledge[ "agent.tasks.knowledges.Disengage" ] = t
 
 return result
