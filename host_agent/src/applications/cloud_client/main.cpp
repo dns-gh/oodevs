b6bff17f9ec42c9561f44d2f7ba2e3349bd7808f
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-09-05 09:33:42 +0000

    [client] Add command-line arguments, implement register/unregister & pave the way for run & display

--- host_agent/src/applications/cloud_client/main.cpp
+++ host_agent/src/applications/cloud_client/main.cpp
@@ -22,6 +22,12 @@ void RegisterProtocolHandler( const QString& protocol, const QString& cmd )
     QSettings open( "HKEY_CLASSES_ROOT\\" + protocol + "\\shell\\open\\command", QSettings::NativeFormat );
     open.setValue( ".", cmd );
 }
+
+void UnregisterProtocolHandler( const QString& protocol )
+{
+    QSettings opt( "HKEY_CLASSES_ROOT\\" + protocol, QSettings::NativeFormat );
+    opt.remove( "" );
+}
 }
 
 struct QtLog : public cpplog::BaseLogger
@@ -33,19 +39,28 @@ struct QtLog : public cpplog::BaseLogger
     }
 };
 
+enum Command
+{
+    CMD_REGISTER,
+    CMD_UNREGISTER,
+    CMD_RUN,
+    CMD_DISPLAY,
+    CMD_COUNT,
+};
+
 struct Main : public QMainWindow
 {
     explicit Main( const runtime::Runtime_ABC& runtime )
         : QMainWindow( 0 )
         , runtime_   ( runtime )
+        , cmd_       ( CMD_DISPLAY )
+        , done_      ( false )
     {
         QCoreApplication::setOrganizationName( APP_COMPANY );
         QCoreApplication::setApplicationName( APP_NAME );
         ui_.setupUi( this );
-        QString app = QString::fromStdWString( runtime_.GetModuleFilename().wstring() );
-        QString cmd = QString( "\"%1\" \"%2\"" ).arg( app ).arg( "%1" );
-        RegisterProtocolHandler( "sword", cmd );
-        ui_.statusbar->showMessage( QCoreApplication::arguments().join( " " ) );
+        ParseArguments();
+        ProcessCommand();
     }
 
     virtual ~Main()
@@ -53,8 +68,79 @@ struct Main : public QMainWindow
         // NOTHING
     }
 
+    void ParseArguments()
+    {
+        const QStringList list = QCoreApplication::arguments();
+        for( QStringList::const_iterator it = list.begin() + 1; it != list.end(); ++it )
+            if( *it == "--root" && ++it != list.end() )
+            {
+                root_ = *it;
+            }
+            else if( *it == "--register" )
+            {
+                cmd_ = CMD_REGISTER;
+            }
+            else if( *it == "--unregister" )
+            {
+                cmd_ = CMD_UNREGISTER;
+            }
+            else if( *it == "--run" && ++it != list.end() )
+            {
+                cmd_ = CMD_RUN;
+                url_ = *it;
+            }
+            else
+            {
+                throw std::runtime_error( "unknown parameter " + it->toStdString() );
+            }
+    }
+
+    void ProcessCommand()
+    {
+        switch( cmd_ )
+        {
+            case CMD_REGISTER:
+                Register();
+                break;
+
+            case CMD_UNREGISTER:
+                Unregister();
+                break;
+
+            case CMD_RUN:
+                throw std::exception( "run not implemented" );
+                break;
+
+            case CMD_DISPLAY:
+                break;
+        }
+    }
+
+    void Register()
+    {
+        QString app = QString::fromStdWString( runtime_.GetModuleFilename().wstring() );
+        QString cmd = QString( "\"%1\" \"%2\"" ).arg( app ).arg( "%1" );
+        RegisterProtocolHandler( "sword", cmd );
+        done_ = true;
+    }
+
+    void Unregister()
+    {
+        UnregisterProtocolHandler( "sword" );
+        done_ = true;
+    }
+
+    bool IsDone() const
+    {
+        return done_;
+    }
+
     const runtime::Runtime_ABC& runtime_;
     Ui::Main ui_;
+    QFileInfo root_;
+    Command cmd_;
+    QString url_;
+    bool done_;
 };
 
 int main( int argc, char* argv[] )
@@ -69,6 +155,8 @@ int main( int argc, char* argv[] )
         QtLog log;
         runtime::Factory factory( log );
         Main gui( factory.GetRuntime() );
+        if( gui.IsDone() )
+            return 0;
         gui.show();
         return app.exec();
     }
