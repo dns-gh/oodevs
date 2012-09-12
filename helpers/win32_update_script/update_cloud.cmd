d57afd061a3231c6d3096a5dd7c09f60d70a1676
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-09-12 14:10:55 +0000

    [helpers] Add win32 script to automatically install/update sword cloud
    The license is missing on purpose, you need to generate one with lmcrypt

---
+++ helpers/win32_update_script/update_cloud.cmd
@@ -0,0 +1,11 @@
+@echo off
+net use \\install /USER:<USER> <PASSWORD> 1>NUL
+set FILE=sword_cloud_vc100_x64_setup.exe
+set HOME=c:\Users\Admin
+copy /B /V /Y "\\install\Masa\common\dev\delivery\internal\cloud\current\%FILE%" "%HOME%" 1>NUL
+"%HOME%\%FILE%" /S /D=%HOME%\sword_cloud
+net stop "sword cloud"
+copy /B /V /Y "%HOME%\cloud_server.config" "%HOME%\sword_cloud\bin"
+copy /B /V /Y "%HOME%\license.lic" "%HOME%\sword_cloud\bin"
+net start "sword cloud"
+exit
