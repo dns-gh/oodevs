e72f1a2f3c6a45a40e9c5552e73d2e931f2a32cd
Mathieu Igarza <mathieu.igarza@masagroup.net>
2012-09-13 16:09:35 +0000

    [ MILITARY ][ DEC ][ PHY ] - moving the "Bypass" mission (agent only) from Military to FR and WW

--- data/data/models/ada/decisional/dia5/models/military/src/queries/getPositionsToBypass.lua
+++ data/data/models/ada/decisional/dia5/models/military/src/queries/getPositionsToBypass.lua
@@ -5,7 +5,7 @@ queryImplementation "getPositionsToBypass"
         if not next( allRes ) then
             allRes = {}
             local point, objectPosition, scaledObject
-            for _, element in pairs ( params.elementsToBypass ) do     
+            for _, element in pairs ( params.elementsToBypass ) do
                 scaledObject = DEC_Geometrie_AgrandirLocalisation(  element:getLocalisation() , 50 )
                 objectPosition = DEC_Geometrie_ComputeNearestBorder( meKnowledge:getPosition(), scaledObject )
                 point = CreateKnowledge( world.Point, objectPosition )
