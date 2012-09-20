f03621823361514698e00165b1054ccfcfc300e6
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-09-20 20:41:44 +0000

    [simulation_kernel] removed warning

--- src/libraries/simulation_kernel/Entities/Orders/MIL_OrderManager_ABC.cpp
+++ src/libraries/simulation_kernel/Entities/Orders/MIL_OrderManager_ABC.cpp
@@ -61,7 +61,7 @@ void MIL_OrderManager_ABC::Update()
                     pController_->Start( pMission_ );
                 bNewMissionStarted_ = true;
             }
-            catch( std::exception& e )
+            catch( std::exception& )
             {
                 if( pController_ && pMission_.get() )
                     pController_->Stop( pMission_ );
