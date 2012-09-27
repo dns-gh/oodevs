6ad30ca481bfd490d78ce89d5d2241beacfd78f5
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-09-27 09:00:39 +0000

    [setup] Manually control when sword cloud service is stopped/started

--- helpers/win32_update_script/update_cloud.cmd
+++ helpers/win32_update_script/update_cloud.cmd
@@ -3,8 +3,8 @@ net use \\install /USER:<USER> <PASSWORD> 1>NUL
 set FILE=sword_cloud_vc100_x64_setup.exe
 set HOME=c:\Users\Admin
 copy /B /V /Y "\\install\Masa\common\dev\delivery\internal\cloud\current\%FILE%" "%HOME%" 1>NUL
-"%HOME%\%FILE%" /S /D=%HOME%\sword_cloud
 net stop "sword cloud"
+"%HOME%\%FILE%" /S /D=%HOME%\sword_cloud /DSKIP_SERVICE_CONTROL
 copy /B /V /Y "%HOME%\cloud_server.config" "%HOME%\sword_cloud\bin"
 copy /B /V /Y "%HOME%\license.lic" "%HOME%\sword_cloud\bin"
 net start "sword cloud"
