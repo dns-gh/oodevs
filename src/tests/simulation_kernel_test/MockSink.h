b3e4b3512d690a5c74109482d0783be64224c177
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-09-27 04:47:20 +0000

    [simulation_kernel] fixed missing functions in MockSink

--- src/tests/simulation_kernel_test/MockSink.h
+++ src/tests/simulation_kernel_test/MockSink.h
@@ -24,7 +24,9 @@ MOCK_BASE_CLASS( MockSink, sword::Sink_ABC )
     MOCK_METHOD( ExecutePerceptions, 0 );
     MOCK_METHOD( ExecuteCommands, 0 );
     MOCK_METHOD( ApplyEffects, 0 );
-    MOCK_METHOD( UpdateModel, 2 );
+    MOCK_METHOD( UpdateModel, 3 );
+    MOCK_METHOD( UpdateKnowledges, 0 );
+    MOCK_METHOD( UpdateUrbanModel, 1 );
 
     MOCK_METHOD_EXT( Create, 3, MIL_AgentPion*( const MIL_AgentTypePion&, MIL_Automate&, xml::xistream& ), CreateFromXml );
     MOCK_METHOD_EXT( Create, 3, MIL_AgentPion*( const MIL_AgentTypePion&, MIL_Automate&, const MT_Vector2D& ), CreateFromPoint );
