a26a0cb105a874913d5f91319f10cd34a9192bad
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-09-28 11:57:50 +0000

    [simulation_kernel] fixed C4996 warnings due to update to latest steel

--- src/libraries/simulation_kernel/simulation_kernel_pch.h
+++ src/libraries/simulation_kernel/simulation_kernel_pch.h
@@ -12,6 +12,10 @@
 #ifndef __MIL_pch_h_
 #define __MIL_pch_h_
 
+#pragma warning( disable: 4996 ) // 'function': was declared deprecated
+#pragma warning( disable: 4786 ) // identifier was truncated to '255' characters in the debug information
+#pragma warning( disable: 4355 ) // 'this' : used in base member initializer list
+
 #include <boost/serialization/extended_type_info.hpp> // $$$$ MCO : see http://old.nabble.com/-Serialization--extended_type_info.hpp-to27448440.html
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
@@ -29,9 +33,5 @@
 
 //$$$ DEGUEU
 #include "MT_Tools/MT_Assert.h"
-
-#pragma warning( disable: 4996 ) // 'function': was declared deprecated
-#pragma warning( disable: 4786 ) // identifier was truncated to '255' characters in the debug information
-#pragma warning( disable: 4355 ) // 'this' : used in base member initializer list
 
 #endif // __MIL_pch_h_
