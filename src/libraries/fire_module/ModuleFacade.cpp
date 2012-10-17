188543c8cc2ce000a5d122bec49cd53f5d2fa002
Silvin Lubecki <silvin.lubecki@masagroup.net>
2012-10-17 15:05:14 +0000

    [fire_module]: adding try/catch arround hooks

--- src/libraries/fire_module/ModuleFacade.cpp
+++ src/libraries/fire_module/ModuleFacade.cpp
@@ -16,7 +16,6 @@
 #include <module_api/Log.h>
 #include <xeumeuleu/xml.hpp>
 #include <boost/bind.hpp>
-#include <boost/bind.hpp>
 
 using namespace sword;
 using namespace sword::fire;
@@ -26,16 +25,33 @@ namespace
     ModuleFacade* module = 0;
 }
 
+#define TRY try {
+#define CATCH( hook ) \
+    } \
+    catch( std::exception& e ) \
+    { \
+        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, e.what() ); \
+    } \
+    catch( ... ) \
+    { \
+        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, "Unknown exception during " #hook ); \
+    }
+
 DEFINE_HOOK( GetForceRatio, double, ( const SWORD_Model* model, const SWORD_Model* entity ) )
 {
-    return sword::fire::Knowledge_RapForLocal().GetValue( model, entity ); // $$$$ MCO 2012-05-15: cache this
+    TRY
+        return sword::fire::Knowledge_RapForLocal().GetValue( model, entity ); // $$$$ MCO 2012-05-15: cache this
+    CATCH( GetForceRatio )
+    return 0;
 }
 DEFINE_HOOK( GetDangerousEnemies, void, ( const SWORD_Model* model, const SWORD_Model* entity,
-    void(*visitor)( const SWORD_Model* knowledge, void* userData ), void* userData ) )
+                                          void(*visitor)( const SWORD_Model* knowledge, void* userData ), void* userData ) )
 {
-    sword::fire::Knowledge_RapForLocal rapfor;
-    const Knowledge_RapForLocal::T_KnowledgeAgents& enemies = rapfor.GetDangerousEnemies( model, entity ); // $$$$ MCO 2012-05-15: cache this and/or GetDangerousEnemies()
-    std::for_each( enemies.begin(), enemies.end(), boost::bind( visitor, _1, userData ) );
+    TRY
+        sword::fire::Knowledge_RapForLocal rapfor;
+        const Knowledge_RapForLocal::T_KnowledgeAgents& enemies = rapfor.GetDangerousEnemies( model, entity ); // $$$$ MCO 2012-05-15: cache this and/or GetDangerousEnemies()
+        std::for_each( enemies.begin(), enemies.end(), boost::bind( visitor, _1, userData ) );
+    CATCH( GetDangerousEnemies )
 }
 DEFINE_HOOK( GetAmmunitionForIndirectFire, const char*, ( const SWORD_Model* model, const SWORD_Model* firer, const char* type, const MT_Vector2D* target ) )
 {
@@ -44,19 +60,10 @@ DEFINE_HOOK( GetAmmunitionForIndirectFire, const char*, ( const SWORD_Model* mod
         ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, "Invalid null type in GetAmmunitionForIndirectFire" );
         return 0;
     }
-    try
-    {
+    TRY
         assert( module );
         return RoleAction_IndirectFiring().GetAmmunitionForIndirectFire( *module, model, firer, type, target );
-    }
-    catch( std::exception& e )
-    {
-        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, e.what() );
-    }
-    catch( ... )
-    {
-        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, "Unknown exception during GetAmmunitionForIndirectFire" );
-    }
+    CATCH( GetAmmunitionForIndirectFire )
     return 0;
 }
 
