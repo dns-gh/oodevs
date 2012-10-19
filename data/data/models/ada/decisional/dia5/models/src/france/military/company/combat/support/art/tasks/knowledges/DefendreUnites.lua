e0841ee713af7be5b4b64a21e2441c06e62d2eea
Grégory Gougé <gregory.gouge@masagroup.net>
2012-10-19 13:13:31 +0000

    [Decisional] Fix Jira SWBUG-9029 - Scipio - Mission impossible ASA Defendre

--- data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/support/art/tasks/knowledges/DefendreUnites.lua
+++ data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/support/art/tasks/knowledges/DefendreUnites.lua
@@ -1,7 +1,11 @@
 return
 {
     getEntities = function( self, params )
-        return params.entities
+        if not myself.leadData.entitiesIndex then
+            myself.leadData.entitiesIndex = 0
+        end
+        myself.leadData.entitiesIndex = myself.leadData.entitiesIndex % #params.entities + 1
+        return { params.entities[myself.leadData.entitiesIndex] }
     end,
     
     -- point du stationnement
@@ -9,6 +13,6 @@ return
         if params.meetingPoint and params.meetingPoint ~= NIL then
            return integration.randomPositionOnCircle( params.meetingPoint, 100 )
         end
-        return params.objective
+        return params.entities[ 1 ] -- Le PC va vers 1 des unités à défendre si pas de position donnée par l'opérateur
     end,
 }
