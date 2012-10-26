a69bfd78d89eeb3fe6d57641a076775296b159cb
Grégory Gougé <gregory.gouge@masagroup.net>
2012-10-26 12:10:27 +0000

    [Decisional] Séparation des bases : suppression des DEC_Trace.

--- data/data/models/ada/decisional/dia5/models/security/src/agent/ontology/classes/LocalizedElement.lua
+++ data/data/models/ada/decisional/dia5/models/security/src/agent/ontology/classes/LocalizedElement.lua
@@ -4,14 +4,14 @@
 method "reachIt" ( masalife.brain.integration.startStopAction( 
 { 
     start = function( self, pathType )
-        DEC_Trace( "Start movement toward objective ")
+        integration.displayTrace( "Start movement toward objective ")
         return integration.startMoveToIt( self, pathType )
     end,
     started = function( self, pathType )
         return integration.updateMoveToIt( self, pathType )
     end, 
     stop = function( self )
-        DEC_Trace( " Objective reached" )
+        integration.displayTrace( " Objective reached" )
         return integration.deselectMoveToIt( self )
     end
 } ) )
