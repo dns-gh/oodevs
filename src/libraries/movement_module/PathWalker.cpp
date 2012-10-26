a4a5f8a1f1ea813bfa6c6daa4249cffde515f83e
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-10-26 18:54:25 +0000

    [movement_module] changed log message

--- src/libraries/movement_module/PathWalker.cpp
+++ src/libraries/movement_module/PathWalker.cpp
@@ -450,7 +450,7 @@ bool PathWalker::TryToMoveTo( const PathResult& path, const MT_Vector2D& vNewPos
 
     if( speed_ <= 0 )
     {
-        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, "Current speed is not positive" );
+        ::SWORD_Log( SWORD_LOG_LEVEL_ERROR, "Current speed is negative" );
         return false;
     }
 
