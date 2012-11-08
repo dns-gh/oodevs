bab0943ec02f922d25f998b639c59d9c7f8dbf81
Benoît Amiaux <benoit.amiaux@masagroup.net>
2012-11-08 16:06:51 +0000

    [replayer_app] Cleanup replayer_app options

--- src/applications/replayer_app/replayer_app.cpp
+++ src/applications/replayer_app/replayer_app.cpp
@@ -14,48 +14,52 @@
 #include "MT_Tools/MT_Logger.h"
 #include "tools/Codec.h"
 #include "tools/WinArguments.h"
+
+#ifdef _MSC_VER
 #pragma warning( push, 0 )
+#endif
 #include <boost/program_options.hpp>
+#ifdef _MSC_VER
 #pragma warning( pop )
+#endif
+
 #include <windows.h>
-#include <boost/optional.hpp>
 
 namespace
 {
 typedef std::vector< std::string > T_Args;
-boost::optional< std::string > GetOption( const T_Args& args, const std::string& name )
+std::string GetOption( const T_Args& args, const std::string& name, const std::string& def )
 {
     T_Args::const_iterator it = std::find( args.begin(), args.end(), name );
     if( it != args.end() )
         if( ++it != args.end() )
             return *it;
-    return boost::none;
+    return def;
+}
+
+bool HasFlag( const T_Args& args, const std::string& name )
+{
+    return std::find( args.begin(), args.end(), name ) != args.end();
 }
 }
 
 int RunReplayer(  HINSTANCE hinstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow )
 {
-    const T_Args argv = boost::program_options:: split_winmain( lpCmdLine );
-    boost::optional< std::string > opt = GetOption( argv, "--debug-dir" );
-    const std::string debugRoot = opt == boost::none ? "./Debug" : *opt;
-    const bool replayLog = std::find( argv.begin(), argv.end(), "--no-log" ) == argv.end();
+    const T_Args argv = boost::program_options::split_winmain( lpCmdLine );
+    const std::string debugRoot = GetOption( argv, "--debug-dir", "./Debug" );
 
     MT_CrashHandler::SetRootDirectory( debugRoot );
 
     int nResult = EXIT_SUCCESS;
     MT_ConsoleLogger        consoleLogger;
     MT_LOG_REGISTER_LOGGER( consoleLogger );
-    bool verbose = false;
     try
     {
-        // verbose mode
-        verbose = std::find( argv.begin(), argv.end(), "--verbose" ) != argv.end();
-
 #if !defined( _DEBUG ) && ! defined( NO_LICENSE_CHECK )
-        license_gui::LicenseDialog::CheckLicense( "sword-replayer", !verbose );
+        license_gui::LicenseDialog::CheckLicense( "sword-replayer", !HasFlag( argv, "verbose" ) );
 #endif
         tools::SetCodec();
-        App app( hinstance, hPrevInstance, lpCmdLine, nCmdShow, replayLog );
+        App app( hinstance, hPrevInstance, lpCmdLine, nCmdShow, !HasFlag( argv, "--no-log" ) );
         app.Execute();
     }
     catch( std::exception& e )
