c579540941d844cf98548e3525b3c0c271c0e7fb
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-11-14 07:14:22 +0000

    [simulation] sorted defines

--- src/libraries/simulation_kernel/Adapters/FireHooks.cpp
+++ src/libraries/simulation_kernel/Adapters/FireHooks.cpp
@@ -49,8 +49,8 @@
 
 using namespace sword;
 
-#define GET_ROLE( node, role ) (*core::Convert( node ))[ "data" ].GetUserData< MIL_AgentPion >().GetRole< role >()
 #define GET_PION( node ) (*core::Convert( node ))[ "data" ].GetUserData< MIL_AgentPion >()
+#define GET_ROLE( node, role ) (*core::Convert( node ))[ "data" ].GetUserData< MIL_AgentPion >().GetRole< role >()
 #define GET_DATA( node, data ) (*core::Convert( node ))[ "data" ].GetUserData< data >()
 
 namespace
