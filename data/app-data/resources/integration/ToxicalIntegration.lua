594a656cec6e652e5bfd37067d3d16ddf0b36a07
Grégory Gougé <gregory.gouge@masagroup.net>
2012-11-22 15:03:18 +0000

    [integration] CERES story 1309 - Safety units equipped with a new type of sensor can detect toxic cloud
    Mise à jour méthode d'intégration, en attendant d'avoir la bonne fonction DEC.

--- data/app-data/resources/integration/ToxicalIntegration.lua
+++ data/app-data/resources/integration/ToxicalIntegration.lua
@@ -1,4 +1,10 @@
 integration.isDetectedToxicalPlume = function( agent )
-    -- $$$ GGE Wait lgy DEC function 
-    return false
+    -- $$$ GGE Wait lgy DEC function meanwhile test with NBC
+    local objects = {}
+    objects = integration.getCollidingObjectsFromType( "nbc cloud" )
+    if #objects > 0 then
+        return true
+    else
+        return false
+    end
 end
