0ffe56c9d321ba716ef770381da88ba34f3b165d
Joël Soicher <joel.soicher@masagroup.net>
2012-11-26 15:10:56 +0000

    [simulation] fix SWBUG-9261 - Scipio 380612 -P10_245 RENS - Comportement unités de détection électronique Merged revision(s) 36159 from branches/sword-scipio-stab

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Communications/PHY_RolePion_Communications.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Communications/PHY_RolePion_Communications.cpp
@@ -410,7 +410,7 @@ void PHY_RolePion_Communications::UpdateKnowledgesFromObjectCollision( const DEC
 // -----------------------------------------------------------------------------
 bool PHY_RolePion_Communications::CanReceive() const
 {
-    return jammers_.empty() && !bBlackoutReceivedActivated_;
+    return jammers_.empty() && !bBlackoutReceivedActivated_ && !entity_.IsDead();
 }
 
 // -----------------------------------------------------------------------------
@@ -419,7 +419,7 @@ bool PHY_RolePion_Communications::CanReceive() const
 // -----------------------------------------------------------------------------
 bool PHY_RolePion_Communications::CanEmit() const
 {
-    return jammers_.empty() && !bBlackoutEmmittedActivated_;
+    return jammers_.empty() && !bBlackoutEmmittedActivated_ && !entity_.IsDead();
 }
 
 // -----------------------------------------------------------------------------
