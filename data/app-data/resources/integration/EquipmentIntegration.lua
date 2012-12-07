db9af4b8b462f76d1632094fa23da08bb1c58920
Grégory Gougé <gregory.gouge@masagroup.net>
2012-12-07 12:12:10 +0000

    [integration] Fix jira SWBUG-9332 - Scipio LOG - Lors du changement d'état en santé, la recherche de pion capable doit se faire d'abord dans la BL

--- data/app-data/resources/integration/EquipmentIntegration.lua
+++ data/app-data/resources/integration/EquipmentIntegration.lua
@@ -476,6 +476,7 @@ integration.activateLogisticChains = function( self )
         DEC_Sante_ActiverChaine()
         DEC_Maintenance_ActiverChaine()
         DEC_Ravitaillement_ActiverChaine()
+        DEC_Sante_ActiverFonctionSoin()
     end
 end
 
@@ -484,6 +485,7 @@ integration.desactivateLogisticChains = function( self )
         DEC_Sante_DesactiverChaine()
         DEC_Maintenance_DesactiverChaine()
         DEC_Ravitaillement_DesactiverChaine()
+        DEC_Sante_DesactiverFonctionSoin()
     end
 end
 
