c3cd6e57e8b68e2c775eb2e50f62a77866441f75
Mathieu Champlon <mathieu.champlon@masagroup.net>
2012-12-08 17:52:23 +0000

    [fire_module] reworked cache on dangerosity for even more optimizations

--- src/libraries/fire_module/Knowledge_RapForLocal.cpp
+++ src/libraries/fire_module/Knowledge_RapForLocal.cpp
@@ -13,12 +13,13 @@
 #include <wrapper/Hook.h>
 #include <module_api/Log.h>
 #include <xeumeuleu/xml.h>
+#include <boost/container/flat_map.hpp>
 
 using namespace sword;
 using namespace sword::fire;
 
 DECLARE_HOOK( EvaluateDangerosity, double, ( const SWORD_Model* agent, const SWORD_Model* target ) )
-DECLARE_CACHED_HOOK( EvaluateDangerosity2, 2, double, ( const SWORD_Model* agent, const SWORD_Model* target ) )
+DECLARE_HOOK( EvaluateDangerosity2, double, ( const SWORD_Model* agent, const SWORD_Model* target ) )
 
 double Knowledge_RapForLocal::rRapForIncreasePerTimeStepDefaultValue_ = 0;
 
@@ -44,14 +45,52 @@ namespace
         return ( rRapForBoundMax - rRapForBoundMin ) / rBaseTimeValue;
     }
 
+    typedef boost::container::flat_map< std::pair< std::size_t, std::size_t >, double > T_DangerosityCache;
+    std::map< std::size_t, T_DangerosityCache > caches;
+
+    void FillCache( T_DangerosityCache& cache, std::size_t id, const wrapper::View& knowledges, const wrapper::View& model )
+    {
+        const wrapper::View& friends = model[ "friends" ][ id ];
+        const wrapper::View& enemies = model[ "enemies" ][ id ];
+        const std::size_t fs = friends.GetSize();
+        const std::size_t es = enemies.GetSize();
+        cache.reserve( fs * es );
+        for( std::size_t i = 0; i < fs; ++i )
+        {
+            const SWORD_Model* f = knowledges[ static_cast< std::size_t >( friends.GetElement( i ) ) ];
+            for( std::size_t j = 0; j < es; ++j )
+            {
+                const SWORD_Model* e = knowledges[ static_cast< std::size_t >( enemies.GetElement( j ) ) ];
+                cache.insert( cache.end(), std::make_pair( std::make_pair( i, j ), GET_HOOK( EvaluateDangerosity2 )( f, e ) ) );
+            }
+        }
+    }
+    const T_DangerosityCache& GetCache( const wrapper::View& model, const wrapper::View& entity )
+    {
+        const std::size_t k = entity[ "knowledges" ];
+        T_DangerosityCache& cache = caches[ k ];
+        if( cache.empty() )
+            FillCache( cache, k, model[ "knowledges" ][ k ], model );
+        return cache;
+    }
+
+    DEFINE_HOOK( Tick, 0, void, () )
+    {
+        caches.clear();
+        if( GET_PREVIOUS_HOOK( Tick ) )
+            GET_PREVIOUS_HOOK( Tick )();
+    }
+
     double GetRapForLocal( const wrapper::View& model, const wrapper::View& entity, Knowledge_RapForLocal::T_KnowledgeAgents& dangerousEnemies, bool(*filter)( const SWORD_Model* knowledge, void* userData ), void* userData, double defaultValue )
     {
+        const T_DangerosityCache& cache = GetCache( model, entity );
         const unsigned int id = entity[ "knowledges" ];
         const wrapper::View& knowledges = model[ "knowledges" ][ id ];
         double rTotalFightScoreEnemy  = 0;
         double rTotalFightScoreFriend = 0;
         // 1 - Compute the enemy fight score, and get the dangerous enemies
         const wrapper::View& enemies = model[ "enemies" ][ id ];
+        std::vector< std::size_t > ids;
         for( std::size_t i = 0; i < enemies.GetSize(); ++i )
         {
             const SWORD_Model* knowledgeEnemy = knowledges[ static_cast< unsigned int >( enemies.GetElement( i ) ) ];
@@ -62,6 +101,7 @@ namespace
             {
                 rTotalFightScoreEnemy += rDangerosity;
                 dangerousEnemies.push_back( knowledgeEnemy );
+                ids.push_back( i );
             }
         }
         // 2 - Compute the friend fight scores against the agent local enemies
@@ -73,8 +113,8 @@ namespace
             const SWORD_Model* knowledgeFriend = knowledges[ static_cast< unsigned int >( friends.GetElement( i ) ) ];
             if( ! filter( knowledgeFriend, userData ) )
                 continue;
-            for( Knowledge_RapForLocal::CIT_KnowledgeAgents it = dangerousEnemies.begin(); it != dangerousEnemies.end(); ++it )
-                rTotalFightScoreFriend += GET_HOOK( EvaluateDangerosity2 )( knowledgeFriend, *it );
+            for( auto it = ids.begin(); it != ids.end(); ++it )
+                rTotalFightScoreFriend += cache.find( std::make_pair( i, *it ) )->second;
         }
         return ( rTotalFightScoreFriend / dangerousEnemies.size() ) / rTotalFightScoreEnemy;
     }
