d783a177a747c7d3134cc82b8ebf0f6cbd2e25ed
Nicolas Petit <nicolas.petit@masagroup.net>
2012-12-14 13:55:13 +0000

    [Preparation]Fix Jira SWBUG-6994 Les textes descriptifs des données et fonctions lors de la création de scores sont en anglais Merged revision(s) 37032 from trunk:

--- src/libraries/indicators/Primitive.h
+++ src/libraries/indicators/Primitive.h
@@ -74,6 +74,7 @@ private:
     //@{
     void ReadParameter( xml::xistream& xis );
     QString BuildParameterList() const;
+    void ReadComment( xml::xistream& xis );
     //@}
 
     //! @name Types
@@ -86,7 +87,7 @@ private:
     //@{
     const QString name_;
     const std::string category_;
-    const QString comment_;
+    std::string comment_;
     const std::string type_;
     const DataTypeFactory& types_;
     QString prototype_;
