e3402b7745532406df9cc3b4229731f14f772b60
Lucie Mignot <lucie.mignot@masagroup.net>
2012-12-28 14:18:17 +0000

    [Decisionnel + Physical] JIRA 9072 : Scipio 380490 -PSG7_150 MELEE Mission JALONNER - Comportement inadapté
    Code de GGE + ajout de l'ODC "décrocher" à la mission automate jalonner

--- data/data/models/ada/decisional/dia5/models/src/sword/military/company/tasks/knowledges/ScreenEffect.lua
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/company/tasks/knowledges/ScreenEffect.lua
@@ -12,18 +12,31 @@ return
             self.numberEchelons = 1 -- par défaut les pions décrochent sur 1 échelon
         end
 
+        -- découpage du fuseau principal en sous-fuseaus pour chaque unité
         local fuseaux = DEC_DecouperFuseau( meKnowledge.nbPionsMain )
         for _, fuseau in pairs( fuseaux ) do
             myself.leadData.fuseaux[ #myself.leadData.fuseaux + 1 ] = fuseau
         end
 
-        -- Organisation du dsipositif initial à 300 metres devant la LCAR (moins en zurb ?)
+        -- Organisation du dsipositif initial à 300 metres devant la LCAR (moins en zurb ?) si elle est présente
         local LimaId = DEC_GetLima( eTypeLima_LCAR )
         if LimaId > 0 and meKnowledge.nbPionsMain > 0 then
             pointsBeforeLimas = DEC_Geometrie_CalculerPositionsParRapportALima( LimaId, 300, meKnowledge.nbPionsMain)
             for _, point in pairs( pointsBeforeLimas ) do
                 myself.leadData.scoutPoints[ #myself.leadData.scoutPoints + 1 ]= CreateKnowledge( sword.military.world.Point, point )
             end
+        end
+        -- Si pas de lima on va s'aligner au niveau de l'unité la plus éloignée du début du fuseau
+        local advance
+        myself.leadData.advanceMax = 0
+        local units = integration.getEntitiesFromAutomatCommunication( meKnowledge, "none", false )
+        for i=1, #units do
+           local unit = units[i]
+           local coord = unit:getPosition()
+           advance = DEC_Geometrie_PositionAdvanceAlongFuseauAutomat( coord )
+           if advance > myself.leadData.advanceMax then
+               myself.leadData.advanceMax = advance
+           end
         end
     end,
 
@@ -35,7 +48,8 @@ return
     end,
  
     getScreenPositions = function( self, params, entity )
-        local positions = {}
+        local positions = {} -- découpage en fuseau
+        local lcar = false
         myself.leadData.fuseauxIndex = myself.leadData.fuseauxIndex or 0
         myself.leadData.fuseauxIndex = myself.leadData.fuseauxIndex % #myself.leadData.fuseaux + 1
         local fuseau = myself.leadData.fuseaux[ myself.leadData.fuseauxIndex ]
@@ -43,12 +57,19 @@ return
             local point = myself.leadData.scoutPoints[i]
             if DEC_Geometrie_EstPointDansFuseau_AvecParamFuseau( fuseau, point.source ) then
                 positions[ #positions + 1 ] = point
+                lcar = true
             end
         end
-
-       --Ajout de la destination finale
-        positions[ #positions + 1 ] = params.meetingPoint
-
+        
+        -- 1ere position : alignement sur l'unité la plus éloignée du début du fuseau si on n'a pas de LCAR
+        if not lcar then
+            local pos = DEC_Geometrie_CalculerPointSurFuseau( fuseau, myself.leadData.advanceMax )
+            positions[ #positions + 1 ] = CreateKnowledge( sword.military.world.Point, pos )
+        end
+        
+        --Ajout de la destination finale
+        positions[ #positions + 1 ] = CreateKnowledge( sword.military.world.Point, DEC_Geometrie_CalculerPointArriveePourFuseau( fuseau ) )
+        
         if not myself.leadData.currentMoveToPosition then
             myself.leadData.currentMoveToPosition = 0
         end
