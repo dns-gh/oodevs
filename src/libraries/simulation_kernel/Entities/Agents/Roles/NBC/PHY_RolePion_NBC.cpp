796831f2a90c3a454e6f8367e8d18c509e60eacd
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2013-01-03 17:54:33 +0000

    [simulation]Fix JIRA #SWBUG-9230 Scipio 380566 - P9_252 NRBC - Vol de drone contrariée : no poisoning of machines Merged revision(s) 37371 from branches/sword-scipio-stab

--- src/libraries/simulation_kernel/Entities/Agents/Roles/NBC/PHY_RolePion_NBC.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/NBC/PHY_RolePion_NBC.cpp
@@ -130,6 +130,8 @@ void PHY_RolePion_NBC::serialize( Archive& file, const unsigned int )
 // -----------------------------------------------------------------------------
 void PHY_RolePion_NBC::Poison( const MIL_ToxicEffectManipulator& contamination )
 {
+    if( pion_.GetType().GetUnitType().IsAutonomous() )
+        return;
     if( bNbcProtectionSuitWorn_ )
         return;
     if( ! intoxicated_ && ! poisoned_ )
