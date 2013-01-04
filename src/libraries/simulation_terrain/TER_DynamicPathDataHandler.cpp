b9a9b08cb525204b3cf4272ae1e708b9dd768cc4
Patrick Mezard <patrick.mezard@masagroup.net>
2013-01-04 20:54:29 +0000

    [simulation_terrain] disable dynamic data handler if singletons are not there
    This let us create TER_LimitData without having to initialize
    TER_World and friends, which is convenient for tests.

--- src/libraries/simulation_terrain/TER_DynamicPathDataHandler.cpp
+++ src/libraries/simulation_terrain/TER_DynamicPathDataHandler.cpp
@@ -9,6 +9,7 @@
 
 #include "simulation_terrain_pch.h"
 #include "TER_DynamicPathDataHandler.h"
+#include "TER_World.h"
 #include "TER_PathFindManager.h"
 
 TER_DynamicPathDataHandler::TER_DynamicPathDataHandler()
@@ -19,15 +20,22 @@ TER_DynamicPathDataHandler::TER_DynamicPathDataHandler()
 
 TER_DynamicPathDataHandler::~TER_DynamicPathDataHandler()
 {
-    if( pPathfindData_ )
-        TER_PathFindManager::GetPathFindManager().RemoveDynamicData( *pPathfindData_ );
+    Reset( 0 );
 }
 
 void TER_DynamicPathDataHandler::Reset( TER_DynamicData* data )
 {
+    // This is ugly but let us run tests creating TER_LimitData
+    // without having to instanciante all the TER_World machinery.
+    TER_World* w = &TER_World::GetWorld();
+    if( !w )
+        return;
+    TER_PathFindManager* m = &w->GetPathFindManager();
+    if( !m )
+        return;
     if( pPathfindData_ )
-        TER_PathFindManager::GetPathFindManager().RemoveDynamicData( *pPathfindData_ );
+        m->RemoveDynamicData( *pPathfindData_ );
     if( data )
-        TER_PathFindManager::GetPathFindManager().AddDynamicData( *data );
+        m->AddDynamicData( *data );
     pPathfindData_ = data;
 }
