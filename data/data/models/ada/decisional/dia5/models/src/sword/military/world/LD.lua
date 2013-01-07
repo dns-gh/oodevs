bdd281dfac2de1083cef426fdaa0cc61e6da5ebc
Grégory Gougé <gregory.gouge@masagroup.net>
2013-01-07 10:42:10 +0000

    [Decisional] Fix Jira SWBUG-9525 - Scipio 345254 - Le comportement de la mission CIRCULATION Appuyer Mouvement n'est pas correct
    Attention, désormais il est possible de dépasser une ligne de déboucher en utilisant l'ordre de conduite poursuivre (en plus de l'ordre de conduite déboucher qui lui n'est quasiment jamais disponible). C'était le cas en V1.

--- data/data/models/ada/decisional/dia5/models/src/sword/military/world/LD.lua
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/world/LD.lua
@@ -17,6 +17,9 @@ return
         dependencies = "none",
         method = function( self )
                     local order = integration.query.getFirstFragOrderFromType( "Rep_OrderConduite_Deboucher" )
+                    if order == nil then -- fragOrder 'resume movement' could be used to move when agent is waiting on a LD
+                        order = integration.query.getFirstFragOrderFromType( "Rep_OrderConduite_Poursuivre" )
+                    end
                     return DEC_GetMissionLimaFlag( DEC_GetLimaPoint( DEC_GetDestPoint( self.source ) ) ) or order
                  end
     }
