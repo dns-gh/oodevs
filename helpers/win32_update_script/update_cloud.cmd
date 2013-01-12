663c4b6e109a33315dde9b1cb6c5a8cc91cbb597
Patrick Mezard <patrick.mezard@masagroup.net>
2013-01-12 15:59:56 +0000

    [build] reference file1 instead of install
    Merged revision(s) 37735 from cloud/branches/1.0

--- helpers/win32_update_script/update_cloud.cmd
+++ helpers/win32_update_script/update_cloud.cmd
@@ -1,12 +1,12 @@
 @echo off
-net use \\install /USER:<USER> <PASSWORD> 1>NUL
+net use \\file1 /USER:<USER> <PASSWORD> 1>NUL
 set FILE=sword_cloud_vc100_x64_setup.exe
 set BIN=sword_cloud_vc100_x64_bin.zip
 set HOME=c:\Users\Admin
 copy /B /V /Y "%HOME%\%FILE%" "%HOME%\%FILE%.backup" 1>NUL
 copy /B /V /Y "%HOME%\%BIN%"  "%HOME%\%BIN%.backup" 1>NUL
-copy /B /V /Y "\\install\Masa\common\dev\delivery\internal\cloud\current\%FILE%" "%HOME%" 1>NUL
-copy /B /V /Y "\\install\Masa\common\dev\delivery\internal\cloud\current\%BIN%"  "%HOME%" 1>NUL
+copy /B /V /Y "\\file1\Masa\common\dev\delivery\internal\cloud\current\%FILE%" "%HOME%" 1>NUL
+copy /B /V /Y "\\file1\Masa\common\dev\delivery\internal\cloud\current\%BIN%"  "%HOME%" 1>NUL
 net stop "sword cloud"
 "%HOME%\%FILE%" /S /SKIP_SERVICE_CONTROL /D=%HOME%\sword_cloud
 copy /B /V /Y "%HOME%\cloud_server.config" "%HOME%\sword_cloud\bin"
