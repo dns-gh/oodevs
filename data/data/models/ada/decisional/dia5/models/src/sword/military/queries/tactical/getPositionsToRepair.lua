10b7456fa2e69d3e56b0089887d03ceccb86b26b
Grégory Gougé <gregory.gouge@masagroup.net>
2013-02-04 11:44:50 +0000

    [Decisional] Fix problème de positionnement des unités qui ont la mission réparer sur un abattis. L'unité n'arrivait pas à rejoindre la position de réparation. Modification de la query en conséquence : obtenir une position atteignable (l'objet à réparer ne bloque pas l'unité).

--- data/data/models/ada/decisional/dia5/models/src/sword/military/queries/tactical/getPositionsToRepair.lua
+++ data/data/models/ada/decisional/dia5/models/src/sword/military/queries/tactical/getPositionsToRepair.lua
@@ -6,7 +6,15 @@ queryImplementation "getPositionsToRepair"
             if masalife.brain.core.class.isOfType( objective, sword.military.world.UrbanBlock ) then
                 allRes[ #allRes + 1 ] = objective:getMyPosition()
             else
-                allRes[ #allRes + 1 ] = CreateProxyKnowledge( sword.military.world.AvoidingArea, objective )
+               scaledObject = DEC_Geometrie_AgrandirLocalisation( objective:getLocalisation() , 50 )
+               if myself.taskParams.meetingPoint and myself.taskParams.meetingPoint ~= NIL then
+                   positionNextTo = myself.taskParams.meetingPoint:getPosition()
+               else
+                   positionNextTo = meKnowledge:getPosition()
+               end
+               objectPosition = DEC_Geometrie_ComputeNearestBorder( positionNextTo, scaledObject )
+               point = CreateKnowledge( sword.military.world.Point, objectPosition )
+               allRes[ #allRes + 1 ] = point
             end
         end
         return allRes
