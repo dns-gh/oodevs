abae40db3830db9c02636765a838535b652915fc
Nicolas Petit <nicolas.petit@masagroup.net>
2013-02-06 16:25:50 +0000

    [simlation_app] Fix Jira SWBUG-9760 ALGERIE - CR sur tir d'artillerie fratricide Add death condition for sending messages with units are in indirect fire area

--- src/libraries/simulation_kernel/Entities/Agents/Units/Dotations/PHY_DotationCategory_IndirectFire.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Units/Dotations/PHY_DotationCategory_IndirectFire.cpp
@@ -160,7 +160,7 @@ void PHY_DotationCategory_IndirectFire::ApplyEffect( const MIL_Agent_ABC* pFirer
         for( TER_Agent_ABC::CIT_AgentPtrVector itTarget = targets.begin(); itTarget != targets.end(); ++itTarget )
         {
             MIL_Agent_ABC& target = static_cast< PHY_RoleInterface_Location& >( **itTarget ).GetAgent();
-            if( target.GetRole< PHY_RoleInterface_Location >().GetHeight() > 0 )
+            if( target.IsDead() || target.GetRole< PHY_RoleInterface_Location >().GetHeight() > 0 )
                 continue;
             PHY_RoleInterface_ActiveProtection& targetRoleProtection = target.GetRole< PHY_RoleInterface_ActiveProtection >();
             if( targetRoleProtection.DestroyIndirectFire( dotationCategory_ ) )
@@ -213,7 +213,7 @@ void PHY_DotationCategory_IndirectFire::ApplyEffect( const MIL_Agent_ABC* pFirer
         for( TER_Agent_ABC::CIT_AgentPtrVector itTarget = targets.begin(); itTarget != targets.end(); ++itTarget )
         {
             MIL_Agent_ABC& target = static_cast< PHY_RoleInterface_Location& >( **itTarget ).GetAgent();
-            if( target.GetRole< PHY_RoleInterface_Location >().GetHeight() > 0 )
+            if( target.IsDead() || target.GetRole< PHY_RoleInterface_Location >().GetHeight() > 0 )
                 continue;
             target.GetRole< PHY_RoleInterface_HumanFactors >().NotifyAttacked();
             PHY_RoleInterface_ActiveProtection& targetRoleProtection = target.GetRole< PHY_RoleInterface_ActiveProtection >();
