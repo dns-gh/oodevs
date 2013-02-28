c308901f6497835d8532c0d916ce9be9b7b76f3f
Benoît Amiaux <benoit.amiaux@masagroup.net>
2013-02-28 11:36:29 +0000

    [web] Update to bootstrap 2.3.0

--- node/www/debug/uglify.cmd
+++ node/www/debug/uglify.cmd
@@ -1,7 +1,7 @@
 @echo off
 set PATH=c:\cygwin\bin;%PATH%
 cat jquery-1.8.2.js ^
-    bootstrap-2.1.1.js ^
+    bootstrap-2.3.0.js ^
     underscore-1.3.3.js ^
     backbone-0.9.2.js ^
     handlebars-1.0.0.beta.6.js ^
