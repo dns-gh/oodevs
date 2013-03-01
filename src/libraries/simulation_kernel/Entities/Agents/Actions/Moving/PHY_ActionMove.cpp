9fc6a35b961cc61eb309d72ac599a51ccc1f00fb
Laurent Di Cesare <laurent.dicesare@masagroup.net>
2013-03-01 17:05:55 +0000

    [simulation]Fix JIRA # SWBUG-9969 [PERF] Action Time atteint des valeurs supérieures à 10s Merged revision(s) 39634 from branches/5.1

--- src/libraries/simulation_kernel/Entities/Agents/Actions/Moving/PHY_ActionMove.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Actions/Moving/PHY_ActionMove.cpp
@@ -64,38 +64,37 @@ PHY_ActionMove::~PHY_ActionMove()
 bool PHY_ActionMove::UpdateObjectsToAvoid()
 {
     T_KnowledgeObjectVector knowledges;
+    T_KnowledgeObjectVector newKnowledges;
     MIL_PathObjectFilter filter;
     pion_.GetKnowledgeGroup()->GetKnowledgeObjectContainer().GetObjectsAtInteractionHeight( knowledges, pion_, filter );
-    for( auto it = knowledges.begin(); it != knowledges.end(); )
+    for( auto it = knowledges.begin(); it != knowledges.end(); ++it )
     {
         double cost = pMainPath_->GetPathClass().GetObjectCost( (*it)->GetType() );
-        if( 0. == cost )
-            it = knowledges.erase( it );
-        else
-            ++it;
+        if( 0. != cost )
+            newKnowledges.push_back( *it );
     }
-    if( knowledges != objectsToAvoid_ )
+    if( newKnowledges != objectsToAvoid_ )
     {
-        objectsToAvoid_ = knowledges;
+        objectsToAvoid_ = newKnowledges;
         geometrySignatures_.clear();
-        for( auto it = knowledges.begin(); it != knowledges.end(); ++it )
+        for( auto it = newKnowledges.begin(); it != newKnowledges.end(); ++it )
             geometrySignatures_.push_back( (*it)->GetLocalisation() );
         return true;
     }
     else
     {
         bool modified = false;
-        for( std::size_t i = 0; i < knowledges.size(); ++i )
+        for( std::size_t i = 0; i < newKnowledges.size(); ++i )
         {
-            if( geometrySignatures_[i] != knowledges[i]->GetLocalisation() )
+            if( geometrySignatures_[i] != newKnowledges[i]->GetLocalisation() )
             {
-                geometrySignatures_[i] = knowledges[i]->GetLocalisation();
+                geometrySignatures_[i] = newKnowledges[i]->GetLocalisation();
                 modified = true;
             }
         }
         if( modified )
         {
-            objectsToAvoid_ = knowledges;
+            objectsToAvoid_ = newKnowledges;
             return true;
         }
     }
