a67548abc3f02bb681b6cd004938e6f7ba7504d0
Patrick Mezard <patrick.mezard@masagroup.net>
2013-03-07 20:38:41 +0000

    [util] fix silly variable scoping bug in TailFiles()
    The file and the reader variables were defined in the closure instead of
    outside, and were thus closed after the first call successfully opening
    the file.

--- src/swapi/simu/util.go
+++ src/swapi/simu/util.go
@@ -15,54 +15,67 @@ type TailHandler func(line string) bool
 // Once it is opened, keep logging all file lines until a read fails or the
 // quit channel is signalled.
 func TailFiles(paths []string, quit chan int, handler TailHandler) {
-	readers := make([]func() bool, 0, len(paths))
+	readers := make([]func(flush bool) bool, 0, len(paths))
 	for _, path := range paths {
 		// Duplicate variable for the closure
 		logPath := path
 		pending := ""
 		var reader *bufio.Reader
-		fn := func() bool {
+		var fp *os.File
+		fn := func(terminate bool) bool {
+			defer func() {
+				if terminate && fp != nil {
+					fp.Close()
+				}
+			}()
 			if reader == nil {
 				fp, err := os.Open(logPath)
 				if err != nil {
 					return false
 				}
-				defer fp.Close()
 				reader = bufio.NewReader(fp)
 			}
 
 			for {
 				line, err := reader.ReadString('\n')
 				pending += line
-				if err != nil {
-					return err != io.EOF
+				if err == io.EOF {
+					if terminate {
+						handler(pending)
+					}
+					break
 				}
-				if handler(pending) {
-					return true
+				if handler(pending) || err != nil {
+					terminate = true
+					break
 				}
 				pending = ""
 			}
-			panic("unreachable")
+			return terminate
 		}
 		readers = append(readers, fn)
 	}
 
 	ticker := time.NewTicker(1 * time.Second)
+	terminate := false
 	for {
 		select {
 		case <-ticker.C:
 			break
 		case <-quit:
-			return
+			terminate = true
 		}
 		for i := 0; i != len(readers); {
-			if readers[i]() {
+			if readers[i](false) {
 				readers = append(readers[:i], readers[i+1:]...)
 				continue
 			}
 			i++
 		}
-		if len(readers) <= 0 {
+		if terminate || len(readers) <= 0 {
+			for _, reader := range readers {
+				reader(true)
+			}
 			break
 		}
 	}
