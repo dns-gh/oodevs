1310491b38ab6632aa90811fbe22db8ae46e8308
Patrick Mezard <patrick.mezard@masagroup.net>
2013-03-07 20:38:42 +0000

    [startup_test] use smaller crossroad-empty exercise

--- src/simtests/startup_test.go
+++ src/simtests/startup_test.go
@@ -40,7 +40,7 @@ func MakeOpts() *simu.SimOpts {
 	if len(rundir) > 0 {
 		opts.RunDir = &rundir
 	}
-	opts.ExerciseName = "worldwide/Egypt"
+	opts.ExerciseName = "tests/crossroad-empty"
 	opts.DispatcherAddr = fmt.Sprintf("localhost:%d", testPort+5)
 	opts.SimulationAddr = fmt.Sprintf("localhost:%d", testPort+6)
 	return &opts
