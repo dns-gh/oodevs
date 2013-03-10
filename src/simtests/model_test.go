36579fd88642a7eca123793d3859d46d54277cd2
Patrick Mezard <patrick.mezard@masagroup.net>
2013-03-10 18:48:21 +0000

    [model_test] replace coded comparison with model string dumps
    These are much easier to debug and edit.

--- src/simtests/model_test.go
+++ src/simtests/model_test.go
@@ -1,10 +1,56 @@
 package simtests
 
 import (
+	"fmt"
 	. "launchpad.net/gocheck"
+	"sort"
+	"strings"
 	"swapi"
 	"time"
 )
+
+type prettyPrinter struct {
+	shift string
+	lines []string
+}
+
+func (p *prettyPrinter) Shift() {
+	p.shift += "  "
+}
+
+func (p *prettyPrinter) Unshift() {
+	p.shift = p.shift[:len(p.shift)-2]
+}
+
+func (p *prettyPrinter) P(format string, a ...interface{}) {
+	s := fmt.Sprintf(format, a...)
+	p.lines = append(p.lines, p.shift+s)
+}
+
+func (p *prettyPrinter) GetOutput() string {
+	return strings.Join(p.lines, "\n") + "\n"
+}
+
+func printParties(p *prettyPrinter, parties map[uint32]*swapi.Party) *prettyPrinter {
+	printParty := func(p *prettyPrinter, party *swapi.Party) {
+		p.P("Name: %s", party.GetName())
+	}
+
+	keys := []int{}
+	for k := range parties {
+		keys = append(keys, int(k))
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		party := parties[uint32(k)]
+		p.P("Party[%v]", k)
+		p.Shift()
+		printParty(p, party)
+		p.Unshift()
+	}
+	return p
+}
 
 func (s *TestSuite) TestModelInitialization(c *C) {
 	sim := startSimOnExercise(c, "crossroad-small-empty", 1000, false)
@@ -16,16 +62,12 @@ func (s *TestSuite) TestModelInitialization(c *C) {
 	c.Assert(ok, Equals, true) // model initialization timed out
 	model := client.Model
 
-	expectedParties := map[uint32]*swapi.Party{
-		1: swapi.NewParty(1, "party1"),
-		2: swapi.NewParty(2, "party2"),
-	}
-	parties := model.GetParties()
-	c.Assert(len(parties), Equals, len(expectedParties)) // parties count mismatch
-	for k, v := range parties {
-		other, ok := expectedParties[k]
-		c.Assert(ok, Equals, true, Commentf("unexpected party %v:%v",
-			v.GetId(), v.GetName()))
-		c.Assert(v.GetName(), Equals, other.GetName()) // party names mismatch
-	}
+	dump := printParties(&prettyPrinter{}, model.GetParties()).GetOutput()
+	expected := "" +
+		`Party[1]
+  Name: party1
+Party[2]
+  Name: party2
+`
+	c.Assert(dump, Equals, expected)
 }
