2f97675bd96ffebd428e70628b4fea80e0819df1
Grégory Gougé <gregory.gouge@masagroup.net>
2013-03-18 17:25:59 +0000

    [Simulation_kernel][Decisional] Merge from scipio-stab rév 40247 - Fix Jira SWBUG-9932 - Scipio 390038 - Dysfonctionnnements sur la mission Escorte de prisonnier La fonction DEC qui vérifiait si l'agent pouvait transporter des unités ne vérifiait pas si elle avait la capacité de le faire. Egalement, problème d'un booleen false non pris en compte pour le parametre withEquipment pour la mission automate.

--- src/libraries/simulation_kernel/Entities/Agents/Actions/Transport/PHY_RoleAction_Transport.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Actions/Transport/PHY_RoleAction_Transport.cpp
@@ -416,12 +416,11 @@ bool PHY_RoleAction_Transport::CanTransportPion( MIL_Agent_ABC& transported, boo
     if( weightComp->TotalTransportedWeight() <= 0. )
         return false;
 
-    if( !bTransportOnlyLoadable )
-    {
-        std::auto_ptr< TransportCapacityComputer_ABC > computer( owner_->GetAlgorithms().transportComputerFactory_->CreateCapacityComputer() );
-        if( weightComp->HeaviestTransportedWeight() > owner_->Execute( *computer ).MaxComposanteTransportedWeight() )
-            return false;
-    }
+    std::auto_ptr< TransportCapacityComputer_ABC > capacityComp( owner_->GetAlgorithms().transportComputerFactory_->CreateCapacityComputer() );
+    owner_->Execute( *capacityComp );
+    if( capacityComp->WeightLoadedPerTimeStep() == 0 || ( !bTransportOnlyLoadable && weightComp->HeaviestTransportedWeight() > capacityComp->MaxComposanteTransportedWeight() ) )
+        return false;
+
     return true;
 }
 
