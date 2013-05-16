5bd5f9ec8ef47b611c614f37929f6339a634f724
Newfel Messouci <newfel.messouci@masagroup.net>
2013-05-16 12:44:52 +0000

    [ integration ] Fixed "SWBUG-10393 Mission impossible Automate ENG. Construct", added hasDotation and canDoIt methods in initTaskKnowledge

--- data/app-data/resources/integration/BaseTaskKnowledge.lua
+++ data/app-data/resources/integration/BaseTaskKnowledge.lua
@@ -9,6 +9,20 @@ initTaskKnowledge = function( result )
         computeEffectsLevels = function( self )
             return nil
         end,
+        
+        hasDotation = function( self, entity, obstacle )
+            return integration.hasDotationToBuildObject( entity.source, tostring( obstacle:getType() ) )
+        end,
+
+        canDoIt = function( self, entity, obstacle )
+            local localisation = obstacle:getLocalisation()
+            if localisation ~= nil then
+                return integration.canBuildObjectWhenLoadedWithLocation( 
+                       entity.source, tostring( obstacle:getType() ), localisation )
+            else
+                return false
+        end
+    end,
     }
 
     for i,j in pairs( result ) do
