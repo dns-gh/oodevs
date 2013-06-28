440629d1b9869df5a3d425697331f62c6a8a2f93
Benoît Amiaux <benoit.amiaux@masagroup.net>
2013-06-28 12:10:02 +0000

    [build] Add default user parameters

--- build/defargs.cmd
+++ build/defargs.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
 set ROOT=%~dp0..
-echo --node %ROOT%/node/bin/node.exe --proxy %ROOT%/proxy/bin/proxy.exe --node_root %ROOT%/node/www --simulation %ROOT%/out/vc100_x64/sword/simulation_app.exe --replayer %ROOT%/out/vc100_x64/sword/replayer_app.exe --timeline %ROOT%/run/vc100_x64/timeline_server.exe
+echo --node %ROOT%/node/bin/node.exe --proxy %ROOT%/proxy/bin/proxy.exe --node_root %ROOT%/node/www --simulation %ROOT%/out/vc100_x64/sword/simulation_app.exe --replayer %ROOT%/out/vc100_x64/sword/replayer_app.exe --timeline %ROOT%/run/vc100_x64/timeline_server.exe  --user "admin@masagroup.net" --display "Admin" --password "admin"
 pause
