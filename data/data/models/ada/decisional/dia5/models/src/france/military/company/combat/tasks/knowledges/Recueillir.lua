09c6b30fd77e7cc15d4d876b7f7e77feab974302
Lucie Mignot <lucie.mignot@masagroup.net>
2013-07-09 15:41:16 +0000

    [Decisional] SWBUG-10650 : Scipio 390174 - La lima de type ligne de recueil n?est pas renseignée

--- data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/tasks/knowledges/Recueillir.lua
+++ data/data/models/ada/decisional/dia5/models/src/france/military/company/combat/tasks/knowledges/Recueillir.lua
@@ -21,13 +21,13 @@ return
         end
         self.nbScout = integration.reconPlatoons( listPlatoonAlly )
 
-        local pointsOnLimas = DEC_Geometrie_GetPointsLimas( eTypeLima_LR, ( self.nbMain ) )		
+        local pointsOnLimas = DEC_Geometrie_GetPointsLimas( eTypeLima_LR, ( self.nbMain ) )
         for _, points in pairs( pointsOnLimas ) do
             for _, point in pairs( points ) do
                 self.mainPositions[#self.mainPositions + 1] = CreateKnowledge( sword.military.world.Point, point )
             end
         end
-        if not pointsOnLimas or #pointsOnLimas == 0 then
+        if self.nbMain ~= 0 and (not pointsOnLimas or #pointsOnLimas == 0 ) then
             DEC_RC( eRC_LimaParameterNotPresent, eTypeLima_LR )
         end
         if #self.mainPositions == 0 then
@@ -40,7 +40,7 @@ return
                     self.scoutPositions[#self.scoutPositions + 1] = CreateKnowledge( sword.military.world.Point, point )
                 end
             end
-            if not pointsOnLimas or #pointsOnLimas == 0 then
+            if self.nbScout ~=0 and (not pointsOnLimas or #pointsOnLimas == 0 )then
                 DEC_RC( eRC_LimaParameterNotPresent, eTypeLima_LIA )
             end
         end
