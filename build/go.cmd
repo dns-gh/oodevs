84991c5531afd1faf0100182f85e1f1f928bfcb7
Patrick Mezard <patrick.mezard@masagroup.net>
2013-07-12 14:34:44 +0200

    [tests] simplify test configure with -projectRoot/$GOSWORD_ROOT
    The -projectRoot test flag is added to both test modules, and can be
    used to deduce -applications and -root-dir. It should reference gosword
    root directory. It defaults to $GOSWORD_ROOT, which is set by the
    go.sh/go.bat setup scripts. Running tests now looks like:
    
      $ go test simtests

--- build/go.cmd
+++ build/go.cmd
@@ -3,3 +3,4 @@ for /F %%i in ("%~dp0..") do set ROOT=%%~fi
 set PATH=%ROOT%/bin/golang_amd64/bin;%PATH%
 set GOPATH=%ROOT%/lib/vc100_x64;%ROOT%
 set GOROOT=%ROOT%/bin/golang_amd64
+set GOSWORD_ROOT=%ROOT%
