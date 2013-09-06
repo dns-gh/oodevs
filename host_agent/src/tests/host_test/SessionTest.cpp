c70a42b28f85132dd1f49c0cb30e9290ba84c77d
Benoît Amiaux <benoit.amiaux@masagroup.net>
2013-09-06 18:36:53 +0200

    [host] Fix swbug #10870: Empty orbats after some time
    When reloading a session configuration, sides were not reloaded
    properly, which resulted into an empty orbat in the simulation.
    
    The bug resides in the fact that ReadConfig assumes sides have been
    preloaded into the configuration from the exercise settings in
    order to prevent displaying invalid sides.

--- host_agent/src/tests/host_test/SessionTest.cpp
+++ host_agent/src/tests/host_test/SessionTest.cpp
@@ -65,6 +65,22 @@ namespace
         "\"exercise\":{\"name\":\"a\",\"checksum\":\"c\"},"
         "\"terrain\":{\"name\":\"a\",\"checksum\":\"c\"},"
         "\"model\":{\"name\":\"a\",\"checksum\":\"c\"}";
+    const std::string sides =
+        "{"
+        "    \"sides\": {"
+        "        \"no_side_objects\": \"true\","
+        "        \"list\": {"
+        "            \"1\": {"
+        "                \"name\": \"Local militia\","
+        "                \"created\": \"true\""
+        "            },"
+        "            \"3\": {"
+        "                \"name\": \"Friend forces\","
+        "                \"created\": \"true\""
+        "            }"
+        "        }"
+        "    }"
+        "}";
 
     int BlockUntil( Event& start, Event& end, int code )
     {
@@ -119,6 +135,8 @@ namespace
             , plugins ( fs, "" )
             , any_idx ( 0 )
         {
+            MOCK_RESET( node->GetExerciseProperties );
+            MOCK_EXPECT( node->GetExerciseProperties ).returns( FromJson( sides ) );
             MOCK_EXPECT( fs.GetDirectorySize ).returns( 0 );
             MOCK_EXPECT( fs.IsDirectory ).returns( false) ;
             MOCK_EXPECT( fs.MakePaths );
@@ -305,7 +323,8 @@ BOOST_FIXTURE_TEST_CASE( session_converts, Fixture )
 BOOST_FIXTURE_TEST_CASE( session_reloads, Fixture )
 {
     const Tree save = MakeSession()->Save();
-    ReloadSession( save );
+    const auto copy = ReloadSession( save )->Save();
+    BOOST_CHECK_EQUAL( ToJson( save ), ToJson( copy ) );
 }
 
 BOOST_FIXTURE_TEST_CASE( session_starts_and_reloads, Fixture )
