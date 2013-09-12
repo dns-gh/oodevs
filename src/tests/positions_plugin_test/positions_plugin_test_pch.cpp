4d6941fec07bd772bbf96e5c720eb622b2e2887f
Benoît Amiaux <benoit.amiaux@masagroup.net>
2013-09-12 16:48:42 +0200

    [tests] Fix protobuf memory leaks in positions_plugin_test

--- src/tests/positions_plugin_test/positions_plugin_test_pch.cpp
+++ src/tests/positions_plugin_test/positions_plugin_test_pch.cpp
@@ -9,6 +9,7 @@
 
 #include "positions_plugin_test_pch.h"
 #include <string>
+#include <google/protobuf/stubs/common.h>
 
 namespace
 {
@@ -37,4 +38,20 @@ std::string BOOST_RESOLVE( const std::string& filename )
     if( data_directory.empty() )
         return filename;
     return data_directory + '/' + filename;
+}
+
+namespace
+{
+    struct ProtobufConfig
+    {
+        ProtobufConfig()
+        {
+            GOOGLE_PROTOBUF_VERIFY_VERSION;
+        }
+        ~ProtobufConfig()
+        {
+            google::protobuf::ShutdownProtobufLibrary();
+        }
+    };
+    BOOST_GLOBAL_FIXTURE( ProtobufConfig );
 }
