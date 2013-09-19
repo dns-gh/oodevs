a9e65908ed14f160a2005b8606db841ef17a6977
Antoine Blossier <antoine.blossier@masagroup.net>
2013-09-19 11:52:54 +0200

    [adn] move MissionNameValidator to ADN_ExlucdeRegExpValidator, so we can re-use this non-matching regexp validation if needed * fix tooltip on mission name

--- src/applications/adaptation_app/ADN_Missions_GUI.cpp
+++ src/applications/adaptation_app/ADN_Missions_GUI.cpp
@@ -30,37 +30,9 @@
 #include "ADN_TextEdit.h"
 #include "ADN_HtmlViewer.h"
 #include "ADN_enums.h"
+#include "ADN_ExcludeRegExpValidator.h"
 #include "ENT/ENT_Tr.h"
 #include <boost/lexical_cast.hpp>
-
-namespace
-{
-    class MissionNameValidator : public QValidator
-    {
-    public:
-        MissionNameValidator( QObject * parent = 0 )
-            : QValidator( parent )
-            , regExp_( "[/\"<>|*\?:\\\\]" )
-        {}
-
-        ~MissionNameValidator(){}
-
-        virtual void fixUp( QString& input ) const
-        {
-            int index = regExp_.lastIndexIn( input );
-            if( index != -1 )
-                input.remove( index, 1 );
-        }
-
-        virtual State validate( QString& input, int& ) const
-        {
-            return ( regExp_.lastIndexIn( input ) == -1 ) ? Acceptable : Invalid;
-        }
-
-    private:
-        QRegExp regExp_;
-    };
-}
 
 // -----------------------------------------------------------------------------
 // Name: ADN_Missions_GUI constructor
@@ -163,9 +135,8 @@ QWidget* ADN_Missions_GUI::BuildMissions( E_MissionType eMissionType )
     // Info holder
     QWidget* pInfoHolder = builder.AddFieldHolder( 0 );
     ADN_EditLine_ABC* nameField = builder.AddLocalizedField( missions, pInfoHolder, "name", tr( "Name" ), vInfosConnectors[ eName ] );
-    nameField->setToolTip( tr( "Mission name cannot contain the following characters: / < > * \\ : \" |" ) );
-
-    builder.SetValidator( new MissionNameValidator() );
+    nameField->SetToolTip( tr( "Mission name cannot contain the following characters: / < > * \\ : \" |" ) );
+    builder.SetValidator( new ADN_ExcludeRegExpValidator( "[/\"<>|*\?:\\\\]", nameField ) );
     builder.AddField< ADN_EditLine_String >( pInfoHolder, "type", tr( "Type" ), vInfosConnectors[ eDiaType ] );
     if( eMissionType != eMissionType_FragOrder )
     {
