714a0cc2f103838b5be413a4aa4e689afdcec4f4
Patrick Mezard <patrick.mezard@masagroup.net>
2013-09-26 14:21:53 +0200

    [protocol] register new exec_script magic action

--- src/libraries/protocol/Helpers.cpp
+++ src/libraries/protocol/Helpers.cpp
@@ -77,6 +77,7 @@ const mapping::MagicUnitAction mapping::MagicUnitAction::data_[] = {
     { "unit_creation",                      sword::UnitMagicAction::unit_creation },
     { "unload_unit",                        sword::UnitMagicAction::unload_unit },
     { "change_posture",                     sword::UnitMagicAction::change_posture },
+    { "exec_script",                        sword::UnitMagicAction::exec_script },
 };
 const size_t mapping::MagicUnitAction::size_ = COUNT_OF( mapping::MagicUnitAction::data_ );
 
