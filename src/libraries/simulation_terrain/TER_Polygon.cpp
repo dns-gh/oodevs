a1a5253a667a72bac7f041bb30d935ac6ca199df
Patrick Mezard <patrick.mezard@masagroup.net>
2013-09-26 20:57:36 +0200

    [ter] un-inline TER_Polygon.inl

--- src/libraries/simulation_terrain/TER_Polygon.cpp
+++ src/libraries/simulation_terrain/TER_Polygon.cpp
@@ -10,7 +10,6 @@
 //*****************************************************************************
 #include "simulation_terrain_pch.h"
 #include "TER_Polygon.h"
-#include "TER_Polygon.inl"
 #include "MT_Tools/MT_Polyline.h"
 #include "MT_Tools/MT_Droite.h"
 
@@ -640,4 +639,61 @@ bool TER_Polygon::IsInBoundingBox( const MT_Vector2D& p, double rPrecision /*=0*
 bool TER_Polygon::IsInside( const MT_Vector2D& vPos ) const
 {
     return IsInside( vPos, 0.1 );
+}
+
+//-----------------------------------------------------------------------------
+// Name: TER_Polygon::GetBorderPoints
+// Created: JDY 03-05-21
+//-----------------------------------------------------------------------------
+const T_PointVector& TER_Polygon::GetBorderPoints() const
+{
+    assert(pData_);
+    return pData_->borderVector_;
+}
+
+//-----------------------------------------------------------------------------
+// Name: TER_Polygon::Reset
+// Created: JDY 03-05-19
+//-----------------------------------------------------------------------------
+void TER_Polygon::Reset(const T_PointPtrVector& points,bool bConvexHull )
+{
+    *this=TER_Polygon( points, bConvexHull );
+}
+
+//-----------------------------------------------------------------------------
+// Name: TER_Polygon::Reset
+// Created: NLD 2003-07-25
+//-----------------------------------------------------------------------------
+void TER_Polygon::Reset( const T_PointVector& points, bool bConvexHull )
+{
+    *this=TER_Polygon( points, bConvexHull ); //$$ c'est de la merde !
+}
+
+//-----------------------------------------------------------------------------
+// Name: TER_Polygon::Reset
+// Created: JDY 03-05-19
+//-----------------------------------------------------------------------------
+void TER_Polygon::Reset()
+{
+    *this=*empty_polygon;
+}
+
+//-----------------------------------------------------------------------------
+// Name: TER_Polygon::IsNull
+// Created: JDY 03-05-19
+//-----------------------------------------------------------------------------
+bool TER_Polygon::IsNull() const
+{
+    return pData_->bIsNull_;
+}
+
+// -----------------------------------------------------------------------------
+// Name: TER_Polygon::GetArea
+// Created: NLD 2003-10-17
+// -----------------------------------------------------------------------------
+double TER_Polygon::GetArea() const
+{
+    if( pData_->rArea_ == -1 )
+        pData_->rArea_ = ComputeArea();
+    return pData_->rArea_;
 }
