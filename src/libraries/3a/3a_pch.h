d194d277ec988b88688cb3fe6d050d865d6effb4
Patrick Mezard <patrick.mezard@masagroup.net>
2013-10-05 13:40:40 +0200

    [3a] add precompiled header
    Full build:
    original:      27s
    touched pch:   14s
    untouched pch: 10s
    
    Single file build (Medical something):
    original:      9s
    untouched pch: 6s
    
    Boost headers were added by copy/pasting the output of:
    
      ack 'include.*boost' src/libraries/3a/ -h | sort | uniq
    
    Protocol.h shaves around 5s overall. I think adding it in the pch is
    fine because the vast majority of 3a files includes it a way or another.
    This would require a global refactoring to make it different.

---
+++ src/libraries/3a/3a_pch.h
@@ -0,0 +1,37 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2013 Mathematiques Appliquees SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef AAR_PCH_H
+#define AAR_PCH_H
+
+#pragma warning( disable: 4786 ) // identifier was truncated to '255' characters in the debug information
+#pragma warning( disable: 4355 ) // 'this' : used in base member initializer list
+
+#include <boost/algorithm/string.hpp>
+#include <boost/assign.hpp>
+#include <boost/bind.hpp>
+#include <boost/bind/protect.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/function.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/type_traits/integral_constant.hpp>
+#include <boost/type_traits/is_arithmetic.hpp>
+#include <boost/variant.hpp>
+#include <xeumeuleu/xml.hpp>
+#include "protocol/Protocol.h"
+
+#endif //AAR_PCH_H
+
