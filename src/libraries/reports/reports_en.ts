9a5aa7b88d532576e97b9eba06e452bad8d06081
Mathieu Champlon <mathieu.champlon@masagroup.net>
2013-10-14 07:44:14 +0200

    [reports] fixed ts files

--- src/libraries/reports/reports_en.ts
+++ src/libraries/reports/reports_en.ts
@@ -2,33 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en" sourcelanguage="en">
 <context>
-    <name>Localisation</name>
-    <message>
-        <source>Circle</source>
-        <translation>Circle</translation>
-    </message>
-    <message>
-        <source>Ellipse</source>
-        <translation>Ellipse</translation>
-    </message>
-    <message>
-        <source>Line</source>
-        <translation>Line</translation>
-    </message>
-    <message>
-        <source>Polygon</source>
-        <translation>Polygon</translation>
-    </message>
-    <message>
-        <source>Point</source>
-        <translation>Point</translation>
-    </message>
-    <message>
-        <source>Sector</source>
-        <translation>Sector</translation>
-    </message>
-</context>
-<context>
     <name>ReportFactory</name>
     <message>
         <source>Unhandled report parameter type: &apos;%1&apos;.</source>
