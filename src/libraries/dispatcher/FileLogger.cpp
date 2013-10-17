dns-gh/oodevs

commit dfc490f10bc66588352413349ec6eb7d428492e0

SWORD-1912 *_plugin.log now use the same configuration as dispatcher.log

src/libraries/dispatcher/FileLogger.cpp
@@ -9,10 +9,10 @@
 
 #include "dispatcher_pch.h"
 #include "FileLogger.h"
+#include "Config.h"
+#include "tools/RotatingLog.h"
 #include <tools/Path.h>
-#pragma warning( push, 0 )
 #include <boost/date_time/posix_time/posix_time.hpp>
-#pragma warning( pop )
 
 using namespace dispatcher;
 
@@ -20,8 +20,9 @@ using namespace dispatcher;
 // Name: FileLogger constructor
 // Created: SBO 2011-05-19
 // -----------------------------------------------------------------------------
-FileLogger::FileLogger( const tools::Path& filename )
-    : output_( filename, std::ios::out | std::ios::trunc )
+FileLogger::FileLogger( const tools::Path& filename, const Config& config )
+    : log_( new tools::RotatingLog( *this, filename, config.GetDispatcherLogFiles(), config.GetDispatcherLogSize(), ! config.HasCheckpoint() ) )
+    , sizeInBytes_( config.IsDispatcherLogInBytes() )
 {
     // NOTHING
 }
@@ -77,9 +78,22 @@ namespace
 // -----------------------------------------------------------------------------
 void FileLogger::LogMessage( const std::string& severity, const std::string& message )
 {
+    std::stringstream s;
+    s << "[" << Timestamp() << "] " << severity << " - " << message;
     boost::mutex::scoped_lock locker( mutex_ );
-    output_ << "[" << Timestamp() << "] "
-            << severity << " - "
-            << message << std::endl;
-    output_.flush();
+    log_->Write( s.str() );
+}
+
+std::size_t FileLogger::Write( std::ostream& os, const std::string& line )
+{
+    os << line << std::flush;
+    return sizeInBytes_ ? line.size() : 1;
+}
+
+std::streamoff FileLogger::ComputeSize( const tools::Path& filename ) const
+{
+    if( sizeInBytes_ )
+        return filename.FileSize();
+    tools::Ifstream file( filename );
+    return std::count( std::istreambuf_iterator< char >( file ), std::istreambuf_iterator< char >(), '\n');
 }

src/libraries/dispatcher/FileLogger.h
@@ -12,29 +12,34 @@
 
 #include "Logger_ABC.h"
 #include "tools/FileWrapper.h"
+#include "tools/Log_ABC.h"
 #pragma warning( push, 0 )
 #include <boost/thread/mutex.hpp>
 #pragma warning( pop )
+#include <boost/scoped_ptr.hpp>
 
 namespace tools
 {
     class Path;
+    class RotatingLog;
 }
 
 namespace dispatcher
 {
+    class Config;
+
 // =============================================================================
 /** @class  FileLogger
     @brief  File logger
 */
 // Created: SBO 2011-05-19
 // =============================================================================
-class FileLogger : public Logger_ABC
+class FileLogger : public Logger_ABC, private tools::Log_ABC
 {
 public:
     //! @name Constructors/Destructor
     //@{
-    explicit FileLogger( const tools::Path& filename );
+             FileLogger( const tools::Path& filename, const Config& config );
     virtual ~FileLogger();
     //@}
 
@@ -51,12 +56,19 @@ private:
     void LogMessage( const std::string& severity, const std::string& message );
     //@}
 
+    //! @name Operations
+    //@{
+    virtual std::size_t Write( std::ostream& os, const std::string& line );
+    virtual std::streamoff ComputeSize( const tools::Path& filename ) const;
+    //@}
+
 private:
     //! @name Member data
     //@{
-    tools::Ofstream output_;
     boost::mutex mutex_;
-    //@}
+    boost::scoped_ptr< tools::RotatingLog > log_;
+    bool sizeInBytes_;
+//@}
 };
 }
 

src/libraries/dispatcher/PluginFactory.cpp
@@ -188,7 +188,7 @@ void PluginFactory::LoadPlugin( const tools::Path& name, xml::xistream& xis )
             throw MASA_EXCEPTION( "failed to load library: '" + library.ToUTF8() + "'" );
         CreateFunctor createFunction = LoadFunction< CreateFunctor >( module, "CreateInstance" );
         DestroyFunctor destroyFunction = LoadFunction< DestroyFunctor >( module, "DestroyInstance" );
-        boost::shared_ptr< Logger_ABC > logger( new FileLogger( config_.BuildSessionChildFile( name + "_plugin.log" ) ) );
+        boost::shared_ptr< Logger_ABC > logger( new FileLogger( name + "_plugin.log", config_ ) );
         boost::shared_ptr< Plugin_ABC > plugin( createFunction( model_, staticModel_, simulation_, clients_, config_, *logger, xis ), boost::bind( destroyFunction, _1, boost::ref( *logger ) ) );
         if( !plugin.get() )
             throw MASA_EXCEPTION( "CreateFunctor returned an error (see details in plugin log file)" );

commit fe9a2c888a1c50fbf06cbdeac3bb38e8d81edc9a

[dispatcher] fixed plugin log files being created in working directory instead of session directory
Regression introduced in dfc490f10bc66588352413349ec6eb7d428492e0

src/libraries/dispatcher/FileLogger.cpp
@@ -19,7 +19,8 @@ using namespace dispatcher;
 // Created: SBO 2011-05-19
 // -----------------------------------------------------------------------------
 FileLogger::FileLogger( const tools::Path& filename, const Config& config )
-    : log_( filename, config.GetDispatcherLogFiles(), config.GetDispatcherLogSize(), ! config.HasCheckpoint(), config.IsDispatcherLogInBytes() )
+    : log_( config.BuildSessionChildFile( filename ), config.GetDispatcherLogFiles(),
+        config.GetDispatcherLogSize(), ! config.HasCheckpoint(), config.IsDispatcherLogInBytes() )
 {
     // NOTHING
 }