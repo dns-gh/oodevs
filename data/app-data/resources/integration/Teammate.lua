ad3a6aa5670bc1aa6a1455aa1a9533f771cf6a7f
Mathieu Igarza <mathieu.igarza@masagroup.net>
2013-10-21 10:21:12 +0200

    Modify LeadIntegration to fix story 2152

--- data/app-data/resources/integration/Teammate.lua
+++ data/app-data/resources/integration/Teammate.lua
@@ -6,3 +6,11 @@ integration.ensureVictimsEvacuation = function( victimsUnit, medicalUnit )
     DEC_EvacuerBlessesVersTC2( victimsUnit.source, medicalUnit.source )
     return true
 end
+
+integration.AgentIsMoving = function( agent )
+    return agent.source:DEC_Agent_EstEnMouvement( )
+end
+
+integration.AgentIsInTown = function( agent )
+    return agent.source:DEC_Agent_EnVille()
+end
