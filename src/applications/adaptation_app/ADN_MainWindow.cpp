ff9c84ef4eb915ee1ad6c782bad16ba945662d5a
Antoine Blossier <antoine.blossier@masagroup.net>
2013-10-23 11:36:49 +0200

    [adn] purge consistency menu when closing a base

--- src/applications/adaptation_app/ADN_MainWindow.cpp
+++ src/applications/adaptation_app/ADN_MainWindow.cpp
@@ -294,6 +294,11 @@ void ADN_MainWindow::PurgeGUI()
     disconnect( mainTabWidget_.get(), SIGNAL( ForwardEnabled( bool ) ), actionForward_, SLOT( setEnabled( bool ) ) );
     mainLayout_->removeWidget( mainTabWidget_.get() );
     mainTabWidget_.reset();
+    // Menu
+    menuConsistencyTables_->clear();
+    consistencyTables_.clear();
+    consistencyLists_.clear();
+
     // ObjectNameManager
     gui::ObjectNameManager::getInstance()->Purge();
 }
