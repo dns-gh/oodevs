694dbf12c0aa6860a9e994f63015445359c24301
Loic Geffroy <loic.geffroy@masagroup.net>
2013-10-24 18:10:36 +0200

    [build] Fix defargs helper

--- build/defargs.cmd
+++ build/defargs.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
 set ROOT=%~dp0..
-echo --node %ROOT%/node/bin/node.exe --proxy %ROOT%/proxy/bin/proxy.exe --node_root %ROOT%/node/www --simulation %ROOT%/out/vc100_x64/sword/simulation_app.exe --replayer %ROOT%/out/vc100_x64/sword/replayer_app.exe --timeline %ROOT%/run/vc100_x64/timeline_server.exe  --user "admin@masagroup.net" --display "Admin" --password "admin"
+echo --node %ROOT%/bin/node.exe --proxy %ROOT%/bin/proxy.exe --node_root %ROOT%/go/www --simulation %ROOT%/out/vc100_x64/sword/simulation_app.exe --replayer %ROOT%/out/vc100_x64/sword/replayer_app.exe --timeline %ROOT%/run/vc100_x64/timeline_server.exe  --user "admin@masagroup.net" --display "Admin" --password "admin"
 pause
