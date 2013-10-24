2eb631c7572c2ad71915cb1c027aac922ec5742e
Mathieu Champlon <mathieu.champlon@masagroup.net>
2013-10-24 22:33:09 +0200

    [simulation] simplified serialization for MIL_NbcAgentType

--- src/libraries/simulation_kernel/Entities/Objects/NBCAttribute.cpp
+++ src/libraries/simulation_kernel/Entities/Objects/NBCAttribute.cpp
@@ -107,18 +107,9 @@ void NBCAttribute::ReadNBCAgent( xml::xistream& xis )
 void NBCAttribute::load( MIL_CheckPointInArchive& file , const unsigned int )
 {
     file >> boost::serialization::base_object< ObjectAttribute_ABC >( *this );
-
-    std::size_t nNbrNbcAgents;
     file >> danger_;
     file >> nForm_;
-    file >> nNbrNbcAgents;
-    while( nNbrNbcAgents-- )
-    {
-        unsigned int nID;
-        file >> nID;
-        if( !Insert( nID ) )
-            throw MASA_EXCEPTION( "Unknown 'AgentNBC' '" + boost::lexical_cast<std::string>( nID ) + "' for NBC object" );
-    }
+    file >> agents_;
 }
 
 // -----------------------------------------------------------------------------
@@ -128,23 +119,9 @@ void NBCAttribute::load( MIL_CheckPointInArchive& file , const unsigned int )
 void NBCAttribute::save( MIL_CheckPointOutArchive& file, const unsigned int ) const
 {
     file << boost::serialization::base_object< ObjectAttribute_ABC >( *this );
-
     file << danger_;
     file << nForm_;
-    std::size_t size = agents_.size();
-    for( auto it = agents_.begin(); it != agents_.end(); ++it )
-    {
-        if( !*it )
-            --size;
-    }
-    file << size;
-    for( auto it = agents_.begin(); it != agents_.end(); ++it )
-    {
-        if( !*it )
-            continue;
-        unsigned id = (*it)->GetID();
-        file << id;
-    }
+    file << agents_;
 }
 
 // -----------------------------------------------------------------------------
