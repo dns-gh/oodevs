dec436745ca864bbcf3dba3918286215b5ae0313
newfelMessouci <nmi@masagroup.net>
2013-11-04 18:00:10 +0100

    [integration] Added integration.getHQknowledgeOfTC2 and integration.changeDotationValue methods, and added dotation types constants

--- data/app-data/resources/integration/LogisticIntegration.lua
+++ data/app-data/resources/integration/LogisticIntegration.lua
@@ -136,3 +136,20 @@ end
 integration.getHQofTC2 = function( )
     return DEC_Pion_PcDeTC2()
 end
+
+-- Returns the knowledge of the HQ of the current TC2 (returns nil if there is no current TC2)
+integration.getHQknowledgeOfTC2 = function( )
+    local tc2 = DEC_Pion_PcDeTC2()
+    if tc2 then
+        return CreateKnowledge( integration.ontology.types.agent, tc2 )
+    end
+    return nil
+end
+
+-- Changes the value of a dotation type for the agent 
+-- with respect to a capacity factor
+-- @param dotationType : integer, the type of dotation (e.g. eDotation_Fuel)
+-- @param capacityFactor : float, the proportion (over the total capacity) of dotation that will be set
+integration.changeDotationValue = function( dotationType, capacityFactor )
+    DEC_ChangeValeurDotations2( dotationType, capacityFactor )
+end
--- data/app-data/resources/integration/Types.lua
+++ data/app-data/resources/integration/Types.lua
@@ -325,3 +325,12 @@ eAmbianceMission_None = 2
 -- firing type enumeration
 eDirectFires = 0
 eIndirectFires = 1
+
+-- Dotation type
+eDotation_Ammunition = 0
+eDotation_Fuel = 1
+eDotation_Explosives = 2
+eDotation_Mine = 3
+eDotation_BarbedWire = 4
+eDotation_Piece = 5
+eDotation_Ration = 6
