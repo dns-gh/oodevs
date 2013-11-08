608ba4f9c191f61aacecf6c33791a5cb47b8c88a
Patrick Mezard <patrick.mezard@masagroup.net>
2013-11-08 17:19:20 +0100

    [dec_test] test polygon creation

--- src/simtests/dec_test.go
+++ src/simtests/dec_test.go
@@ -108,11 +108,51 @@ end
 		`19\.4\d*`, "")
 }
 
-func (s *TestSuite) TestDecGeometry(c *C) {
-	sim, client := connectAndWaitModel(c, NewAllUserOpts(ExCrossroadSmallOrbat))
-	defer sim.Stop()
+const geometryHelpers = `
+function CreatePolygonFromXY(coords)
+    if not coords then
+        return nil
+    end
+    local points = {}
+    for i = 1, #coords/2 do
+        points[i] = DEC_Geometrie_CreerPointXY(coords[2*i-1], coords[2*i])
+    end
+    return DEC_Geometrie_CreerLocalisationPolygone(points)
+end
 
-	testCreatePoint := `
+function CreatePolygonFromLatLong(coords)
+    if not coords then
+        return nil
+    end
+    local points = {}
+    for i = 1, #coords/2 do
+        points[i] = DEC_Geometrie_CreerPointLatLong(coords[2*i-1], coords[2*i])
+    end
+    return DEC_Geometrie_CreerLocalisationPolygone(points)
+end
+
+function PointToString(point)
+    return string.format("(%.2f, %.2f)", point:X(), point:Y())
+end
+
+function PolygonToString(poly)
+    if not poly then
+        return "nil"
+    end
+    output = "["
+    if poly then
+        local points = DEC_Geometrie_ListePointsLocalisation(poly)
+        for i = 1, #points do
+        output = output .. string.format("(%.2f, %.2f), ", points[i]:X(), points[i]:Y())
+        end
+    end
+    output = output .. "]"
+    return output
+end
+`
+
+func testDecGeometryPoints(c *C, client *swapi.Client) {
+	script := geometryHelpers + `
 function TestFunction()
     points = {
         DEC_Geometrie_CreerPoint(),
@@ -123,7 +163,7 @@ function TestFunction()
     result = ""
     for i = 1, #points do
         p = points[i]
-        result = result .. string.format("(%.2f, %.2f)\n", p:X(), p:Y())
+        result = result .. PointToString(p) .. "\n"
     end
     return result
 end
@@ -133,5 +173,66 @@ end
 (1750510.29, -2899797.56)
 (10000.00, 20000.00)
 `)
-	checkScript(c, client, testCreatePoint, nil, expected, "")
+	checkScript(c, client, script, nil, expected, "")
+}
+
+func testDecGeometryPolygon(c *C, client *swapi.Client) {
+	script := geometryHelpers + `
+function TestFunction()
+    local tests = {
+        { nil, {
+            {1, 2},
+        }},
+        { {}, {
+            {1, 2},
+        }},
+        { { 5, 5 }, {
+            {1, 2},
+        }},
+        { { 5, 5, 6, 6 }, {
+            {1, 2},
+        }},
+        { { 0, 0, 5, 0, 5, 5, 0, 0 }, {
+            {2, 1},
+            -- Stupid precision
+            {6, 6},
+            -- This one is really outside
+            {10, 10},
+        }},
+    }
+    local output = ""
+    for i = 1, #tests do
+        local poly = CreatePolygonFromXY(tests[i][1])
+        local points = tests[i][2]
+        for j = 1, #points do
+            local p = DEC_Geometrie_CreerPointXY(points[j][1], points[j][2])
+            local result = DEC_Geometrie_EstPointDansLocalisation(p, poly)
+            local relation = "is in"
+            if not result then
+                relation = "is not in"
+            end
+            output = output .. string.format("%s %s %s\n", PointToString(p),
+                relation, PolygonToString(poly))
+        end
+    end
+    return output
+end
+`
+	expected := regexp.QuoteMeta("" +
+		`(1.00, 2.00) is not in nil
+(1.00, 2.00) is in []
+(1.00, 2.00) is in [(5.00, 5.00), ]
+(1.00, 2.00) is in [(5.00, 5.00), (6.00, 6.00), ]
+(2.00, 1.00) is in [(0.00, 0.00), (5.00, 0.00), (5.00, 5.00), (0.00, 0.00), ]
+(6.00, 6.00) is in [(0.00, 0.00), (5.00, 0.00), (5.00, 5.00), (0.00, 0.00), ]
+(10.00, 10.00) is not in [(0.00, 0.00), (5.00, 0.00), (5.00, 5.00), (0.00, 0.00), ]
+`)
+	checkScript(c, client, script, nil, expected, "")
+}
+
+func (s *TestSuite) TestDecGeometry(c *C) {
+	sim, client := connectAndWaitModel(c, NewAllUserOpts(ExCrossroadSmallOrbat))
+	defer sim.Stop()
+	testDecGeometryPoints(c, client)
+	testDecGeometryPolygon(c, client)
 }
