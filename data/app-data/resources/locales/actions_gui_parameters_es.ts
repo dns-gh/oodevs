18c5ba994cd6cc35dc008c41b6990f4a66101440
ldcMasa <laurent.dicesare@masagroup.net>
2013-11-18 11:10:12 +0100

    [actions_gui] Fix JIRA # SWBUG-11291 Translation : the parameters for a mission are not translated when there is more than 1 choice
    Added translations for locationcomposite types.
    Also removed unused translation file.

---
+++ data/app-data/resources/locales/actions_gui_parameters_es.ts
@@ -0,0 +1,39 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS[]>
+<TS version="2.0" language="es" sourcelanguage="en">
+  <context>
+    <name>actions_gui_Param</name>
+    <message>
+      <source>agent</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>agentknowledge</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>automat</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>crowdknowledge</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>objectknowledge</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>point</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>polygon</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>urbanknowledge</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+</TS>
---
+++ data/app-data/resources/locales/actions_gui_parameters_fr.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en">
+<context>
+    <name>actions_gui_Param</name>
+    <message>
+        <source>agent</source>
+        <translation>agent</translation>
+    </message>
+    <message>
+        <source>agentknowledge</source>
+        <translation>connaissance d&apos;agent</translation>
+    </message>
+    <message>
+        <source>automat</source>
+        <translation>automate</translation>
+    </message>
+    <message>
+        <source>crowdknowledge</source>
+        <translation>connaissance de foule</translation>
+    </message>
+    <message>
+        <source>objectknowledge</source>
+        <translation>connaissance d&apos;objet</translation>
+    </message>
+    <message>
+        <source>point</source>
+        <translation>point</translation>
+    </message>
+    <message>
+        <source>polygon</source>
+        <translation>polygone</translation>
+    </message>
+    <message>
+        <source>urbanknowledge</source>
+        <translation>bloc urbain</translation>
+    </message>
+</context>
+</TS>
