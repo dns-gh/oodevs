5ed5c0a4702c99e9ed2ed8be5b8b675fc011aaed
Patrick Mezard <patrick.mezard@masagroup.net>
2013-12-02 17:04:19 +0100

    Revert "[swapi] Read header in two steps like c++ clients"
    This reverts commit af18d4d49a4255f6daa039849c10c7417ca30a29.

--- src/swapi/message.go
+++ src/swapi/message.go
@@ -234,18 +234,10 @@ func DecodeMessage(msg *SwordMessage, tag uint32, data []byte) error {
 }
 
 func (r *Reader) Parse(header *Header, buffer []byte) ([]byte, error) {
-	hsize := uint32(0)
-	err := binary.Read(r.io, binary.BigEndian, &hsize)
+	err := binary.Read(r.io, binary.BigEndian, header)
 	if err != nil {
 		return nil, err
 	}
-	htag := uint32(0)
-	err = binary.Read(r.io, binary.BigEndian, &htag)
-	if err != nil {
-		return nil, err
-	}
-	header.Size = hsize
-	header.Tag = htag
 	size := int(header.Size - 4)
 	if size > MaxMessageSize {
 		return nil, fmt.Errorf("packet size too big %d", size)
