b8d651e101b1e3a87f03ff3c6e47a9bdfacde44d
newfelMessouci <newfel.messouci@masagroup.net>
2013-12-31 15:30:15 +0100

    [integration] Added documentation in Unit.lua

--- data/app-data/resources/integration/Unit.lua
+++ data/app-data/resources/integration/Unit.lua
@@ -1,38 +1,55 @@
-------------------------------------------------------------------
---- IDENTIFIABLE INTERFACE IMPLEMENTATION SPECIFIC TO AN UNIT
-------------------------------------------------------------------
-
-integration.identificationPriority = function( self )
-    return math.max( self:proximityLevel(), 1 ) / 100
+--- Returns the provided agent knowledge's priority to be identified
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Float, the identification priority (between 0 and 1)
+integration.identificationPriority = function( agent )
+    return math.max( agent:proximityLevel(), 1 ) / 100
 end
 
-integration.canIdentifyIt = function( self )
-    return not self:isIdentified() and masalife.brain.knowledge.me.body:computeIdentificationCapability( self, masalife.brain.knowledge.me.body ) ~= 0
+--- Returns true if the body can identify the provided agent knowledge, false otherwise
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Boolean, whether or not the body can identify the agent knowledge
+integration.canIdentifyIt = function( agent )
+    return not agent:isIdentified() and masalife.brain.knowledge.me.body:computeIdentificationCapability( agent, masalife.brain.knowledge.me.body ) ~= 0
 end
 
-integration.isIdentifiedPredicate = {
-    dependencies = { self = { "identificationLevel_" } },
-    method = function( self )
-        return self:identificationLevel() >= 66 and self:identificationLevel() <= 100
-    end
-}
+--- Returns true if the provided agent knowledge is identified, false otherwise
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Boolean, whether or not the agent knowledge is identified
+integration.isIdentifiedPredicate = function( agent )
+    return agent:identificationLevel() >= 66 and agent:identificationLevel() <= 100
+end
 
+--- Sets the body's decisional state of the provided category at the given value
+-- For instance, the category can be 'Echelon', 'Contact'...
+-- @param category String, the name of the category
+-- @param value String, the new value of the category
 integration.setDecisionalState = function( category, value)
     DEC_DecisionalState( category, value )
 end
 
+--- Sets the force ratio state to the provided value
+-- @param value Integer, the force ratio state (e.g. eForceRatioStateFavorable)
 integration.setForceRatioState = function( value )
     DEC_Agent_ChangeEtatRapportDeForce( value )
 end
 
+--- Sets the operational state to the provided value
+-- @param value Integer, the operational state (e.g. eEtatDestruction_Tactique)
 integration.setOperationalState = function( value )
     DEC_Agent_ChangeEtatOperationnel( value )
 end
 
+--- Returns the provided agent's major operational state
+-- @param agent The simulation source of an agent
+-- @return Float (between 0 and 1)
 integration.getAgentMajorOpsState = function( agent )
     return agent:DEC_Agent_EtatOpsMajeur()
 end
 
+--- Returns the provided agent's operational state
+-- If no agent is provided, this function returns the body's operational state instead
+-- @param agent The simulation source of an agent (optional)
+-- @return Float (between 0 and 1)
 integration.getAgentOpsState = function( agent )
     if agent then
         return agent:DEC_Agent_EtatOps()
@@ -41,18 +58,38 @@ integration.getAgentOpsState = function( agent )
     end
 end
 
+--- Returns the provided agent knowledge's operational state
+-- @param agent The simulation source of an agent knowledge
+-- @return Float (between 0 and 1)
+integration.getKnowledgeAgentOperationalState = function( agent )
+    return DEC_ConnaissanceAgent_EtatOps( agent )
+end
+
+--- Returns true if the provided agent is immobilized, false otherwise
+-- @param agent The simulation source of an agent
+-- @return Boolean, whether or not the agent is immobilized
 integration.isAgentImmobilized = function( agent )
     return DEC_Agent_EstImmobilise( agent )
 end
 
+--- Returns true if the provided agent knowledge is valid, false otherwise
+-- @param agent The simulation source of an agent knowledge
+-- @return Boolean, whether or not the agent knowledge is valid
 integration.isKnowledgeAgentValid = function( agent )
     return DEC_ConnaissanceAgent_EstValide( agent )
 end
 
-integration.isKnowledgesAgentsInArea = function( area )
+--- Returns all the agent knowledges inside the provided area
+-- @param area The simulation source of an area
+-- @return List of simulation sources of agent knowledges
+integration.getAgentKnowledgesInArea = function( area )
     return DEC_Connaissances_UnitesEnnemiesVivantesDansZone( area )
 end
 
+--- Returns all the friendly units inside the provided area
+-- The body will not be among the returned friendly units
+-- @param area The simulation source of an area
+-- @return Table of simulation sources of friendly agent knowledges
 integration.getFriendsInArea = function( area )
     local friends = DEC_Connaissances_UnitesAmiesDansZone( area )
     local DEC_ConnaissanceAgent_EnAgent = DEC_ConnaissanceAgent_EnAgent
@@ -66,10 +103,18 @@ integration.getFriendsInArea = function( area )
     return friends
 end
 
+--- Returns all the friendly units inside the circle defined by
+--- the provided center and distance.
+-- @param position The simulation source of a position, the center of the circle
+-- @param distance Float, the radius of the circle
+-- @return List of simulation sources of all living agent knowledges in the circle
 integration.getKnowledgesLivingAgentsInCircle = function( position, distance )
     return DEC_Connaissances_UnitesEnnemiesVivantesDansCercle( position, distance )
 end
 
+--- Returns the flying height for the provided agent knowledge
+-- @param agent Simulation source of an agent knowledge
+-- @return Float
 integration.getHeightForKnowledgeAgent = function( agent )
     return DEC_ConnaissanceAgent_Altitude( agent )
 end
@@ -90,21 +135,17 @@ integration.getTacticalFlyingHeight = function( agent )
     return DEC_Agent_GetTacticalFlyingHeight( agent.source )
 end
 
-integration.getKnowledgeAgentOperationalState = function( agent )
-    return DEC_ConnaissanceAgent_EtatOps( agent )
-end
-
--- Allows the unit to deactivate default behaviours, such as self-protection for instance.
+--- Allows the unit to deactivate default behaviours, such as self-protection for instance.
 -- The table myself.deactivations contains all the current deactivated behaviours.
--- @param behaviour : A string describing a behaviour (e.g. "selfprotect")
+-- @param behaviour String describing a behaviour (e.g. "selfprotect")
 integration.deactivate = function( behaviour )
     local myself = myself
     myself.deactivations = myself.deactivations or {}
     myself.deactivations[ behaviour ] = ( myself.deactivations[ behaviour ] or 0 ) + 1
 end
 
--- Allows the unit to reactivate previously deactivated behaviours.
--- @param behaviour : A string describing a behaviour (e.g. "selfprotect")
+--- Allows the unit to reactivate previously deactivated behaviours.
+-- @param behaviour String describing a behaviour (e.g. "selfprotect")
 integration.reactivate = function( behaviour )
     local myself = myself
     myself.deactivations = myself.deactivations or {}
@@ -117,32 +158,35 @@ integration.reactivate = function( behaviour )
     end
 end
 
--- Returns true if the provided behaviour is currently inactive, false otherwise
--- @param behaviour : A string describing a behaviour (e.g. "selfprotect")
+--- Returns true if the provided behaviour is currently inactive, false otherwise
+-- @param behaviour String describing a behaviour (e.g. "selfprotect")
 integration.isInactive = function( behaviour )
     return myself.deactivations and myself.deactivations[ behaviour ]
 end
 
-------------------------------------------------------------------
---- SAFEGUARDABLE INTERFACE IMPLEMENTATION SPECIFIC TO AN UNIT
-------------------------------------------------------------------
-integration.threatLevel = function( self )
-    return self:computeAggressiveness()
+--- Returns the threat level of the provided agent knowledge
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Float (between 0 and 100)
+integration.threatLevel = function( agent )
+    return agent:computeAggressiveness()
 end
 
-------------------------------------------------------------------
---- FLEEABLE INTERFACE IMPLEMENTATION SPECIFIC TO AN UNIT
-------------------------------------------------------------------
-integration.fleePriority = function( self )
-    return math.max( self:proximityLevel(), 1 ) / 100
+--- Returns the provided agent's priority to flee
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Float, the fleeing priority (between 0 and 1)
+integration.fleePriority = function( agent )
+    return math.max( agent:proximityLevel(), 1 ) / 100
 end
 
-------------------------------------------------------------------
---- COMMANDING INTERFACE IMPLEMENTATION SPECIFIC TO AN UNIT
--------------------------------------------------------------------
-
-
-integration.unit_commandEfficiency = function( self, individualTask, leadTask, maxNormalization, taskParams, threat )
+--- Returns the command effiency of the given task regarding its potential effects
+-- @param commander Masalife knowledge of a company
+-- @param individualTask Masalife knowledge of a task knowledge
+-- @param leadTask Masalife knowledge of a task knowledge
+-- @param maxNormalization Float, the max value that can be returned by this method
+-- @param taskParam List of task parameters
+-- @param threat Table
+-- @return Float
+integration.unit_commandEfficiency = function( commander, individualTask, leadTask, maxNormalization, taskParams, threat )
 local effects =
 {
     -- Mvt effects
@@ -213,49 +257,77 @@ local effects =
     return maxNormalization - res / #effects
 end
 
-integration.unit_communicate = function( self, task, objectives )
-    integration.SendMessage( "Order", self, { order = task.name, params = objectives } )
+--- Sends an "Order" message to the provided receiver with the given parameters
+-- @param receiver Masalife knowledge of an agent
+-- @param task Table
+-- @param objectives Table
+integration.unit_communicate = function( receiver, task, objectives )
+    integration.SendMessage( "Order", receiver, { order = task.name, params = objectives } )
 end
 
-integration.isThreateningOrHostilePredicate = {
-     dependencies = "none",
-        method = function( self )
-            return self:isHostile() or self:isThreatening()
-        end
-    }
+--- Returns true if the provided agent knowledge is threatening or hostile, false otherwise
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return Boolean, whether or not the agent knowledge is hostile or threatening
+integration.isThreateningOrHostilePredicate = function( self )
+    return self:isHostile() or self:isThreatening()
+end
 
+--- Returns the name of the provided agent knowledge
+-- @param agent Masalife knowledge of an agent knowledge
+-- @return String
 integration.getName = function( agent )
     return DEC_GetSzName( agent.source )
 end
 
+--- Returns all the wounded or dead units inside the circle defined by
+--- the provided center and distance.
+-- @param position The simulation source of a position, the center of the circle
+-- @param distance Float, the radius of the circle
+-- @return List of simulation sources of all wounded or dead agent knowledges in the circle
 integration.getWoundedOrDeadUnitsInCircle = function( position, radius )
-    local woundedUnitsInCircle = {}
-    woundedUnitsInCircle = DEC_Connaissances_UnitesBlesseesOuTueesDansCercle( position, radius )
-    return woundedUnitsInCircle
+    return DEC_Connaissances_UnitesBlesseesOuTueesDansCercle( position, radius )
 end
 
+--- Returns all the civilians in the provided area
+-- @param area Masalife knowledge of an area
+-- @return List of simulation sources of all civilian units in the area
 integration.getCiviliansInArea = function( area )
     return DEC_Connaissances_UnitesCivilesDansZone( area.source )
 end
 
+--- Returns all the wounded units in the provided area
+-- @param area Masalife knowledge of an area
+-- @return List of simulation sources of all wounded units in the area
 integration.getWoundedInArea = function( area )
     return DEC_Connaissances_UnitesBlesseesDansZone( area.source )
 end
 
+--- Returns the firing distance necessary for the provided platoon to be
+--- able to shoot the body with the given probability to hit
+-- @param platoon Masalife knowledge of an agent knowledge
+-- @param ph Float, the probability to hit
+-- @return Float
 integration.getFiringDistanceToEngageMe = function( platoon, ph )
-    return DEC_Tir_PorteeMaxPourEtreTireParUnite( platoon.source, ph)
+    return DEC_Tir_PorteeMaxPourEtreTireParUnite( platoon.source, ph )
 end
 
--- Friend is flying?
+--- Returns true if the provided agent is flying, false otherwise
+-- @param teammate Masalife knowledge of an agent knowledge
+-- @return Boolean, whether or not the agent knowledge is flying
 integration.agentIsFlying = function( teammate )
     return teammate.source:DEC_Agent_EstEnVol()
 end
 
+--- Returns true if the provided agent can fly, false otherwise
+-- @param teammate Masalife knowledge of an agent knowledge
+-- @return Boolean, whether or not the agent knowledge can fly
 integration.agentCanFly = function( teammate )
     return DEC_Agent_PionCanFly( teammate.source )
 end
 
-integration.getCollidingCrowds = function( self )
+--- Returns all the crowds knowledges colliding with the body
+-- @return List of masalife knowledges of crowds
+integration.getCollidingCrowds = function()
     local simCrowds = DEC_Connaissances_CollisionsPopulations()
     local crowds = {}
     for i = 1, #simCrowds do
@@ -263,3 +335,9 @@ integration.getCollidingCrowds = function( self )
     end
     return crowds
 end
+
+------------------------------------------------------------------
+--- DECLARATIONS ENSURING BACKWARDS COMPATIBILITY
+------------------------------------------------------------------
+
+integration.isKnowledgesAgentsInArea = integration.getAgentKnowledgesInArea
