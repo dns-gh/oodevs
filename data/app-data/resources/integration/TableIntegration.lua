7a79d4039c15d5d92a26a8047381ad11251ce452
newfelMessouci <newfel.messouci@masagroup.net>
2014-01-05 18:35:50 +0100

    [integration] Added documentation in TableIntegration.lua

--- data/app-data/resources/integration/TableIntegration.lua
+++ data/app-data/resources/integration/TableIntegration.lua
@@ -1,26 +1,9 @@
---- Return true if element is present in elements in teh table
--- @param t: table
--- @param k: the element
--- @author LMT
--- @release 2010-12-09
-existsInside = function( t, k )
-    for _,content in pairs( t ) do
-        for _,element in pairs( content ) do
-            if element == k then
-                return true
-            end
-         end
-    end
-    return false
-end
-
---- Return true if element is present in a table
--- @param t: table
--- @param k: the element
--- @author DDA
--- @release 2010-04-16
+--- Return true if the provided element is a value of the given table.
+-- @param t Table
+-- @param k Any type, the seeked element
+-- @return Boolean
 exists = function( t, k )
-    for _,content in pairs( t ) do
+    for _, content in pairs( t ) do
         if content == k then
             return true
         end
@@ -28,111 +11,114 @@ exists = function( t, k )
     return false
 end
 
---- Return true if the index is valid
--- @param t: table
--- @param i: index
--- @author DDA
--- @release 2010-06-04
-existsIndex = function( t, i )
-    for index,_ in pairs( t ) do
-        if index == i then
-            return true
+--- Returns true if the given table of table contains a table with
+-- the provided element as a value.
+-- @param t Table containing only tables as values
+-- @param k Any type, the seeked element
+-- @return Boolean
+existsInside = function( t, k )
+    for _, content in pairs( t ) do
+        for _, element in pairs( content ) do
+            if element == k then
+                return true
+            end
         end
     end
     return false
 end
 
---- Return fusion table, veirify if elem is already in table
--- @param listl: first list
--- @param list2: the second one to add in the first one
--- @author GGE
--- @release 2010-08-20
-fusionList = function( listl, list2 )
+--- Returns true if the provided index is a key of the given table.
+-- @param t Table
+-- @param i Any type, the seeked index
+-- @return Boolean
+existsIndex = function( t, i )
+    return t[i] ~= nil
+end
+
+--- Returns a list containing all the values of the two given tables.
+--- Values that appear in both tables only appear once in the returned list.
+-- @param table1 First table
+-- @param table2 Second table
+-- @return List
+fusionList = function( table1, table2 )
     local res = {}
-    for _, elem in pairs( listl or emptyTable ) do
-      res[#res+1] = elem
+    for _, elem in pairs( table1 or emptyTable ) do
+        res[ #res+1 ] = elem
     end
-    for _, elem in pairs( list2 or emptyTable ) do
-        if not exists(res, elem) then
-            res[#res+1] = elem
+    for _, elem in pairs( table2 or emptyTable ) do
+        if not exists( res, elem ) then
+            res[ #res+1 ] = elem
         end
     end
     return res
-   
 end
 
--- Return the returnList without the unit
--- @param unit : element to remove
--- @param returnList : list where we want to remove
--- @author LMT
--- @release 2012-01-31
-removeFromList = function( unit, returnList )
-    for i, elemListReturn in pairs( returnList ) do
-        if unit == elemListReturn then
-            table.remove( returnList, i )
+--- Removes the given value from the provided list, and returns it
+-- @param value Any type, the seeked value
+-- @param list List upon which the removal will take place
+-- @return List, the provided list without the given value
+removeFromList = function( value, list )
+    for i, k in pairs( list ) do
+        if value == k then
+            table.remove( list, i )
             break
         end
     end
-    return returnList
+    return list
 end
 
--- Return the returnList without the unit
--- @param list : list of elements to remove
--- @param returnList : list from which the elements must be removed
--- @author NMI
--- @release 2013-07-12
-removeListFromList = function( list, returnList )
-    for _, listElement in pairs( list ) do
-        for i, elemListReturn in pairs( returnList ) do
-            if listElement == elemListReturn then
-                table.remove( returnList, i )
+-- Removes all the given values from the provided list, and returns it
+-- @param values Table, list of values to remove
+-- @param list List upon which the removal will take place
+-- @return List, the provided list without the given values
+removeListFromList = function( values, list )
+    for _, value in pairs( values ) do
+        for i, k in pairs( list ) do
+            if value == k then
+                table.remove( list, i )
                 break
             end
         end
     end
-    return returnList
+    return list
 end
 
---- Returns a deep copy of the table given in parameter
--- @param tableFrom : list of elements to copy
+--- Returns a deep copy of the given table
+-- @param tableFrom Table of elements to copy
+-- @return Table
 copyTable =  function ( tableFrom ) 
     local tableTo = {}
-    for k,v in pairs( tableFrom or emptyTable ) do
+    for k, v in pairs( tableFrom or emptyTable ) do
         tableTo[k] = v
     end
     return tableTo
 end
 
---- Returns the size of the table given in parameter
--- @param table: the table
--- @return the size of the table
--- @author NMI
--- @release 2013-05-23
+--- Returns the size of the given table
+-- @param table Table
+-- @return Integer, the number of distinct keys in the given table
 function tableSize ( table )
     local cpt = 0
     for k in pairs( table or emptyTable ) do
-      cpt = cpt + 1
+        cpt = cpt + 1
     end
     return cpt
 end
 
 
---- Clears the table given in parameter
--- @param table: the table
--- @author NMI
--- @release 2013-05-24
+--- Clears the given table
+-- @param table Table
 function clearTable ( table )
     for k in pairs ( table or emptyTable ) do
         table[ k ] = nil
     end
 end
 
---- Returns the inverse of the table given in parameter
+--- Returns the inverse of the given table
 -- For instance : if tableFrom = { 1 = "a", 2 = "b", 3 = "a" },
 -- then the function returns { "a" = { 1, 3 }, "b" = { 2 } }
--- @param tableFrom: the table
--- @author NMI
--- @release 2013-09-02 
+-- @param tableFrom Table
+-- @return Table
 integration.inverseTable = function ( tableFrom )
     local tableTo = {}
     for k, v in pairs( tableFrom or emptyTable ) do
@@ -143,8 +129,8 @@ integration.inverseTable = function ( tableFrom )
 end
 
 --- Shuffles the provided list using a Fisher-Yates shuffle
--- @param list: An integer-indexed table
--- @return Returns a random permutation of the list
+-- @param list List
+-- @return List, a random permutation of the given list
 integration.shuffleList = function( list )
     for i = #list, 2, -1 do
         local j = getRandomNumber( 1, i )
