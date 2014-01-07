ccd37a74ab2f195a11c9a988986cecb0e37afa88
Gregory Gouge <gregory.gouge@masagroup.net>
2014-01-07 11:38:38 +0100

    [integration] documentation of AnimatableIntagration.lua

--- data/app-data/resources/integration/AnimatableIntegration.lua
+++ data/app-data/resources/integration/AnimatableIntegration.lua
@@ -1,20 +1,13 @@
--------------------------------------------------------------------------------
--- Animatable Implementation : 
--- Regroup function to manage artillery field
--- @author MGD
--- @created 2010-02-18
--- @modified MGD 2010-04-19
---
--- This file is part of a MASA library or program.
--- Refer to the included end-user license agreement for restrictions.
---
--- Copyright (c) 2010 Mathématiques Appliquées SA (MASA)
--------------------------------------------------------------------------------
+-------------------------------------------------------------------
+---- ANIMATABLE INTERFACE IMPLEMENTATION
+-------------------------------------------------------------------
 
---- Return if the unit has animation capacity
--- @param knowledge on an object
--- @author MGD
--- @release 2010-02-15
+-- ----------------------------------------------------------------
+-- Animate Object
+-- ----------------------------------------------------------------
+--- Returns true if unit has the capacity to animate the provided object, false otherwise
+-- @param object an object knowledge
+-- @return Boolean, whether or not this entity can animate the object knowledge
 integration.canAnimateIt = function( object )
   return DEC_ConnaissanceObjet_PeutEtreAnime( object.source )
 end
@@ -28,6 +21,8 @@ end
 -- So, if no unit is currently working onto the provided object, the expected value is '0'. 
 -- Note that, in such a case, the 'DEC_ConnaissanceObjet_NiveauAnimation' function returns '1' because
 -- the object is considered as fully animated by the simulation (it doesn't need to be animated to be active).
+-- @param object an object knowledge
+-- @return Boolean, whether or not this entity can animate the object knowledge
 integration.animationLevel = function( object )
     if DEC_ConnaissanceObjet_PeutEtreAnime( object.source ) then -- animation level depends on the number of animating units.
         return DEC_ConnaissanceObjet_NiveauAnimation( object.source ) * 100 -- /!\ return 1 if object cannot be animated.
@@ -36,11 +31,18 @@ integration.animationLevel = function( object )
     end
 end
 
+--- Start animating an object. A report is sent.
+-- @param object Object knowledge
+-- @return true
 integration.startAnimateIt = function( object )
     object.actionAnimation = DEC__StartAnimerObjet( object.source )
     meKnowledge:RC( eRC_DebutAnimationObjet, object.source )
     return true
 end
+
+--- Stop animating an object. A report is sent
+-- @param object Object knowledge
+-- @return true
 integration.stopAnimateIt = function( object )
     object.actionAnimation = DEC__StopAction( object.actionAnimation )
     if integration.animationLevel( object ) == 0 then
@@ -49,8 +51,12 @@ integration.stopAnimateIt = function( object )
     return false
 end
 
--- Chanel crowd 
-
+-- ----------------------------------------------------------------
+-- Channel Crowd
+-- ----------------------------------------------------------------
+--- Allows the unit to channel crowd in an area
+-- @param area Area knowledge
+-- @return true
 integration.channelIt = function ( area )
     DEC_Agent_CanaliserPopulation( area.source )
 end
