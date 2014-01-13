dns-gh/oodevs

commit 512edc2556afcca6edf03ecfb0a2a632f18714f8

[gaming][simulation_kernel] Fix SWBUG-11423 "Teleporting should not reset the brain anymore, we changed it for the units, it should be the same for crowds." * Cleaning

src/libraries/simulation_kernel/Entities/Populations/MIL_Population.cpp
@@ -281,6 +281,7 @@ void MIL_Population::load( MIL_CheckPointInArchive& file, const unsigned int )
     unsigned int nAttitudeID;
     file >> nAttitudeID;
     pDefaultAttitude_ = MIL_PopulationAttitude::Find( nAttitudeID );
+    DEC_PopulationDecision* pRole;
     assert( pDefaultAttitude_ );
     file >> rArmedIndividuals_
          >> rNewArmedIndividuals_
@@ -298,16 +299,11 @@ void MIL_Population::load( MIL_CheckPointInArchive& file, const unsigned int )
          >> pKnowledgeGroup_
          >> bHasDoneMagicMove_
          >> pAffinities_
-         >> pExtensions_;
-    MT_Vector2D tmp;
-    file >> tmp;
-    vBarycenter_.reset( new MT_Vector2D( tmp ) );
-    {
-        DEC_PopulationDecision* pRole;
-        file >> pRole;
-        RegisterRole( *pRole );
-        RegisterRole( *new DEC_Representations() );
-    }
+         >> pExtensions_
+         >> vBarycenter_
+         >> pRole;
+    RegisterRole( *pRole );
+    RegisterRole( *new DEC_Representations() );
 }
 
 // -----------------------------------------------------------------------------

src/libraries/gaming/PopulationConcentration.cpp
@@ -21,17 +21,17 @@
 // Created: HME 2005-09-30
 // -----------------------------------------------------------------------------
 PopulationConcentration::PopulationConcentration( kernel::Options& options, const sword::CrowdConcentrationCreation& message, const kernel::CoordinateConverter_ABC& converter, float density )
-    : options_( options )
-    , position_( converter.ConvertToXY( message.position() ) )
-    , nID_( message.concentration().id() )
-    , density_( density )
-    , nHealthyHumans_( 0 )
-    , nWoundedHumans_( 0 )
+    : options_            ( options )
+    , position_           ( converter.ConvertToXY( message.position() ) )
+    , nID_                ( message.concentration().id() )
+    , density_            ( density )
+    , nHealthyHumans_     ( 0 )
+    , nWoundedHumans_     ( 0 )
     , nContaminatedHumans_( 0 )
-    , nDeadHumans_( 0 )
-    , radius_( 0 )
-    , deadRadius_( 0 )
-    , attitude_( static_cast< E_PopulationAttitude > ( 0 ) )
+    , nDeadHumans_        ( 0 )
+    , radius_             ( 0 )
+    , deadRadius_         ( 0 )
+    , attitude_           ( ePopulationAttitude_Calme )
 {
     AddExtension( *this );
 }

src/libraries/gaming/PopulationFlow.cpp
@@ -34,7 +34,7 @@ PopulationFlow::PopulationFlow( const sword::CrowdFlowCreation& message, const C
     , nContaminatedHumans_( 0 )
     , nDeadHumans_        ( 0 )
     , rDensity_           ( 0 )
-    , attitude_           ( static_cast< E_PopulationAttitude > ( 0 ) )
+    , attitude_           ( ePopulationAttitude_Calme )
 {
     AddExtension( *this );
 }

commit 2e6469fbe9100b9a4fa3eb52aaa0214b4a996082

[simulation_kernel] SWBUG-11577 fixed serialization crash
The refactoring in 512edc2556afcca6edf03ecfb0a2a632f18714f8 broke serialization.

src/libraries/simulation_kernel/Entities/Populations/MIL_Population.cpp
@@ -276,7 +276,7 @@ DEC_PopulationDecision& MIL_Population::GetDecision()
 void MIL_Population::load( MIL_CheckPointInArchive& file, const unsigned int )
 {
     file >> boost::serialization::base_object< MIL_Entity_ABC >( *this );
-    file >> const_cast< MIL_Army_ABC*& >( pArmy_ );
+    file >> pArmy_;
     idManager_.GetId( GetID(), true );
     unsigned int nAttitudeID;
     file >> nAttitudeID;
@@ -333,7 +333,7 @@ void MIL_Population::save( MIL_CheckPointOutArchive& file, const unsigned int ) 
          << bHasDoneMagicMove_
          << pAffinities_
          << pExtensions_
-         << (*vBarycenter_);
+         << vBarycenter_;
     SaveRole< DEC_PopulationDecision >( *this, file );
 }
 