336c03d3c0793108c40e2b0fd41c785091492ecf
Newfel Messouci <newfel.messouci@masagroup.net>
2014-01-15 15:59:11 +0100

    [integration] Moved the "Deprecated" comments

--- data/app-data/resources/integration/Observable.lua
+++ data/app-data/resources/integration/Observable.lua
@@ -5,10 +5,10 @@
 -- @name element_perceptionLevel
 -- @class function
 -- @description Return the perception level of the Element knowledge by the agent.
+-- Deprecated
 -- @param self The Element knowledge to observe.
 -- @return Return the value of the perception level, between 0 and 100.
 --
---- Deprecated
 integration.element_perceptionLevel = function( self )
     return self:getPerception()
 end
@@ -18,10 +18,10 @@ end
 -- @class function
 -- @description Return true if the knowledge is considered as a friend by the agent. A knowledge is friendly if the hostility level value is
 -- smaller than 33.
+-- Deprecated
 -- @param self The knowledge to observe.
 -- @return Return true if the knowledge is friendly, false otherwise.
 --
---- Deprecated
 integration.generic_isFriend = function( self )
     return self:hostilityLevel() < 33
 end
@@ -31,10 +31,10 @@ end
 -- @class function
 -- @description Return true if the knowledge is considered as neutral for the agent. A knowledge is neutral if the hostility level value is
 -- between 33 and 66 included.
+-- Deprecated
 -- @param self The knowledge to observe.
 -- @return Return true if the knowledge is neutral, false otherwise.
 --
---- Deprecated
 integration.generic_isNeutral = function( self )
     return self:hostilityLevel() >= 33 and self:hostilityLevel() <= 66
 end
@@ -44,10 +44,10 @@ end
 -- @class function
 -- @description Return true if the knowledge is considered as hostile for the agent. A knowledge is hostile if the hostility level value is upper
 -- than 66.
+-- Deprecated
 -- @param self The knowledge to observe.
 -- @return Return true if the knowledge is hostile, false otherwise.
 --
---- Deprecated
 integration.generic_isHostile = {
     dependencies = {},
     method = function( self )
@@ -59,10 +59,10 @@ integration.generic_isHostile = {
 -- @name generic_observationPriority
 -- @class function
 -- @description Return the priority for observe a knowledge. The priority depends on the perception level and the proximity level.
+-- Deprecated
 -- @param self The knowledge to observe.
 -- @return The value of the observation priority, between 0 and 1.
 --
---- Deprecated
 integration.generic_observationPriority = function( self )
     return math.max( self:proximityLevel(), 1 ) / 100
 end
