7e3901eeb0d9aec95483cb03d08e949811af37ba
mathieuIgarza <mathieu.igarza@masagroup.net>
2014-01-30 17:40:58 +0100

    [ integration ] - add test to test deprecated method definition.

--- data/app-data/resources/integration/Reconnoitrable.lua
+++ data/app-data/resources/integration/Reconnoitrable.lua
@@ -7,7 +7,7 @@
 -- @description Return priority to reconnoiter a target knowledge
 -- @param target The knowledge to reconnoiter
 -- @return Return a priority between 0.01 and 1
---
+-- deprecated
 integration.generic_reconnaissancePriority = function( target )
     return math.max( target:proximityLevel(), 1 ) / 100
 end
@@ -16,7 +16,8 @@ end
 -- @description Return true if the conditions to reconnoiter the knowledge are completed.
 -- @param target The knowledge to reconnoiter.
 -- @return True if the reconnaissance of the knowledge is possible, false otherwise.
---
+-- deprecated
 integration.me_canReconnoitreIt = function( target )
-    return masalife.brain.knowledge.me.body:computeReconnaissanceCapability( target, masalife.brain.knowledge.me.body ) > 0
+    return meKnowledge.computeReconnaissanceCapability 
+           and meKnowledge:computeReconnaissanceCapability( target, meKnowledge ) > 0 or true
 end
