9c1ad233ce36572ab287f470f604cd3a7f90d927
Gregory Gouge <gregory.gouge@masagroup.net>
2014-01-31 12:27:40 +0100

    [integration] add documentation to Commander.lua

--- data/app-data/resources/integration/Commander.lua
+++ data/app-data/resources/integration/Commander.lua
@@ -1,75 +1,159 @@
+-------------------------------------------------------------------
+---- BYPASSABLE INTERFACE IMPLEMENTATION
+-------------------------------------------------------------------
+
+--- Returns the company location
+-- Can be called by an agent or a company
+-- @param commander Ditectia commander
+-- @return location Simulation location
 integration.getCommanderPosition = function( commander )
     return DEC_Automate_Position( commander.source )
 end
 
+--- Returns the company subordinates with the HQ
+-- Can be called by an agent or a company
+-- @param commander Directia commander
+-- @return list of simulation agents
 integration.getSubordinateAgentsFromCommander = function( commander )
     return DEC_Automate_PionsDeAutomateAvecPC( commander.source )
 end
 
-integration.getSubordinatePosition = function( subordinate )
-    return DEC_Automate_Position( subordinate.source ) -- $$$ GGE: TODO a virer ? Doublon de getCommanderPosition. Etrange qu'un subordonné appelle une fonction DEC niveau automate
+--- Deprecated  : use integration.getCommanderPosition
+-- Returns the company location
+-- Can be called by an agent or a company
+-- @param subordinate Directia commander
+-- @return location Simulation location 
+integration.getSubordinatePosition = function( commander )
+    return DEC_Automate_Position( commander.source ) 
 end
 
+--- Returns the company subordinates with the HQ
+-- Can be called by an agent or a company
+-- @param commander Simulation commander
+-- @return list of simulation agents
 integration.getAgentsFromCommander = function( commander )
     return commander:DEC_Automate_PionsAvecPC()
 end
 
+--- Returns true if the company is engaged, false otherwise
+-- This method can only be called by an agent
+-- @param self Deprecated, unused
+-- @return Boolean, whether or not the company is engaged
 integration.isCommanderEngaged = function( self )
     return DEC_Agent_AutomateEstEmbraye()
 end
 
+--- Returns true if the company of the provided simulation agent is engaged, false otherwise
+-- This method can only be called by an agent
+-- @param agent Simulation agent
+-- @return Boolean, whether or not the company is engaged
 integration.isAgentCommanderEngaged = function( agent )
     return agent:DEC_Agent_AutomateEstEmbraye()
 end
 
+--- Returns the HQ of the company
+-- Can be called by an agent or a company
+-- @param self Deprecated, unused
+-- @return Simulation agent
 integration.commanderGetHQUnit = function( self )
     return DEC_Automate_PionPC()
 end
 
+--- Returns the company of the unit
+-- Can be called by an agent or a company
+-- @param entity Simulation agent
+-- @return Simulation commander
 integration.getCommander = function( entity )
     return DEC_GetAutomate( entity )
 end
 
+--- Returns true if the unit is contaminated, false otherwise
+-- This method can only be called by a company
+-- @param entity Simulation agent
+-- @return Boolean, whether or not the company is contaminated
 integration.isCommanderContaminated = function( entity )
     return DEC_Automate_PionEstContamine( entity )
 end
 
+--- Returns true if the provided agent is poisoned
+-- This method can only be called by a company
+-- @param entity Simulation agent
+-- @return Boolean, whether or not the company is poisoned
 integration.isCommanderPoisoned = function( entity )
     return DEC_Automate_PionEstEmpoisonne( entity )
 end
 
+--- Returns the rules of engagement for company against crowd
+-- This method can only be called by a company
+-- @return Integer, the rules of engagement type among one of the following : 
+-- <ul> <li> eEtatROEPopulation_EmploiForceInterdit (forbidden fire) </li>
+-- <li> eEtatROEPopulation_MaintienADistanceParMoyensNonLetaux (non-lethal weapons) </li>
+-- <li> eEtatROEPopulation_ArmesLetalesAutorisees (lethal weapons allowed) </li> </ul>
 integration.getPopulationROE = function( )
     return DEC_Automate_ROEPopulation( )
 end
 
+--- Returns true if method DEC_Automate_ROEPopulation exists in simulation
+-- This method can only be called by a company
+-- @see integration.getCommanderROE
+-- @return Boolean, whether or not the method exists in simulation
 integration.getPopulationROEExist = function( )
     return DEC_Automate_ROEPopulation
 end
 
+--- Returns the rules of engagement for company against military units 
+-- This method can only be called by a company
+-- @return rules of engagement type, Integer among one of the following
+-- <ul> <li> eRoeStateFreeFire </li>
+-- <li> eRoeStateRestrictedFire </li>
+-- <li> eRoeStateFireByOrder </li> </ul>  
 integration.getCommanderROE = function( )
     return DEC_Automate_ROE( )
 end
 
+--- Returns true if method DEC_Automate_ROEPopulation exists in simulation
+-- This method can only be called by a company
+-- @see integration.getCommanderROE 
+-- @return Boolean, whether or not the method exists in simulation
 integration.getCommanderROEExist = function( )
     return DEC_Automate_ROE
 end
 
+--- Returns true if the company is engaged, false otherwise
+-- Can be called by an agent or a company
+-- @return Boolean, whether or not the company is engagd 
 integration.isEngagedCommander = function( )
     return DEC_Automate_EstEmbraye( )
 end
 
-integration.getCommanderEfficacity = function( commander, capacity )
-    return DEC_Automate_GetEfficacite( commander, capacity )
+--- Returns the agent efficiency for the selected capacity
+-- This method can only be called by a company
+-- @param agent DirectIA agent
+-- @param capacity, Integer among one of the following :
+-- <ul> <li> PionEfficiencyRecon </li>
+-- <li> ePionEfficiencyCombatSupport </li>
+-- <li> ePionEfficiencyCombat </li>
+-- <li> ePionEfficiencyMobilitySupport </li>
+-- <li> ePionEfficiencyCounterMobilitySupport </li>
+-- <li> ePionEfficiencyProtectionSupport </li>
+-- <li> ePionEfficiencyEngineeringSupport </li>
+-- <li> ePionEfficiencyUrbanArea </li>
+-- <li> ePionEfficiencyCommon </li> </ul>  
+-- @return Integer, value is between 0 and 100. The higher the percentage, the higher is the efficiency
+integration.getCommanderEfficacity = function( agent, capacity )
+    return DEC_Automate_GetEfficacite( agent, capacity )
 end
 
----- Returns the HQ of a company (simulation object).
--- Can be called by an agent or a company.
+---- Returns the HQ (simulation agent) of a company
+-- Can be called by an agent or a company
+-- @return Simulation agent
 integration.getHQ = function( )
     return DEC_Automate_PionPCDeAutomate( DEC_GetAutomate( myself ) )
 end
 
----- Returns the HQ of a company (knowledge).
--- Can be called by an agent or a company.
+---- Returns the HQ (directIA agent knowledge) of a company
+-- Can be called by an agent or a company
+-- @return knowledge HQ
 integration.getKnowledgeHQ = function( )
     return CreateKnowledge( integration.ontology.types.agent, integration.getHQ() )
 end
