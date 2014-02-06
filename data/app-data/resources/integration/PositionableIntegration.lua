119520cb055127d7b05c8846ae5e092acbf17a8d
Newfel Messouci <newfel.messouci@masagroup.net>
2014-02-06 12:41:15 +0100

    [integration] Added documentation in PositionableIntegration.lua and added the integration.getInstallationLevel method

--- data/app-data/resources/integration/PositionableIntegration.lua
+++ data/app-data/resources/integration/PositionableIntegration.lua
@@ -1,25 +1,21 @@
--------------------------------------------------------------------------------
--- generic.knowledge.Positionnable Implementation :
--- Regroup function to manage platoon equipement
--- @author MGD
--- @created 2010-06-03
---
--- This file is part of a MASA library or program.
--- Refer to the included end-user license agreement for restrictions.
---
--- Copyright (c) 2010 Mathématiques Appliquées SA (MASA)
--------------------------------------------------------------------------------
-
---Positionnable Implementation
+--- Returns true if this entity is posted, false otherwise.
+--- Sets the myself.location variable to the given localized element.
+-- This method can only be called by an agent or a crowd.
+-- @see integration.startMoveToIt
+-- @see integration.isPosted
+-- @param reachable Localized element knowledge
+-- @return Boolean
 integration.takeUpPosition = function( reachable )
     myself.location = reachable
-    if DEC_Agent_NiveauInstallation() >= eNiveauInstallation_Poste then -- corresponding to E_PostureType::ePosturePoste
-        -- reportFunction(eRC_EnPosteFaceADirectionDangereuse )
-        return true
-    end
-    return false
+    return integration.isPosted()
 end
 
+--- Starts taking up position on the given object if it can be occupied.
+-- Returns true if the given object can be occupied, false otherwise.
+-- May display a report.
+-- This method can only be called by an agent.
+-- @param object Object knowledge
+-- @return Boolean
 integration.takeUpPositionObject = function( object )
     if( DEC_ConnaissanceObjet_PeutEtreOccupe( object.source ) ) then
         myself.actionOccupy = DEC__StartOccuperObjet( object.source )
@@ -31,41 +27,49 @@ integration.takeUpPositionObject = function( object )
     end
 end
 
-
---- Indicate if the unit is posted
--- @param the unit
--- @return bool
--- @author MGD
--- @created 2010-06-03
-integration.isPosted = function( self )
-    if DEC_Agent_NiveauInstallation() >= eNiveauInstallation_Poste then
-        return true
-    end
-    return false
+--- Returns true if this entity is posted, false otherwise.
+-- This method can only be called by an agent or a crowd.
+-- @return Boolean
+integration.isPosted = function()
+    return DEC_Agent_NiveauInstallation() >= eNiveauInstallation_Poste
 end
 
---- Indicate if the unit is parked
--- @param the unit
--- @return bool
--- @author LMT
--- @created 2012-02-18
-integration.isParked = function( self )
-    if DEC_Agent_NiveauInstallation() >= eNiveauInstallation_PosteAmenage then
-        return true
-    end
-    return false
+--- Returns true if this entity is parked, false otherwise.
+-- This method can only be called by an agent or a crowd.
+-- @return Boolean
+integration.isParked = function()
+    return DEC_Agent_NiveauInstallation() >= eNiveauInstallation_PosteAmenage
 end
 
--- Returns "true" if the issued position is in a contaminating object.
+--- Returns the installation level of this entity.
+-- This method can only be called by an agent or a crowd.
+-- @return Integer, the installation level among one of the following constants :
+-- <ul> <li> eNiveauInstallation_Mouvement </li>
+-- <li> eNiveauInstallation_MouvementDiscret </li>
+-- <li> eNiveauInstallation_Arret </li>
+-- <li> eNiveauInstallation_PosteReflexe </li>
+-- <li> eNiveauInstallation_Poste </li>
+-- <li> eNiveauInstallation_PosteAmenage </li>
+-- <li> eNiveauInstallation_PostePrepareGenie </li> </ul>
+integration.getInstallationLevel = function()
+    return DEC_Agent_NiveauInstallation()
+end
+
+--- Returns true if the given position is inside a contaminating object, false otherwise.
+-- @param position Localized element knowledge, defining a "getPosition" method returning a simulation position.
+-- @return Boolean
 integration.isPositionContaminated = function( position )
-    return DEC_ObjectKnowledge_IsPositionInside( myself ,"contamination", position:getPosition() )
+    return DEC_ObjectKnowledge_IsPositionInside( myself, "contamination", position:getPosition() )
 end
 
--- Returns "true" if the last position of the itinerary is in a contaminating object.
+--- Returns true if the last position of the given itinerary is inside a contaminating object, false otherwise.
+-- @see integration.isPositionContaminated
+-- @param position Localized element knowledge, defining a "getPosition" method returning a simulation position.
+-- @return Boolean
 integration.isItineraryContaminated = function( itinerary )
     local pos = DEC_Itineraire_DernierPoint( itinerary.source )
     if pos then
-        return DEC_ObjectKnowledge_IsPositionInside( myself ,"contamination", pos )
+        return integration.isPositionContaminated( pos )
     end
     return false
 end
