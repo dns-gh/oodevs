78db7ecce1a7e4e1ac16bb15a00ecf797e94bd87
Benoît Amiaux <benoit.amiaux@masagroup.net>
2014-02-12 15:57:23 +0100

    [actions] swbug #11763: Don't ignore message list parameters

--- src/tests/actions_test/ActionsTest.cpp
+++ src/tests/actions_test/ActionsTest.cpp
@@ -22,6 +22,7 @@
 #include "clients_kernel/Time_ABC.h"
 #include "clients_kernel/StaticModel.h"
 #include "clients_kernel/AgentTypes.h"
+#include "clients_kernel/MagicActionType.h"
 #include "protocol/Protocol.h"
 #include <tools/Resolver.h>
 #include <protocol/ServerPublisher_ABC.h>
@@ -123,4 +124,100 @@ BOOST_FIXTURE_TEST_CASE( serialize_null_parameter_when_type_attribute_missing, F
     // second and fifth parameter are optional
     BOOST_CHECK( parameters.elem( 1 ).null_value() );
     BOOST_CHECK( parameters.elem( 4 ).null_value() );
+}
+
+namespace
+{
+    void Filter( xml::xostream& xos, xml::xistream& xis )
+    {
+        xis >> xml::list([&]( const std::string&, const std::string& name, xml::xistream& sub ){
+            xos << xml::start( name );
+            Filter( xos, sub );
+            xos << xml::end;
+        });
+        xis >> xml::attributes([&]( const std::string&, const std::string& name, xml::xistream& sub ){
+            if( name != "name" )
+                xos << xml::attribute( name, sub );
+        });
+    }
+}
+
+// test whether action_abc factory gives same output
+// with either xml or protobuf input
+BOOST_FIXTURE_TEST_CASE( serializes_change_equipment_state, Fixture )
+{
+    const QDateTime now = QDateTime::fromString( "2010-09-01T15:18:40", Qt::ISODate );
+    const std::string input =
+    "<action id='change_equipment_state' name='NO TRANSLATION' target='17' time='" + now.toString( Qt::ISODate ).toStdString() + "' type='magicunit'>"
+    "   <parameter name='EquipmentStates' type='list'>"
+    "       <parameter name='EquipmentStates' type='list'>"
+    "           <parameter name='EquipmentStates' type='identifier' value='1'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='2'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='3'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='4'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='5'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='6'/>"
+    "           <parameter name='EquipmentStates' type='quantity' value='7'/>"
+    "           <parameter name='EquipmentStates' type='list'>"
+    "               <parameter name='EquipmentStates' type='identifier' value='13'/>"
+    "           </parameter>"
+    "       </parameter>"
+    "   </parameter>"
+    "</action>";
+    xml::xistringstream xis( input );
+    MockAgent agent;
+    MOCK_EXPECT( entityResolver.FindAgent ).with( 17u ).returns( &agent );
+    MOCK_EXPECT( agent.GetId ).returns( 17u );
+    MOCK_EXPECT( agent.GetName ).returns( "agent" );
+    staticModel.types_.tools::StringResolver< kernel::MagicActionType >::Register(
+        "change_equipment_state", *new kernel::MagicActionType( "change_equipment_state", sword::UnitMagicAction::change_equipment_state ) );
+    std::auto_ptr< Action_ABC > fromXml( actionFactory.CreateAction( xis >> xml::start( "action" ) ) );
+    BOOST_REQUIRE( fromXml.get() );
+    auto serialize = [&]( const Action_ABC& action ) -> std::string {
+        xml::xostringstream xos;
+        action.Serialize( xos << xml::start( "action" ) );
+        return xos.str();
+    };
+    auto checkxml = [&]( const std::string expected, const std::string& obtained ){
+        xml::xistringstream xis1( expected ), xis2( obtained );
+        xml::xostringstream xos1, xos2;
+        Filter( xos1, xis1 );
+        Filter( xos2, xis2 );
+        BOOST_REQUIRE_EQUAL( xos1.str(), xos2.str() );
+    };
+    auto publish = [&]( const Action_ABC& action ) -> sword::ClientToSim {
+        sword::ClientToSim msg;
+        MockPublisher publisher;
+        MOCK_EXPECT( publisher.ClientToSim ).once().with( mock::retrieve( msg ) );
+        action.Publish( publisher, 42 );
+        return msg;
+    };
+    auto checkmagic = [&]( const sword::ClientToSim& msg ){
+        BOOST_REQUIRE( msg.message().has_unit_magic_action() );
+        const auto& magic = msg.message().unit_magic_action();
+        BOOST_REQUIRE( magic.has_parameters() );
+        BOOST_REQUIRE_EQUAL( magic.parameters().elem_size(), 1 );
+        const auto& states = magic.parameters().elem( 0 );
+        BOOST_REQUIRE_EQUAL( states.value_size(), 1 );
+        const auto& value = states.value( 0 );
+        BOOST_REQUIRE_EQUAL( value.list_size(), 8 );
+        const auto& breaks = value.list( 7 );
+        BOOST_REQUIRE_EQUAL( breaks.list_size(), 1 );
+        const auto& list = breaks.list( 0 );
+        BOOST_REQUIRE( list.has_identifier() );
+        BOOST_REQUIRE_EQUAL( list.identifier(), 13u );
+    };
+    const auto str1 = serialize( *fromXml );
+    checkxml( input, str1 );
+    const auto msg1 = publish( *fromXml );
+    checkmagic( msg1 );
+    // check proto factory now
+    MOCK_EXPECT( time.GetDateTime ).returns( now );
+    MOCK_EXPECT( entityResolver.FindEntity ).with( 17u ).returns( &agent );
+    std::auto_ptr< Action_ABC > fromProto( actionFactory.CreateAction( msg1, false ) );
+    BOOST_REQUIRE( fromProto.get() );
+    const auto str2 = serialize( *fromProto );
+    checkxml( input, str2 );
+    const auto msg2 = publish( *fromProto );
+    BOOST_REQUIRE_EQUAL( msg1.DebugString(), msg2.DebugString() );
 }
