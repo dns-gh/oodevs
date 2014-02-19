e9c9c9d47515918be2ae0561743838bdf9a661a9
ldcMasa <laurent.dicesare@masagroup.net>
2014-02-19 17:17:22 +0100

    [simulation] Cleanup trailing whitespaces.

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Logistic/PHY_RolePionLOG_Maintenance.h
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Logistic/PHY_RolePionLOG_Maintenance.h
@@ -51,7 +51,7 @@ public:
     void load( MIL_CheckPointInArchive&, const unsigned int );
     void save( MIL_CheckPointOutArchive&, const unsigned int ) const;
     //@}
-        
+
     //! @name Operations
     //@{
     virtual void Update( bool bIsDead );
