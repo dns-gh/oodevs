ac3ab2446f65d5633032caae3a4e0811e643ffde
Patrick Mezard <patrick.mezard@masagroup.net>
2014-02-19 14:03:12 +0100

    [build] remove unused launcher_app and launcher_test_app

--- src/applications/launcher_test_app/launcher_test_app_pch.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// launcher_test_app.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "launcher_test_app_pch.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
