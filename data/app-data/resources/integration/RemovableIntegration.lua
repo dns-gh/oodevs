930b256379c4f041b9282934a954a9c6299c1512
mathieuIgarza <mathieu.igarza@masagroup.net>
2014-02-26 16:20:46 +0100

    [ integration ] - Add tests in order to fix story SWORD-2288 - make the method 'canRemoveIt' method compatible with urban block (allowing security agents to deconstruct urban blocks)

--- data/app-data/resources/integration/RemovableIntegration.lua
+++ data/app-data/resources/integration/RemovableIntegration.lua
@@ -64,13 +64,14 @@ end
 -- @author PSN
 -- @release 2010-03-30
 integration.canRemoveIt = function( object )
-    -- $$$ MIA return false if object is not valid.
-    return DEC_Agent_PeutDetruireObjet( object.source )
+    return masalife.brain.core.class.isOfType( object, integration.ontology.types.object ) 
+           and DEC_Agent_PeutDetruireObjet( object.source ) or true  -- return false if object is not valid.
 end
 
 -- $$$ MIA merge with military: return true if agent can remove the objet depending on its physical capabilities
 integration.canRemoveItSecu = function( object )
-    object.canRemoveit = object.canRemoveit or DEC_Agent_PeutDetruireObjet( object.source )
+    object.canRemoveit = masalife.brain.core.class.isOfType( object, integration.ontology.types.object ) 
+                         and DEC_Agent_PeutDetruireObjet( object.source ) or true
     return object.canRemoveit
 end
 
