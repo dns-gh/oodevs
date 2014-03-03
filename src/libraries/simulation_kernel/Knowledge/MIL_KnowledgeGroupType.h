fcd5b969ded27b962a2e8cebfab4ace340f8d31a
Benoît Amiaux <benoit.amiaux@masagroup.net>
2014-03-03 11:47:22 +0100

    [kernel] Cleanup MIL_KnowledgeGroupType

--- src/libraries/simulation_kernel/Knowledge/MIL_KnowledgeGroupType.h
+++ src/libraries/simulation_kernel/Knowledge/MIL_KnowledgeGroupType.h
@@ -10,15 +10,10 @@
 #ifndef __MIL_KnowledgeGroupType_h_
 #define __MIL_KnowledgeGroupType_h_
 
-#include <map>
-
 namespace xml
 {
     class xistream;
 }
-
-class MIL_KnowledgeGroup;
-class MIL_Army_ABC;
 
 // =============================================================================
 // @class  MIL_KnowledgeGroupType
@@ -41,7 +36,7 @@ public:
     static const MIL_KnowledgeGroupType* FindType( unsigned int nID );
     static const MIL_KnowledgeGroupType* FindTypeOrAny( const std::string& strName );
     //@}
-    
+
     //! @name Accessors
     //@{
     double GetKnowledgeAgentMaxLifeTime() const;
@@ -56,32 +51,17 @@ public:
 private:
     MIL_KnowledgeGroupType( const std::string& strName, xml::xistream& xis, double timeFactor );
 
-    //! @name Types
-    //@{
-    typedef std::map< std::string, const MIL_KnowledgeGroupType* > T_KnowledgeGroupTypeMap;
-    typedef T_KnowledgeGroupTypeMap::const_iterator CIT_KnowledgeGroupTypeMap;
-    //@}
-
-    //! @name Helper
-    //@{
-    static void ReadKnowledgeGroup( xml::xistream& xis, double timeFactor );
-    //@}
-
 private:
     //! @name Member data
     //@{
     const std::string  strName_;
     const unsigned int nID_;
-    double rKnowledgeAgentMaxLifeTime_;
-    double rKnowledgeAgentMaxDistBtwKnowledgeAndRealUnit_;
-    double rKnowledgeAgentExtrapolationTime_;
-    double rKnowledgePopulationMaxLifeTime_;
-    double rCommunicationDelay_;
+    const double       rKnowledgeAgentMaxLifeTime_;
+    const double       rKnowledgeAgentMaxDistBtwKnowledgeAndRealUnit_;
+    const double       rKnowledgeAgentExtrapolationTime_;
+    const double       rKnowledgePopulationMaxLifeTime_;
+    const double       rCommunicationDelay_;
     //@}
-
-private:
-    static T_KnowledgeGroupTypeMap knowledgeGroupTypes_;
-    static unsigned int nNextID_;
 };
 
 #endif // __MIL_KnowledgeGroupType_h_
