cd11296fe6e8e5f9497cb46e0b04fee1014787cd
Lucie Mignot <lmt@masagroup.net>
2014-03-04 12:08:21 +0100

    Add comments to "ControlableIntegration" file

--- data/app-data/resources/integration/ControlableIntegration.lua
+++ data/app-data/resources/integration/ControlableIntegration.lua
@@ -1,9 +1,12 @@
 -- Controlable Implementation
 local pointCircleSize = 250 -- meters 
-local pointRecceSpeed   = 3
--- --------------------------------------------------------------------------------
--- Control area
--- --------------------------------------------------------------------------------
+local pointRecceSpeed = 3
+
+--- Start to control an area
+-- An action in the simulation is started
+-- This method can only be called by an agent
+-- @param area The DirectIA area to control
+-- @return true
 integration.startControlArea = function( area )
     area.perceptionID = DEC_Perception_ActivateLocationProgressiveRecce( area.source, 3 )
     area.bActionFinished = false
@@ -13,6 +16,12 @@ integration.startControlArea = function( area )
     reportFunction(eRC_DebutControleZone )
     return true
 end
+
+--- Stop to control an area
+-- The action in the simulation is stopped
+-- This method can only be called by an agent
+-- @param area, the DirectIA area to stop controlling
+-- @return true
 integration.stopControlArea  = function( area )
     DEC_Perception_DesactiverReconnaissanceLocalisation( area.perceptionID )
     reportFunction(eRC_FinControleZone )
@@ -20,26 +29,33 @@ integration.stopControlArea  = function( area )
     return true
 end
 
--- --------------------------------------------------------------------------------
--- Control BU
--- The control effect of an urban bloc is implemented with de recce effect.
--- --------------------------------------------------------------------------------
+--- Start to control an urbanBlock
+-- An action in the simulation is started
+-- This method can only be called by an agent
+-- @param urbanBlock The DirectIA urbanBlock to control
+-- @return true
 integration.startControlUrbanBlock = function( urbanBlock )
     urbanBlock.recceAction = DEC_Perception_ActiverReconnaissanceDansBlocUrbain( urbanBlock.source )
     reportFunction(eRC_DebutControleBlocUrbain )
     return true
 end
+
+--- Stop to control an urbanBlock
+-- The action in the simulation is stopped
+-- This method can only be called by an agent
+-- @param urbanBlock, the DirectIA urbanBlock to stop controlling
+-- @return true
 integration.stopControlUrbanBlock  = function( urbanBlock )
     DEC_Perception_DesactiverReconnaissanceDansBlocUrbain( urbanBlock.recceAction )
     reportFunction(eRC_FinControleBlocUrbain )
     return true
 end
 
--- --------------------------------------------------------------------------------
--- Control Point
--- Le control d'un  point est implémenté avec les capacités de reconnaissance 
--- d'un point
--- --------------------------------------------------------------------------------
+--- Start to control a point
+-- An action in the simulation is started
+-- This method can only be called by an agent
+-- @param point The DirectIA point to control
+-- @return true
 integration.startControlPoint = function( point )
     point.perceptionID = DEC_Perception_ActiverReconnaissancePoint( point.source, pointCircleSize, pointRecceSpeed )
     point.bActionFinished = false
@@ -49,6 +65,12 @@ integration.startControlPoint = function( point )
     reportFunction(eRC_DebutControlPoint )
     return true
 end
+
+--- Stop to control a point
+-- The action in the simulation is stopped
+-- This method can only be called by an agent
+-- @param point, the DirectIA point to stop controlling
+-- @return true
 integration.stopControlPoint = function( point )
     DEC_Perception_DesactiverReconnaissancePoint( point.perceptionID )
     reportFunction(eRC_FinControlPoint )
