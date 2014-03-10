dns-gh/oodevs

commit 6a9e1cabf9928236f3c349be34e3970cd20394a5

[kernel] Allow PHY_Dotation::Supply to remove supplies

src/libraries/simulation_kernel/Entities/Agents/Units/Dotations/PHY_Dotation.cpp
@@ -442,9 +442,10 @@ double PHY_Dotation::Supply( double rSupply )
 {
     if( bDotationBlocked_ )
         return 0.;
-    const double rNewSupply = std::min( rSupply, rCapacity_ - rConsumptionReservation_ - rFireReservation_ - rValue_ );
-    SetValue( rValue_ + rNewSupply );
-    return rNewSupply;
+    const double rNewSupply = std::min( rSupply, rCapacity_ - rConsumptionReservation_ - rFireReservation_ );
+    const double previous = rValue_;
+    SetValue( std::max( 0., rNewSupply ) );
+    return rValue_ - previous;
 }
 
 // -----------------------------------------------------------------------------

commit 67b970c7860038b84f02ae289ef33613cb5df86a

[simulation_kernel] SWORD-2270 fixed PHY_Dotation::Supply
It had been broken in 6a9e1cabf9928236f3c349be34e3970cd20394a5

src/libraries/simulation_kernel/Entities/Agents/Units/Dotations/PHY_Dotation.cpp
@@ -440,10 +440,10 @@ bool PHY_Dotation::HasReachedSupplyThreshold() const
 double PHY_Dotation::Supply( double rSupply )
 {
     if( bDotationBlocked_ )
-        return 0.;
-    const double rNewSupply = std::min( rSupply, rCapacity_ - rConsumptionReservation_ - rFireReservation_ );
+        return 0;
+    const double rValue = std::min( rValue_ + rSupply, rCapacity_ - rConsumptionReservation_ - rFireReservation_ );
     const double previous = rValue_;
-    SetValue( std::max( 0., rNewSupply ) );
+    SetValue( std::max( 0., rValue ) );
     return rValue_ - previous;
 }
 