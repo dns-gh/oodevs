24adb1d7931b68e4d721d58b4ad4c36a6181257b
Mathieu Champlon <mathieu.champlon@masagroup.net>
2014-03-11 08:34:26 +0100

    [integration] added integration.startConsumingResources wrapping DEC_StartConsumingResources

--- data/app-data/resources/integration/LogisticIntegration.lua
+++ data/app-data/resources/integration/LogisticIntegration.lua
@@ -161,3 +161,10 @@ integration.enforceAviationResourcesFromRange = function( range )
     DEC_EnforceAviationResources( myself, range )
 end
 
+-- Triggers the decrease over time of resources for the agent.
+-- @param dotation : the dotation category
+-- @param value : the total fraction of the maximum to decrease
+-- @param duration : the duration of the decrease in seconds
+integration.startConsumingResources = function( dotation, value, duration )
+    DEC_StartConsumingResources( myself, category, value, duration )
+end
