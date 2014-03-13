86178410700bd70d2c6530bba6b1d6994d5bebd2
Loic Geffroy <loic.geffroy@masagroup.net>
2014-03-13 14:55:20 +0100

    [simtests] Check client.Pause response

--- src/simtests/automat_test.go
+++ src/simtests/automat_test.go
@@ -160,7 +160,8 @@ func (s *TestSuite) TestCreateAutomatAndUnits(c *C) {
 	_, _, err = client.CreateAutomatAndUnits(formation.Id, InvalidIdentifier, kg0.Id, pos)
 	c.Assert(err, IsSwordError, "error_invalid_parameter")
 
-	client.Pause()
+	err = client.Pause()
+	c.Assert(err, NotNil)
 
 	// Create INF.Infantry company(contains 6 units)
 	automat, units, err := client.CreateAutomatAndUnits(formation.Id, AutomatType, kg0.Id, pos)
