1fcc335c61a9456c385e79bf0253fc210e066168
Patrick Mezard <patrick.mezard@masagroup.net>
2014-03-15 17:55:47 +0100

    Revert "Merge remote-tracking branch 'lgy/SWBUG-11903' into 5.3"
    This reverts commit 58cca811919fbc720bd658b4f2605add304a7eee, reversing
    changes made to 175ea0a66ec08127611f4f83dfdd045d9b24fc10.
    
    This change fails on vc100 with:
    
          [go] unitmagicaction_test.go:2015:
           [go]     c.Assert(err, IsNil)
           [go] setup_test.go:380:
           [go]     c.Fatalf("errors found in session %s:\n%s", session, err)
           [go] ... Error: errors found in session D:\jenkins\workspace\gosword-5.3\label\gosword\platform\vc100\5.3\data\exercises\crossroad-small-orbat\sessions\20140314T163939-4001217
           [go] Lua errors found in D:\jenkins\workspace\gosword-5.3\label\gosword\platform\vc100\5.3\data\exercises\crossroad-small-orbat\sessions\20140314T163939-400121707\messages.log:
           [go] [2014-03-14 16:39:54] <Logger plugin> <info> **** Time tick 17 - [15:21:38] - Trace - [76] : Sending message failed: ...mmunication/plugin/lua_src/CommunicationProtocol.lu
           [go] resources\integration/CommunicationIntegration.lua:224: in function 'SendMessage'
           [go] ...ecisional/dia5/models/src/military/world/Company.lua:250: in function 'sendDataToNewUnitInAutomat'
           [go] ...ecisional/dia5/models/src/military/world/Company.lua:367: in function 'UpdateNewUnitInAutomat'
           [go] ...ls/src/military/commander/skills/ManageFragOrder.bms:26: in function <...ls/src/military/commander/skills/ManageFragOrder.bms:12>
    
    This is caused by some decisional code now being called at the same tick
    than the unit creation and being activated because the unit is no longer
    marked as dead/neutralized.

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Composantes/PHY_RolePion_Composantes.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Composantes/PHY_RolePion_Composantes.cpp
@@ -145,8 +145,6 @@ PHY_RolePion_Composantes::PHY_RolePion_Composantes( MIL_Agent_ABC& pion, bool in
     {
         owner_->GetType().GetUnitType().InstanciateComposantes( *this );
         DistributeCommanders();
-        UpdateOperationalStates();
-        UpdateMajorComposante();
     }
 }
 
