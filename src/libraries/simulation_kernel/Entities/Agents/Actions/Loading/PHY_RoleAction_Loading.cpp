dns-gh/oodevs

commit ea8813c5d47eb84b230495df49826aa2dde8b394

[simulation_kernel] refactoring to remove more computer factory bloat

src/libraries/simulation_kernel/Entities/Agents/Actions/Loading/PHY_RoleAction_Loading.cpp
@@ -11,11 +11,9 @@
 
 #include "simulation_kernel_pch.h"
 #include "PHY_RoleAction_Loading.h"
-#include "AlgorithmsFactories.h"
 #include "PostureComputer_ABC.h"
-#include "HumanLoadingTimeComputer_ABC.h"
-#include "LoadedStateConsistencyComputer_ABC.h"
-#include "LoadingComputerFactory_ABC.h"
+#include "DefaultHumanLoadingTimeComputer.h"
+#include "DefaultLoadedStateConsistencyComputer.h"
 #include "NetworkNotificationHandler_ABC.h"
 #include "VisionConeNotificationHandler_ABC.h"
 #include "LoadingChangeNotificationHandler_ABC.h"
@@ -26,8 +24,7 @@
 
 BOOST_CLASS_EXPORT_IMPLEMENT( transport::PHY_RoleAction_Loading )
 
-namespace transport
-{
+using namespace transport;
 
 // -----------------------------------------------------------------------------
 // Name: PHY_RoleAction_Loading constructor
@@ -41,7 +38,7 @@ PHY_RoleAction_Loading::PHY_RoleAction_Loading()
     , bHasChanged_    ( true )
     , bHasBeenUpdated_( false )
 {
-        // NOTHING
+    // NOTHING
 }
 
 // -----------------------------------------------------------------------------
@@ -117,11 +114,11 @@ void PHY_RoleAction_Loading::SetUnloadedState()
 // -----------------------------------------------------------------------------
 double PHY_RoleAction_Loading::ComputeLoadingTime() const
 {
-    std::auto_ptr< HumanLoadingTimeComputer_ABC > loadingTimeComputer = owner_->GetAlgorithms().loadingComputerFactory_->CreateHumanLoadingTimeComputer();
-    owner_->Execute( *loadingTimeComputer );
-    if( loadingTimeComputer->GetHumansLoadedPerTimeStep() == 0. )
+    DefaultHumanLoadingTimeComputer loadingTimeComputer;
+    owner_->Execute( loadingTimeComputer );
+    if( loadingTimeComputer.GetHumansLoadedPerTimeStep() == 0 )
         return std::numeric_limits< double >::max();
-    return loadingTimeComputer->GetHumansCount() / loadingTimeComputer->GetHumansLoadedPerTimeStep();
+    return loadingTimeComputer.GetHumansCount() / loadingTimeComputer.GetHumansLoadedPerTimeStep();
 }
 
 // -----------------------------------------------------------------------------
@@ -130,11 +127,11 @@ double PHY_RoleAction_Loading::ComputeLoadingTime() const
 // -----------------------------------------------------------------------------
 double PHY_RoleAction_Loading::ComputeUnloadingTime() const
 {
-    std::auto_ptr< HumanLoadingTimeComputer_ABC > loadingTimeComputer = owner_->GetAlgorithms().loadingComputerFactory_->CreateHumanLoadingTimeComputer();
-    owner_->Execute( *loadingTimeComputer );
-    if( loadingTimeComputer->GetHumansUnloadedPerTimeStep() == 0. )
+    DefaultHumanLoadingTimeComputer loadingTimeComputer;
+    owner_->Execute( loadingTimeComputer );
+    if( loadingTimeComputer.GetHumansUnloadedPerTimeStep() == 0 )
         return std::numeric_limits< double >::max();
-    return loadingTimeComputer->GetHumansCount() / loadingTimeComputer->GetHumansUnloadedPerTimeStep();
+    return loadingTimeComputer.GetHumansCount() / loadingTimeComputer.GetHumansUnloadedPerTimeStep();
 }
 
 // -----------------------------------------------------------------------------
@@ -212,7 +209,8 @@ int PHY_RoleAction_Loading::Unload()
         if( MIL_Time_ABC::GetTime().GetCurrentTimeStep() >= nEndTimeStep_ )
         {
             nState_      = eNothing;
-            MIL_Report::PostEvent( *owner_, report::eRC_DisembarkmentFinished );            SetUnloadedState();
+            MIL_Report::PostEvent( *owner_, report::eRC_DisembarkmentFinished );
+            SetUnloadedState();
             return eEnd;
         }
         return eRunning;
@@ -226,19 +224,15 @@ int PHY_RoleAction_Loading::Unload()
 // -----------------------------------------------------------------------------
 void PHY_RoleAction_Loading::CheckConsistency()
 {
-    std::auto_ptr< LoadedStateConsistencyComputer_ABC > comp = owner_->GetAlgorithms().loadingComputerFactory_->CreateLoadedStateConsistencyComputer();
-    owner_->Execute( *comp );
-
+    DefaultLoadedStateConsistencyComputer comp;
+    owner_->Execute( comp );
     if( bIsLoaded_ )
     {
-        if( !comp->HasValidCarrier() && comp->HasValidLoadable() )
+        if( !comp.HasValidCarrier() && comp.HasValidLoadable() )
             SetUnloadedState();
     }
-    else
-    {
-        if( !comp->HasValidLoadable() )
-            SetLoadedState();
-    }
+    else if( !comp.HasValidLoadable() )
+        SetLoadedState();
 }
 
 // -----------------------------------------------------------------------------
@@ -407,7 +401,3 @@ void PHY_RoleAction_Loading::UnloadFromTransport( const MIL_Agent_ABC& /*transpo
 {
     ForceUnloadedState();
 }
-
-} // namespace transport
-
-

src/libraries/simulation_kernel/AlgorithmsFactories.cpp
@@ -19,7 +19,6 @@
 #include "simulation_kernel/OnComponentFunctorComputerFactory.h"
 #include "simulation_kernel/OnComponentLendedFunctorComputerFactory.h"
 #include "simulation_kernel/DotationComputerFactory.h"
-#include "simulation_kernel/LoadingComputerFactory.h"
 #include "simulation_kernel/UrbanLocationComputerFactory.h"
 #include "simulation_kernel/WeaponReloadingComputerFactory.h"
 #include <boost/serialization/export.hpp>
@@ -41,7 +40,6 @@ AlgorithmsFactories::AlgorithmsFactories()
     , onComponentFunctorComputerFactory_      ( new OnComponentFunctorComputerFactory() )
     , onComponentLendedFunctorComputerFactory_( new OnComponentLendedFunctorComputerFactory() )
     , dotationComputerFactory_                ( new dotation::DotationComputerFactory() )
-    , loadingComputerFactory_                 ( new transport::LoadingComputerFactory() )
     , weaponReloadingComputerFactory_         ( new firing::WeaponReloadingComputerFactory() )
     , urbanLocationComputerFactory_           ( new urbanLocation::UrbanLocationComputerFactory() )
 {

src/libraries/simulation_kernel/AlgorithmsFactories.h
@@ -39,10 +39,6 @@ namespace human
 {
     class HealComputerFactory_ABC;
 }
-namespace transport
-{
-    class LoadingComputerFactory_ABC;
-}
 
 namespace urbanLocation
 {
@@ -92,7 +88,6 @@ public:
     std::auto_ptr< OnComponentFunctorComputerFactory_ABC > onComponentFunctorComputerFactory_;
     std::auto_ptr< OnComponentLendedFunctorComputerFactory_ABC > onComponentLendedFunctorComputerFactory_;
     std::auto_ptr< dotation::DotationComputerFactory_ABC > dotationComputerFactory_;
-    std::auto_ptr< transport::LoadingComputerFactory_ABC > loadingComputerFactory_;
     std::auto_ptr< firing::WeaponReloadingComputerFactory_ABC > weaponReloadingComputerFactory_;
     std::auto_ptr< urbanLocation::UrbanLocationComputerFactory_ABC > urbanLocationComputerFactory_;
     //@}

src/libraries/simulation_kernel/LoadingComputerFactory.cpp
@@ -1,51 +1,0 @@
-// *****************************************************************************
-//
-// This file is part of a MASA library or program.
-// Refer to the included end-user license agreement for restrictions.
-//
-// Copyright (c) 2009 MASA Group
-//
-// *****************************************************************************
-
-#include "simulation_kernel_pch.h"
-#include "LoadingComputerFactory.h"
-#include "DefaultHumanLoadingTimeComputer.h"
-#include "DefaultLoadedStateConsistencyComputer.h"
-
-using namespace transport;
-
-// -----------------------------------------------------------------------------
-// Name: LoadingComputerFactory constructor
-// Created: AHC 2009-10-01
-// -----------------------------------------------------------------------------
-LoadingComputerFactory::LoadingComputerFactory()
-{
-    // NOTHING
-}
-
-// -----------------------------------------------------------------------------
-// Name: LoadingComputerFactory destructor
-// Created: AHC 2009-10-01
-// -----------------------------------------------------------------------------
-LoadingComputerFactory::~LoadingComputerFactory()
-{
-    // NOTHING
-}
-
-// -----------------------------------------------------------------------------
-// Name: LoadingComputerFactory::CreateHumanLoadingTimeComputer
-// Created: AHC 2009-10-01
-// -----------------------------------------------------------------------------
-std::auto_ptr< HumanLoadingTimeComputer_ABC > LoadingComputerFactory::CreateHumanLoadingTimeComputer()
-{
-    return std::auto_ptr< HumanLoadingTimeComputer_ABC >( new DefaultHumanLoadingTimeComputer() );
-}
-
-// -----------------------------------------------------------------------------
-// Name: LoadingComputerFactory::CreateLoadedStateConsistencyComputer
-// Created: AHC 2009-10-01
-// -----------------------------------------------------------------------------
-std::auto_ptr< LoadedStateConsistencyComputer_ABC > LoadingComputerFactory::CreateLoadedStateConsistencyComputer()
-{
-    return std::auto_ptr< LoadedStateConsistencyComputer_ABC >( new DefaultLoadedStateConsistencyComputer() );
-}

src/libraries/simulation_kernel/LoadingComputerFactory.h
@@ -1,42 +1,0 @@
-// *****************************************************************************
-//
-// This file is part of a MASA library or program.
-// Refer to the included end-user license agreement for restrictions.
-//
-// Copyright (c) 2009 MASA Group
-//
-// *****************************************************************************
-
-#ifndef __transport_LoadingComputerFactory_H__
-#define __transport_LoadingComputerFactory_H__
-
-#include "LoadingComputerFactory_ABC.h"
-
-namespace transport
-{
-
-// =============================================================================
-/** @class  LoadingComputerFactory
-    @brief  LoadingComputerFactory
-*/
-// Created: AHC 2009-10-01
-// =============================================================================
-class LoadingComputerFactory: public transport::LoadingComputerFactory_ABC
-{
-public:
-    //! @name Constructors/Destructor
-    //@{
-             LoadingComputerFactory();
-    virtual ~LoadingComputerFactory();
-    //@}
-
-    //! @name Operations
-    //@{
-    virtual std::auto_ptr< HumanLoadingTimeComputer_ABC > CreateHumanLoadingTimeComputer();
-    virtual std::auto_ptr< LoadedStateConsistencyComputer_ABC > CreateLoadedStateConsistencyComputer();
-    //@}
-};
-
-}
-
-#endif /* __transport_LoadingComputerFactory_H__ */

src/libraries/simulation_kernel/LoadingComputerFactory_ABC.h
@@ -1,42 +1,0 @@
-// *****************************************************************************
-//
-// This file is part of a MASA library or program.
-// Refer to the included end-user license agreement for restrictions.
-//
-// Copyright (c) 2009 MASA Group
-//
-// *****************************************************************************
-
-#ifndef __transport_LoadingComputerFactory_ABC_H__
-#define __transport_LoadingComputerFactory_ABC_H__
-
-namespace transport
-{
-    class HumanLoadingTimeComputer_ABC;
-    class LoadedStateConsistencyComputer_ABC;
-
-// =============================================================================
-/** @class  ConsumptionChangeRequestHandler_ABC
-    @brief  ConsumptionChangeRequestHandler_ABC
-*/
-// Created: AHC 2009-10-01
-// =============================================================================
-class LoadingComputerFactory_ABC
-{
-public:
-    //! @name Constructors/Destructor
-    //@{
-             LoadingComputerFactory_ABC() {}
-    virtual ~LoadingComputerFactory_ABC() {}
-    //@}
-
-    //! @name Constructors/Destructor
-    //@{
-    virtual std::auto_ptr< HumanLoadingTimeComputer_ABC > CreateHumanLoadingTimeComputer() = 0;
-    virtual std::auto_ptr< LoadedStateConsistencyComputer_ABC > CreateLoadedStateConsistencyComputer() = 0;
-    //@}
-};
-
-}
-
-#endif /* __transport_LoadingComputerFactory_ABC_H__ */

commit 1796163aa6eddf6ce5cf2377bad0cd6e36f6844a

[simulation_kernel] fixed broken computers
Broken in ea8813c5d47eb84b230495df49826aa2dde8b394 and left out in the subsequent fix in 12202529c750b164ca0ce323309c3d92f55dc939

src/libraries/simulation_kernel/Entities/Agents/Actions/Loading/PHY_RoleAction_Loading.cpp
@@ -115,7 +115,7 @@ void PHY_RoleAction_Loading::SetUnloadedState()
 double PHY_RoleAction_Loading::ComputeLoadingTime() const
 {
     DefaultHumanLoadingTimeComputer loadingTimeComputer;
-    owner_->Execute( loadingTimeComputer );
+    owner_->Execute< HumanLoadingTimeComputer_ABC >( loadingTimeComputer );
     if( loadingTimeComputer.GetHumansLoadedPerTimeStep() == 0 )
         return std::numeric_limits< double >::max();
     return loadingTimeComputer.GetHumansCount() / loadingTimeComputer.GetHumansLoadedPerTimeStep();
@@ -128,7 +128,7 @@ double PHY_RoleAction_Loading::ComputeLoadingTime() const
 double PHY_RoleAction_Loading::ComputeUnloadingTime() const
 {
     DefaultHumanLoadingTimeComputer loadingTimeComputer;
-    owner_->Execute( loadingTimeComputer );
+    owner_->Execute< HumanLoadingTimeComputer_ABC >( loadingTimeComputer );
     if( loadingTimeComputer.GetHumansUnloadedPerTimeStep() == 0 )
         return std::numeric_limits< double >::max();
     return loadingTimeComputer.GetHumansCount() / loadingTimeComputer.GetHumansUnloadedPerTimeStep();
@@ -225,7 +225,7 @@ int PHY_RoleAction_Loading::Unload()
 void PHY_RoleAction_Loading::CheckConsistency()
 {
     DefaultLoadedStateConsistencyComputer comp;
-    owner_->Execute( comp );
+    owner_->Execute< LoadedStateConsistencyComputer_ABC >( comp );
     if( bIsLoaded_ )
     {
         if( !comp.HasValidCarrier() && comp.HasValidLoadable() )