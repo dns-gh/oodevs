05135618d072a61c6724fd34a355af8824a164c9
Loic Geffroy <loic.geffroy@masagroup.net>
2014-03-25 17:26:03 +0100

    [protocol] fix mistake: rename pathfind_request_ask ->pathfind_request_ack

--- src/libraries/protocol/ClientSenders.h
+++ src/libraries/protocol/ClientSenders.h
@@ -161,7 +161,7 @@ namespace client
     GENERATE_MESSAGE( sword::SimToClient, sword, UrbanUpdate                                    , urban_update                              )
     GENERATE_MESSAGE_WITH_CLIENTID( sword::SimToClient, sword, ControlEnableVisionConesAck      , control_enable_vision_cones_ack           )
     GENERATE_MESSAGE_WITH_CLIENTID( sword::SimToClient, sword, ListEnabledVisionConesAck        , list_enabled_vision_cones_ack             )
-    GENERATE_MESSAGE_WITH_CLIENTID( sword::SimToClient, sword, PathfindRequestAck               , pathfind_request_ask                      )
+    GENERATE_MESSAGE_WITH_CLIENTID( sword::SimToClient, sword, PathfindRequestAck               , pathfind_request_ack                      )
 }
 
 #endif // __protocol_ClientSenders_h_
