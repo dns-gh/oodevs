158fe8582666ceefcdff90102227b430f00aaf09
ldcMasa <laurent.dicesare@masagroup.net>
2014-03-31 13:47:45 +0200

    [clients_gui] Fix JIRA # SWBUG-9693 ALGERIE - La transparence est mal affichée dans les icônes de l'orbat
    Better to remove the glColor4f totally.

--- src/libraries/clients_gui/GlToolsBase.cpp
+++ src/libraries/clients_gui/GlToolsBase.cpp
@@ -148,12 +148,9 @@ void GlToolsBase::BindIcon( const char** xpm )
 // -----------------------------------------------------------------------------
 void GlToolsBase::SetCurrentColor( float r, float g, float b, float a /* = 1*/ )
 {
-    glPushAttrib( GL_CURRENT_BIT );
-    glColor4f( r, g, b, a );
     renderer_->SetCurrentColor( r, g, b, a );
     graphics_->SetCurrentColor( r, g, b, a );
     symbols_->SetCurrentColor( r, g, b, a );
-    glPopAttrib();
 }
 
 // -----------------------------------------------------------------------------
