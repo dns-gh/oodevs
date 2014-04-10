ca91a2727720e435ab97da31218d4b1830ccae06
Mathieu Champlon <mathieu.champlon@masagroup.net>
2014-04-10 15:18:20 +0200

    [tools] fixed WinArguments option parsing
    We want HasOption to return false for '--foo=false' and '--foo false'

--- src/tests/tools_test/WinArguments.cpp
+++ src/tests/tools_test/WinArguments.cpp
@@ -14,13 +14,17 @@ BOOST_AUTO_TEST_CASE( win_arguments )
 {
     BOOST_CHECK( tools::WinArguments( L"app.exe --foo" ).HasOption( "--foo" ) );
     BOOST_CHECK( tools::WinArguments( L"app.exe --foo=true" ).HasOption( "--foo" ) );
-    BOOST_CHECK_EQUAL( "true", tools::WinArguments( L"app.exe --foo=true" ).GetOption( "--foo", "ERROR" ) );
+    BOOST_CHECK( !tools::WinArguments( L"app.exe --foo=false" ).HasOption( "--foo" ) );
+    BOOST_CHECK( tools::WinArguments( L"app.exe --foo true" ).HasOption( "--foo" ) );
+    BOOST_CHECK( !tools::WinArguments( L"app.exe --foo false" ).HasOption( "--foo" ) );
     BOOST_CHECK( !tools::WinArguments( L"app.exe --foobar" ).HasOption( "--foo" ) );
+
+    BOOST_CHECK_EQUAL( "true", tools::WinArguments( L"app.exe --foo=true" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "true", tools::WinArguments( L"app.exe --foo true" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "true", tools::WinArguments( L"app.exe --foo=true" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "ERROR", tools::WinArguments( L"app.exe --foobar=true" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "ERROR", tools::WinArguments( L"app.exe --foobar true" ).GetOption( "--foo", "ERROR" ) );
-    BOOST_CHECK_EQUAL( "ERROR", tools::WinArguments( L"app.exe --foo --bar" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "ERROR", tools::WinArguments( L"app.exe --foo" ).GetOption( "--foo", "ERROR" ) );
+    BOOST_CHECK_EQUAL( "ERROR", tools::WinArguments( L"app.exe --foo --bar" ).GetOption( "--foo", "ERROR" ) );
     BOOST_CHECK_EQUAL( "", tools::WinArguments( L"app.exe --foo=" ).GetOption( "--foo", "ERROR" ) );
 }
