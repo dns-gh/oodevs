d2ab2f4d634ffd50a79f13feb2dbd7226d82c5de
Newfel Messouci <newfel.messouci@masagroup.net>
2014-05-06 11:43:15 +0200

    [integration] Added documentation in QueriesIntegration.lua

--- data/app-data/resources/integration/QueriesIntegration.lua
+++ data/app-data/resources/integration/QueriesIntegration.lua
@@ -1,7 +1,13 @@
+-- Deprecated
 function integration.getAvoidingPositionsFor( entity )
     return { CreateProxyKnowledge( integration.ontology.types.avoidingArea, entity ) }
 end
 
+--- Returns all the objects with at least one of the provided types in the given area.
+-- @see Types.lua for the object types enumeration
+-- @param localisation Simulation area
+-- @param objectTypes List, the sought object types (e.g. eTypeObjectMines, eTypeObjectLinearMinedArea...)
+-- @return List of object knowledges
 function integration.getObjectsInArea( localisation, objectTypes )
     local integration = integration
     local lstObjects = DEC_ObjectKnowledgesInZone( localisation, objectTypes )
@@ -10,69 +16,71 @@ function integration.getObjectsInArea( localisation, objectTypes )
     end
     
     local res = {}
-    local nObjects = #lstObjects
     local CreateKnowledge = CreateKnowledge
-    for i = 1, nObjects do
-        local object = lstObjects[ i ]
-        res[#res + 1] = CreateKnowledge( integration.ontology.types.object, object )
+    for i = 1, #lstObjects do
+        res[ i ] = CreateKnowledge( integration.ontology.types.object, lstObjects[ i ] )
     end
     return res
 end
 
+--- Returns a list of safety positions defined as :
+-- <ul> <li> crossroads at the given radius of this entity </li>
+-- <li> positions along roads at the given safety distance of the previously found crossroads </li> </ul>
+-- This method can only be called by an agent.
+-- @see integration.findSafetyPositions
+-- @param radius Float, the radius around the entity (in meters)
+-- @param safetyDistance Float, the safety distance around the crossroads (in meters)
+-- @return List of point knowledges
 function integration.getSafetyPositions( radius, safetyDistance )
     -- Warning: If this method is called with different parameters, the simulation cache will be invalidated so
     -- it's good practice not to call it with different parameters for the same brain.
-    local points = integration.getSafetyPositions( radius, safetyDistance )
+    local points = integration.findSafetyPositions( radius, safetyDistance )
     local result = {}
     local CreateKnowledge = CreateKnowledge
-    local nPoints = #points
-    for i = 1, nPoints do
-        local point = points[ i ]
-        result[ #result + 1 ] = CreateKnowledge( integration.ontology.types.point, point )
+    for i = 1, #points do
+        result[ i ] = CreateKnowledge( integration.ontology.types.point, points[ i ] )
     end
     return result
 end
 
+--- Returns all the crowds in the given area.
+-- This method can only be called by an agent or by a company.
+-- @see integration.getCrowds
+-- @param area Area knowledge
+-- @return List of crowd knowledges
 function integration.getCrowdsInArea( area )
     local allRes = {}
     local crowds = integration.getCrowds()
     local CreateKnowledge = CreateKnowledge
     local DEC_ConnaissancePopulation_EstDansZone = DEC_ConnaissancePopulation_EstDansZone 
-    local nCrowd = #crowds
-    for i = 1, nCrowd do
+    for i = 1, #crowds do
         local crowd = crowds[ i ]
         if DEC_ConnaissancePopulation_EstDansZone( crowd, area.source ) then
             allRes[ #allRes + 1 ] = CreateKnowledge( integration.ontology.types.population, crowd )
         end
     end
-    local nRes = #allRes
-    for j = 1, nRes do
-        local crowd = allRes[ j ]
-        if not DEC_ConnaissancePopulation_EstDansZone( crowd.source, area.source ) then
-            allRes[ j ] = nil
-        end
-    end
     return allRes
 end
 
+--- Returns a list of all terrorist agent knowledges at the given distance of this entity.
+-- This method can only be called by an agent.
+-- @param distance Float, the distance (in meters, 600 by default).
+-- @return List of agent knowledges
 function integration.getNearbyTerrorists( distance )
-    local minDistance = distance
-    if minDistance == nil then -- not issued
-        minDistance = 600
-    end
-    local terrorists = DEC_Connaissances_TerroristsAProximite( minDistance )    
+    local terrorists = DEC_Connaissances_TerroristsAProximite( distance or 600 )    
     local newResult = {}
-    local nTerrorists = #terrorists
-    for i = 1, nTerrorists do
-        local terrorist = terrorists[ i ]
-        if not exists( newResult, terrorist ) then
-            newResult[ #newResult + 1 ] = CreateKnowledge( integration.ontology.types.agentKnowledge, terrorist )
-        end
+    for i = 1, #terrorists do
+        newResult[ i ] = CreateKnowledge( integration.ontology.types.agentKnowledge, terrorists[ i ] )
     end
     return newResult
 end
 
-
+--- Returns a list of deployment areas close to the given area.
+-- This method can only be called by an agent.
+-- @param area Area knowledge
+-- @param nbAreas Integer, the number of area subdivisions searched
+-- during the execution of the method
+-- @return List of object knowledges
 integration.getImplantationObjects = function( area, nbAreas )
     local objectKn = {}
     local existingObject
@@ -93,6 +101,7 @@ integration.getImplantationObjects = function( area, nbAreas )
         for i = 1, #platoons do -- Est ce que ce point est trafficable pour tous les pions de l'automate
             if not integration.isPointInUrbanBlockTrafficableForPlatoon( platoons[i],barycentre ) then
                 nonTrafficablePosition = true
+                break
             end
         end
         if nonTrafficablePosition then -- Ce point n'est pas trafficable pour au moins un pion de l'automate.
@@ -109,7 +118,7 @@ integration.getImplantationObjects = function( area, nbAreas )
             elseif next(positionInLocalisation) then
                 localisation = DEC_Geometrie_ConvertirPointEnLocalisation( next(positionInLocalisation) )
             else
-                reportFunction(eRC_TrafficablePositionFindingInZone )
+                reportFunction( eRC_TrafficablePositionFindingInZone )
                 localisation = DEC_Geometrie_ConvertirPointEnLocalisation( barycentre )
             end
         else 
@@ -136,30 +145,41 @@ integration.getImplantationObjects = function( area, nbAreas )
     return objectKn
 end
 
-function integration.getCollidingObjectsFromType( type)
-    local lstObjects = DEC_Connaissances_CollisionsObjetsDeType( type )
+--- Returns a list of all objects of the given type colliding with this entity.
+-- This method can only be called by an agent.
+-- @see Types.lua for the object types enumeration
+-- @param objectType String, the sought object type (e.g. eTypeObjectMines, eTypeObjectLinearMinedArea...)
+-- @return List of object knowledges
+function integration.getCollidingObjectsFromType( objectType )
+    local lstObjects = DEC_Connaissances_CollisionsObjetsDeType( objectType )
     if not lstObjects then
         return {}
     end
     
     local res = {}
-    local nObjects = #lstObjects
     local integration = integration
     local CreateKnowledge = CreateKnowledge
-    for i = 1, nObjects do
-        local object = lstObjects[ i ]
-        res[#res + 1] = CreateKnowledge( integration.ontology.types.object, object )
+    for i = 1, #lstObjects do
+        res[ i ] = CreateKnowledge( integration.ontology.types.object, lstObjects[ i ] )
     end
     return res
 end
 
+--- Returns a list of all detected agent knowledges in
+-- this entity's current area of responsibility.
+-- This method can only be called by an agent.
+-- @return List of agent knowledges
 integration.unitesDetecteesDansFuseau = function()
     return DEC_Connaissances_UnitesDetecteesDansFuseau()
 end
 
--- Convert a list of elements (companies and units) into a list of valid agents
--- @param a list of elements like companies or units
--- @return a list of valid agents
+--- Convert a list of elements (companies and units) into a list of valid agents :
+-- <ul> <li> Companies will be converted to the list of its valid subordinates </li>
+-- <li> Agent knowledges will be converted to agents </li> </ul>
+-- @param elements List of company knowledges and/or DirectIA agent knowledges.
+-- Agent knowledges must define a "isValid" method returning true if the knowledge is valid,
+-- false otherwise.
+-- @return List of valid DirectIA agents
 integration.getAgentFromListOfElements = function ( elements )
     local entities = {}
     for i = 1, #elements do
