b84579d6b78c35cc6c15bfd23373517a99f8634b
Newfel Messouci <newfel.messouci@masagroup.net>
2014-05-14 14:27:12 +0200

    [integration] Added documentation to IlluminableIntegration.lua

--- data/app-data/resources/integration/IlluminableIntegration.lua
+++ data/app-data/resources/integration/IlluminableIntegration.lua
@@ -1,37 +1,44 @@
---Default Illuminable Implementation
-
-local eActionIllumination_Impossible = 0
-local eActionIllumination_EnemyDestroyed = 1
-local eActionIllumination_NoCapacity = 2
-local eActionIllumination_NoAmmo = 3
-local eActionIllumination_Running = 4
-local eActionIllumination_Finished = 5
-local eActionIllumination_None = 6
-
---- Return if the unit has illumination capacity regarding target distance
--- @param knowledge on an agent 
--- @author MGD
--- @release 2010-02-15
+--- Returns true if this entity can illuminate the given agent knowledge.
+-- @param unit DirectIA agent knowledge
+-- @return Boolean
 integration.canIlluminateIt = function( unit )
   return DEC_ConnaissanceAgent_PeutEtreIllumine( unit.source )
 end
 
---- Return if the unit has illumination capacity
--- @param knowledge on an agent 
--- @author MGD
--- @release 2010-12-03
+--- Returns true if this entity has illumination capacity, false otherwise.
+-- This method can only be called by an agent
+-- @return Boolean
 integration.canIlluminate = function()
     return DEC_ConnaissanceAgent_PeutIlluminer()
 end
 
+--- Returns true if the given agent knowledge is definitively illuminated, false otherwise.
+-- This method can only be called by an agent.
+-- @param target DirectIA agent knowledge
+-- @return Boolean
 integration.isDefinitelyIlluminated = function( target )
     return DEC_ConnaissanceAgent_EstDefinitivementIllumine( target.source )
 end
 
-integration.illuminationLevel = function( self )
-    return DEC_ConnaissanceAgent_EstIllumine( self.source ) and 100 or 0
+--- Returns the illumination level of the given agent knowledge :
+-- <ul> <li> 100 if the given agent knowledge is currently illuminated </li>
+-- <li> 0 otherwise </li> </ul>
+-- @param agent DirectIA Agent knowledge
+-- @return Integer
+integration.illuminationLevel = function( agent )
+    return DEC_ConnaissanceAgent_EstIllumine( agent.source ) and 100 or 0
 end
 
+--- Starts illuminating the given agent knowledge, in order for it to become known
+-- to the first agent of the allies list parameter.
+-- This method displays a report.
+-- This method can only be called by an agent.
+-- @see integration.startedIlluminateIt
+-- @see integration.stopIlluminateIt
+-- @param target DirectIA agent knowledge, the target to illuminate
+-- @param allies List of DirectIA agent knowledges (only the first element of the list
+-- will be taken into account)
+-- @return Boolean, false
 integration.startIlluminateIt = function( target, allies )
     target[myself] = target[myself] or {}
     target[myself].actionIlluminate = DEC_StartIlluminer( target.source, allies[1].source  )
@@ -40,18 +47,35 @@ integration.startIlluminateIt = function( target, allies )
     return false
 end
 
+--- Continues illuminating the given agent knowledge.
+-- This method can only be called by an agent.
+-- @see integration.startIlluminateIt
+-- @see integration.stopIlluminateIt
+-- @param target DirectIA agent knowledge
+-- @return Boolean, whether or not the illumination action is finished.
 integration.startedIlluminateIt = function( target ) 
     if target[myself].actionState == eActionIllumination_Finished then -- IndirectFire hit target
         return true
     end
+    return false
 end
 
+--- Stops illuminating the given agent knowledge.
+-- This method can only be called by an agent.
+-- @see integration.startIlluminateIt
+-- @see integration.startedIlluminateIt
+-- @param target DirectIA agent knowledge
+-- @return Boolean, false
 integration.stopIlluminateIt = function( target )
     target[myself] = target[myself] or {}
     target[myself].actionIlluminate = DEC__StopAction( target[myself].actionIlluminate )
     return false
 end
 
+--- Returns true if the given agent has illumination resources, false otherwise.
+-- This method can only be called by an agent
+-- @param agent DirectIA agent
+-- @return Boolean
 integration.canAgentIlluminate = function( agent )
     return DEC_Agent_PeutIllumine( agent )
 end
