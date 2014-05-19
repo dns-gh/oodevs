d873868b131daeadaf9e5671f4429a3f0424a0a2
Newfel Messouci <newfel.messouci@masagroup.net>
2014-05-19 11:31:04 +0200

    [integration] Updated comments in KeyPoint.lua

--- data/app-data/resources/integration/KeyPoint.lua
+++ data/app-data/resources/integration/KeyPoint.lua
@@ -1,5 +1,8 @@
 --- Returns the efficiency of the given entity to observe the given objective.
--- This method can only be called by an entity defining a "computePerceptionCapability" method returning a float between 0 and 100
+-- For instance, this method can be used in a skill whose goal is to observe an objective
+-- via several positions given as parameters, in order to determine which of these positions
+-- is the best to observe the given objective (i.e. the closest one to the objective).
+-- This method can only be called by an agent
 -- @param entity Knowledge defining a "proximityLevel" method returning a float between 0 and 100
 -- @param objective Knowledge, the objective to observe
 -- @return Float between 0 and 1, 0 meaning the lowest efficiency and 1 meaning the highest efficiency
@@ -8,6 +11,9 @@ integration.observationEfficiency = function( entity, objective )
 end
 
 --- Returns the efficiency of the given entity to approach the given objective.
+-- For instance, this method can be used in a skill whose goal is to approach an objective
+-- via several positions given as parameters, in order to determine which of these positions
+-- is the best to approach the given objective (i.e. the closest one to the objective).
 -- @param entity Knowledge defining a "proximityLevel" method returning a float between 0 and 100
 -- @param objective Knowledge, the objective to approach
 -- @return Float between 0 and 1, 0 meaning the lowest efficiency and 1 meaning the highest efficiency
@@ -25,6 +31,9 @@ integration.identificationEfficiency = function( entity, objective )
 end
 
 --- Returns the efficiency of the given entity to flee the given objective.
+-- For instance, this method can be used in a skill whose goal is to flee an objective
+-- via several positions given as parameters, in order to determine which of these positions
+-- is the best to flee the given objective (i.e. the furthest one from the objective).
 -- @param entity Knowledge defining a "getProximity" method returning a float between 0 and 100
 -- @param objective Knowledge, the objective to flee from
 -- @return Float between 0 and 1, 0 meaning the lowest efficiency and 1 meaning the highest efficiency
@@ -60,13 +69,13 @@ local isInNearestNeighbors = function( position, objective )
     return objective:getProximity( position ) > 0 and masalife.brain.knowledge.me.body:computeMovementCapability( position ) > 0
 end
 ------------------------------------------------------------------
--- @name isReachingFor
+-- Deprecated
+-- @name isReachingForPredicate
 -- @class function
 -- @description boolean knowledge class method that returns true whether the indexed knowledge instance is a good position to
 -- reach a given objective ( i.e. a close enough position ).If used, both "me" and the indexed knowledge instance must have a
 -- terrainAnalysisPosition_ attribute.
 -- @return true whether the indexed knowledge is a good position, or false otherwise.
--- Deprecated
 integration.isReachingForPredicate = {
     dependencies = { me = { "location" }, objective = { "terrainAnalysisPosition_" },  },
     method = function( self, objective )
@@ -80,23 +89,25 @@ integration.isReachingForPredicate = {
 }
 
 --- Returns the efficiency of the given entity to reach the given objective.
--- @param entity Knowledge defining "proximityLevel" and "getProximity" methods,
--- both returning a float between 0 and 100
+-- For instance, this method can be used in a skill whose goal is to reach an objective
+-- via several positions given as parameters, in order to determine which of these positions
+-- is the best to reach the given objective (i.e. the closest one to the objective).
+-- @param entity Knowledge defining a "getProximity" method, returning a float between 0 and 100
 -- @param target Knowledge, the objective to reach
 -- @return Float between 0 and 1, 0 meaning the lowest efficiency and 1 meaning the highest efficiency
 integration.reachEfficiency = function( entity, target )
-    return ( ( entity:proximityLevel() / 100 + entity:getProximity( target ) ) ) / 101
+    return ( ( entity:getProximity( meKnowledge ) / 100 + entity:getProximity( target ) ) ) / 101
 end
 
 ------------------------------------------------------------------
--- @name isIdentifyingFor
+-- Deprecated
+-- @name isIdentifyingForPredicate
 -- @class function
 -- @description Method which returns if a knowledge is a good position to identify a set of objectives. From a good position,
 -- a potential identification of the objective is possible thanks to the identification capability of the agent.
 -- @param self the position to be used for identification
 -- @param objective the objective to observe
 -- @return true, if the position respects the previous contraints, false otherwise.
--- Deprecated
 integration.isIdentifyingForPredicate = {
     dependencies = { me = { "terrainAnalysisPosition_" }, objective = { "terrainAnalysisPosition_" } },
     method = function( self, objective )
