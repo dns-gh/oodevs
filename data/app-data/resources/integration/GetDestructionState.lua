0ecc5b65b1ec64f0cb9256746b65ad8a75271654
Newfel Messouci <newfel.messouci@masagroup.net>
2014-05-23 09:37:23 +0200

    [integration] Deleted useless integration files GetDestructionState.lua, GetIdentificationState.lua and GetOperationalCapacity.lua

--- data/app-data/resources/integration/GetDestructionState.lua
+++
@@ -1,8 +0,0 @@
---- MASA Life Brain default getDestructionState
--- <br/>This file registers the getDestructionState related default implementations.
--- <br/>The destruction state of a knowledge can be modified by the simulation thanks to attribute "destructionLevel_"
-
-masalife.brain.knowledge.addPerceptionCallBack( function( type, source, attributeList, lifeTime, ... )
-       attributeList.destructionLevel_ = attributeList.destructionLevel_ or 0
-       return attributeList
-   end )
