c6b3858dd9b66feec4dbcc5e58cc6c343a719db6
Newfel Messouci <newfel.messouci@masagroup.net>
2014-05-28 14:29:17 +0200

    [integration][simulation_kernel] Renamed the integration.getOrderConduiteRenforcerEnEquipementsParameter to integration.getEquipmentTypeListParameter, and changed the binding of the DEC function

--- src/libraries/simulation_kernel/Entities/Orders/MIL_FragOrder.h
+++ src/libraries/simulation_kernel/Entities/Orders/MIL_FragOrder.h
@@ -96,7 +96,7 @@ public:
     T_MaintenancePriorityVector GetOrderConduiteModifierPrioritesReparations() const;
     std::vector< DEC_Decision_ABC* > GetOrderConduiteModifierPrioritesTactiquesBlesses() const;
     std::vector< DEC_Decision_ABC* > GetOrderConduiteModifierPrioritesTactiquesReparations() const;
-    std::vector< const PHY_ComposanteTypePion* > GetOrderConduiteRenforcerEnEquipements() const;
+    std::vector< const PHY_ComposanteTypePion* > GetEquipmentTypeListParameter( const std::string& ) const;
     int GetOrderConduiteModifierRegimeTravailMaintenance() const;
     int GetOrderConduitePopulationChangerAttitude() const;
     const DEC_Decision_ABC* GetPionARenforcer() const;
