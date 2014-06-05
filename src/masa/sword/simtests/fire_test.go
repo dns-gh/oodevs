b1cc489e234fa8ee5848d6e5a63d6bc91787845a
Patrick Mezard <patrick.mezard@masagroup.net>
2014-06-05 19:27:48 +0200

    [fire_test] fix TestActiveProtection (SWBUG-12467)
    I am not completely sure why the fix works, I suspect there is a race
    between the unit attribute update and the resource consumption caused by
    the active protection. But I ran it at least 100 times in a noisy
    environment and it did not fail while it previously did after 15
    attempts.

--- src/masa/sword/simtests/fire_test.go
+++ src/masa/sword/simtests/fire_test.go
@@ -331,7 +331,6 @@ func (s *TestSuite) TestFireOrderCreationOnUnit(c *C) {
 }
 
 func (s *TestSuite) TestActiveProtection(c *C) {
-	c.Skip("unreliable, http://jira.masagroup.net/browse/SWBUG-12467")
 	// VW Active Protection has:
 	// - Hard kill active protection from "Shell Of Death", comsumming
 	//   2 "ammmunition"
@@ -388,6 +387,10 @@ func (s *TestSuite) TestActiveProtection(c *C) {
 		},
 	})
 	c.Assert(err, IsNil)
+	waitCondition(c, client.Model, func(data *swapi.ModelData) bool {
+		return data.Units[target.Id].Resources[ammunitionId].Quantity == 1 &&
+			data.Units[target.Id].Resources[smoke1Id].Quantity == 99
+	})
 	err = client.CreateFireOnLocation(pos, ShellOfDeathId, 1)
 	c.Assert(err, IsNil)
 	waitCondition(c, client.Model, func(data *swapi.ModelData) bool {
