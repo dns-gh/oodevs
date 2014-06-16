a1688fc1d09d469b8e7e299a84e0992d1e5dfd6b
Joel Soicher <jsr@masagroup.net>
2014-06-16 17:43:20 +0200

    [clients] clients DetectionMapIterator is now using simulation PHY_RawVisionDataIterator algorithm
    Conflicts:
    	src/libraries/clients_kernel/DetectionMapIterator.cpp
    	src/libraries/clients_kernel/DetectionMapIterator.h

--- src/libraries/clients_kernel/DetectionMapIterator.h
+++ src/libraries/clients_kernel/DetectionMapIterator.h
@@ -22,31 +22,38 @@ namespace kernel
 */
 // Created: AGE 2006-04-06
 // =============================================================================
-class DetectionMapIterator : boost::noncopyable
+class DetectionMapIterator : private boost::noncopyable
 {
 public:
     //! @name Constructors/Destructor
     //@{
-    DetectionMapIterator( const DetectionMap& map, const geometry::Point2f& from, const geometry::Point2f& to );
+             DetectionMapIterator( const DetectionMap& map, const geometry::Point3f& vBeginPos, const geometry::Point3f& vEndPos );
+    virtual ~DetectionMapIterator();
     //@}
 
     //! @name Operations
     //@{
     void Increment();
-
     bool IsDone() const;
+    float Length() const;
     short Altitude() const;
     short Elevation() const;
     bool IsInTown() const;
     bool IsInForest() const;
-
     geometry::Point2f CurrentPoint() const;
     //@}
 
 private:
     //! @name Helpers
     //@{
-    geometry::Vector2f ComputeIncrement();
+    // Transformations entre l'espace réel et l'espace de l'algorithme
+    void ToAlgorithmSpace ( float& rX, float& rY ) const;
+    template< typename T >
+    void ToRealSpace( T& x, T& y ) const;
+    void OffsetToRealSpace( int& dX, int& dY ) const;
+    void AlignFirstPointOnGrid();
+    void SetOutPointXY( float x, float y );
+    void SetOutPointZ( float z );
     //@}
 
 private:
@@ -54,17 +61,59 @@ private:
     //@{
     const DetectionMap& map_;
     float cellSize_;
-
-    std::pair< unsigned, unsigned > endCell_;
-    float xEnd_, yEnd_;
-
     std::pair< unsigned, unsigned > currentCell_;
-    float xCurrent_, yCurrent_;
 
-    float xOffset_, yOffset_;
-    float xIncrease_, yIncrease_;
+    // gradients
+    float rDz_;
+    float rDl_;
 
-    bool horizontalLine_;
+    // Equations de la droite
+    // y = a0 * x + b0
+    // x = a1 * y + b1
+    float rA0_;
+    float rA1_;
+    float rB0_;
+    float rB1_;
+
+    // Informations de tranformation de l'espace réel en l'espace idéal ( 0, 45° ) pour l'algo de parcours
+    bool bSwap_;
+    bool bNegX_;
+    bool bNegY_;
+    bool bSwapOffset_;
+
+    // Case réelle initiale
+    int nCellColOffset_;
+    int nCellRowOffset_;
+
+    // information de fin ( pour faire un end() d'itérateur à la STL )
+    enum E_IteratorState
+    {
+        eRunning,
+        eLastPoint,
+        eEnd
+    };
+    E_IteratorState eIteratorState_;
+
+    // Distance parcourue dans la cellule courrante
+    float rLength_;
+
+    // Distance totale restant à parcourir ( condition de fin )
+    float rRemainingLength_;
+
+    // Prochaine position de l'itérateur
+    float rAlreadyUsedDX_; // Projection de la distance déjà parcourue sur l'axe des x dans la précédente itération
+    int nNextCellCol_;
+    int nNextCellRow_;
+    geometry::Point3f vOutPoint_; // X, Y dans l'espace de l'algorithme, Z non transformée
+    const float originalEndAltitude_;
+
+    // Informations sur l'environnement précedent
+    float rGroundCoeff_;
+    float rEnvCoeff_;
+
+    // Environnement courrant
+    bool bIsInGround_;
+    bool bIsInEnv_;
     //@}
 };
 
