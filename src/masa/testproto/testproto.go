24dcc75eb9178ba6a9ca09fff5844686c45b007f
Patrick Mezard <patrick.mezard@masagroup.net>
2014-06-19 17:14:14 +0200

    [go] add dummy package file to make go 1.3 happy
    For some reason, "go build masa/..." fails on packages containing only
    test files.

---
+++ src/masa/testproto/testproto.go
@@ -0,0 +1,11 @@
+// ****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2014 MASA Group
+//
+// ****************************************************************************
+// Dummy package files, go build fails on package containing only test files
+// since 1.3.
+package testproto
