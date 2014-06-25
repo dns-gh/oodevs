e49fd0fc4cd1c87e676276ddaf79b0604d6137c8
ldcMasa <laurent.dicesare@masagroup.net>
2014-06-25 08:52:07 +0200

    [integration] Fix typo: s/prams/params/

--- data/app-data/resources/integration/ToolsQueries.lua
+++ data/app-data/resources/integration/ToolsQueries.lua
@@ -76,7 +76,7 @@ local treatIndirectFireFragOrder = function ( )
             params.entity = parameterValue.entity
             params.munition = parameterValue.munition
             params.interventionType = parameterValue.interventionType
-            prams.requester = parameterValue.requester
+            params.requester = parameterValue.requester
         end
     end
 
