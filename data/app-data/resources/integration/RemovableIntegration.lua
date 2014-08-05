ffd1985697e965025ee07e34546c9b3568f2af9a
mathieuIgarza <mathieu.igarza@masagroup.net>
2014-08-05 15:31:47 +0200

    [ integration ] - Fix syntax error.

--- data/app-data/resources/integration/RemovableIntegration.lua
+++ data/app-data/resources/integration/RemovableIntegration.lua
@@ -85,7 +85,7 @@ integration.canRemoveIt = function( object )
     if masalife.brain.core.class.isOfType( object, integration.ontology.types.object ) then
         return DEC_Agent_PeutDetruireObjet( object.source )
     elseif masalife.brain.core.class.isOfType( object, integration.ontology.types.urbanBlock ) then
-        return true -- decreasing the structural state of urban blocks don't require specific physical ability.
+        return true -- decreasing the structural state of urban blocks doesn't require specific physical ability.
     end
     return false
 end
