ee5d859a076e8c0ac98e769265a0c6ce538e23d3
Lucie Mignot <lmt@masagroup.net>
2014-08-19 11:29:30 +0200

    [Integration] On veut que la distanceMax soit = 20000 pour les unités volantes

--- data/app-data/resources/integration/ReachableIntegration.lua
+++ data/app-data/resources/integration/ReachableIntegration.lua
@@ -203,7 +203,11 @@ integration.normalizedInversedDistance = function( pos1, pos2 )
     if not pos1 or not pos2 then
         return 1
     end
-    local distanceMax = DEC_Detection_Distance and DEC_Detection_Distance() or 4000 -- Scipio compatibility
+    if integration.isFlying() then -- distance scale is much bigger for flying agents
+        local distanceMax = 20000
+    else
+        local distanceMax = DEC_Detection_Distance and DEC_Detection_Distance() or 4000 -- Scipio compatibility
+    end
     if pos1.getLocalisation and pos1:getLocalisation() then
         return normalizedInverseDistanceSimList( listLocalisationPoints( pos1 ), pos2, distanceMax )
     end
