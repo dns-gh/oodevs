d326080850875e2fc6585792dbc4dee4572665ec
Benoît Amiaux <benoit.amiaux@masagroup.net>
2014-09-05 18:37:55 +0200

    [swtest] gocheck logger is thread-safe now

--- src/masa/sword/swtest/helpers.go
+++ src/masa/sword/swtest/helpers.go
@@ -13,11 +13,9 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/pmezard/go-difflib/difflib"
 	"launchpad.net/gocheck"
-	"log"
 	"regexp"
 	"runtime"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -91,19 +89,12 @@ func DeepEquals(c *gocheck.C, result, expected interface{}) {
 }
 
 type TestLogger struct {
-	mutex sync.Mutex
-	c     *gocheck.C
+	c *gocheck.C
 }
 
 func (t *TestLogger) Printf(format string, a ...interface{}) {
 	fmt := time.Now().Format(time.Stamp) + ": " + strings.TrimSpace(format)
-	if false {
-		t.mutex.Lock()
-		defer t.mutex.Unlock()
-		t.c.Logf(fmt, a...)
-	} else {
-		log.Printf(fmt, a...)
-	}
+	t.c.Logf(fmt, a...)
 }
 
 func MakeGocheckLogger(c *gocheck.C) *TestLogger {
