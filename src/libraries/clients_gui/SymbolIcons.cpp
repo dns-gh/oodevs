87cfbc4c33796a8b7c7f0750f2c2ace418b538d1
Mathieu Champlon <mathieu.champlon@masagroup.net>
2014-11-05 13:29:10 +0100

    Revert "[clients_gui] simplify image grabbing"
    This reverts commit 15189db00241df1cc31843682133902fabbed68b.

--- src/libraries/clients_gui/SymbolIcons.cpp
+++ src/libraries/clients_gui/SymbolIcons.cpp
@@ -89,7 +89,11 @@ QPixmap SymbolIcons::GenerateSymbol( const SymbolIcon& symbol )
         const geometry::Point2f center( 220, 310 );
         Draw( symbol.symbol_, center, 0.5f );
     }
-    return QPixmap( context_->grabFrameBuffer().smoothScale( symbol.size_ ) );
+    glFinish();
+    QImage image( iconSize, iconSize, QImage::Format_ARGB32 );
+    glReadPixels( 0, 0, iconSize, iconSize, GL_BGRA_EXT, GL_UNSIGNED_BYTE, image.bits() );
+    QPixmap result( image.mirror().smoothScale( symbol.size_ ) );
+    return result;
 }
 
 const QPixmap& SymbolIcons::GetSymbol( const SymbolIcon& symbol )
