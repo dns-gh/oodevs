2fc1d6a0801714aed227b1fec0df6a842966ad78
mathieuIgarza <mathieu.igarza@masagroup.net>
2014-11-07 17:21:24 +0100

    [ integration ] - Fix a bug in method integration.isPointInUrbanBlockTrafficable when called by a comander agent - Add test to know if the agent is an automat or not. - Add a new method 'integration.isAutomat'

--- data/app-data/resources/integration/Commander.lua
+++ data/app-data/resources/integration/Commander.lua
@@ -53,10 +53,16 @@ integration.commanderGetHQUnit = function( )
 end
 
 --- Returns the company of the agent
--- @param entity Simulation agent
+-- @param entity Simulation agent or company
 -- @return Simulation commander
 integration.getCommander = function( entity )
     return DEC_GetAutomate( entity )
+end
+
+--- Returns 'true' if the agent is a company (an automat). It returns 'false' otherwise
+-- @return Boolean, whether or not the agent is a company
+integration.isAutomat = function()
+    integration.getCommander( myself ) == myself
 end
 
 --- Returns true if the agent is contaminated, false otherwise
