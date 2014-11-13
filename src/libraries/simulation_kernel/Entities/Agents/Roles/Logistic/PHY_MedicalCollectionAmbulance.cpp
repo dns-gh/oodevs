6a7ad83cea886f1f78d16a4d6161f1b16323a18d
Joel Soicher <jsr@masagroup.net>
2014-11-13 14:37:10 +0100

    [simulation] 81961710 - fix SWBUG-13223 - LOG - lorsque l'on a 3 niveaux de BL les blessés ne remontent pas au niveau de la BL3
    [Finished #81961710]

--- src/libraries/simulation_kernel/Entities/Agents/Roles/Logistic/PHY_MedicalCollectionAmbulance.cpp
+++ src/libraries/simulation_kernel/Entities/Agents/Roles/Logistic/PHY_MedicalCollectionAmbulance.cpp
@@ -237,7 +237,11 @@ bool PHY_MedicalCollectionAmbulance::DoSearchForDestinationArea()
     {   
         auto it = consigns_.begin();
         if( it != consigns_.end() )
-            pDestinationArea_ = pLogisticManager->MedicalFindAlternativeHealingHandler( const_cast< PHY_MedicalHumanState& > ((*it)->GetHumanState() ) );
+        {
+            pDestinationArea_ = pLogisticManager->MedicalFindAlternativeHealingHandler( ( *it )->GetHumanState() );
+            if( pLogisticSuperior && !pDestinationArea_ )
+                pDestinationArea_ = pLogisticSuperior->MedicalFindAlternativeHealingHandler( ( *it )->GetHumanState() );
+        }
         if( !pDestinationArea_ && !pLogisticSuperior )
             return true; // $$$ Bof : pour sortir les human states qui ne seront jamais traités
     }
