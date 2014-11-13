4de9a0c7c4cefc1b09e45a09ec9ee349ed10e392
Loic Geffroy <loic.geffroy@masagroup.net>
2014-11-13 18:01:15 +0100

    [simulation_kernel] Fix bad tasker in decisional_state message.

--- src/libraries/simulation_kernel/Decision/DEC_EntityFunctions.cpp
+++ src/libraries/simulation_kernel/Decision/DEC_EntityFunctions.cpp
@@ -7,10 +7,21 @@
 
 void DEC_EntityFunctions::DecisionalState( const DEC_Decision_ABC* agent, const std::string& key, const std::string& value )
 {
-    client::DecisionalState msg;
-    msg().mutable_source()->mutable_crowd()->set_id( agent->GetEntity().GetID() );
-    msg().set_key( key.c_str() );
-    msg().set_value( value.c_str() );
-    msg.Send( NET_Publisher_ABC::Publisher() );
+    if( agent )
+    {
+        client::DecisionalState msg;
+        const auto id = agent->GetEntity().GetID();
+        switch( agent->GetKind() )
+        {
+        case DEC_Decision_ABC::ePion:
+            msg().mutable_source()->mutable_unit()->set_id( id );
+        case DEC_Decision_ABC::eAutomate:
+            msg().mutable_source()->mutable_automat()->set_id( id );
+        case DEC_Decision_ABC::ePopulation:
+            msg().mutable_source()->mutable_crowd()->set_id( id );
+        }
+        msg().set_key( key.c_str() );
+        msg().set_value( value.c_str() );
+        msg.Send( NET_Publisher_ABC::Publisher() );
+    }
 }
- 
