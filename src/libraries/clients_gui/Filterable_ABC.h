09b37baa903d4acd01a81cadd6e6fe3425c12a5d
Mathieu Champlon <mathieu.champlon@masagroup.net>
2014-11-18 06:51:26 +0100

    [clients_gui] remove dead code

--- src/libraries/clients_gui/Filterable_ABC.h
+++ src/libraries/clients_gui/Filterable_ABC.h
@@ -14,7 +14,6 @@ namespace gui
 {
     class Filter_ABC;
     class RichView_ABC;
-    class StandardModel;
 
 // =============================================================================
 /** @class  Filterable_ABC
@@ -25,27 +24,15 @@ namespace gui
 class Filterable_ABC
 {
 public:
-    //! @name Constructors/Destructor
-    //@{
              Filterable_ABC() {}
     virtual ~Filterable_ABC() {}
-    //@}
 
-    //! @name Abstract Operation
-    //@{
     virtual void ApplyFilters( const std::map< int, std::vector< std::shared_ptr< Filter_ABC > > >& filters ) = 0;
     virtual void CreateFilters( RichView_ABC& richView ) = 0;
     virtual void Purge() = 0;
     virtual QHeaderView* GetHeader() const = 0;
-    //@}
-
-private:
-    //! @name Member
-    //@{
-    StandardModel* model_;
-    //@}
 };
 
-} //! namespace gui
+}
 
 #endif // __gui_Filterable_ABC_h
