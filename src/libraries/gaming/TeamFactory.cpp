dns-gh/oodevs

commit 00eb1a8822d267eb9c49670b66934a587520107d

[gaming][clients_gui] Removed T_CanBeRenamedFunctor from EntityImplementation
Rationale:
In many cases, the functor was not defined, thus wasting space. In most other cases, it was a std::function containing a closure object containing reference to profile_, which is less efficient than just storing a pointer to profile_.
A side effect is a cleanup of Team construction for the anonymous team.
TacticalLine_ABC destructor also fixed to avoid crash on disconnect.

src/libraries/gaming/TeamFactory.cpp
@@ -78,7 +78,7 @@ TeamFactory::~TeamFactory()
 // -----------------------------------------------------------------------------
 kernel::Team_ABC* TeamFactory::CreateTeam( const sword::PartyCreation& message )
 {
-    Team* result = new Team( message, controllers_, actionsModel_, [=]( const kernel::Team_ABC& team ){ return profile_.CanDoMagic( team ); } );
+    Team* result = new Team( message, controllers_, actionsModel_, profile_ );
     gui::PropertiesDictionary& dico = result->Get< gui::PropertiesDictionary >();
     result->Attach( *new UrbanKnowledges( *result, controllers_.controller_, model_.urbanKnowledgeFactory_ ) );
     result->Attach< kernel::Diplomacies_ABC > ( *new Diplomacies( controllers_.controller_, model_.teams_ ) );
@@ -122,10 +122,7 @@ namespace
 // -----------------------------------------------------------------------------
 kernel::Team_ABC* TeamFactory::CreateNoSideTeam()
 {
-    auto* result = new gui::EntityImplementation< kernel::Team_ABC >( controllers_.controller_, 0,
-                                                                      tools::translate( "TeamFactory", "No side" ),
-                                                                      &actionsModel_,
-                                                                      [=]( const kernel::Team_ABC& team ){ return profile_.CanDoMagic( team ); } );
+    Team* result = new Team( controllers_, 0, tools::translate( "TeamFactory", "No side" ), actionsModel_, profile_ );
     result->Attach< kernel::Diplomacies_ABC >( *new NoSideDiplomacy() );
     result->Attach< kernel::TacticalHierarchies >( *new TeamTacticalHierarchies( controllers_.controller_, *result ) );
     result->Update( kernel::InstanciationComplete() );
@@ -143,8 +140,7 @@ kernel::Formation_ABC* TeamFactory::CreateFormation( const sword::FormationCreat
         static_cast< kernel::Entity_ABC*>( &model_.teams_.Resolver< kernel::Formation_ABC >::Get( message.parent().id() ) ) :
         static_cast< kernel::Entity_ABC*>( &model_.teams_.Resolver< kernel::Team_ABC >::Get( message.party().id() ) );
 
-    Formation* result = new Formation( message, controllers_.controller_, actionsModel_,
-                                       [=]( const kernel::Formation_ABC& formation ){ return profile_.CanDoMagic( formation ); } );
+    Formation* result = new Formation( message, controllers_.controller_, actionsModel_, profile_ );
     result->Attach< Lives_ABC >( *new FormationLives( *result ) );
     gui::PropertiesDictionary& dico = result->Get< gui::PropertiesDictionary >();
     std::string symbol = message.has_app6symbol() ? message.app6symbol() : std::string();

src/applications/gaming_app/PathfindLayer.cpp
@@ -443,7 +443,7 @@ void PathfindLayer::OpenEditingMode( kernel::Entity_ABC* entity,
 {
     if( !entity )
         return;
-    edited_.reset( new Pathfind( controllers_.controller_, actions_, converter_, *entity, pathfind, true, []( const kernel::Pathfind_ABC& ){ return true; } ) );
+    edited_.reset( new Pathfind( controllers_.controller_, actions_, converter_, *entity, pathfind, true, 0 ) );
     target_ = entity;
     controllers_.ChangeMode( eModes_Itinerary );
     ::Select( controllers_, *entity );

src/libraries/clients_gui/EntityImplementation.h
@@ -46,19 +46,12 @@ class EntityImplementation : public I
                            , public kernel::Serializable_ABC
 {
 public:
-    //! @name Types
-    //@{
-    typedef std::function< bool( const I& ) >       T_CanBeRenamedFunctor;
-    //@}
-
-public:
     //! @name Constructors/Destructor
     //@{
              EntityImplementation( kernel::Controller& controller,
                                    unsigned long id,
                                    const QString& name,
-                                   actions::ActionsModel* actionsModel = 0,
-                                   const T_CanBeRenamedFunctor& canBeRenamedFunctor = T_CanBeRenamedFunctor() );
+                                   actions::ActionsModel* actionsModel = 0 );
     virtual ~EntityImplementation();
     //@}
 
@@ -104,7 +97,6 @@ private:
     //@{
     kernel::Controller& controller_;
     PropertiesDictionary* dictionary_;
-    const T_CanBeRenamedFunctor canBeRenamedFunctor_;
     //@}
 
 protected:
@@ -125,11 +117,9 @@ template< typename I >
 EntityImplementation< I >::EntityImplementation( kernel::Controller& controller,
                                                  unsigned long id,
                                                  const QString& name,
-                                                 actions::ActionsModel* actionsModel,
-                                                 const T_CanBeRenamedFunctor& canBeRenamedFunctor /* = T_CanBeRenamedFunctor() */ )
+                                                 actions::ActionsModel* actionsModel )
     : controller_( controller )
     , dictionary_( new PropertiesDictionary( controller ) )
-    , canBeRenamedFunctor_( canBeRenamedFunctor )
     , actionsModel_( actionsModel )
     , id_( id )
     , name_( name )
@@ -284,7 +274,7 @@ void EntityImplementation< I >::ForceNewId( unsigned long id )
 template< typename I >
 bool EntityImplementation< I >::CanBeRenamed() const
 {
-    return canBeRenamedFunctor_ ? canBeRenamedFunctor_( *this ) : true;
+    return true;
 }
 
 }

src/libraries/clients_gui/Team.cpp
@@ -26,9 +26,8 @@ using namespace gui;
 Team::Team( Controllers& controllers,
             unsigned long id,
             const QString& name,
-            actions::ActionsModel* actionsModel,
-            const T_CanBeRenamedFunctor& canBeRenamedFunctor /* = T_CanBeRenamedFunctor() */ )
-    : EntityImplementation< Team_ABC >( controllers.controller_, id, name, actionsModel, canBeRenamedFunctor )
+            actions::ActionsModel* actionsModel )
+    : EntityImplementation< Team_ABC >( controllers.controller_, id, name, actionsModel )
     , controllers_( controllers )
 {
     // NOTHING

src/libraries/clients_gui/Team.h
@@ -38,8 +38,7 @@ public:
              Team( kernel::Controllers& controller,
                    unsigned long id,
                    const QString& name,
-                   actions::ActionsModel* actionsModel = 0,
-                   const T_CanBeRenamedFunctor& canBeRenamedFunctor = T_CanBeRenamedFunctor() );
+                   actions::ActionsModel* actionsModel = 0 );
     virtual ~Team();
     //@}
 

src/libraries/gaming/Agent.cpp
@@ -12,15 +12,16 @@
 #include "Attributes.h"
 #include "Diplomacies.h"
 #include "equipments.h"
+#include "clients_gui/DictionaryUpdated.h"
 #include "clients_gui/GLOptions.h"
 #include "clients_gui/GLView_ABC.h"
+#include "clients_gui/PropertiesDictionary.h"
 #include "clients_gui/Viewport_ABC.h"
 #include "clients_kernel/AgentType.h"
-#include "clients_gui/PropertiesDictionary.h"
+#include "clients_kernel/App6Symbol.h"
 #include "clients_kernel/CommunicationHierarchies.h"
 #include "clients_kernel/Displayer_ABC.h"
-#include "clients_gui/DictionaryUpdated.h"
-#include "clients_kernel/App6Symbol.h"
+#include "clients_kernel/Profile_ABC.h"
 #include "clients_kernel/Styles.h"
 #include "clients_kernel/Tools.h"
 
@@ -34,8 +35,8 @@ Agent::Agent( const sword::UnitCreation& message,
               Controller& controller,
               const tools::Resolver_ABC< AgentType >& resolver,
               actions::ActionsModel& actionsModel,
-              const T_CanBeRenamedFunctor& canBeRenamedFunctor )
-    : EntityImplementation< Agent_ABC >( controller, message.unit().id(), QString( message.name().c_str() ), &actionsModel, canBeRenamedFunctor )
+              const kernel::Profile_ABC& profile )
+    : EntityImplementation< Agent_ABC >( controller, message.unit().id(), QString( message.name().c_str() ), &actionsModel )
     , type_       ( resolver.Get( message.type().id() ) )
     , controller_( controller )
     , initialized_( false )
@@ -43,6 +44,7 @@ Agent::Agent( const sword::UnitCreation& message,
     , speed_( 0 )
     , direction_( 0 )
     , sensorsDirection_( 0 )
+    , profile_( profile )
 {
     if( name_.isEmpty() )
         name_ = QString( "%1 %L2" ).arg( type_.GetLocalizedName().c_str() ).arg( message.unit().id() );
@@ -216,4 +218,13 @@ void Agent::DoUpdate( const sword::UnitAttributes& message )
     if( message.has_name() )
         SetName( QString::fromStdString( message.name() ) );
     Touch();
+}
+
+// -----------------------------------------------------------------------------
+// Name: Agent::CanBeRenamed
+// Created: LDC 2014-11-14
+// -----------------------------------------------------------------------------
+bool Agent::CanBeRenamed() const
+{
+    return profile_.CanDoMagic( *this );
 }

src/libraries/gaming/Agent.h
@@ -27,9 +27,10 @@ namespace sword
 
 namespace kernel
 {
-    class Controller;
     class AgentType;
+    class Controller;
     class Equipments_ABC;
+    class Profile_ABC;
 }
 
 // =============================================================================
@@ -53,7 +54,7 @@ public:
                    kernel::Controller& controller,
                    const tools::Resolver_ABC< kernel::AgentType >& resolver,
                    actions::ActionsModel& actionsModel,
-                   const T_CanBeRenamedFunctor& canBeRenamedFunctor );
+                   const kernel::Profile_ABC& profile );
     virtual ~Agent();
     //@}
 
@@ -70,6 +71,7 @@ public:
     unsigned int GetMoveDirection() const;
     float GetSymbolWidth() const;
     float GetSymbolDepth() const;
+    virtual bool CanBeRenamed() const;
     //@}
 
 private:
@@ -94,6 +96,7 @@ private:
     float speed_;
     unsigned int direction_;
     unsigned int sensorsDirection_;
+    const kernel::Profile_ABC& profile_;
     //@}
 };
 

src/libraries/gaming/AgentFactory.cpp
@@ -135,8 +135,7 @@ kernel::Automat_ABC* AgentFactory::Create( const sword::AutomatCreation& message
     const kernel::AutomatType* type = static_.types_.tools::Resolver< kernel::AutomatType >::Find( message.type().id() );
     if( !type )
         return 0;
-    Automat* result = new Automat( message, controllers_.controller_, *type, actionsModel_,
-                                   [=]( const kernel::Automat_ABC& automat ){ return profile_.CanDoMagic( automat ); } );
+    Automat* result = new Automat( message, controllers_.controller_, *type, actionsModel_, profile_ );
     gui::PropertiesDictionary& dictionary = result->Get< gui::PropertiesDictionary >();
     result->Attach( *new gui::EntityType< kernel::AutomatType >( *result, *type, dictionary ) );
     result->Attach< kernel::CommunicationHierarchies >( *new AutomatHierarchies( controllers_.controller_, *result, model_.knowledgeGroups_, dictionary ) );
@@ -200,8 +199,7 @@ namespace
 // -----------------------------------------------------------------------------
 kernel::Agent_ABC* AgentFactory::Create( const sword::UnitCreation& message )
 {
-    Agent* result = new Agent( message, controllers_.controller_, static_.types_, actionsModel_,
-                               [=]( const kernel::Agent_ABC& agent ){ return profile_.CanDoMagic( agent ); } );
+    Agent* result = new Agent( message, controllers_.controller_, static_.types_, actionsModel_, profile_ );
     gui::PropertiesDictionary& dictionary = result->Get< gui::PropertiesDictionary >();
     result->Attach< gui::CriticalIntelligence >( *new gui::CriticalIntelligence( *result, controllers_.controller_, dictionary ) );
     result->Attach< Lives_ABC >( *new Lives( *result, controllers_.controller_ ) );
@@ -255,8 +253,7 @@ kernel::Population_ABC* AgentFactory::Create( const sword::CrowdCreation& messag
     if( !type )
         return 0;
     const kernel::Team_ABC& team = model_.teams_.GetTeam( message.party().id() );
-    Population* result = new Population( message, controllers_, static_.coordinateConverter_, *type, actionsModel_,
-                                         [=]( const kernel::Population_ABC& crowd ){ return profile_.CanDoMagic( crowd ); } );
+    Population* result = new Population( message, controllers_, static_.coordinateConverter_, *type, actionsModel_, profile_ );
     gui::PropertiesDictionary& dictionary = result->Get< gui::PropertiesDictionary >();
     result->Attach< kernel::CommunicationHierarchies >( *new PopulationHierarchiesCommunication( controllers_.controller_, *result, model_.knowledgeGroups_.FindCrowdKnowledgeGroup( team ) ) );
     result->Attach( *new gui::EntityType< kernel::PopulationType >( *result, *type, dictionary ) );
@@ -281,8 +278,7 @@ kernel::Population_ABC* AgentFactory::Create( const sword::CrowdCreation& messag
 kernel::Inhabitant_ABC* AgentFactory::Create( const sword::PopulationCreation& message )
 {
     const kernel::InhabitantType& type = static_.types_.tools::Resolver< kernel::InhabitantType >::Get( message.type().id() );
-    Inhabitant* result = new Inhabitant( message, controllers_.controller_, model_.urbanObjects_, type, static_.objectTypes_, actionsModel_,
-                                         [=]( const kernel::Inhabitant_ABC& inhabitant ){ return profile_.CanDoMagic( inhabitant ); } );
+    Inhabitant* result = new Inhabitant( message, controllers_.controller_, model_.urbanObjects_, type, static_.objectTypes_, actionsModel_, profile_ );
     gui::PropertiesDictionary& dictionary = result->Get< gui::PropertiesDictionary >();
     result->Attach( *new gui::EntityType< kernel::InhabitantType >( *result, type, dictionary ) );
     AddPositions( *result, controllers_, simulation_, isInReplay_, *new InhabitantPositions( message, model_.urbanObjects_, *result ) );

commit 70ac00c9780f720b790ec39117cc1a956da7492d

[gaming] fix crash when quitting gaming_app
Regression introduced in 00eb1a8822d267eb9c49670b66934a587520107d

src/libraries/gaming/TeamFactory.cpp
@@ -122,7 +122,7 @@ namespace
 // -----------------------------------------------------------------------------
 kernel::Team_ABC* TeamFactory::CreateNoSideTeam()
 {
-    Team* result = new Team( controllers_, 0, tools::translate( "TeamFactory", "No side" ), actionsModel_, profile_ );
+    auto* result = new gui::EntityImplementation< kernel::Team_ABC >( controllers_.controller_, 0, tools::translate( "TeamFactory", "No side" ), &actionsModel_ );
     result->Attach< kernel::Diplomacies_ABC >( *new NoSideDiplomacy() );
     result->Attach< kernel::TacticalHierarchies >( *new TeamTacticalHierarchies( controllers_.controller_, *result ) );
     result->Update( kernel::InstanciationComplete() );