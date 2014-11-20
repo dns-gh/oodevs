b9c4642c88f4ead07daa57f9872859b95a4bc15f
Mathieu Champlon <mathieu.champlon@masagroup.net>
2014-11-20 14:07:19 +0100

    [clients_gui] fix signal adapter when registering for the 'destroyed' signal
    QObject emits from its destructor the 'destroyed' signal then destroys its children, however QWidget inherits from QObject but also destroys its children in its destructor.
    Therefore when connecting from a QWidget 'destroyed' signal to a slot in one of its children, it silently does nothing.
    This fixes the issue by removing the adapter as child of the sender and handling the destruction manually.

--- src/libraries/clients_gui/SignalAdapter.cpp
+++ src/libraries/clients_gui/SignalAdapter.cpp
@@ -10,17 +10,32 @@
 #include "clients_gui_pch.h"
 #include "SignalAdapter.h"
 #include "moc_SignalAdapter.cpp"
+#include <boost/algorithm/string/erase.hpp>
 
 using namespace gui;
 
-SignalAdapter::SignalAdapter( QObject* parent, const std::function< void() >& f )
-    : QObject( parent )
-    , f_( f )
+SignalAdapter::SignalAdapter( QObject* sender, const char* signal, const std::function< void() >& f )
+    : f_( f )
 {
-    // NOTHING
+    connect( sender, signal, this, SLOT( Handle() ) );
+    // If sender is a QWidget, its children will be deleted in its destructor
+    // before its QObject parent has a chance to emit 'destroyed' signals to
+    // them, therefore we cannot have sender as parent for the adapter.
+    connect( sender, SIGNAL( destroyed( QObject* ) ), this, SLOT( Destroy() ) );
+}
+
+void SignalAdapter::Destroy()
+{
+    delete this;
 }
 
 void SignalAdapter::Handle()
 {
     f_();
+}
+
+bool gui::connect( QObject* sender, const char* signal, const std::function< void() >& f )
+{
+    new gui::SignalAdapter( sender, signal, f );
+    return true;
 }
