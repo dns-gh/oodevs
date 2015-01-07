0d61ceb3abfa349dcad7822b9aaf7f49f46511e3
slubecki <sli@masagroup.net>
2015-01-07 11:29:37 +0100

    [schemas][tools][checkmigrations] Improve checkmigrations tool to also test added files * Add xsd for migrations.xml * Clean duplicated added files (also added in 4.3.4 version)

--- src/masa/checkmigrations/checkmigrations.go
+++ src/masa/checkmigrations/checkmigrations.go
@@ -23,30 +23,41 @@ type Upgrade struct {
 	Apply string `xml:"apply,attr"`
 }
 
-type Migrations struct {
-	XMLName  xml.Name  `xml:"migrations"`
-	Upgrades []Upgrade `xml:"migration>upgrade"`
+type Added struct {
+	AssignFile string `xml:"assign-file,attr"`
 }
 
-// Lists all XSL files referenced in supplied migrations.xml.
-func listXsl(migrationsFile string) ([]string, error) {
+type Migrations struct {
+	XMLName    xml.Name  `xml:"migrations"`
+	Upgrades   []Upgrade `xml:"migration>upgrade"`
+	AddedFiles []Added   `xml:"added-files>added-file"`
+}
+
+// Lists all XSL files referenced in supplied migrations.xml
+// and added xml files.
+func listXsl(migrationsFile string) ([]string, []string, error) {
 	fp, err := os.Open(migrationsFile)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer fp.Close()
 
 	x := &Migrations{}
 	err = xml.NewDecoder(fp).Decode(x)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	files := []string{}
 	for _, u := range x.Upgrades {
 		files = append(files, u.Apply)
 	}
 	sort.Strings(files)
-	return files, nil
+	addedFiles := []string{}
+	for _, a := range x.AddedFiles {
+		addedFiles = append(addedFiles, a.AssignFile)
+	}
+	sort.Strings(addedFiles)
+	return files, addedFiles, nil
 }
 
 var (
@@ -79,6 +90,51 @@ func normPath(path, base string) (string, error) {
 	}
 	rel = strings.ToLower(filepath.ToSlash(rel))
 	return rel, nil
+}
+
+// Checks new added files referenced in migrations.xml exist
+func checkAddedExist(addedFiles []string, resourcesDir string) error {
+	// List existing added files to cross-check with referenced ones
+	existing := map[string]struct{}{}
+	existingSorted := []string{}
+
+	schemasDir := filepath.Join(resourcesDir, "schemas")
+
+	err := filepath.Walk(schemasDir, func(path string, fi os.FileInfo, err error) error {
+		if err != nil || fi.IsDir() || !strings.Contains(path, "added-files") {
+			return err
+		}
+		rel, err := normPath(path, resourcesDir)
+		if err != nil {
+			return err
+		}
+		existing[rel] = struct{}{}
+		existingSorted = append(existingSorted, rel)
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	sort.Strings(existingSorted)
+
+	failed := false
+	for _, added := range addedFiles {
+		added := strings.ToLower(added)
+		if _, ok := existing[added]; !ok {
+			failed = true
+			fmt.Fprintf(os.Stderr, "error: %s is in migrations.xml but does not exist\n", added)
+		}
+		delete(existing, added)
+	}
+	failed = failed || len(existing) > 0
+	for added := range existing {
+		fmt.Fprintf(os.Stderr, "error: %s exists but is not in migrations.xml\n", added)
+	}
+	if failed {
+		return fmt.Errorf("not all added files exist")
+	}
+	return nil
 }
 
 // Checks XSL files referenced in migrations.xml exist and existing XSL files
@@ -159,11 +215,15 @@ RESOURCESDIR expected`)
 	}
 	resourcesDir := args[0]
 	migrationsFile := filepath.Join(resourcesDir, "migrations.xml")
-	xsls, err := listXsl(migrationsFile)
+	xsls, added, err := listXsl(migrationsFile)
 	if err != nil {
 		return err
 	}
-	return checkXslExist(xsls, resourcesDir)
+	err = checkXslExist(xsls, resourcesDir)
+	if err != nil {
+		return err
+	}
+	return checkAddedExist(added, resourcesDir)
 }
 
 func main() {
