e1460af4883a105618aabaf507500d4f2fe19ab3
dns-gh <dimitri.nicolas@masagroup.net>
2015-03-15 21:43:07 +0100

    read a file line by line

--- godev/goscripts/goimg.go
+++ godev/goscripts/goimg.go
@@ -6,25 +6,39 @@ import (
     "image/png"
     "fmt"
     "os"
+    "bufio"
 )
 
 func main() {
 	// Read ASCII data
 	// TODO
+	asciiFile, err := os.Open("test.asc")
+	if err != nil {
+        fmt.Println(err)
+		os.Exit(1)
+    }
+    defer asciiFile.Close()
+    scanner := bufio.NewScanner(asciiFile)
+	scanner.Split(bufio.ScanLines)       
+  	for scanner.Scan() {
+    	fmt.Println(scanner.Text())
+  	}
 
 	// Create a PNG file from data
-    f, err := os.Create("test.png")
+    imageFile, err := os.Create("test.png")
     if err != nil {
         fmt.Println(err)
 		os.Exit(1)
     }
-    m := image.NewNRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{256, 256}})
+
+    rgbaData := image.NewNRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{256, 256}})
     for y := 0; y < 256; y++ {
         for x := 0; x < 256; x++ {
-                m.SetNRGBA(x, y, color.NRGBA{uint8(x), uint8((x + y) / 2), uint8(y), 215})
+                rgbaData.SetNRGBA(x, y, color.NRGBA{uint8(x), uint8((x + y) / 2), uint8(y), 215})
         }
-    }        
-    if err = png.Encode(f, m); err != nil {
+    }
+
+    if err = png.Encode(imageFile, rgbaData); err != nil {
         fmt.Println(err)
 		os.Exit(1)
     }
