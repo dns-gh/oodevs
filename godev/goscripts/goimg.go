0d1cfebbed10f9055594ad3f36079a12fe932ced
dns-gh <dimitri.nicolas@masagroup.net>
2015-03-16 22:08:20 +0100

    add some parsing for the first values of the ascii file. It remains to parse the actual data...

--- godev/goscripts/goimg.go
+++ godev/goscripts/goimg.go
@@ -7,23 +7,37 @@ import (
     "fmt"
     "os"
     "bufio"
+    "strconv"
 )
+
+type asciiData struct {
+	ncols int64
+	nrows int64
+	xllcorner float64
+	yllcorner float64
+	CELLSIZE float64
+	NODATA_VALUE int64
+	asciiRect [][]float64
+}
 
 func main() {
 	// Read ASCII data
 	// TODO
-	asciiFile, err := os.Open("test.asc")
+	var lines []string
+	lines, err := readLines("test.asc")
 	if err != nil {
         fmt.Println(err)
 		os.Exit(1)
     }
-    defer asciiFile.Close()
-    scanner := bufio.NewScanner(asciiFile)
-	scanner.Split(bufio.ScanLines)       
-  	for scanner.Scan() {
-    	fmt.Println(scanner.Text())
-  	}
-
+    
+	var rdata asciiData
+	rdata.ncols, err = strconv.ParseInt(lines[0][6:], 0, 64)
+	rdata.nrows, err = strconv.ParseInt(lines[1][6:], 0, 64)
+	rdata.xllcorner, err = strconv.ParseFloat(lines[2][10:], 64)
+	rdata.yllcorner, err = strconv.ParseFloat(lines[3][10:], 64)
+	rdata.CELLSIZE, err = strconv.ParseFloat(lines[4][9:], 64)
+	rdata.NODATA_VALUE, err = strconv.ParseInt(lines[5][13:], 0, 64)
+	fmt.Println(rdata.CELLSIZE)
 	// Create a PNG file from data
     imageFile, err := os.Create("test.png")
     if err != nil {
@@ -41,5 +55,26 @@ func main() {
     if err = png.Encode(imageFile, rgbaData); err != nil {
         fmt.Println(err)
 		os.Exit(1)
+    }
+}
+
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
     }
 }
