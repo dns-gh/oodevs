f3835879c4b9d8b305802e1409ceb98f080e5044
Goolpi <goolpi@hotmail.com>
2015-04-21 23:30:53 +0200

    first version of the LoadFromXMLFile function in ResourceManager

---
+++ cppdev/ooEngine/ooEngine/ResourceManager.cpp
@@ -0,0 +1,77 @@
+#include "ResourceManager.h"
+
+using namespace tinyxml2;
+
+ResourceManager::ResourceManager()
+{
+    currentScope_ = 0;
+    resourceCount_ = 0;
+}
+
+unsigned int ResourceManager::GetResourceCount() const
+{
+    return resourceCount_;
+}
+
+bool ResourceManager::LoadFromXMLFile( const std::string& filename )
+{
+    XMLDocument doc;
+    if( doc.LoadFile( filename.c_str( ) ) == XMLError::XML_SUCCESS )
+    {
+        XMLNode* tree = doc.FirstChildElement( "Resources" );
+        if( tree )
+        {
+            for( XMLNode* child = tree->FirstChild(); child; child->NextSibling() )
+            {
+                XMLElement* elem = child->ToElement();
+                if( elem )
+                {
+                    Resource* resource = NULL;
+                    for( const XMLAttribute* att = elem->FirstAttribute(); att; att->Next() )
+                    {
+                        std::string attName = att->Name( );
+                        std::string attValue = att->Value( );
+                        if( attName == "type" )
+                        {
+                            if( attValue == "Graphic" )
+                            {
+                                // load of the resource by the graphic manager
+                                // resource = RenderManager->LoadResourceFromXML( elem );
+                            }
+                            else if( attValue == "text" )
+                            {
+                                // load of the resource by the config manager
+                                // resource = ConfigManager->LoadResourceFromXML( elem );
+                            }
+                        }
+
+                        if( resource )
+                        {
+                            unsigned int id = -1;
+                            unsigned int scope = -1;
+                            std::string filename;
+                            if( attName == "ID" )
+                            {
+                                id = std::atoi( attValue.c_str() );
+                            }
+                            else if( attName == "SceneScope" )
+                            {
+                                scope = std::atoi( attValue.c_str( ) );
+                            }
+                            else if( attName == "SceneScope" )
+                            {
+                                filename = attValue;
+                            }
+                            if( !resource->Initialize( id, scope, filename ) )
+                                OOTHROW( 2, "Error when loading a resource from an XML file" );
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+        return true;
+    }
+    return false;
+}
---
+++ cppdev/ooEngine/ooEngine/ResourceManager.h
@@ -0,0 +1,63 @@
+#ifndef _RESOURCEMANAGER_H
+#define _RESOURCEMANAGER_H
+
+#include "EngineObject.h"
+#include "ErrorLogManager.h"
+#include <iostream>
+#include <list>
+#include <map>
+#include <string>
+#include "tinyxml2.h"
+
+typedef enum {
+    RESOURCE_NUL = 0,
+    RESOURCE_GRAPHIC = 1,
+}RESOURCE_TYPE;
+
+class Resource : public EngineObject
+{
+public:
+    Resource( )
+    {
+        id_ = -1;
+        scope_ = -1;
+        type_ = RESOURCE_NUL;
+    }
+    bool Initialize( unsigned id, unsigned scope, std::string filename )
+    {
+        id_ = id;
+        scope_ = scope;
+        filename_ = filename;
+        if( id != -1 && scope != -1 && filename != "" )
+            return true;
+        return false;
+    }
+    virtual ~Resource(){};
+    virtual void Load(){};
+    virtual void UnLoad(){};
+
+private:
+    unsigned int id_;
+    unsigned int scope_;
+    std::string filename_;
+    RESOURCE_TYPE type_;
+};
+
+class ResourceManager : public EngineObject
+{
+public:
+    ResourceManager();
+
+    bool LoadFromXMLFile( const std::string& filename );
+    void SetCurrentScope( unsigned int scope );
+    Resource* FindResourceByID( const unsigned int& id ) const;
+    unsigned int GetResourceCount() const;
+    void Clear();
+
+private:
+    unsigned int currentScope_;
+    unsigned int resourceCount_;
+    std::map< unsigned int, std::list< Resource* > > resourceByScope_;
+};
+
+#endif
