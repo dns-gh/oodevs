9e340e04672bd1d0a1490552aae6d30f1e996c96
dns-gh <dimi.nicolas@gmail.com>
2015-07-30 00:11:46 +0200

    [comtools] first commit for communication tools. Simple chat to be improved. - uses:     "bootstrap": "~3.3.5",     "ent": "~2.2.0",     "express": "~4.13.1",     "socket.io": "~1.3.6"

---
+++ comtools/README.txt
@@ -0,0 +1,4 @@
+- Install npm and run the following for module installation
+$ npm install
+- To run the chat server:
+$ npm chat_server.js
---
+++ comtools/chat_server.js
@@ -0,0 +1,33 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // safier communication
+    fs = require('fs');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/js/bootstrap.min.js', function (req, res) {
+  res.sendFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js');
+});
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+});
+
+io.sockets.on('connection', function (socket, login) {
+    
+    socket.on('new_client', function(login) {
+        login = ent.encode(login);
+        socket.login = login;
+        socket.emit('new_client', login);
+        socket.broadcast.emit('new_client', login);
+    });
+
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.emit('message', {login: socket.login, message: message});
+        socket.broadcast.emit('message', {login: socket.login, message: message});
+    }); 
+});
+
+server.listen(8080);
