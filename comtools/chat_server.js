3fa912a6708645c0797ef26b8ef4420f442b0b9f
dns-gh <dimi.nicolas@gmail.com>
2015-08-03 00:45:07 +0200

    [chat] fix some mixup chat and user lists and display last 10 messages to newly connected users

--- comtools/chat_server.js
+++ comtools/chat_server.js
@@ -1,5 +1,28 @@
-chats = require("./array_api");
-users = require("./array_api");
+var chats = new Array();
+var users = new Array();
+
+function GetLastElements(list, num) {
+    var last = new Array()
+    for (i = list.length - 1; i >= list.length - num && i >=0 ; --i) { 
+        last.push(list[i]);
+    }
+    return last;
+}
+
+function Contains(list, elem) {
+    index = list.indexOf(elem);
+    if (index > -1) {
+        return true;
+    }
+    return false;
+}
+
+function RemoveElement(list,elem) {
+    index = list.indexOf(elem);
+    if (index > -1) {
+        list.splice(index, 1);
+    }
+}
 
 var app = require('express')(),
     server = require('http').createServer(app),
@@ -23,23 +46,25 @@ app.get('/css/blog-like.css', function (req, res) {
 io.sockets.on('connection', function (socket) {
 
     socket.on('user_login', function(login) {
-        if (users.Contains(login))
+        if (Contains(users,login) || login == null || login == "")
             return socket.emit('user_login_failed',login);
-        users.PushElement(login)
+        users.push(login)
         encoded_login = ent.encode(login);
         socket.login = encoded_login;
         socket.emit('user_login', encoded_login);
+        socket.emit('users_list', users);
+        socket.emit('last_chats_list', GetLastElements(chats,10));
         socket.broadcast.emit('user_login', encoded_login);
     });
 
     socket.on('disconnect', function(other) {
         socket.broadcast.emit('user_disconnect', socket.login);
-        users.RemoveElement(socket.login)
+        RemoveElement(users,socket.login)
         console.log(socket.login, " disconnected [", other, "]")
     });
 
     socket.on('message', function (message) {
-        chats.PushElement(message);
+        chats.push({login: socket.login, message: message});
         message = ent.encode(message);
         socket.emit('message', {login: socket.login, message: message});
         socket.broadcast.emit('message', {login: socket.login, message: message});
